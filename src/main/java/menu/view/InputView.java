9437c8b74d463cba4304018e8ae280e8566d2b61
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 16:20:57 +0900

    feat(InputView): 문자열 리스트 입력 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,13 +1,32 @@
 package menu.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
     private static class InstanceHolder {
         private static final InputView INSTANCE = new InputView();
     }
-    
+
+    private static final String DELIMITER = ",";
+
     private InputView(){}
 
     public static InputView getInstance() {
         return InputView.InstanceHolder.INSTANCE;
+    }
+
+    public List<String> readStringList() {
+        String input = Console.readLine().trim();
+        return parseInput(input);
+    }
+
+    private List<String> parseInput(String input) {
+        return Arrays.stream(input.split(DELIMITER))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 }
