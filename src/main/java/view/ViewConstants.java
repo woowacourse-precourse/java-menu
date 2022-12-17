34fdf8a5f1003b232a3b15acfe2b98fe8fa5f60f
Devtist <yt930310@snu.ac.kr>
2022-12-17 13:17:34 +0900

    feat: ViewConstants 정리

--- src/main/java/view/ViewConstants.java
+++ src/main/java/view/ViewConstants.java
@@ -2,4 +2,8 @@ package view;
 
 public class ViewConstants {
     public static final String PROGRAM_START = "점심 메뉴 추천을 시작합니다.";
+    public static final String ASKING_INPUT_COACH_NAMES = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    public static final String ASKING_INPUT_INEDIBLE_MENU = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
+    public static final String RECOMMEND_RESULT = "메뉴 추천 결과입니다.";
+    public static final String END = "추천을 완료했습니다.";
 }
