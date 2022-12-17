7caf112c44b47a7d697555a81709ab4129770aff
kevstevie <jianna442@gmail.com>
2022-12-17 16:15:23 +0900

    refactor: 상수 하드코딩 수정

--- src/main/java/menu/MenuSelector.java
+++ src/main/java/menu/MenuSelector.java
@@ -7,10 +7,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MenuSelector {
+    private static final int MENU_MIN = 1;
+    private static final int MENU_MAX = 5;
+    private static final int WEEK_MAX = 2;
+    private static final int ZERO = 0;
+
+
     private List<String> categoryRepository = new ArrayList<>();
 
     public String pickCategory() {
-        int random = Randoms.pickNumberInRange(1, 5);
+        int random = Randoms.pickNumberInRange(MENU_MIN, MENU_MAX);
         Dish dish = Arrays.stream(Dish.values()).filter(s -> s.number == random)
                 .findFirst().get();
         if (checkAmount(dish)) {
@@ -21,13 +27,13 @@ public class MenuSelector {
     }
 
     private boolean checkAmount(Dish dish) {
-        int result = 0;
+        int result = ZERO;
         for (String menu : categoryRepository) {
             if (dish.name.equals(menu)) {
                 result++;
             }
         }
-        return result >= 2;
+        return result >= WEEK_MAX;
     }
 
     public List<String> getCategoryRepository() {
