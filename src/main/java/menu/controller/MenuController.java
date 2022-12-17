e99e2c3e9cbaf51ef2aa7f2695db7d72d8daff51
khsrla9806 <kimhunsope@naver.com>
2022-12-17 14:11:12 +0900

    feat(MenuController): 해당 카테고리에 있는 메뉴를 랜덤으로 추천하는 메서드 구현

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,23 +1,29 @@
 package menu.controller;
 
+import menu.domain.Menu;
 import menu.view.InputView;
 import menu.view.OutputView;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class MenuController {
+    private Map<String, List<String>> categoriesAndMenu;
     private List<String> coachName;
     private Map<String, List<String>> coachDislikeFood;
 
+    private Menu menu;
     private InputView inputView;
     private OutputView outputView;
 
     public MenuController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+        this.menu = new Menu();
+        this.categoriesAndMenu = menu.getMenu();
     }
 
     private void setCoachName() {
@@ -30,8 +36,14 @@ public class MenuController {
     }
 
     public String pickTodayRandomCategory() {
-        List<String> categories = List.of("일식", "한식", "중식", "아시안", "양식");
+        List<String> categories = categoriesAndMenu.keySet().stream().collect(Collectors.toList());
         String category = categories.get(Randoms.pickNumberInRange(0, 4));
         return category;
+    }
+
+    public String recommendRandomMenu(String category) {
+        List<String> selectableMenu = categoriesAndMenu.get(category);
+        String selectedMenu = Randoms.shuffle(selectableMenu).get(0);
+        return selectedMenu;
     }
 }
