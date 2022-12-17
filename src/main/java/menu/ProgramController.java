ef3e0e8d056d9ac58ab67bb4cef4b23b5076fa70
euna <christinejung10@daum.net>
2022-12-17 16:54:43 +0900

    add(ProgramController): enum Week

--- src/main/java/menu/ProgramController.java
+++ src/main/java/menu/ProgramController.java
@@ -6,6 +6,20 @@ import menu.view.OutputView;
 
 public class ProgramController {
 
+    public enum Week {
+        MONDAY("월요일"), TUESDAY("화요일"), WEDNESDAY("수요일"), THURSDAY("목요일"), FRIDAY("금요일");
+
+        private String name;
+
+        Week(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
     public void runProgram() {
         OutputView.printStartMessage();
         CoachController.setCoaches();
