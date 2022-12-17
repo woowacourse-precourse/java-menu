7606747e643fe2fbe5db7ccc7de76d98490934b7
SH <gram753@naver.com>
2022-12-17 14:57:44 +0900

    feat: ERROE 메세지 추가

--- src/main/java/menu/Constant.java
+++ src/main/java/menu/Constant.java
@@ -5,4 +5,8 @@ public class Constant {
 	public static final String COACH_INPUT_MSG = "코치의 이름을 입력해 주세요. (, 로 구분)";
 
 	public static final String MENU_INPUT_MSG = "(이)가 못 먹는 메뉴를 입력해 주세요.\n";
+
+	public static final String WRONG_NAME_MSG = "[ERROR] 이름의 길이가 잘못되었습니다.";
+	public static final String WRONG_MEMBER_MSG = "[ERROR] 식사하기 적합한 인원이 아닙니다.";
+
 }
