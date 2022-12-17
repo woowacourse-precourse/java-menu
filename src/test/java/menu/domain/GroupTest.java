b2356edb10a39881be8940b3c8b578ad51f77bd5
woosung1223 <dntjd991223@naver.com>
2022-12-17 17:23:37 +0900

    feat: 코치가 같은 이름을 가질 경우 예외를 발생시키는지에 대한 테스트 생성

--- src/test/java/menu/domain/GroupTest.java
+++ src/test/java/menu/domain/GroupTest.java
@@ -13,13 +13,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GroupTest {
     @ParameterizedTest
-    @MethodSource("parameterProvider")
+    @MethodSource("coachProvider")
     void 코치의_수가_범위를_벗어나면_예외를_발생시키는지_테스트(List<Coach> coaches) {
         assertThatThrownBy(() -> new Group(coaches))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    private Stream<Arguments> parameterProvider() {
+    private Stream<Arguments> coachProvider() {
         return Stream.of(
                 Arguments.of(
                         List.of()),
@@ -29,6 +29,20 @@ class GroupTest {
                         List.of(new Coach(new CoachName("우성1")), new Coach(new CoachName("우성2")),
                                 new Coach(new CoachName("우성3")), new Coach(new CoachName("우성4")),
                                 new Coach(new CoachName("우성5")),  new Coach(new CoachName("우성6"))))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("indistinctCoachProvider")
+    void 코치가_같은_이름을_가질_경우_예외를_발생시키는지_테스트(List<Coach> coaches) {
+        assertThatThrownBy(() -> new Group(coaches))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private Stream<Arguments> indistinctCoachProvider() {
+        return Stream.of(
+                Arguments.of(
+                        List.of(new Coach(new CoachName("우성1")), new Coach(new CoachName("우성1"))))
         );
     }
 }
