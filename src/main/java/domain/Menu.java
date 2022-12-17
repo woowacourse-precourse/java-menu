284db0b03cbe607a225a701ea36fe4401f7573b9
saint6839 <saint6839@gmail.com>
2022-12-17 14:20:51 +0900

    feat(domain) : 존재하는 메뉴인지 검증하는 메서드 구현
    - contains 비교를 위한 equals 재정의

--- src/main/java/domain/Menu.java
+++ src/main/java/domain/Menu.java
@@ -1,13 +1,45 @@
 package domain;
 
+import java.util.Objects;
+
 public class Menu {
     private final String name;
 
     public Menu(String name) {
+        validateExistMenu(name);
         this.name = name;
     }
 
+    private void validateExistMenu(String name) {
+        if (MenuByCategoryData.hasMenu(name)) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 존재하지 않는 메뉴입니다.");
+    }
+
+
     public String getName() {
         return name;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+
+        Menu menu = (Menu) o;
+
+        System.out.println(this.name);
+        System.out.println(((Menu) o).name);
+
+        return name.equals(menu.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
     }
 }
