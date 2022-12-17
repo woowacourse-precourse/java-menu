c83b8ee7921381ca4ba68ed6744780ce41543331
minaver <psmjoshua@gmail.com>
2022-12-17 13:27:51 +0900

    feat(Menu): Menu VO 초기화

--- src/main/java/menu/vo/Menu.java
+++ src/main/java/menu/vo/Menu.java
@@ -1,4 +1,15 @@
 package menu.vo;
 
+import menu.enums.FoodCategory;
+
+import java.util.List;
+
 public class Menu {
+    private final FoodCategory foodCategory;
+    private final List<String> foods;
+
+    public Menu(FoodCategory foodCategory, List<String> foods) {
+        this.foodCategory = foodCategory;
+        this.foods = foods;
+    }
 }
