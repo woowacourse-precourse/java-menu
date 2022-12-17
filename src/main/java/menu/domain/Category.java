896778a9220e0ca0f0cd9595680c41eacedfd2a4
yujaemin <woawoa0513@gmail.com>
2022-12-17 14:45:19 +0900

    feat: Category 열거형 클래스에 카테고리 번호 필드 추가
    카테고리 번호를 의미하는 categoryNumber 필드 추가

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,19 +1,25 @@
 package menu.domain;
 
 public enum Category {
-    JAPANESE("일식"),
-    KOREAN("한식"),
-    CHINESE("중식"),
-    ASIAN("아시안"),
-    WESTERN("양식");
+    JAPANESE("일식", 1),
+    KOREAN("한식", 2),
+    CHINESE("중식", 3),
+    ASIAN("아시안", 4),
+    WESTERN("양식", 5);
 
     private final String name;
+    private final int categoryNumber;
 
-    Category(String name) {
+    Category(String name, int categoryNumber) {
         this.name = name;
+        this.categoryNumber = categoryNumber;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getCategoryNumber() {
+        return categoryNumber;
     }
 }
