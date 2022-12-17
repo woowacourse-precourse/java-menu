be6104fd20823ea2b8995c88924be4b1367dc91a
yujaemin <woawoa0513@gmail.com>
2022-12-17 14:54:49 +0900

    feat: 카테고리 선정,추가 함수 구현
    같은 카테고리가 1주일 내에 2번을 초과해서 선정되지 않도록 카테고리를 선정한다.

--- src/main/java/menu/domain/CategoryHistory.java
+++ src/main/java/menu/domain/CategoryHistory.java
@@ -1,16 +1,32 @@
 package menu.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Collections;
 import java.util.List;
 
 public class CategoryHistory {
     private List<Category> categoryHistory;
 
-    public void updateCategoryHistory(Category category) {
-        categoryHistory.add(category);
+    public void addCategory() {
+        categoryHistory.add(selectCategory());
     }
 
     public List<Category> getCategoryHistory() {
         return Collections.unmodifiableList(categoryHistory);
+    }
+
+    public Category selectCategory() {
+        while (true) {
+            int categoryNumber = Randoms.pickNumberInRange(1, 5);
+            Category category = Category.getCategoryByNumber(categoryNumber);
+
+            long categoryCount = categoryHistory.stream()
+                    .filter(c -> c.equals(category))
+                    .count();
+
+            if (categoryCount < 2) {
+                return category;
+            }
+        }
     }
 }
