9f069762d8be94f99a8f4956ed935eb1ce6d918e
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-12-17 17:30:27 +0900

    feat: 추천 기능 대략 완성

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,1 +1,1 @@
-package menu.view;public class OutputView {    private static final String START_MESSAGE= "점심 메뉴 추천을 시작합니다.";    public void printStartMessage() {        System.out.println(START_MESSAGE);    }}
+package menu.view;public class OutputView {    private static final String START_MESSAGE= "점심 메뉴 추천을 시작합니다.";    private static final String RESULT_MESSAGE= "메뉴 추천 결과입니다.";    public void printStartMessage() {        System.out.println(START_MESSAGE);    }    public void printResultMessage() {        System.out.println(RESULT_MESSAGE);    }}
