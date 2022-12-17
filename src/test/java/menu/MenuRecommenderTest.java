1cbe32010f971807830d00a9366feb585cb0436b
swonny <wonny921@gmail.com>
2022-12-17 17:42:08 +0900

    test: test코드 추가

--- src/test/java/menu/MenuRecommenderTest.java
+++ src/test/java/menu/MenuRecommenderTest.java
@@ -1,50 +1,44 @@
 package menu;
 
+import constant.Category;
+import constant.Week;
 import menu.domain.Coach;
 import menu.domain.MenuRecommender;
 import org.junit.jupiter.api.Test;
 import repository.MenuForWeekRepository;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class MenuRecommenderTest {
     @Test
-    public void testMenuRecommender() {
+    public void testMenuRecommender_못먹는메뉴미포함() {
+        Coach coach1 = new Coach("승원");
+        Coach coach2 = new Coach("자인");
+        coach1.addHateFoodList(List.of("김치찌개", "김밥", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"));
+        coach2.addHateFoodList(List.of("김치찌개", "김밥", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"));
         List<Coach> coaches = List.of(new Coach("승원"), new Coach("자인"));
         MenuForWeekRepository.initializeCoaches(coaches);
         MenuRecommender.recommendMenus(coaches);
+        LinkedHashMap<Coach, List<String>> result = MenuForWeekRepository.getMenuResult();
+        for (Coach coach : result.keySet()) {
+            assertThat(
+                    result.get(coach).stream().anyMatch(menu -> coach.isHateFood(menu))
+            ).isFalse();
+        }
     }
 
     @Test
-    public void testMenuRecommender_() {
+    public void testMenuRecommender_카테고리중복아님테스트() {
         List<Coach> coaches = List.of(new Coach("승원"), new Coach("자인"));
         MenuForWeekRepository.initializeCoaches(coaches);
         MenuRecommender.recommendMenus(coaches);
+        LinkedHashMap<Week, Category> result = MenuForWeekRepository.getCategoriesPerWeek();
+        int sameCategoryCounts = (int) result.values().stream()
+                .filter(category -> result.values().equals(category))
+                .count();
+        assertThat(sameCategoryCounts > 2).isFalse();
     }
-
-    // 테스트 사용 예시
-//    @ParameterizedTest
-//    @ValueSource(ints = {1, 10, 20})
-//    void 사용자_재시도_테스트(int trials) {
-//
-//    }
-//
-//
-//    @ParameterizedTest
-//    @MethodSource("generateMovableBridge")
-//    @DisplayName("게임에서 이겼는지 확인 (=사용자가 마지막 위치에 있는지 확인)")
-//    void 마지막_위치_확인_테스트(List<String> movableSide) {
-//    }
-//
-//    static Stream<Arguments> generateMovableBridge() {
-//        return Stream.of(
-//                Arguments.of(Arrays.asList("U", "U", "U")),
-//                Arguments.of(Arrays.asList("U", "U", "U", "U")),
-//                Arguments.of(Arrays.asList("U", "U", "U", "U", "U")),
-//                Arguments.of(Arrays.asList(
-//                        "U", "U", "U", "U", "U", "U", "U", "U", "U", "U",
-//                        "U", "U", "U", "U", "U", "U", "U", "U", "U", "U"
-//                ))
-//        );
-//    }
 }
--- src/test/java/repository/MenuForWeekRepositoryTest.java
+++ src/test/java/repository/MenuForWeekRepositoryTest.java
@@ -1,11 +1,9 @@
 package repository;
 
 import constant.Category;
-import constant.Week;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MenuForWeekRepositoryTest {
     @Test
@@ -13,20 +11,4 @@ class MenuForWeekRepositoryTest {
         assertThat(MenuForWeekRepository.isCategoryAvailableToAdd(Category.AMERICAN_FOOD))
                 .isTrue();
     }
-
-//    @Test
-//    public void testIsCategoryAvailableToAdd_추가못하는경우() {
-//        MenuForWeekRepository.addCategory(Week.MONDAY, Category.AMERICAN_FOOD);
-//        MenuForWeekRepository.addCategory(Week.TUESDAY, Category.AMERICAN_FOOD);
-//        assertThat(MenuForWeekRepository.isCategoryAvailableToAdd(Category.AMERICAN_FOOD))
-//                .isFalse();
-//    }
-//
-//    @Test
-//    public void testIsCategoryAvailableToAdd_추가못하는경우() {
-//        MenuForWeekRepository.addCategory(Week.MONDAY, Category.AMERICAN_FOOD);
-//        MenuForWeekRepository.addCategory(Week.TUESDAY, Category.AMERICAN_FOOD);
-//        assertThat(MenuForWeekRepository.isCategoryAvailableToAdd(Category.AMERICAN_FOOD))
-//                .isFalse();
-//    }
 }
