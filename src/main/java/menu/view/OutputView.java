018344953808bc90082900820c267c8e0a01546e
hyun-seung <ardimento22@naver.com>
2022-12-17 13:32:39 +0900

    [FEAT] Output : 코치들 이름 입력 문구 출력

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -3,6 +3,8 @@ package menu.view;
 public class OutputView {
 
     private static final String SERVICE_START = "점심 메뉴 추천을 시작합니다.";
+    private static final String GET_COACH_NAMES = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private static final String GET_CANT_EAT_MANU = "(이)가 못 먹는 메뉴를 입력해 주세요.";
     private static final String MENU_RESULT_INTRO = "메뉴 추천 결과입니다.";
     private static final String MENU_RESULT_ENDING = "추천을 완료했습니다.";
     private static final String SORTAION = "구분";
@@ -13,5 +15,9 @@ public class OutputView {
 
     public void printServiceStart() {
         System.out.println(SERVICE_START);
+    }
+
+    public void printGetCoachNames() {
+        System.out.println(GET_COACH_NAMES);
     }
 }
