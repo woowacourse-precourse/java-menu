6f34c0a3904fa760bf7b96ded50161e9da454c9f
yejin <kwonyj1022@naver.com>
2022-12-17 17:04:32 +0900

    docs: 구현할 기능 목록 추가 (Run 클래스)

--- src/main/java/menu/Run.java
+++ src/main/java/menu/Run.java
@@ -1,32 +1,55 @@
 package menu;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import camp.nextstep.edu.missionutils.Randoms;
 import menu.View.InputView;
-import net.bytebuddy.pool.TypePool;
+import menu.View.OutputView;
 
 public class Run {
-    List<Coach> coaches = new ArrayList<>();
-    List<String> coachesName;
+    private List<Coach> coaches = new ArrayList<>();
+    private List<String> coachesName = new ArrayList<>();
+    private List<String> choosedCategories = new ArrayList<>();
+    private Map<String, List> menu;
+
 
     public Run() {
+        coachesName = getCoachesName();
+        coaches = makeCoaches(coachesName);
+        new OutputView().printResult(choosedCategories, coaches);
+    }
+
+    private List<String> getCoachesName() {
         while(true) {
             try {
-                getInput();
+                return new InputView().getCoachesName();
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    private void getInput() {
-        coachesName = new InputView().getCoachesName();
-        for (int i = 0; i < coachesName.size(); i++) {
-            Coach coach = new Coach();
-            coach.setName(coachesName.get(i));
-            coach.setHateFood(new InputView().getHateFood(coachesName.get(i)));
+    private List<String> getHateFood(String name) {
+        while(true) {
+            try {
+                return new InputView().getHateFood(name);
+            } catch(IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
-
-
+    private List<Coach> makeCoaches(List<String> coachesName) {
+        for (int i = 0; i < coachesName.size(); i++) {
+            String name = coachesName.get(i);
+            Coach coach = new Coach();
+            coach.setName(name);
+            List<String> hateFood = getHateFood(name);
+            coach.setHateFood(hateFood);
+            coaches.add(coach);
+        }
+        return coaches;
+    }
 }
