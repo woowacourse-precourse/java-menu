f2170c8c874340610f5754ca868c8e731d1277a3
ckn1070 <ckn1070@naver.com>
2022-12-17 13:45:43 +0900

    feat(menu): 무작위로 카테고리 생성 기능 구현

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -1,6 +1,30 @@
-
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Category {
+
+    public Category() {
+    }
+
+    public String getCategory() {
+        String category = typeToCategory(Randoms.pickNumberInRange(1, 5));
+        return category;
+    }
+
+    public String typeToCategory(int type) {
+        if (type == CategoryType.JAPAN.getType()) {
+            return CategoryType.JAPAN.getCategory();
+        }
+        if (type == CategoryType.KOREA.getType()) {
+            return CategoryType.KOREA.getCategory();
+        }
+        if (type == CategoryType.CHINA.getType()) {
+            return CategoryType.CHINA.getCategory();
+        }
+        if (type == CategoryType.ASIAN.getType()) {
+            return CategoryType.ASIAN.getCategory();
+        }
+        return CategoryType.WESTERN.getCategory();
+    }
 
 }
 
