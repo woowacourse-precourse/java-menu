36ec6c71bfd5028b133a80f514155425f2fa0603
LJW25 <dbjwl@g.skku.edu>
2022-12-17 13:47:56 +0900

    feat: Recommender 클래스 정의

--- src/main/java/menu/Recommender.java
+++ src/main/java/menu/Recommender.java
@@ -1,4 +1,21 @@
 package menu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Recommender {
+
+    List<Coach> coaches;
+    List<Integer> categories;
+
+    Recommender(List<Coach> coaches){
+        this.coaches = coaches;
+        this.categories = makeRandomCategories();
+    }
+
+    private List<Integer> makeRandomCategories(){
+        List<Integer> randomCategory = new ArrayList<>();
+        return randomCategory;
+    }
+
 }
