1b295bf85d2aec4410612d16e564aab9c9c684c9
Gibeom Nam <rlqja0523@naver.com>
2022-12-17 17:25:02 +0900

    feat: 코치의 개수를 확인하는 기능

--- src/main/java/menu/exception/CoachException.java
+++ src/main/java/menu/exception/CoachException.java
@@ -1,14 +1,26 @@
 package menu.exception;
 
+import menu.utils.Converter;
+
 import java.util.List;
 
 public class CoachException extends CommonException {
+
+    private final Converter converter;
+
+    public CoachException(Converter converter) {
+        this.converter = converter;
+    }
 
     @Override
     public void verifyException(String input) {
         checkEmpty(input);
         checkBlank(input);
         checkSeperatedByComma(input, 1, 4);
+
+        List<String> names = converter.convertToList(input);
+        checkNameLength(names);
+        checkCoachCount(names);
     }
 
     private void checkNameLength(List<String> names) {
@@ -16,6 +28,12 @@ public class CoachException extends CommonException {
             if (name.length() < 2 || name.length() > 4) {
                 throw new IllegalArgumentException(ExceptionCode.NOT_CORRECT_COACH_NAME.getMessage());
             }
+        }
+    }
+
+    private void checkCoachCount(List<String> names) {
+        if (names.size() < 2 || names.size() > 5) {
+            throw new IllegalArgumentException(ExceptionCode.OUT_OF_RANGE_COACH_COUNT.getMessage());
         }
     }
 }
