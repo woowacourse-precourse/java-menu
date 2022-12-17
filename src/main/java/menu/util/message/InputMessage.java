63753f5b42c421dc3e21330b28683e9bb9f5c395
HaODay <ycis5013@naver.com>
2022-12-17 17:34:23 +0900

    feat(InputMessage): 입력 메시지 관련 열거형 생성

---
+++ src/main/java/menu/util/message/InputMessage.java
@@ -0,0 +1,6 @@
+package menu.util.message;
+
+public class InputMessage {
+    public static final String INPUT_COACH_NAME = "\n코치의 이름을 입력해 주세요. (, 로 구분)";
+    public static final String INPUT_CANNOT_EAT_MENU = "%n%s(이)가 못 먹는 메뉴를 입력해 주세요.%n";
+}
