30c9d01499d81451e3ae645a3fb54fbd8c19f2ad
yenawee <yaena0319@naver.com>
2022-12-17 16:06:47 +0900

    fix : 못먹는 메뉴가 나올 시 메뉴 추첨 다시 랜덤

--- src/main/java/menu/Coach.java
+++ src/main/java/menu/Coach.java
@@ -23,6 +23,9 @@ public class Coach {
     }
 
     public boolean isValidMenu(Menu menu){
+        if (!dislikeFood.isEmpty() && dislikeFood.stream().anyMatch(foodName -> foodName.equals(menu.getMenuName()))){
+            return false;
+        }
         if (!recommendedFood.isEmpty() && recommendedFood.stream().anyMatch(foodName -> foodName.equals(menu.getMenuName()))){
             return false;
         }
