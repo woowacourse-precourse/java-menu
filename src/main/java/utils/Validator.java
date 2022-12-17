9787fcef93a52a25b77327b8fba8b0842185dc51
Egenieee <hhgg0925@naver.com>
2022-12-17 17:52:41 +0900

    fix: 검증 로직 수정 및 에러메시지 수정

--- src/main/java/utils/Validator.java
+++ src/main/java/utils/Validator.java
@@ -8,9 +8,9 @@ import java.util.stream.Collectors;
 
 public class Validator {
     private static final String DELIMITER = ",";
-    private static final String INVALID_COACH_COUNT_OUTPUT_MESSAGE = "코치는 최소 2명 ~ 최대 5명 입력해야합니다.";
-    private static final String INVALID_COACH_NAME_OUTPUT_MESSAGE = "코치 이름은 최소 2글자 ~ 최대 5글자로 입력해야합니다.";
-    private static final String INVALID_FOOD_NAME_OUTPUT_MESSAGE = "정해진 음식 이름을 입력해야합니다.";
+    private static final String INVALID_COACH_COUNT_OUTPUT_MESSAGE = "[ERROR] 코치는 최소 2명 ~ 최대 5명 입력해야합니다.";
+    private static final String INVALID_COACH_NAME_OUTPUT_MESSAGE = "[ERROR] 코치 이름은 최소 2글자 ~ 최대 5글자로 입력해야합니다.";
+    private static final String INVALID_FOOD_NAME_OUTPUT_MESSAGE = "[ERROR] 정해진 음식 이름을 입력해야합니다.";
 
     public static boolean isValidCoachNames(String coachNames) {
         List<String> coaches = splitter(coachNames);
@@ -30,10 +30,10 @@ public class Validator {
     }
 
     private static void checkValidCoachLetterCount(List<String> coaches) {
-        boolean isOverTwoLetter = coaches.stream().anyMatch(name -> name.length() > 1);
-        boolean isUnderSixLetter = coaches.stream().anyMatch(name -> name.length() < 6);
+        boolean isOverTwoLetter = coaches.stream().anyMatch(name -> name.length() < 2);
+        boolean isUnderSixLetter = coaches.stream().anyMatch(name -> name.length() > 5);
 
-        if (!isOverTwoLetter || !isUnderSixLetter) {
+        if (isOverTwoLetter && isUnderSixLetter) {
             throw new IllegalArgumentException(INVALID_COACH_NAME_OUTPUT_MESSAGE);
         }
     }
