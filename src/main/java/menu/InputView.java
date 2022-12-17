528dab29733fe1e4bab950947316bfd38cb41038
ckn1070 <ckn1070@naver.com>
2022-12-17 16:46:51 +0900

    feat(menu): Output 메시지를 출력하기 위한 기능 구현

--- src/main/java/menu/InputView.java
+++ src/main/java/menu/InputView.java
@@ -33,7 +33,7 @@ public class InputView {
 enum InputMessage {
 
     INITIALIZATION("점심 메뉴 추천을 시작합니다."),
-    COACH("\n코치의 이름을 입력해 주세요. (, 로 구분)"),
+    COACH("코치의 이름을 입력해 주세요. (, 로 구분)"),
     INEDIBLE("(이)가 못 먹는 메뉴를 입력해 주세요.");
 
     private final String message;
