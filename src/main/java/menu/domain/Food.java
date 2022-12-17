325b30758014716eda296c0ec9e28d9a289c0839
starwook <star.wookk@gmail.com>
2022-12-17 16:54:04 +0900

    feat: Add Food.java

---
+++ src/main/java/menu/domain/Food.java
@@ -0,0 +1,21 @@
+package menu.domain;
+
+import menu.resource.Category;
+
+public class Food {
+    private String name;
+    private Category category;
+
+    public Food(String name,Category category){
+        this.name = name;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+}
