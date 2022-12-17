61ddb3209625403670cfeb5cd8090dfbd4e2c0e5
hectick <chyeon1223@cau.ac.kr>
2022-12-17 14:15:32 +0900

    rename: test파일에 잘못 넣어놨던거 main으로 싹 이동

---
+++ src/main/java/menu/controller/MainController.java
@@ -0,0 +1,37 @@
+package menu.controller;
+
+import menu.constant.Category;
+import menu.constant.Day;
+import menu.model.Recommender;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainController {
+    List<Category> categories;
+    Recommender recommender;
+
+    public MainController(){
+        categories = new ArrayList<>();
+        recommender = new Recommender();
+    }
+
+    public void execute(){
+        OutputView.printServiceStartMessage();
+        OutputView.printGetCoachNamesMessage();
+        String[] coachNames = InputView.readCoachNames();
+
+    }
+
+    private void setCategories(){
+        for(int i = 0; i < Day.size(); i++){
+            setCategoryOfDay(Day.get(i));
+        }
+    }
+
+    private void setCategoryOfDay(Day day){
+        Category recommendedCategory = recommender.recommendCategory();
+    }
+}
