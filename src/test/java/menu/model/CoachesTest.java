02a7f7eed8e23fed33b40adc33379f7b1d954ea6
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 17:37:38 +0900

    test(Coaches): 클랙스 기능 테스트 추가
    1. 코치 객체 가져오기 테스트

---
+++ src/test/java/menu/model/CoachesTest.java
@@ -0,0 +1,36 @@
+package menu.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class CoachesTest {
+    Coaches coaches = new Coaches();
+
+    @BeforeEach
+    void initCoaches() {
+        List<String> names = Arrays.asList("JJ", "pobi", "test");
+        for (String name : names) {
+            coaches.addCoach(new Coach(name));
+        }
+    }
+
+    @DisplayName("코치 객체 가져오기 테스트")
+    @Test
+    void getCoaches() {
+        List<Coach> coachNames = coaches.getCoaches();
+        List<String> expectResult = Arrays.asList("JJ", "pobi", "test");
+
+        for (int i = 0; i < coachNames.size(); i++) {
+            assertThat(coachNames.get(i).getName()).isEqualTo(expectResult.get(i));
+        }
+    }
+}
