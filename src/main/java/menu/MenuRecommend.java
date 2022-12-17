6c6753889836a43fda94ac3a0af72ba8c016f45f
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-12-17 14:43:05 +0900

    feat(MenuRecommend): selectMenu 메서드 생성

--- src/main/java/menu/MenuRecommend.java
+++ src/main/java/menu/MenuRecommend.java
@@ -3,10 +3,17 @@ package menu;
 import camp.nextstep.edu.missionutils.Randoms;
 import menu.menu.Category;
 
+import java.util.List;
+
 public class MenuRecommend {
 
     public String selectCategory() {
         String category = Category.get(Randoms.pickNumberInRange(1, 5));
         return category;
+    }
+
+    public String selectMenu(List<String> menus) {
+        String menu = Randoms.shuffle(menus).get(0);
+        return menu;
     }
 }
