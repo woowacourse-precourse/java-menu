682ec421b80d124fa53ded67f721ec6e7402d45d
ksh-beginner <ohk9134@naver.com>
2022-12-17 13:37:28 +0900

    feat(OutputView) : 점심 메뉴 추천 시작 문구 출력 기능 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -2,6 +2,12 @@ package menu.view;
 
 public class OutputView {
 
+    private static final String LUNCH_MENU_RECOMMEND_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+
+    public void printLunchMenuRecommendMessage() {
+        printMessage(LUNCH_MENU_RECOMMEND_MESSAGE);
+    }
+
     private void printBlankLine() {
         System.out.println();
     }
