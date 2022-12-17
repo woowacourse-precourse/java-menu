faae6669bd2a6a21206f9c8d947049254df05330
jiwon <ljwon77@daum.net>
2022-12-17 16:40:19 +0900

    style: 입력 메시지에 개행 추가

--- src/main/java/menu/constant/InputMessage.java
+++ src/main/java/menu/constant/InputMessage.java
@@ -5,7 +5,7 @@ import menu.util.EnumUtil;
 public enum InputMessage implements EnumUtil {
     START_RECOMMEND("점심 메뉴 추천을 시작합니다.\n")
     , GET_COACH_NAME("코치의 이름을 입력해 주세요. (, 로 구분)")
-    , GET_NOT_EAT_MENU("%s(이)가 못 먹는 메뉴를 입력해 주세요.")
+    , GET_NOT_EAT_MENU("\n%s(이)가 못 먹는 메뉴를 입력해 주세요.")
     , RECOMMEND_RESULT("메뉴 추천 결과입니다.")
     , STOP_RECOMMEND("추천을 완료했습니다.");
 
