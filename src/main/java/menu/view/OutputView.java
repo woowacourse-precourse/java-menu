83a9307f1025481350c33ebb457e2469e2d8d74d
hanueleee <ezzeezz@naver.com>
2022-12-17 14:00:26 +0900

    feat(view): 서비스 종료 문구 출력 기능 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -2,13 +2,14 @@ package menu.view;
 
 public class OutputView {
     private final String START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    private final String END_MESSAGE = "추천을 완료했습니다.";
     public void printStartMessage() {
         System.out.println(START_MESSAGE);
     }
     public void printMenu() {
-        // 최종 메뉴 출력
+        // 최종 메뉴 출력ㅅ
     }
     public void printEndMessage() {
-
+        System.out.println(END_MESSAGE);
     }
 }
