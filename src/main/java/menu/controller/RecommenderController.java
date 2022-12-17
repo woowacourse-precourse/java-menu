82eed5c8e8abb30be4661bcb1ebee0455aa2fc92
2014012606 <2014012606@hanyang.ac.kr>
2022-12-17 14:34:46 +0900

    refactor: 추천된 카테고리목록을 controller가 가지고 있는 것으로 수정

--- src/main/java/menu/controller/RecommenderController.java
+++ src/main/java/menu/controller/RecommenderController.java
@@ -1,10 +1,13 @@
 package menu.controller;
 
+import menu.model.Category;
 import menu.model.Coach;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RecommenderController {
+    private List<Category> recommendedCategories = new ArrayList<>();
     private List<Coach> coaches;
 
     public void run() {
--- src/main/java/menu/model/CategoryRecommender.java
+++ src/main/java/menu/model/CategoryRecommender.java
@@ -7,18 +7,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class CategoryRecommender {
-    private List<Category> recommendedCategories = new ArrayList<>();
-
     public Category recommendCategory() {
         List<Category> categories = new ArrayList<>(List.of(Category.values()));
-        Category category;
-        while (true) {
-            category = categories.get(Randoms.pickNumberInRange(1,5) - 1);
-            if (Collections.frequency(recommendedCategories, category) > 2) {
-                recommendedCategories.add(category);
-                break;
-            }
-        }
+        Category category = categories.get(Randoms.pickNumberInRange(1,5) - 1);
         return category;
     }
 }
