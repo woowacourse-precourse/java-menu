31638a074d9ee56a389a7c3c42936fb1dba7e52b
은지 <midzy0228@gmail.com>
2022-12-17 15:50:14 +0900

    feat: 카테고리 추천 기능 구현

--- src/main/java/menu/model/RecommendMachine.java
+++ src/main/java/menu/model/RecommendMachine.java
@@ -1,2 +1,28 @@
-package menu.model;public class RecommendMachine {
+package menu.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RecommendMachine {
+    private static final Integer MAX_RECOMMEND_CATEGORY_COUNT = 2;
+    private static final Integer DAY_OF_SIZE = 5;
+    public List<String> recommendCategory() {
+        List<String> result = new ArrayList<>();
+        while (result.size() <= DAY_OF_SIZE) {
+            String recommending = Category.recommendCategory();
+            if (canRecommend(result, recommending)) {
+                result.add(recommending);
+            }
+        }
+        return result;
+    }
+
+    public void recommendFood(Menu menu, String category) {
+        String men = menu.recommendMenu(category);
+    }
+
+    private boolean canRecommend(List<String> category, String recommend) {
+        return Collections.frequency(category, recommend) <= MAX_RECOMMEND_CATEGORY_COUNT;
+    }
 }
