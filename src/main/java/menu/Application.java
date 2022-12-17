24661fac858dd71ede6a9178a5c490d16ef7bd8d
Eun-chan Cho <eunchan1001@gmail.com>
2022-12-17 16:21:48 +0900

    feat(Application): implement Application class

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,13 @@
 package menu;
 
+import menu.Controller.Service;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Service service = new Service();
+        service.generateCoachs();
+        service.setInedible();
+        service.makeRecommendations();
+        service.endService();
     }
 }
