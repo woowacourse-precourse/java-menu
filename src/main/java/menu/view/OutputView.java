f34467cbf2e296091b1bb38ba83d3b4c6722d1f2
hanueleee <ezzeezz@naver.com>
2022-12-17 13:59:37 +0900

    feat(view): 서비스 시작 문구 출력 기능 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,7 +1,14 @@
 package menu.view;
 
 public class OutputView {
+    private final String START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    public void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
     public void printMenu() {
         // 최종 메뉴 출력
+    }
+    public void printEndMessage() {
+
     }
 }
