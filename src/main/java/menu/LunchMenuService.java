676acc2e7d47cc5b6fee8578c4034971d8e1e1b7
신채원 <cw121@naver.com>
2022-12-17 15:50:34 +0900

    feat: 생성된 카테고리에 따라 메뉴를 랜덤으로 메뉴를 추천해주는 기능 구현

---
+++ src/main/java/menu/LunchMenuService.java
@@ -0,0 +1,26 @@
+package menu;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+import static menu.Menu.staticMenu;
+
+public class LunchMenuService {
+
+    private List<List<String>>menus = staticMenu;
+    private final CategoryMaker categoryMaker;
+    private final Category category;
+
+    public LunchMenuService(){
+        this.categoryMaker = new CategoryMaker(new CategoryRandomGenerator());
+        this.category = new Category(categoryMaker.makeCategories());
+    }
+
+    public String randomRecommendMenu(int index){
+        int food = category.getPositions().get(index);
+        System.out.println(food);
+        return Randoms.shuffle(menus.get(food-1)).get(0);
+    }
+
+}
