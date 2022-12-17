f942a8550dc0a5967e76d2e8031348792b70ebce
2014012606 <2014012606@hanyang.ac.kr>
2022-12-17 14:07:51 +0900

    refactor: 쓰이지 않는 argument 삭제

--- src/main/java/menu/model/MenuRecommender.java
+++ src/main/java/menu/model/MenuRecommender.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class MenuRecommender {
-    public static String recommendMenu(Category category, Coach coach) {
+    public static String recommendMenu(Category category) {
         List<String> menus = category.getMenus();
         String menu = Randoms.shuffle(menus).get(0);
         return menu;
