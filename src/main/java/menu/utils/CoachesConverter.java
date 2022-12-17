4a87825f79e9c9a620c48ba4750b94bb68f58288
woo-chang <clllickme@naver.com>
2022-12-17 16:00:27 +0900

    feat(CoachesConverter): 중복되는 코치가 존재할 때 예외를 발생시키는 기능 구현

--- src/main/java/menu/utils/CoachesConverter.java
+++ src/main/java/menu/utils/CoachesConverter.java
@@ -1,7 +1,9 @@
 package menu.utils;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import menu.domain.Coach;
 
@@ -12,6 +14,7 @@ public class CoachesConverter {
 
     public static List<Coach> convert(String input) {
         List<Coach> coaches = Arrays.stream(input.split(","))
+                .map(s -> s.trim())
                 .map(Coach::new)
                 .collect(Collectors.toList());
         validate(coaches);
@@ -21,6 +24,12 @@ public class CoachesConverter {
     private static void validate(List<Coach> coaches) {
         if (coaches.size() < 2 || coaches.size() > 5) {
             throw new IllegalArgumentException("코치는 최소 2명, 최대 5명까지 식사를 함께 한다.");
+        }
+
+        Set<Coach> uniqueCoaches = new HashSet<>(coaches);
+
+        if (uniqueCoaches.size() != coaches.size()) {
+            throw new IllegalArgumentException("중복되는 코치가 존재합니다.");
         }
     }
 }
