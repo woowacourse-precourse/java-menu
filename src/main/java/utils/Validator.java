76583abae02e70f012aacd1e597c1ea4dad3de7d
Egenieee <hhgg0925@naver.com>
2022-12-17 14:45:44 +0900

    fix: 먹지 못하는 음식 입력 칸 빈칸으로 놔둘 경우, 발생하는 오류 수정

--- src/main/java/utils/Validator.java
+++ src/main/java/utils/Validator.java
@@ -61,6 +61,10 @@ public class Validator {
     }
 
     public static boolean isValidFoodName(String hateFoods) {
+        if (hateFoods.length() == 0 ) {
+            return true;
+        }
+
         try {
             checkValidFoodNames(hateFoods);
         } catch (IllegalArgumentException exception) {
