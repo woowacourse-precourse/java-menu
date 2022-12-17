3fdbcfb8dbc6e1d0f2942cfde82e3ec269a85d84
hoin <apicad0307@gmail.com>
2022-12-17 17:18:52 +0900

    refactor: 필요없는 주석 코드 제거

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -2,7 +2,6 @@ package menu.domain;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public enum Category {
     JAPANESE(1, "일식", Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
@@ -11,30 +10,32 @@ public enum Category {
     ASIAN(4, "아시안", Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")),
     WESTERN(5, "양식", Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
 
-    private int index;
-    private String category;
-    private List<String> menus;
+    private final int index;
+    private final String category;
+    private final List<String> menus;
 
-    Category(int index, String category, List<String> menus){
+    Category(int index, String category, List<String> menus) {
         this.index = index;
         this.category = category;
         this.menus = menus;
     }
 
-    public String getCategory(){
-        return category;
-    }
-    public List<String> getMenus(){
-        return menus;
-    }
-    public static Category indexToCategory(Integer idx){
+    public static Category indexToCategory(Integer idx) {
         Category ca = Arrays.stream(Category.values()).filter(
                 category -> category.index == idx
         ).findAny().orElseThrow();
         return ca;
     }
 
-    public boolean check(String menu){
+    public String getCategory() {
+        return category;
+    }
+
+    public List<String> getMenus() {
+        return menus;
+    }
+
+    public boolean check(String menu) {
         return menus.stream().anyMatch(m -> m.equals(menu));
     }
 }
