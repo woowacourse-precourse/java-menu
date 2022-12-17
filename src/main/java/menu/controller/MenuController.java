7ed457cb19e1e8d70b2466852dc3f7146a60fd4e
Gibeom Nam <rlqja0523@naver.com>
2022-12-17 17:28:56 +0900

    feat: 코치 인스턴스 생성 기능

---
+++ src/main/java/menu/controller/MenuController.java
@@ -0,0 +1,38 @@
+package menu.controller;
+
+import menu.model.Coach;
+import menu.service.MenuService;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final MenuService menuService;
+    private final List<Coach> coaches = new ArrayList<>();
+
+    public MenuController(InputView inputView, OutputView outputView, MenuService menuService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.menuService = menuService;
+    }
+
+    private void initCoaches(String name) {
+        Coach coach = new Coach(name);
+        setCoachNoMenu(coach);
+        setCoachMyMenu(coach);
+        coaches.add(coach);
+    }
+
+    private void setCoachNoMenu(Coach coach) {
+        List<String> noMenu = inputView.getNoMenu(coach);
+        coach.setNoMenu(noMenu);
+    }
+
+    private void setCoachMyMenu(Coach coach) {
+        menuService.getMenus(coach);
+    }
+}
