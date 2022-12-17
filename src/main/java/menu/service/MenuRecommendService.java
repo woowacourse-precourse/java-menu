79ea421f9d05b44838b3720ac5f0bb08062418b1
zeunala <zeunala@naver.com>
2022-12-17 14:53:00 +0900

    feat: 제공되면 안되는 메뉴 추천하지 않도록 하는 기능 추가

--- src/main/java/menu/service/MenuRecommendService.java
+++ src/main/java/menu/service/MenuRecommendService.java
@@ -2,16 +2,52 @@ package menu.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import menu.domain.MenuRepository;
-import java.util.List;
+
+import java.util.*;
 
 public class MenuRecommendService {
+    /**
+     * 코치별 제공되면 안되는 메뉴들의 집합을 저장한다.
+     */
+    private static final Map<String, Set<String>> bannedMenuByCoach = new HashMap<>();
+
     public static String recommendMenu(String coach, String category) {
-        String selectedMenu = pickMenu(category);
+        String selectedMenu;
+        do {
+            selectedMenu = pickMenu(category);
+        } while (isBannedMenu(coach, selectedMenu));
+        addBannedMenu(coach, selectedMenu); // 제공된 메뉴 재추천 못하게 함
         return selectedMenu;
     }
 
     private static String pickMenu(String category) {
         List<String> targetMenu = MenuRepository.findMenuListByCategory(category);
         return Randoms.shuffle(targetMenu).get(0);
+    }
+
+    public static void addBannedMenu(String coach, String menu) {
+        if (!containsMenu(coach, menu)) {
+            bannedMenuByCoach.get(coach).add(menu);
+        }
+    }
+
+    /**
+     * 해당 코치가 해당 음식 제공이 금지되어 있는지 여부 반환
+     */
+    private static boolean isBannedMenu(String coach, String menu) {
+        if (containsMenu(coach, menu)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * coach에 대한 menu가 있는지 확인하며 coach에 대한 리스트가 없을 경우 초기화도 진행한다.
+     */
+    private static boolean containsMenu(String coach, String menu) {
+        if (!bannedMenuByCoach.containsKey(coach)) {
+            bannedMenuByCoach.put(coach, new HashSet<>());
+        }
+        return bannedMenuByCoach.get(coach).contains(menu);
     }
 }
