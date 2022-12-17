f4e1eba2ce595bb3c41ffe23f419eee43e724b6f
dooboocookie <bkh3030@gmail.com>
2022-12-17 17:43:24 +0900

    feat: 모델에 검증처리 추가

--- src/main/java/menu/domain/UnavailableMenu.java
+++ src/main/java/menu/domain/UnavailableMenu.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UnavailableMenu {
+    private final static String ERROR_SIZE = "[ERROR] 못 먹는 음식은 2개까지만 입력가능합니다.";
 
     private final List<String> unavailableMenu;
 
@@ -12,7 +13,14 @@ public class UnavailableMenu {
     }
 
     public void addMenu(String menu) {
+        validateSize(unavailableMenu.size());
         unavailableMenu.add(menu);
+    }
+
+    private void validateSize(int size) {
+        if(size >= 2) {
+            throw new IllegalArgumentException(ERROR_SIZE);
+        }
     }
 
     public boolean hasMenu(String menu) {
