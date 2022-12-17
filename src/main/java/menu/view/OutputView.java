8e205c7c2b50bc3d1b932a8e19805660e793a869
moonstal1506 <moonstal1506@gmail.com>
2022-12-17 15:31:17 +0900

    feat: 예외 상황 시 에러 문구를 출력

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -2,11 +2,17 @@ package menu.view;
 
 public class OutputView {
 
+    private static final String ERROR = "[ERROR] ";
+
     private OutputView() {
     }
 
     public static void printServiceStartMessage() {
         System.out.println("점심 메뉴 추천을 시작합니다.");
         System.out.println();
+    }
+
+    public static void printError(String message) {
+        System.out.println(ERROR + message);
     }
 }
