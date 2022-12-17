3a0113e2439c3755962496764be6fca6a847a9b9
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-12-17 15:20:29 +0900

    Feat(SuggestMenuResultDto): 추가 추천 결과 관련 dto

--- src/main/java/menu/dto/SuggestMenuResultDto.java
+++ src/main/java/menu/dto/SuggestMenuResultDto.java
@@ -2,10 +2,30 @@ package menu.dto;
 
 import menu.Category;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SuggestMenuResultDto {
 
     private List<Category> categories;
+    private Map<String, List<String>> coachWeeklyMenu = new HashMap<>();
 
+
+    public SuggestMenuResultDto(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public void addCoachMenuList(String coachName, List<String> menuList){
+        coachWeeklyMenu.put(coachName, new ArrayList<>(menuList));
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public Map<String, List<String>> getCoachWeeklyMenu() {
+        return coachWeeklyMenu;
+    }
 }
