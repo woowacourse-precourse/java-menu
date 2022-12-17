188ee2c9938b4092ff8ee47af556977ba3be96cf
hojunking96 <junsong96@naver.com>
2022-12-17 16:55:53 +0900

    feats(MainController) : 실행을 시작하는 메인 컨트롤러 구현, 다음 컨트롤러와 연결

---
+++ src/main/java/menu/MainController.java
@@ -0,0 +1,42 @@
+package menu;
+
+import java.util.List;
+
+public class MainController {
+    private final Coaches coaches;
+    private final RecommendController recommend;
+
+    public MainController() {
+        coaches = new Coaches();
+        recommend = new RecommendController(coaches);
+    }
+
+    public void run() {
+        OutputView.printRecommendStart();
+        initCoaches();
+        initNotPreferMenu();
+        recommend.recommendStart();
+
+    }
+
+
+    private void initCoaches() {
+        String input = InputView.requestCoachName();
+        List<String> coachNames = List.of(input.split(","));
+        for (String e : coachNames) {
+            Coach coach = new Coach(e);
+            this.coaches.addCoach(coach);
+        }
+    }
+
+    private void initNotPreferMenu() {
+        for (Coach coach : coaches.getCoaches()) {
+            String input = InputView.requestNotPreferMenu(coach.getName());
+            List<String> notPreferMenuNames = List.of(input.split(","));
+            for (String e : notPreferMenuNames) {
+                coach.addNotPreferMenu(e);
+            }
+        }
+    }
+
+}
