35c0001ac8c2e99a0a2179befd9bce420ecbd7c3
anneu <b.imnuj@gmail.com>
2022-12-17 13:45:26 +0900

    feat: 코치 이름 입력 기능 구현

--- src/main/java/menu/controller/MenuDecideController.java
+++ src/main/java/menu/controller/MenuDecideController.java
@@ -12,5 +12,6 @@ public class MenuDecideController {
     public void run() {
         output.printStart();
         output.askCoachName();
+        String coachName = input.readCoachName();
     }
 }
--- src/main/java/menu/view/Input.java
+++ src/main/java/menu/view/Input.java
@@ -1,4 +1,15 @@
 package menu.view;
 
+import java.util.Scanner;
+
 public class Input {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String readCoachName() {
+        return read();
+    }
+
+    public String read() {
+        return scanner.nextLine();
+    }
 }
