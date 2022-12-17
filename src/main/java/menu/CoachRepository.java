8ae4de6ddfff557c13e012ff7a01f7c19a9efaa1
ingpyo <juninpyo72@gmail.com>
2022-12-17 14:43:03 +0900

    feat:코치의 못먹는 음식 정보 저장소

---
+++ src/main/java/menu/CoachRepository.java
@@ -0,0 +1,13 @@
+package menu;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CoachRepository {
+	Map<String, List<String>> coachsInformation = new HashMap<>();
+
+	public void setCoachs(String inputCoach,List<String> inputNotEatFoods) {
+		coachsInformation.put(inputCoach, inputNotEatFoods);
+	}
+}
