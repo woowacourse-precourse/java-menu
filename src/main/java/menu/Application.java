04f1791f41296c181832ee7a9f4f6c390ff4a6db
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 17:22:03 +0900

    feat: 메인 함수 프로그램 실행 기능 구현

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,9 @@
 package menu;
 
+import menu.domain.MenuRecommand;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new MenuRecommand().play();
     }
 }
