0c36c1d8cf8212f1cf9ec05e529a9294160db691
Cyma-s <ssol0319@naver.com>
2022-12-17 16:19:54 +0900

    feat(OutputView) : 추천 결과 출력 함수 추가

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,5 +1,10 @@
 package menu.view;
 
+import java.util.List;
+import menu.domain.Category;
+import menu.domain.Coach;
+import menu.domain.Menu;
+
 public class OutputView {
     private final static String SERVICE_START = "점심 메뉴 추천을 시작합니다.\n";
     private final static String RECOMMEND_RESULT_DAY_WEEK = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]";
@@ -7,8 +12,46 @@ public class OutputView {
     private final static String RECOMMEND_RESULT_PARTITION = " | ";
     private final static String RECOMMEND_RESULT_START = "[ ";
     private final static String RECOMMEND_RESULT_END = " ]";
+    private final static String SERVICE_END = "추천을 완료했습니다.";
 
     public void printServiceStart() {
         System.out.println(SERVICE_START);
+    }
+
+    public void printRecommendResult(List<Coach> coaches, List<Category> categories) {
+        System.out.println(RECOMMEND_RESULT_DAY_WEEK);
+        printCategories(categories);
+        printCoachesMenu(coaches);
+        System.out.println();
+        System.out.println(SERVICE_END);
+    }
+
+    public void printCategories(List<Category> categories) {
+        System.out.printf(RECOMMEND_RESULT_CATEGORY);
+        for (Category category : categories) {
+            System.out.printf(category.getCategoryName() + RECOMMEND_RESULT_PARTITION);
+        }
+        System.out.println(RECOMMEND_RESULT_END);
+    }
+
+    public void printCoachesMenu(List<Coach> coaches) {
+        for (Coach coach : coaches) {
+            printCoachMenu(coach);
+        }
+    }
+
+    public void printCoachMenu(Coach coach) {
+        System.out.printf(RECOMMEND_RESULT_START);
+        System.out.printf(coach.getCoachName() + RECOMMEND_RESULT_PARTITION);
+        List<Menu> menus = coach.getRecommendMenus();
+        for (int i = 0; i < menus.size(); i++) {
+            Menu menu = menus.get(i);
+            if(i != menus.size() - 1) {
+                System.out.printf(menu.getMenuName() + RECOMMEND_RESULT_PARTITION);
+                continue;
+            }
+            System.out.printf(menu.getMenuName());
+        }
+        System.out.println();
     }
 }
