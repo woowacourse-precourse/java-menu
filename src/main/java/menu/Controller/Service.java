720ff0fb3239ca552b1236f130dac2226b30c4ab
Eun-chan Cho <eunchan1001@gmail.com>
2022-12-17 14:50:06 +0900

    feat(Service): create Service Controller

---
+++ src/main/java/menu/Controller/Service.java
@@ -0,0 +1,10 @@
+package menu.Controller;
+
+import menu.View.InputView;
+import menu.View.Outputview;
+
+public class Service {
+    private final InputView input = new InputView();
+    private final Outputview output = new Outputview();
+
+}
