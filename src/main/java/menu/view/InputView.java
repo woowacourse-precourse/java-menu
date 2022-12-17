aeb789174f5371e59a1ad3d5a30862f44bd93b47
akskflwn <akskflwn@gmail.com>
2022-12-17 17:47:38 +0900

    입출력 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -8,18 +8,24 @@ import menu.domain.Menu;
 
 public class InputView {
 
-    public List<String> readCoachNames() {
+    public static List<String> readCoachNames() {
         OutPutView.printStartMessage();
 
         OutPutView.printRequestCoachNames();
         String input = Console.readLine();
-
+        validateCoachNames(input);
         return Arrays.stream(input.split(",")).collect(Collectors.toList());
     }
 
-    public List<String> getCanNotEatMenus(String name) {
+    private static void validateCoachNames(String input) {
+        if(input.split(",").length<2 || input.split(",").length >4){
+            throw new IllegalArgumentException("코치는 최소 2명부터 4명까지가 최대입니다.");
+        }
+    }
+//
+    public static List<String> getCanNotEatMenus(String name) {
         OutPutView.printCatNotEatMenuEach(name);
-        String input=Console.readLine();
+        String input = Console.readLine();
         return Arrays.stream(input.split(",")).collect(Collectors.toList());
     }
 }
