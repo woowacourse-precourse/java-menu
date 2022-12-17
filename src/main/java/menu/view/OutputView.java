51349b7fa73d81e28d55f5a93b6a193ac4ee1319
ksh-beginner <ohk9134@naver.com>
2022-12-17 16:51:28 +0900

    feat(OuptutView) : 메뉴 추천 결과 문구 출력 기능 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,10 +1,20 @@
 package menu.view;
 
+import menu.model.DayOfTheWeek;
+
+import java.text.MessageFormat;
+import java.util.List;
+
 public class OutputView {
 
     private static final String LUNCH_MENU_RECOMMEND_MESSAGE = "점심 메뉴 추천을 시작합니다.";
     private static final String READ_COACH_NAME_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
     private static final String CANNOT_EAT_FOODS_BY_COACH_MESSAGE = "(이)가 못 먹는 메뉴를 입력해 주세요.";
+    private static final String RECOMMEND_MENU_RESULT_MESSAGE = "메뉴 추천 결과입니다.";
+    private static final String RESULT_MESSAGE_FORMAT = "[ {0} ]";
+    private static final String SPLIT_MESSAGE = "구분";
+    private static final String JOIN_DELIMITER = " | ";
+    private static final String CATEGORY_MESSAGE = "카테고리";
 
     public void printLunchMenuRecommendMessage() {
         printMessage(LUNCH_MENU_RECOMMEND_MESSAGE);
@@ -16,7 +26,13 @@ public class OutputView {
     }
 
     public void printCannotEatFoodsByCoachMessage(String coachName) {
+        printBlankLine();
         printMessage(coachName + CANNOT_EAT_FOODS_BY_COACH_MESSAGE);
+    }
+
+    public void printMenuRecommendResultMessage() {
+        printBlankLine();
+        printMessage(RECOMMEND_MENU_RESULT_MESSAGE);
     }
 
     private void printBlankLine() {
