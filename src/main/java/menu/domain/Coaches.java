82c2a441c417eace5345ba19186ca7426a14aa8a
kat8813 <ohseo8813@gmail.com>
2022-12-17 13:48:28 +0900

    <feat>(Coaches.java): 최대 코치수 추가

--- src/main/java/menu/domain/Coaches.java
+++ src/main/java/menu/domain/Coaches.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Coaches {
 
     private final static int MIN_SIZE = 2;
+    private final static int MAX_SIZE = 5;
 
     private final List<Coach> coaches;
 
@@ -13,9 +14,18 @@ public class Coaches {
         this.coaches = coaches;
     }
 
+
     private void validate(List<Coach> coaches) {
         if (coaches.size() < MIN_SIZE) {
             throw new IllegalArgumentException("코치는 최소 2명 이상 입력해야 합니다.");
         }
+        
+        if (coaches.size() > MAX_SIZE) {
+            throw new IllegalArgumentException("코치는 최대 5명 입니다.");
+        }
+    }
+
+    public List<Coach> getCoaches() {
+        return coaches;
     }
 }
