189cb92d3ae6da32b7da710d9002604dfdd1dc4e
Woojin Lee <dhkep03@gmail.com>
2022-12-17 14:02:44 +0900

    feat(Day): 요일 상수 집합 구현

--- src/main/java/menu/domain/constant/Day.java
+++ src/main/java/menu/domain/constant/Day.java
@@ -1,4 +1,15 @@
 package menu.domain.constant;
 
-public class Day {
+public enum Day {
+    MONDAY("월요일"),
+    TUESDAY("화요일"),
+    WEDNESDAY("수요일"),
+    THURSDAY("목요일"),
+    FRIDAY("금요일");
+
+    private String dayInKorean;
+
+    Day(String dayInKorean) {
+        this.dayInKorean = dayInKorean;
+    }
 }
