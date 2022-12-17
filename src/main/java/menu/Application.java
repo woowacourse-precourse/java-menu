b5d03dfb3f142f11f48006d8830a1552b3bb13c3
alstjr706 <alstjr706@naver.com>
2022-12-17 15:24:04 +0900

    feat : (점심 메뉴 추천) 애플리케이션 작성

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,8 +1,20 @@
 package menu;
 
+import menu.controller.LunchMenuMachine;
+import menu.domain.MenuMaker;
+import menu.view.InputView;
+import menu.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 인풋뷰 생성자 안에 검증 클래스 넣기.
+        InputView inputView = new InputView();
+
+        MenuMaker menuMaker = new MenuMaker(inputView);
+
+        LunchMenuMachine lunchMenuMachine = new LunchMenuMachine(inputView, new OutputView(), menuMaker);
+        lunchMenuMachine.run();
+
     }
 }
 
