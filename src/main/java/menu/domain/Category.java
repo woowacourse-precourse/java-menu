2dafb6d65e79740fcab5cf19928e50c859e63e46
seokjin8678 <rnrn8678@naver.com>
2022-12-17 17:09:58 +0900

    fix: 카테고리 잘못된 값 수정

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -7,8 +7,8 @@ public enum Category {
     JAPANESE("일식", 1),
     KOREAN("한식", 2),
     CHINESE("중식", 3),
-    WESTERN("양식", 4),
-    ASIAN("아시안", 5);
+    ASIAN("아시안", 4),
+    WESTERN("양식", 5);
 
     private final String name;
     private final int number;
