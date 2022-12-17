2b9b9111ee11b061141b81ec4e6e4e269b935db4
2014012606 <2014012606@hanyang.ac.kr>
2022-12-17 13:30:34 +0900

    feat: 코치의 이름과 먹지 못하는 음식을 관리하는 Coach 클래스 추가

---
+++ src/main/java/menu/model/Coach.java
@@ -0,0 +1,12 @@
+package menu.model;
+
+import java.util.List;
+
+public class Coach {
+    private String name;
+    private List<String> cantEatMenu;
+
+    public Coach(String name, List<String> cantEatMenu) {
+        this.name = name;
+    }
+}
