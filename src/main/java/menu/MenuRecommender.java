aa1ea19c83d87e36d07dd59ac4e6097b893dba13
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-12-17 14:02:00 +0900

    <feat>(MenuRecommender.java): MenuRecommender 생성자, 멤버변수 작성

---
+++ src/main/java/menu/MenuRecommender.java
@@ -0,0 +1,16 @@
+package menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuRecommender {
+    private List<Member> members;
+    private List<String> categories;
+
+    public MenuRecommender() {
+        members = new ArrayList<>();
+        categories = new ArrayList<>();
+    }
+
+    
+}
