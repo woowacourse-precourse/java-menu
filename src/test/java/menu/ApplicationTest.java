7e39bffdc84c6a70f50b236e8666a9dc6d6687da
thdwoqor <celebarte01@gmail.com>
2022-12-17 15:40:20 +0900

    test: 코치수 예외 테스트 추가

--- src/test/java/menu/ApplicationTest.java
+++ src/test/java/menu/ApplicationTest.java
@@ -20,11 +20,21 @@ import org.mockito.MockedStatic;
 
 public class ApplicationTest extends NsTest {
     private static final String ERROR_DIFFERENT_LENGTHS = "[ERROR] 코치의 이름은 최소 2글자, 최대 4글자이다.";
+    private static final String ERROR_DIFFERENT_COACH_LENGTHS = "[ERROR] 코치는 최소 2명, 최대 5명까지 식사를 함께 한다.";
+
     private static final Duration RANDOM_TEST_TIMEOUT = Duration.ofSeconds(10L);
 
     @DisplayName("전체 기능 테스트")
     @Nested
     class AllFeatureTest {
+        @Test
+        void 코치_수_예외_테스트() {
+            assertSimpleTest(() -> {
+                runException("토미,제임스,포코,코코,렉사이,엘리스");
+                assertThat(output()).contains(ERROR_DIFFERENT_COACH_LENGTHS);
+            });
+        }
+
         @Test
         void 코치_이름_예외_테스트() {
             assertSimpleTest(() -> {
