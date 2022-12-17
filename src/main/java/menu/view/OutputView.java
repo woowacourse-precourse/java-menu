58b78ba58077ded64b178875f4b3ea1d7e6f5c9f
iamjooon2 <iamjooon2@gmail.com>
2022-12-17 13:22:23 +0900

    feat: 시작 문구를 출력하는 기능

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,11 @@
+package menu.view;
+
+public class OutputView {
+
+    public void initMessage() {
+        System.out.println("점심 메뉴 추천을 시작합니다.\n"
+                + "\n"
+                + "코치의 이름을 입력해 주세요. (, 로 구분)");
+    }
+
+}
