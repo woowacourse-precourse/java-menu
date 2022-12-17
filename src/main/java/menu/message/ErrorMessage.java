2c3f65fb03124fda3409a8d96e0bbd54742c1a4c
yujaemin <woawoa0513@gmail.com>
2022-12-17 13:37:19 +0900

    feat: 못 먹는 음식 에러메시지 추가
    못 먹는 음식 입력 상황에서 발생할 수 있는 예외에 출력할 에러메시지 추가

--- src/main/java/menu/message/ErrorMessage.java
+++ src/main/java/menu/message/ErrorMessage.java
@@ -6,4 +6,6 @@ public class ErrorMessage {
     public static final String INCORRECT_INPUT_FORMAT = HEADER + "입력값과 구분자(,)만 입력 가능합니다.";
     public static final String LESS_THAN_MINIMUM_NUMBER_OF_COACH = HEADER + "코치는 최소 2명 이상 입력해야 합니다.";
     public static final String OVER_MAXIMUM_NUMBER_OF_COACH = HEADER + "코치는 최대 5명까지 입력 가능합니다.";
+
+    public static final String OVER_MAXIMUM_NUMBER_OF_DISLIKE_FOOD = HEADER + "못 먹는 음식은 최대 2개까지 입력 가능합니다";
 }
