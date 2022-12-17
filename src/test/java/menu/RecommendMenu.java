dce822b411adc6959524cf954fccd66a7dc5dc92
kimdahee0815 <diana815@naver.com>
2022-12-17 14:46:06 +0900

    feat(pickRandomCategory) : 랜덤한 카테고리를 먼저 뽑는 함수를 생성한다

--- src/test/java/menu/RecommendMenu.java
+++ src/test/java/menu/RecommendMenu.java
@@ -2,24 +2,29 @@ package menu;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class RecommendMenu {
 	
 	ArrayList<String> coachName;
 	LinkedHashMap<String, ArrayList<String>> notEatingMenuForEachCoach;
 	
-	enum category {
-		JAPANESE(1), KOREAN(2), CHINESE(3), ASIAN(4), WESTERN(5);
+	enum categories {
+		JAPANESE("일식", 1), KOREAN("한식", 2), CHINESE("중식", 3), ASIAN("아시안", 4), WESTERN("양식", 5);
 		
 		private final int value;
-		category(int value){
+		private final String category;
+		
+		categories(String category, int value){
+			this.category = category;
 			this.value = value;
 		}
-		
 		public int getValue() {
 			return value;
 		}
-		
+		public String getCategory() {
+			return category;
+		}	
 	}
 	
 	public RecommendMenu(ArrayList<String> coachName, LinkedHashMap<String, ArrayList<String>> notEatingMenuForEachCoach ) {
@@ -32,6 +37,17 @@ public class RecommendMenu {
 		System.out.println("메뉴 추천 결과입니다.");
 		System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
 		System.out.println("[ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]");
+	}
+	
+	public String pickRandomCategory() {
+		String category = "";
+		int randNum = Randoms.pickNumberInRange(1, 5);
+		for(categories c : categories.values()) {
+			if(c.getValue() == randNum) {
+				category = c.getCategory();
+			}
+		}
+		return category;
 	}
 	
 	
