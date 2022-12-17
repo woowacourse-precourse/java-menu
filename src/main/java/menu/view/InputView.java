71ec97942d281d35d5eba1b399131ae6d34ab89b
areyouhun <areyouhun@gmail.com>
2022-12-17 14:21:00 +0900

    refactor(inputview): 입력값에 대해 1차 검증하는 기능들을 새로운 클래스로 분리

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,48 +1,35 @@
 package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 
 public class InputView {
 
     private static final String ENTER_CREW_NAMES = "코치의 이름을 입력해 주세요. (, 로 구분)";
     private static final String ENTER_BANNED_FOODS = "(이)가 못 먹는 메뉴를 입력해 주세요.";
-    private static final String EMPTY_INPUT = "입력된 값이 없습니다.";
-    private static final String INVALID_KOREAN_FORMAT = "한글을 올바르게 입력해주세요.";
-    private static final String NON_KOREAN_CHARACTER_INCLUDED = "(.*)[^가-힇](.*)";
     private static final String COMMA = ",";
 
+    private final InputValidator inputValidator;
+
+    public InputView() {
+        inputValidator = new InputValidator();
+    }
+
     public String[] readCrewNames() {
-        final String input = inputBox(ENTER_CREW_NAMES);
-        validate(input);
-        return splitByRegex(input);
+        final String[] inputValues = splitByRegex(inputBox(ENTER_CREW_NAMES));
+        Arrays.stream(inputValues).forEach(inputValidator::validate);
+        return inputValues;
     }
 
     public String[] readBannedFoods(String crewName) {
-        final String input = inputBox(crewName + ENTER_BANNED_FOODS);
-        validate(input);
-        return splitByRegex(input);
+        final String[] inputValues = splitByRegex(inputBox(crewName + ENTER_BANNED_FOODS));
+        Arrays.stream(inputValues).forEach(inputValidator::validate);
+        return inputValues;
     }
 
     private String inputBox(String message) {
         System.out.println(message);
         return Console.readLine();
-    }
-
-    private void validate(String input) {
-        validateBlank(input);
-        validateKorean(input);
-    }
-
-    public void validateBlank(String input) {
-        if (input.isBlank()) {
-            throw new IllegalArgumentException(EMPTY_INPUT);
-        }
-    }
-
-    private void validateKorean(String input) {
-        if (input.matches(NON_KOREAN_CHARACTER_INCLUDED)) {
-            throw new IllegalArgumentException(INVALID_KOREAN_FORMAT);
-        }
     }
 
     private static String[] splitByRegex(String input) {
