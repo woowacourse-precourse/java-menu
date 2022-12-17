c9cd63f29c576eb06ce84efd5c763f3570e29ea7
saint6839 <saint6839@gmail.com>
2022-12-17 15:21:33 +0900

    feat(domain) : 요일 enum 추가

---
+++ src/main/java/domain/DayOfWeek.java
@@ -0,0 +1,11 @@
+package domain;
+
+public enum DayOfWeek {
+    MON("월요일"), TUE("화요일"), WED("수요일"), THU("목요일"), FRI("금요일");
+
+    private final String dayOfWeek;
+
+    DayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+}
