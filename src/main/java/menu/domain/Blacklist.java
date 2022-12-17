4da5246d5f3d4d08ed7aa3a575ae5f84ff15bbe9
drunkenhw <drunkenhw@gmail.com>
2022-12-17 17:09:36 +0900

    refactor(Blacklist): Blacklist에 대한 검증을 Blacklist에서 하도록 변경

--- src/main/java/menu/domain/Blacklist.java
+++ src/main/java/menu/domain/Blacklist.java
@@ -3,13 +3,22 @@ package menu.domain;
 import java.util.List;
 
 public class Blacklist {
+    private static final int BLACKLIST_MAX_SIZE = 2;
+
     private List<Menu> menus;
 
     public Blacklist(List<Menu> menus) {
+        validateBlacklist(menus);
         this.menus = menus;
     }
 
     public boolean isExist(Menu menu) {
         return menus.contains(menu);
+    }
+
+    private void validateBlacklist(List<Menu> blacklist) {
+        if (blacklist.size() > BLACKLIST_MAX_SIZE) {
+            throw new IllegalArgumentException("못 먹는 메뉴는 2가지까지만 가능합니다");
+        }
     }
 }
