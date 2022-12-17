41d22a92ef2265b3382e61b2cf53b59e35bd5f18
= <=>
2022-12-17 13:26:18 +0900

    feat: 메뉴 및 코치 모델 객체 생성

---
+++ src/main/java/menu/model/Coach.java
@@ -0,0 +1,12 @@
+package menu.model;
+
+import java.util.List;
+
+/**
+ * @Author : jeeseob
+ * @CreateAt : 2022/12/17
+ */
+public class Coach {
+    private String name;
+    private List<Menu> hateMenus;
+}
---
+++ src/main/java/menu/model/Menu.java
@@ -0,0 +1,10 @@
+package menu.model;
+
+/**
+ * @Author : jeeseob
+ * @CreateAt : 2022/12/17
+ */
+public class Menu {
+    private MenuCategory menuCategory;
+    private String name;
+}
