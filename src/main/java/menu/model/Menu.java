aa2dd4008a4039b7a66e4511dff506fb41698296
은지 <midzy0228@gmail.com>
2022-12-17 17:53:47 +0900

    refactor: 리스트 초기화 변경

--- src/main/java/menu/model/Menu.java
+++ src/main/java/menu/model/Menu.java
@@ -2,7 +2,7 @@ package menu.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,7 +31,8 @@ public class Menu {
     }
 
     public String recommendFood(String category) {
-        return Randoms.shuffle(menu.get(Category.getCategory(category))).get(0);
+        List<String> result = new ArrayList<>(menu.get(Category.getCategory(category)));
+        return Randoms.shuffle(result).get(0);
     }
 
     private void validate(List<String> dislikeFood) {
@@ -46,23 +47,23 @@ public class Menu {
     }
 
     private List<String> makeKoreanFood() {
-        return Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음");
+        return List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음");
     }
 
     private List<String> makeJapaneseFood() {
-        return Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼");
+        return List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼");
     }
 
 
     private List<String> makeChineseFood() {
-        return Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채");
+        return List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채");
     }
 
     private List<String> makeAsianFood() {
-        return Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜");
+        return List.of("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜");
     }
 
     private List<String> makeWesternFood() {
-        return Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니");
+        return List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니");
     }
 }
