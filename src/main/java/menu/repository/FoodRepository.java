d7f00a1833e80f81916a01c0b6b9905fc6d1c273
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 13:42:53 +0900

    feat(FoodRepository): 필드, 생성자 추가

--- src/main/java/menu/repository/FoodRepository.java
+++ src/main/java/menu/repository/FoodRepository.java
@@ -1,11 +1,18 @@
 package menu.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FoodRepository {
     private static class InstanceHolder {
         private static final FoodRepository INSTANCE = new FoodRepository();
     }
 
-    private FoodRepository(){}
+    private List<String> foods;
+
+    private FoodRepository() {
+        foods = new ArrayList<>();
+    }
 
     public static FoodRepository getInstance() {
         return FoodRepository.InstanceHolder.INSTANCE;
