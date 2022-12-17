702eca933b123bf0a2af02d87a6122d814b84acd
jyeost <pig9619@naver.com>
2022-12-17 13:32:01 +0900

    feat : mvc 패턴을 만들음

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,11 @@
 package menu;
 
+import menu.controller.RecommendMenuController;
+import menu.view.InputView;
+import menu.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new RecommendMenuController(new InputView(), new OutputView()).startRecommend();
     }
 }
---
+++ src/main/java/menu/controller/RecommendMenuController.java
@@ -0,0 +1,18 @@
+package menu.controller;
+
+import menu.view.Input;
+import menu.view.Output;
+
+public class RecommendMenuController {
+    private final Input input;
+    private final Output output;
+
+    public RecommendMenuController(Input input, Output output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public void startRecommend() {
+
+    }
+}
