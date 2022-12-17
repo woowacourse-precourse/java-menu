a1cab2c57c91140c5b260a88da15bd76d082a438
JeonghoPark <pjhpjh0805@naver.com>
2022-12-17 17:10:34 +0900

    refactor: Validator 필요없는 코드 제거 및 하드코딩 상수로 대체

--- src/main/java/menu/util/Validator.java
+++ src/main/java/menu/util/Validator.java
@@ -10,36 +10,45 @@ public class Validator {
 
     private static final String COACH_NAME_LENGTH_ERROR = "코치 이름은 최소 2글자 최대 4글자이여야합니다.";
 
-    private static final String CAN_EAT_EVERYTHING = "";
-
     private static final String HATING_FOOD_LENGTH_ERROR = "못 먹는 메뉴는 최대 2개 까지 가능합니다.";
 
     private static final String NOT_EXIST_MENUS = "메뉴판에 존재하지 않는 메뉴입니다.";
+
+    private static final String COMMA = ",";
+
+    private static final int NAME_LENGTH_MIN_VALUE = 2;
+
+    private static final int NAME_LENGTH_MAX_VALUE = 4;
+
+    private static final int MENU_LENGTH_MAX_VALUE = 2;
+
+    private static final int COACH_NUMBER_MIN_VALUE = 2;
+
+    private static final int COACH_NUMBER_MAX_VALUE = 5;
 
     public static void checkCoachNames(String input) {
         checkCoachNumber(input);
         checkCoachNameLength(input);
     }
 
+    private static void checkCoachNumber(String input) {
+        int length = input.split(COMMA).length;
+        if (length < COACH_NUMBER_MIN_VALUE || length > COACH_NUMBER_MAX_VALUE) {
+            throw new IllegalArgumentException(COACH_NUMBER_ERROR);
+        }
+    }
+
     private static void checkCoachNameLength(String input) {
-        String[] names = input.split(",");
-        for (String name : names) {
-            if (name.length() < 2 || name.length() > 4) {
+        for (String name : input.split(COMMA)) {
+            if (name.length() < NAME_LENGTH_MIN_VALUE || name.length() > NAME_LENGTH_MAX_VALUE) {
                 throw new IllegalArgumentException(COACH_NAME_LENGTH_ERROR);
             }
         }
     }
 
-    private static void checkCoachNumber(String input) {
-        int length = input.split(",").length;
-        if (length < 2 || length > 5) {
-            throw new IllegalArgumentException(COACH_NUMBER_ERROR);
-        }
-    }
-
     public static void checkHatingMenus(String menus) {
         checkMenusLength(menus);
-        for (String menu : menus.split(",")) {
+        for (String menu : menus.split(COMMA)) {
             checkExistMenu(menu);
         }
     }
@@ -52,8 +61,8 @@ public class Validator {
     }
 
     public static void checkMenusLength(String menus) {
-        int length = menus.split(",").length;
-        if (length > 2) {
+        int length = menus.split(COMMA).length;
+        if (length > MENU_LENGTH_MAX_VALUE) {
             throw new IllegalArgumentException(HATING_FOOD_LENGTH_ERROR);
         }
     }
