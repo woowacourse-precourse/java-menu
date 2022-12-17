61120cbaf7013131b1d4fd484ea750a8c051208e
starwook <star.wookk@gmail.com>
2022-12-17 17:05:01 +0900

    feat: Add 새로운 에러 메시지

--- src/main/java/menu/error/ErrorResource.java
+++ src/main/java/menu/error/ErrorResource.java
@@ -6,4 +6,5 @@ public class ErrorResource {
     public static String COACH_BIG = "코치 숫자 초과.";
     public static String NO_EXIST_MENU = "없는 메뉴입니다.";
     public static String TOO_MANY_CANT_EAT ="먹지 못 하는 메뉴가 너무 많습니다.";
+    public static String WRONG_COACH_NAME_LENGTH ="코치 이름의 길이는 2~4가 되어야합니다.";
 }
