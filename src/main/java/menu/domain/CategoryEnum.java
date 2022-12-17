89499d63ddcf77cde1420a6f52ca765b3c9e5c03
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 15:32:28 +0900

    feat: 값에 맞는 요일을 출력하기 위한 Enum Class 구현

--- src/main/java/menu/domain/CategoryEnum.java
+++ src/main/java/menu/domain/CategoryEnum.java
@@ -1,2 +1,29 @@
-package menu.domain;public enum CategoryEnum {
+package menu.domain;
+
+public enum CategoryEnum {
+    MONDAY(1,"월요일"), TUESDAY(2,"화요일"), WEDNESDAY(3,"수요일"), THURSDAY(4,"목요일"), FRIDAY(5,"금요일");
+
+    final private Integer value;
+    final private String msg;
+
+    CategoryEnum(Integer value, String msg) {
+        this.value = value;
+        this.msg = msg;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public static String getMessageByValue(Integer val) {
+        for (CategoryEnum categoryEnum : CategoryEnum.values()) {
+            if (categoryEnum.getValue() == val)
+                return categoryEnum.getMsg();
+        }
+        return "";
+    }
 }
