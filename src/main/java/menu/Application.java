50978f08d759558745ad92d0907450347b6413c1
YoosunMoon <muzikue@naver.com>
2022-12-17 17:12:55 +0900

    feat: 요일별 카테고리 생성 후 출력

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,73 @@
 package menu;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        HashMap<String, HashSet<String>> coachesMealPlan = new HashMap<>();
+        HashMap<String, String> categoryNameOfWeek = new HashMap<>();
+        List<String> week = Arrays.asList("월요일", "화요일", "수요일", "목요일", "금요일");
+        HashMap<String, Set<String>> coachesHateFood = new HashMap<>();
+
+        while (true) {
+            System.out.println("점심 메뉴 추천을 시작합니다.");
+            List<String> coaches = InputView.inputNames();
+
+            for (String name : coaches) {
+                System.out.println(name + "(이)가 못 먹는 메뉴를 입력해 주세요.");
+                coachesHateFood.put(name, new HashSet<>(InputView.inputHateFoods()));
+                System.out.println(coachesHateFood.get(name));
+                System.out.println(coachesHateFood.get(name).size());
+            }
+
+            while (true) {
+                Map<String, Integer> pickedCategoryCount = new HashMap<>();
+                for (int i = 0; i < 5; i++) {
+                    String randomCategory = Category.getCategoryByNumber(Randoms.pickNumberInRange(1, 5)).getName();
+                    pickedCategoryCount.put(randomCategory, pickedCategoryCount.getOrDefault(randomCategory, 0) + 1);
+                    categoryNameOfWeek.put(week.get(i), randomCategory);
+                }
+
+                for (Map.Entry<String, Integer> entry : pickedCategoryCount.entrySet()) {
+                    if (entry.getValue() > 2) {
+                        pickedCategoryCount.clear();
+                        categoryNameOfWeek.clear();
+                        continue;
+                    }
+                }
+                break;
+            }
+
+            System.out.println("메뉴 추천 결과입니다.");
+            System.out.print(categoryNameOfWeek.keySet() + " ");
+            System.out.print(categoryNameOfWeek.values());
+
+            for (int i = 0; i < coachesHateFood.size(); ) {
+                for (Map.Entry<String, Set<String>> entry : coachesHateFood.entrySet()) {
+                    System.out.println("오류원인 : " + categoryNameOfWeek.get(week.get(i)));
+                    String categoryName = categoryNameOfWeek.get(week.get(i));
+                    List<String> menus = Category.getMenusByCategoryName(categoryName);
+                    String menu = Randoms.shuffle(menus).get(0);
+
+                    if (!entry.getValue().contains(menu)) {
+                        int mealPlanSize = coachesMealPlan.get(entry.getKey()).size();
+                        coachesMealPlan.get(entry.getKey()).add(menu);
+                        if (coachesMealPlan.get(entry.getKey()).size() != mealPlanSize) {
+                            i++;
+                        }
+                    }
+                }
+            }
+
+            System.out.println(coachesMealPlan);
+        }
     }
 }
---
+++ src/main/java/menu/Category.java
@@ -0,0 +1,58 @@
+package menu;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Category {
+    JAPANESE("일식", 1, Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
+    KOREAN("한식", 2, Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")),
+    CHINESE("중식", 3, Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
+    ASIAN("아시안", 4, Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")),
+    WESTERN("양식", 5, Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
+
+    private final String name;
+    private final int number;
+    private final List<String> menus;
+
+    Category(final String name, final int number, final List<String> menus) {
+        this.name = name;
+        this.number = number;
+        this.menus = menus;
+    }
+
+    public static Category getCategoryByNumber(int number) {
+        for (Category category : values()) {
+            if (category.getNumber() == number) {
+                return category;
+            }
+        }
+        throw new IllegalArgumentException("[ERROR] 해당 번호 카테고리가 없습니다.");
+    }
+
+    public static List<String> getMenusByCategoryName(String name) {
+        for (Category category : values()) {
+            if (category.getName().equals(name)) {
+                return category.getMenus();
+            }
+        }
+
+        throw new IllegalArgumentException("[ERROR] 해당 카테고리 이름과 일치하는 카테고리가 없습니다.");
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getMenus() {
+        return menus;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + name + "]";
+    }
+}
