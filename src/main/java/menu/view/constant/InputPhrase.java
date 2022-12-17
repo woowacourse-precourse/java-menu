050694400da01adb95e77a47fe315a3884dfe6a1
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 16:47:16 +0900

    feat(view/constant/): 입력 문구 enum 에서 출력 문구 분리

--- src/main/java/menu/view/constant/InputPhrase.java
+++ src/main/java/menu/view/constant/InputPhrase.java
@@ -1,11 +1,8 @@
 package menu.view.constant;
 
 public enum InputPhrase {
-    START("점심 메뉴 추천을 시작합니다."),
     COACH_INPUT("코치의 이름을 입력해 주세요. (, 로 구분)"),
-    COACH_PREFERENCE("%s(이)가 못 먹는 메뉴를 입력해 주세요."),
-    RESULT("메뉴 추천 결과입니다."),
-    FINISH("추천을 완료했습니다.");
+    COACH_PREFERENCE("%s(이)가 못 먹는 메뉴를 입력해 주세요.");
 
     private final String phrase;
 
