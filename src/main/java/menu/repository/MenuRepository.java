ec96c7d57742198eb3a21085399f84d63a21b79b
akskflwn <akskflwn@gmail.com>
2022-12-17 17:48:46 +0900

    feat(Repository) : 메뉴, 요일 저장소 구현

--- src/main/java/menu/repository/MenuRepository.java
+++ src/main/java/menu/repository/MenuRepository.java
@@ -3,14 +3,16 @@ package menu.repository;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import menu.domain.Category;
 import menu.domain.Menu;
 
-public class MenuRepository {
+public class MenuRepository implements Cloneable {
     private static final List<Menu> menus = new ArrayList<>();
 
     static{
+        menus.add(new Menu("BLANK",Category.NOTHING));
         menus.add(new Menu("규동", Category.JAPANESE_FOOD));
         menus.add(new Menu("우동", Category.JAPANESE_FOOD));
         menus.add(new Menu("미소시루", Category.JAPANESE_FOOD));
@@ -72,10 +74,15 @@ public class MenuRepository {
             .orElseThrow(()-> new IllegalArgumentException("존재하지 않는 메뉴 번호입니다"));
     }
 
-    public static List<Menu> findByMenuCategory(Category category) {
+    public static List<Menu> findMenuByCategory(Category category) {
         return menus().stream().filter(menu -> menu.getCategory().equals(category))
             .collect(Collectors.toList());
     }
+
+    public static List<String> getMenuName(List<Menu> menuList) {
+        return menuList.stream().map(menu -> menu.getName()).collect(Collectors.toList());
+    }
+
 //    일식: 규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼
 //한식: 김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음
 //중식: 깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채
--- src/main/java/menu/repository/WeekendRepository.java
+++ src/main/java/menu/repository/WeekendRepository.java
@@ -2,7 +2,9 @@ package menu.repository;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import menu.domain.Category;
 import menu.domain.Weekend;
 
 public class WeekendRepository {
@@ -10,11 +12,11 @@ public class WeekendRepository {
     private static final List<Weekend> weekends = new ArrayList<>();
 
     static {
-        weekends.add(new Weekend("월요일"));
-        weekends.add(new Weekend("화요일"));
-        weekends.add(new Weekend("수요일"));
-        weekends.add(new Weekend("목요일"));
-        weekends.add(new Weekend("금요일"));
+        weekends.add(new Weekend("월요일", Category.NOTHING));
+        weekends.add(new Weekend("화요일", Category.NOTHING));
+        weekends.add(new Weekend("수요일", Category.NOTHING));
+        weekends.add(new Weekend("목요일", Category.NOTHING));
+        weekends.add(new Weekend("금요일", Category.NOTHING));
 
     }
 
@@ -27,5 +29,19 @@ public class WeekendRepository {
             .filter(day -> day.equals(dayOfWeek))
             .findAny()
             .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 요일입니다."));
+    }
+
+
+    public static boolean checkDuplication(List<Weekend> weekend) {
+        HashMap<Category,Integer> map = new HashMap<>();
+        for (Weekend day : weekend) {
+            map.put(day.getCategory(), map.getOrDefault(day.getCategory(),0)+1);
+        }
+        for (Category category : map.keySet()) {
+            if(map.get(category)>3){
+                return true;
+            }
+        }
+        return false;
     }
 }
