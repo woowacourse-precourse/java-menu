69a129da8742131f33f527d18cacc017802607a7
Kayoung <rkdkdudjd@gmail.com>
2022-12-17 13:56:38 +0900

    feat(System): 코치는 최소 두명이어야한다
    아닐 경우 예외처리

--- src/main/java/menu/MenuRecommendSystem.java
+++ src/main/java/menu/MenuRecommendSystem.java
@@ -7,6 +7,7 @@ import menu.view.InputView;
 import menu.view.OutputView;
 
 public class MenuRecommendSystem {
+    public static final String LESS_THAN_MIN_VALUE = "코치는 최소 2명 이상 입력해야 합니다.";
     private List<Coach> coaches;
     /**
      * 메뉴 추천 시스템 동작 메서드*
@@ -19,10 +20,17 @@ public class MenuRecommendSystem {
      * * *
      */
     public void run() {
+        start();
         makeCoaches();
         for(Coach coach: coaches) {
             addMenuCannotToEat(coach);
         }
+    }
+    /**
+     * 시작하기*
+     */
+    public void start() {
+        OutputView.printStartMessage();
     }
 
     /**
@@ -41,6 +49,10 @@ public class MenuRecommendSystem {
     }
 
     private static List<Coach> createCoachesByNames() {
+        List<String> coachNameList = InputView.readCoachNameList();
+        if (coachNameList.size() < 2) {
+            throw new IllegalArgumentException(LESS_THAN_MIN_VALUE);
+        }
         return InputView.readCoachNameList().stream().map(Coach::new).collect(Collectors.toList());
     }
 }
