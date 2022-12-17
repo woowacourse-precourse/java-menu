9af886da96356437c7504b0dba7d3591a1ee3805
sosow0212 <sosow0212@naver.com>
2022-12-17 14:24:06 +0900

    feat : (CategoryGenerator) 카테고리를 만들어주는 기능 구현

---
+++ src/main/java/menu/util/CategoryGenerator.java
@@ -0,0 +1,26 @@
+package menu.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import menu.domain.Categories;
+import menu.domain.menu.FoodStyle;
+
+public class CategoryGenerator {
+    private Categories categories = new Categories();
+
+    public Categories generateCategories() {
+        categories = new Categories();
+        makeCategory();
+        System.out.println(categories.getCategories());
+        return categories;
+    }
+
+    public void makeCategory() {
+        while (!categories.isDone()) {
+            FoodStyle foodStyle = FoodStyle.getFoodStyle(Randoms.pickNumberInRange(1, 5));
+            if (categories.isFullOfCategory(foodStyle)) {
+                continue;
+            }
+            categories.addCategory(foodStyle.getStyle());
+        }
+    }
+}
