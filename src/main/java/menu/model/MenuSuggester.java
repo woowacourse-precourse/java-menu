ec3321534ff1b44d02ddf92c91943b051420d11c
choyeram <choyeram8@gmail.com>
2022-12-17 14:02:18 +0900

    feat: 카테고리 랜덤 정하기

--- src/main/java/menu/model/MenuSuggester.java
+++ src/main/java/menu/model/MenuSuggester.java
@@ -1,15 +1,20 @@
 package menu.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MenuSuggester {
     private List<Coach> coaches;
-    private List<List<String>> menus;
+    private List<List<String>> categories;
+    private HashMap<Integer, Integer> suggestedCategory;
 
     public MenuSuggester() {
         MenuGenerator menuGenerator = new MenuGenerator();
-        menus = menuGenerator.generate();
-        System.out.println(menus);
+        categories = menuGenerator.generate();
+        suggestedCategory = new HashMap<>();
     }
 
     public void setCoaches(List<Coach> coaches) {
@@ -17,6 +22,9 @@ public class MenuSuggester {
     }
 
     public void suggest() {
+        int category = Randoms.pickNumberInRange(1, 5);
+        suggestedCategory.put(category, suggestedCategory.getOrDefault(category, 0) + 1);
 
+        pick
     }
 }
