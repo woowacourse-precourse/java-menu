1b85d5466099aeae8c99354a3012f9ecfe914fd5
DongHo Kim <hkim4410@naver.com>
2022-12-17 14:46:53 +0900

    refactor(Menu): Enum 형으로 리팩토링

--- src/main/java/menu/domain/Menu.java
+++ src/main/java/menu/domain/Menu.java
@@ -1,36 +1,34 @@
 package menu.domain;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Menu {
-    private static final List<String> japanMenus = new ArrayList<>
-            (List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼"));
-    private static final List<String> koreanMenus = new ArrayList<>
-            (List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"));
-    private static final List<String> chineseMenus = new ArrayList<>
-            (List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채"));
-    private static final List<String> asianMenus = new ArrayList<>
-            (List.of("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜"));
-    private static final List<String> westernMenus = new ArrayList<>
-            (List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
+public enum Menu {
+    JAPAN("일식", Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
+    KOREAN("한식", Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")),
+    CHINESE("중식", Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
+    ASIAN("아시안", Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")),
+    WESTERN("양식", Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
 
-    public static boolean isExistMenus(String cantEatMenu) {
-        if (japanMenus.contains(cantEatMenu)) {
-            return true;
-        }
-        if (koreanMenus.contains(cantEatMenu)) {
-            return true;
-        }
-        if (chineseMenus.contains(cantEatMenu)) {
-            return true;
-        }
-        if (asianMenus.contains(cantEatMenu)) {
-            return true;
-        }
-        if (westernMenus.contains(cantEatMenu)) {
-            return true;
-        }
-        return false;
+    private final String name;
+    private final List<String> menus;
+
+    Menu(String name, List<String> menus) {
+        this.name = name;
+        this.menus = menus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getMenus() {
+        return menus;
+    }
+
+    public static boolean isExist(String menu) {
+        return Arrays.stream(Menu.values())
+                .map(Menu::getMenus)
+                .anyMatch(menus -> menus.contains(menu));
     }
 }
