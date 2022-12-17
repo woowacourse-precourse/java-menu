ba943935d6982c15730c9e7832996a09d0ec5754
thdwoqor <celebarte01@gmail.com>
2022-12-17 15:16:52 +0900

    refactor: 상수 값 하드코딩 된 것을 enum으로 변경

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -16,16 +16,27 @@ public class InputView {
     }
 
     public static List<String> readCoachName() {
-        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        System.out.println(Message.COACH_NAME_INPUT_MESSAGE.message);
         String name = Console.readLine();
         String[] names = name.split(",");
         return Arrays.asList(names);
     }
 
     public static List<String> readCantEats(String name) {
-        System.out.println(String.format("\n%s(이)가 못 먹는 메뉴를 입력해 주세요.", name));
+        System.out.println(String.format(Message.CANT_EAT_INPUT_MESSAGE.message, name));
         String menu = Console.readLine();
         String[] menus = menu.split(",");
         return Arrays.asList(menus);
+    }
+
+    private enum Message {
+        COACH_NAME_INPUT_MESSAGE("코치의 이름을 입력해 주세요. (, 로 구분)"),
+        CANT_EAT_INPUT_MESSAGE("\n%s(이)가 못 먹는 메뉴를 입력해 주세요.");
+
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
     }
 }
