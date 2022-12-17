a4096e3a2a2b8ccaf3be0ed7ffb9e53acfdbc23c
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 16:48:06 +0900

    feat(OutputView): 메뉴 추천 결과 출력 기능 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,6 +1,15 @@
 package menu.view;
 
+import menu.view.constant.DayStyle;
+import menu.view.constant.InputPhrase;
+import menu.view.constant.OutputPhrase;
 import menu.view.constant.Prefix;
+import menu.view.constant.RecommendationStyle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class OutputView {
     private static class InstanceHolder {
@@ -19,5 +28,39 @@ public class OutputView {
 
     public void printErrorMessage(String message) {
         System.out.println(Prefix.ERROR.getPrefix() + message);
+    }
+
+    public void printRecommendations(List<String> categories, List<List<String>> recommendations) {
+        printPhrase(OutputPhrase.RESULT.getPhrase());
+        System.out.println(styleDays());
+        System.out.println(styleCategories(categories));
+        recommendations.stream().map(this::styleRecommendation)
+                .forEach(System.out::println);
+    }
+
+    private String styleDays() {
+        List<String> dayOutput = new ArrayList<>();
+        dayOutput.add(RecommendationStyle.DAY.getStyle());
+        Arrays.stream(DayStyle.values()).forEach(day -> dayOutput.add(day.getStyle()));
+        return styleElements(dayOutput);
+    }
+
+    private String styleCategories(List<String> categories) {
+        List<String> categoryOutput = new ArrayList<>();
+        categoryOutput.add(RecommendationStyle.CATEGORY.getStyle());
+        categoryOutput.addAll(categories);
+        return styleElements(categoryOutput);
+    }
+
+    private String styleRecommendation(List<String> recommendation) {
+        return styleElements(recommendation);
+    }
+
+    private String styleElements(List<String> elements) {
+        return elements.stream().collect(Collectors.joining(
+                RecommendationStyle.DELIMITER.getStyle(),
+                RecommendationStyle.START.getStyle(),
+                RecommendationStyle.END.getStyle()
+        ));
     }
 }
