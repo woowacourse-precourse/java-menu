047b40958928726db1a4bea37262d3781a85772b
iamjooon2 <iamjooon2@gmail.com>
2022-12-17 13:31:42 +0900

    feat: 코치의 싫어하는 음식 질문을 출력하는 기능

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -8,4 +8,8 @@ public class OutputView {
                 + "코치의 이름을 입력해 주세요. (, 로 구분)");
     }
 
+    public void askCoachHate(String name) {
+        System.out.printf("%s(이)가 못 먹는 메뉴를 입력해 주세요\n", name);
+    }
+
 }
