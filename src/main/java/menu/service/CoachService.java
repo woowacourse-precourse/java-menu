abb6bc19ed56f64f136572848f0f04a3aee0b666
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 17:07:45 +0900

    feat(CoachService): 코치 등록 서비스 기능 구현

---
+++ src/main/java/menu/service/CoachService.java
@@ -0,0 +1,24 @@
+package menu.service;
+
+import menu.domain.Coach;
+import menu.repository.CoachRepository;
+
+import java.util.List;
+
+public class CoachService {
+    private static class InstanceHolder {
+        private static final CoachService INSTANCE = new CoachService();
+    }
+
+    private final CoachRepository coachRepository = CoachRepository.getInstance();
+
+    private CoachService(){}
+
+    public static CoachService getInstance() {
+        return CoachService.InstanceHolder.INSTANCE;
+    }
+
+    public void registerCoach(List<String> coaches) {
+        coaches.forEach(coachRepository::addCoach);
+    }
+}
