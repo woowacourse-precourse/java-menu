e511f74864689760cd00bdade090b85ef619e917
Eun-chan Cho <eunchan1001@gmail.com>
2022-12-17 15:33:22 +0900

    feat(Service): add makeRecommendations method

--- src/main/java/menu/Controller/Service.java
+++ src/main/java/menu/Controller/Service.java
@@ -8,15 +8,22 @@ import menu.View.Outputview;
 public class Service {
     private final InputView input = new InputView();
     private final Outputview output = new Outputview();
+    private Recommend recommend = new Recommend();
+
     private Coachs coachs;
 
-    void generateCoachs() {
+    public void generateCoachs() {
         this.coachs = this.input.readCoachNames();
     }
 
-    void setInedible() {
+    public void setInedible() {
         for (Coach coach : this.coachs.getCoachs()) {
             coach.setInedible(this.input.readInedibles(coach));
         }
     }
+
+    public void makeRecommendations() {
+        recommend.makeRecommendations(this.coachs);
+    }
+
 }
