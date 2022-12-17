4cbdd507f0693ec2aed6fc358a7e87c120685996
HaODay <ycis5013@naver.com>
2022-12-17 13:36:47 +0900

    feat(OutputView): 서비스 시작 문구 출력 기능 구현

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,10 @@
+package menu.view;
+
+import static menu.util.OutputMessage.START_PROGRAM;
+
+public class OutputView {
+
+    public void printStartMessage(){
+        System.out.println(START_PROGRAM);
+    }
+}
