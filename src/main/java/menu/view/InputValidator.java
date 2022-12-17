f390256e4e6d38b8b4f2372e0bcab64428cd2182
hanueleee <ezzeezz@naver.com>
2022-12-17 14:07:20 +0900

    feat(view): 코치별로 못 먹는 메뉴 입력받는 기능 구현

--- src/main/java/menu/view/InputValidator.java
+++ src/main/java/menu/view/InputValidator.java
@@ -3,4 +3,7 @@ package menu.view;
 public class InputValidator {
     public void validateCoachNamesCommand(String command) {
     }
+
+    public void validateNoEatFoodsCommand(String command) {
+    }
 }
--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 public class InputView {
     private final String READ_COACH_NAMES_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private final String READ_NO_EAT_FOODS_MESSAGE = "(이)가 못 먹는 메뉴를 입력해 주세요.";
     private static final InputValidator inputvalidator = new InputValidator();
+
     public List<String> readCoachNames() {
         System.out.println(READ_COACH_NAMES_MESSAGE);
         String command = Console.readLine();
@@ -15,7 +17,10 @@ public class InputView {
         return Arrays.asList(command.split(","));
     }
 
-    public List<String> readNoEatFoods() {
-        // 못 먹는 메뉴 입력 받기
+    public List<String> readNoEatFoods(String coachName) {
+        System.out.println(coachName + READ_NO_EAT_FOODS_MESSAGE);
+        String command = Console.readLine();
+        inputvalidator.validateNoEatFoodsCommand(command);
+        return Arrays.asList(command.split(","));
     }
 }
