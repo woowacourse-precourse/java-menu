c1b3ad84e71504b7dccf4806617663f73488e5f7
hoin <apicad0307@gmail.com>
2022-12-17 16:45:22 +0900

    feat(output/OutputView.java): 결과 출력 형식에 맞게 구현하는 기능 구현

---
+++ src/main/java/menu/output/OutputView.java
@@ -0,0 +1,48 @@
+package menu.output;
+
+import menu.domain.Category;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class OutputView {
+    public void GAME_START_OUTPUT(){
+        System.out.println(OutputString.GAME_START_OUTPUT);
+    }
+
+    public void format(List<String> lists, String prefix){
+        StringBuilder sb = new StringBuilder("[ " + prefix + " ");
+
+        for (String list : lists) {
+            sb.append("| "+ list +" ");
+        }
+        sb.append("]");
+        System.out.println(sb);
+    }
+    public void format(Set<String> lists, String prefix){
+        StringBuilder sb = new StringBuilder(" [ " + prefix +" ");
+
+        for (String list : lists) {
+            sb.append("| "+ list +" ");
+        }
+        sb.append("]");
+        System.out.println(sb);
+    }
+    public void PRINT_RESULT(List<Category> categories, Map<String, Set<String>> coaches){
+        List<String> day = Arrays.asList("월요일", "화요일", "수요일", "목요일", "금요일");
+        List<String> categoryList = categories.stream().map(c -> c.getCategory()).collect(Collectors.toList());
+
+        System.out.println(OutputString.MENU_RESULT);
+        format(day, OutputString.SPLITTER);
+        format(categoryList, OutputString.CATEGORY);
+        for (String coachName : coaches.keySet()) {
+            Set<String> candidateMenu = coaches.get(coachName);
+            format(candidateMenu, coachName);
+        }
+        System.out.println();
+        System.out.println(OutputString.COMP);
+    }
+}
