56dfe0698688c776c2b9698d1080938a45f03782
minaver <psmjoshua@gmail.com>
2022-12-17 16:48:12 +0900

    test(Menu): 각 코치에게 한 주에 중복되지 않는 '메뉴' 를 추천하는지 확인

--- src/test/java/menu/MenuTest.java
+++ src/test/java/menu/MenuTest.java
@@ -7,20 +7,21 @@ import menu.vo.Coach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class MenuTest {
-    @DisplayName("보여지는 테스트 설명")
+    @DisplayName("각 코치에게 한 주에 중복되지 않는 '메뉴' 를 추천하는지 확인")
     @Test
-    void 테스트_이름() {
+    void 중복되지_않는_메뉴_추천() {
         // given
-
-        // TODO: 프로그램 구현
         List<String> korFood = Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음");
         Menu menu = new Menu(FoodCategory.KOR);
 
@@ -30,18 +31,17 @@ public class MenuTest {
         CoachWeekMenu coachWeekMenu1 = new CoachWeekMenu(coach1);
         CoachWeekMenu coachWeekMenu2 = new CoachWeekMenu(coach2);
 
-        for (int i = 0; i < 3 ; i++) {
-            List<CoachWeekMenu> coachWeekMenus = menu.chooseShuffleMenu(Arrays.asList(coachWeekMenu1, coachWeekMenu2));
-
-            for (CoachWeekMenu coachWeekMenu : coachWeekMenus) {
-                System.out.println(coachWeekMenu.getCoach());
-                System.out.println(coachWeekMenu.getEatMenus());
-            }
-        }
-
         // when
+        List<CoachWeekMenu> coachWeekMenus = new ArrayList<>();
+        for (int i = 0 ; i < Constant.weekNum; i++) {
+            coachWeekMenus = menu.chooseShuffleMenu(Arrays.asList(coachWeekMenu1, coachWeekMenu2));
+        }
+        CoachWeekMenu coachWeekMenu = coachWeekMenus.get(0);
+        Set<String> coachWeekMenuRemoveOverlap = coachWeekMenu.getEatMenus().stream()
+                .collect(Collectors.toSet());
 
         // then
+        assertThat(coachWeekMenu.getEatMenus().size()).isEqualTo(coachWeekMenuRemoveOverlap.size());
 
     }
 }
