190af46d78f71d8121243a20c6bd57dbdb95503b
CAEL0 <kjongmin26@gmail.com>
2022-12-17 15:48:26 +0900

    feat(OutputView): 코치의 추천 메뉴를 출력

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,5 +1,9 @@
 package menu.view;
 
+import menu.domain.Coach;
+
+import java.util.stream.Collectors;
+
 public class OutputView {
 
     public void printStartingRecommendation() {
@@ -9,5 +13,10 @@ public class OutputView {
 
     public void printCompletion() {
         System.out.println(ViewConstants.COMPLETION);
+    }
+
+    private void printCoachMenus(Coach coach) {
+        System.out.println(coach.getResult().stream()
+                .collect(Collectors.joining(ViewConstants.DELIMITER, ViewConstants.FRONT, ViewConstants.BACK)));
     }
 }
