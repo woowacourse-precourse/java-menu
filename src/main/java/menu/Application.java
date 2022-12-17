e2651d19c980fa423474d4cf2763976e5b33abd7
Gibeom Nam <rlqja0523@naver.com>
2022-12-17 17:29:51 +0900

    feat: 인스턴스를 주입하고 프로그램 시작

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,24 @@
 package menu;
 
+import menu.controller.MenuController;
+import menu.service.MenuService;
+import menu.utils.Converter;
+import menu.view.InputView;
+import menu.view.OutputView;
+
 public class Application {
+    private static MenuController menuController;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        init();
+        menuController.start();
+    }
+
+    private static void init() {
+        InputView inputView = new InputView(new Converter());
+        OutputView outputView = new OutputView();
+        MenuService menuService = new MenuService();
+
+        menuController = new MenuController(inputView, outputView, menuService);
     }
 }
