2db4515de91e5bb87807ad81c0d2fd21d94c7432
woosung1223 <dntjd991223@naver.com>
2022-12-17 13:34:02 +0900

    feat: 한 주에 같은 카테고리는 최대 2회만 고를 수 있도록 구성

--- src/main/java/menu/domain/Group.java
+++ src/main/java/menu/domain/Group.java
@@ -1,13 +1,17 @@
 package menu.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Group {
     private final List<String> categoryHistory = new ArrayList<>();
 
     public void makeCategory() {
-        String category = Category.getRandomCategory();
+        String category;
+        do {
+            category = Category.getRandomCategory();
+        } while (Collections.frequency(categoryHistory, category) < 2);
         categoryHistory.add(category);
     }
 }
