3d1a398e0b7a15153eb09d32064bcb0e12917a0c
Gibeom Nam <rlqja0523@naver.com>
2022-12-17 15:32:59 +0900

    feat: 추천 결과를 출력하는 기능

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -6,6 +6,15 @@ import java.util.List;
 
 public class OutputView {
 
+    public void printMenuResult(List<String> categoryNames, List<Coach> coaches) {
+        System.out.println("메뉴 추천 결과입니다.");
+        System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
+        printCategoryName(categoryNames);
+        printCoachMenus(coaches);
+        System.out.println();
+        System.out.println("추천을 완료했습니다.");
+    }
+
     private void printCategoryName(List<String> categoryNames) {
         System.out.print("[ 카테고리");
         for (String name : categoryNames) {
