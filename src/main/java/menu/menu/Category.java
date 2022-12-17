1a7b9d1a40d67b41490c28feb3abd5b783a71123
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-12-17 14:03:17 +0900

    feat(Coach): get 메서드 생성
    - 랜덤 값을 전달받아 이에 해당하는 카테고리 이름을 반환
    - 이를 위해 private getCategoryNumber와 getCategoryName 생성

--- src/main/java/menu/menu/Category.java
+++ src/main/java/menu/menu/Category.java
@@ -5,22 +5,42 @@ import java.util.Collections;
 import java.util.List;
 
 public enum Category {
-    JAPANESE_FOOD(1,
+    JAPANESE_FOOD("일식", 1,
             Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
-    KOREAN_FOOD(2,
+    KOREAN_FOOD("한식", 2,
             Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")),
-    CHINESE_FOOD(3,
+    CHINESE_FOOD("중식", 3,
             Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
-    ASIAN_FOOD(4,
+    ASIAN_FOOD("아시안", 4,
             Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")),
-    WESTERN_FOOD(5,
+    WESTERN_FOOD("양식", 5,
             Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니")),
-    EMPTY(0, Collections.emptyList());
+    EMPTY("카테고리 해당 없음", 0, Collections.emptyList());
 
-    Integer categoryNumber;
-    List<String> menus;
-    Category(Integer categoryNumber, List<String> menus) {
+    private String categoryName;
+    private Integer categoryNumber;
+    private List<String> menus;
+
+    Category(String categoryName, Integer categoryNumber, List<String> menus) {
+        this.categoryName = categoryName;
         this.categoryNumber = categoryNumber;
         this.menus = menus;
+    }
+
+    private int getCategoryNumber() {
+        return categoryNumber;
+    }
+
+    private String getCategoryName() {
+        return categoryName;
+    }
+
+    public static String get(int randomNumber) {
+        for (Category category: Category.values()) {
+            if (randomNumber == category.getCategoryNumber()) {
+                return category.getCategoryName();
+            }
+        }
+        return EMPTY.getCategoryName();
     }
 }
---
+++ src/test/java/menu/coach/Coach.java
@@ -0,0 +1,2 @@
+package menu.coach;public class Coach {
+}
