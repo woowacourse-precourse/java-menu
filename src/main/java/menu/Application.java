b0d1615e6943f1c9403ea48f8eda72e97d8d595b
KSH <tkdgns543@gmail.com>
2022-12-17 16:07:46 +0900

    feat(Application): 입력받은 코치 목록과 못 먹는 음식을 가지고 Coach 리스트로 만든다

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,25 @@
 package menu;
 
+import menu.domain.Coach;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static List<Coach> createCoachList(List<String> coaches, List<List<String>> unavailableMenus) {
+        List<Coach> result = new ArrayList<>();
+
+        for (int coachIndex = 0; coachIndex < coaches.size(); coachIndex++) {
+            String name = coaches.get(coachIndex);
+            List<String> banedMenu = unavailableMenus.get(coachIndex);
+
+            result.add(new Coach(name, banedMenu));
+        }
+
+        return result;
     }
 }
