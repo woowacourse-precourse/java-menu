e4c58acadefa31718997e49f3e76fdf7111ca738
KSH <tkdgns543@gmail.com>
2022-12-17 14:14:04 +0900

    feat(domain): 코치 목록이 담긴 리스트의 제네릭스를 String에서 Coach로 변경

--- src/main/java/menu/domain/MenuRecommender.java
+++ src/main/java/menu/domain/MenuRecommender.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class MenuRecommender {
     private List<Category> categoryHistory = new ArrayList<>();
-    private List<String> coaches = new ArrayList<>();
+    private List<Coach> coaches = new ArrayList<>();
     private static final List<Category> CATEGORIES = Arrays.asList(Category.values());
 
     private Category pickupCategory() {
