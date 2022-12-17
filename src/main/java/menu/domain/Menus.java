649eccf947fdcd399ebe6c3f8c060396c85f6998
greeng00se <thegreengoose7@gmail.com>
2022-12-17 13:16:07 +0900

    feat(Menus): 메뉴 일급 컬렉션 추가

---
+++ src/main/java/menu/domain/Menus.java
@@ -0,0 +1,12 @@
+package menu.domain;
+
+import java.util.List;
+
+public class Menus {
+
+    private final List<Menu> menus;
+
+    public Menus(List<Menu> menus) {
+        this.menus = menus;
+    }
+}
