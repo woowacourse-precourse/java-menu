a7c2ebd7d38b493293e72114d6ab005e948e5559
Cyma-s <ssol0319@naver.com>
2022-12-17 13:30:57 +0900

    feat(InputException) : 코치 이름 길이 예외 추가

--- src/main/java/menu/domain/InputException.java
+++ src/main/java/menu/domain/InputException.java
@@ -3,7 +3,9 @@ package menu.domain;
 public enum InputException {
     DUPLICATE_COACH_NAME("중복된 코치 이름입니다."),
     OUT_OF_COACH_NAME_COUNT_MINIMUM("코치는 최소 2명 이상 입력해야 합니다."),
-    OUT_OF_COACH_NAME_COUNT_MAXIMUM("코치는 최대 5명 이하로 입력해야 합니다.");
+    OUT_OF_COACH_NAME_COUNT_MAXIMUM("코치는 최대 5명 이하로 입력해야 합니다."),
+    OUT_OF_COACH_NAME_LENGTH_MINIMUM("코치 이름은 최소 2글자 이상 입력해야 합니다."),
+    OUT_OF_COACH_NAME_LENGTH_MAXIMUM("코치 이름은 최대 4글자 이하로 입력해야 합니다.");
 
 
     private final String exceptionMessage;
