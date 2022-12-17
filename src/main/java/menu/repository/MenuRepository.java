58342c0a9bd430d1f9b6f80ca475c24b6525a004
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-12-17 14:59:48 +0900

    feat : MenuRepository 싱글톤
    private 으로 생성자를 막아 싱글톤 객체로 생성한다.

--- src/main/java/menu/repository/MenuRepository.java
+++ src/main/java/menu/repository/MenuRepository.java
@@ -7,11 +7,19 @@ import menu.model.Category;
 
 public class MenuRepository {
 
+	private static final MenuRepository instance = new MenuRepository();
+
+	private MenuRepository() {
+	}
+
+	public static MenuRepository getInstance() {
+		return instance;
+	}
+
 	// 카테고리 반환
 	public Category getRandomCategory() throws Exception {
 		int pickNumber = Randoms.pickNumberInRange(1, 5);
-		Category category = Category.getCategory(pickNumber);
-		return category;
+		return Category.getCategory(pickNumber);
 	}
 
 	// 입력된 카테고리로 랜덤 메뉴 반환
