380acd2613ab59f305934d1063a89837b497a123
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-12-17 16:25:13 +0900

    feat(Menu): toString() 재정의

--- src/main/java/menu/domain/Menu.java
+++ src/main/java/menu/domain/Menu.java
@@ -1,1 +1,1 @@
-package menu.domain;public class Menu {    private final String name;    public Menu(String name) {        this.name = name;    }}
+package menu.domain;public class Menu {    private final String name;    public Menu(String name) {        this.name = name;    }    @Override    public String toString() {        return this.name;    }}
