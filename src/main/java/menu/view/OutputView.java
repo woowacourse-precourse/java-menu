09063f14302481994931295e10f46c450daba442
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-12-17 14:28:22 +0900

    feat(OutputView): 점심 메뉴 추천 시작을 출력하는 기능 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,1 +1,1 @@
-package menu.view;public class OutputView {}
+package menu.view;public class OutputView {    private static final String START_MESSAGE= "점심 메뉴 추천을 시작합니다.";    public void printStartMessage() {        System.out.println(START_MESSAGE);    }}
