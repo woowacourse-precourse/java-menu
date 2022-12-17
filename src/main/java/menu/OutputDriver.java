ec8d85081aca2557a3b2516dcdb90310ed2f351c
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-12-17 13:46:01 +0900

    Refactor : 코치 이름 입력 받기 위한 메세지 출력 기능 추가

--- src/main/java/menu/OutputDriver.java
+++ src/main/java/menu/OutputDriver.java
@@ -2,10 +2,11 @@ package menu;
 
 public class OutputDriver {
     public void printStartMessage() {
-        System.out.println("점심 메뉴 추천을 시작합니다.");
+        System.out.println("점심 메뉴 추천을 시작합니다.\n");
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
     }
 
     public void printScanCannotEatMenuMessage(String name) {
-        System.out.println(name+"(이)가 못 먹는 메뉴를 입력해 주세요.");
+        System.out.println("\n" + name + "(이)가 못 먹는 메뉴를 입력해 주세요.");
     }
 }
