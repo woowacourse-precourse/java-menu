e7aa24a4a9f9aa5cb8e4fa33bcf63a8e1dde2f96
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 14:33:52 +0900

    feat: 각 요일의 카테고리를 추천받는 기능 구현

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,2 +1,30 @@
-package menu.domain;public class Category {
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Category {
+    public Category(){};
+
+    public static List<Integer> recommandCategory() {
+        List<Integer> category = new ArrayList<>();
+        while (category.size() < 5 ) {
+            Integer value = Randoms.pickNumberInRange(1,5);
+            if (getCategoryFrequency(category,value) >= 2)
+                continue;
+            category.add(value);
+        }
+        return category;
+    }
+
+    public static Integer getCategoryFrequency(List<Integer> category, Integer value) {
+        int count = 0;
+        for (int i = 0; i < category.size(); i++) {
+            if (category.get(i) == value)
+                count += 1;
+        }
+        return count;
+    }
 }
