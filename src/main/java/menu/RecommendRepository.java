87bbf59328e195033f34972a0d96d551294d1d8b
jaeyeon kim <jakind@naver.com>
2022-12-17 16:36:26 +0900

    feat(RemmendRepository): 미션을 잘못 이해하면서 발생했던 Set 을 HashMap 으로 바꾸어서 Category 의 빈도를 변경하고 측정하는 기능 추가

--- src/main/java/menu/RecommendRepository.java
+++ src/main/java/menu/RecommendRepository.java
@@ -3,29 +3,50 @@ package menu;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RecommendRepository {
     private final int TOTAL_DAY = 5;
+    private final RecommendMachine recommendMachine;
+    private final Map<String, Integer> categoryFrequency;
     private List<String> categories;
 
     public RecommendRepository() {
         this.categories = new ArrayList<>();
+        recommendMachine = new RecommendMachine();
+        categoryFrequency = new HashMap<>();
+        initCategoryFrequency();
+    }
+
+    private void initCategoryFrequency() {
+        Arrays.stream(Category.values())
+                .forEach(category -> categoryFrequency.put(category.toString(), 0));
     }
 
     public void recommendStart(CoachRepository coachRepository) {
         for (int day = 1; day <= TOTAL_DAY; day++) {
             int categoryNumber = 0;
             boolean doNotDecideNumber = true;
-
             while (doNotDecideNumber) {
                 categoryNumber = Randoms.pickNumberInRange(1, 5);
-                doNotDecideNumber = coachRepository.isNotPossibleCategory(Category.from(categoryNumber));
+                doNotDecideNumber = isNotPossible(Category.from(categoryNumber));
             }
-
-            categories.add(Category.from(categoryNumber).toString());
-            coachRepository.foodRecommendForDay(); // 여기서, 이제 recommend 하면 됨
+            Category category = Category.from(categoryNumber);
+            categories.add(category.toString());
+            increaseCategoryFrequency(category);
+            coachRepository.foodRecommendForDay(category, recommendMachine); // 여기서, 이제 recommend 하면 됨
         }
+    }
+
+    public boolean isNotPossible(Category category) {
+        return !(categoryFrequency.get(category.toString()) < 2);
+    }
+
+    public void increaseCategoryFrequency(Category category) {
+        categoryFrequency.put(category.toString(), categoryFrequency.get(category.toString()) + 1);
     }
 
     public List<String> getCategories() {
