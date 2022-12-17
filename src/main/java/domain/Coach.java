bea1d05e63c44f31291a0489e19d933fdcb02d77
saint6839 <saint6839@gmail.com>
2022-12-17 13:25:21 +0900

    feat(domain) : 코치 정보를 담는 클래스 추가
    - 유효성 검증 메서드 추가

---
+++ src/main/java/domain/Coach.java
@@ -0,0 +1,18 @@
+package domain;
+
+public class Coach {
+
+    private final String name;
+
+    public Coach(String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() >= 2 && name.length() <= 4) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 코치 이름은 최소 2글자 이상 최대 4글자 이하로 입력해주세요");
+    }
+}
