6f6c43e3a92b1922936e72647ea47d3d8ab6d12e
JunDonghyuk <ehdgur4814@naver.com>
2022-12-17 13:41:38 +0900

    feat(ErrorMessage): 에러 관련 상수 및 함수 작성

---
+++ src/main/java/menu/error/ErrorConstant.java
@@ -0,0 +1,7 @@
+package menu.error;
+
+public class ErrorConstant {
+    private static final String BASE_ERROR_MESSAGE = "[ERROR] ";
+    public static final String COACH_NUMBER_ERROR_MESSAGE = BASE_ERROR_MESSAGE + "코치 수는 2명에서 5명 입니다.";
+    public static final String COACH_NAME_LENGTH_ERROR_MESSAGE = BASE_ERROR_MESSAGE + " 코치 이름은 2글자에서 4글자 입니다.";
+}
---
+++ src/main/java/menu/error/ErrorMessage.java
@@ -0,0 +1,11 @@
+package menu.error;
+
+public class ErrorMessage {
+    public static void printCoachNameLengthError() {
+        System.out.println(ErrorConstant.COACH_NAME_LENGTH_ERROR_MESSAGE);
+    }
+
+    public static void printCoachNumberError() {
+        System.out.println(ErrorConstant.COACH_NUMBER_ERROR_MESSAGE);
+    }
+}
