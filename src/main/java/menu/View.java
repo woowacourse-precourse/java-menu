d69f32bf7d7eb8f7f8590fe342a2bf6f7ef4e84c
cpot5620 <=cpot5620@naver.com>
2022-12-17 15:38:42 +0900

    feat(View): create method to printResult

--- src/main/java/menu/View.java
+++ src/main/java/menu/View.java
@@ -4,43 +4,65 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class View {
 
-    public void printStartMessage() {
+    public static void printStartMessage() {
         System.out.println("점심 메뉴 추천을 시작합니다.");
     }
 
-    public String requestInputCoachName() {
+    public static void printResult(List<Coach> coaches, List<Category> categories) {
+        System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
+        printCategories(categories);
+        for (int i = 0; i < coaches.size(); i++) {
+            System.out.println(coaches.get(i));
+        }
+    }
+
+    private static void printCategories(List<Category> categories) {
+        String str = "[ 카테고리 | ";
+        for (int i = 0; i < categories.size(); i++) {
+            str += categories.get(i);
+            if (i != categories.size() - 1)
+                str += " | ";
+        }
+        str += " ]";
+
+        System.out.println(str);
+    }
+
+    public static List<String> requestInputCoachName() {
         System.out.println("코치의 이름을 입력해주세요. (,로 구분)");
         String input = receiveInput();
         validateCoaches(input);
 
-        return input;
+        return Arrays.asList(input.split(","));
     }
 
-    public String requestUnwantedMenu(String name) {
+    public static List<String> requestUnwantedMenu(String name) {
         System.out.println(name + "(이)가 못 먹는 메뉴를 입력해 주세요.");
         String input = receiveInput();
         validateMenus(input);
-        return receiveInput();
+        return Arrays.asList(input.split(","));
     }
 
-    private String receiveInput() {
+    private static String receiveInput() {
         String str = Console.readLine();
         return str;
     }
 
 
-    private void validateMenus(String input) {
+    private static void validateMenus(String input) {
         List<String> menus = Arrays.asList(input.split(","));
 
         if (menus.size() > 2)
             throw new IllegalArgumentException("못먹는 메뉴는 최대 2개까지 입니다.");
+
+        if (input.contains(" "))
+            throw new IllegalArgumentException("공백은 입력할 수 없슶니다.");
     }
 
-    private void validateCoaches(String input) {
+    private static void validateCoaches(String input) {
         List<String> coaches = Arrays.asList(input.split(","));
 
         if (coaches.size() < 2 || coaches.size() > 5)
@@ -51,5 +73,8 @@ public class View {
             if (name.length() < 2 || name.length() > 4)
                 throw new IllegalArgumentException("코치의 이름은 2~4글자여야 합니다.");
         }
+
+        if (input.contains(" "))
+            throw new IllegalArgumentException("공백은 입력할 수 없슶니다.");
     }
 }
