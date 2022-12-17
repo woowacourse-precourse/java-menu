4ac920ac620b08d5136c2d86edc3eedaafbf5611
gavin-ko <kas6610@gmail.com>
2022-12-17 17:33:02 +0900

    feat : 서비스 시작 문구 출력 기능

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,4 +1,19 @@
 package menu.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import menu.view.Message.*;
+
+
 public class InputView {
+    List<String> CoachList;
+
+    public List<String> getNameList(){
+        String inputCoaches = Console.readLine();
+        CoachList = Arrays.asList(inputCoaches.split(","));
+        return CoachList;
+    }
+
 }
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,11 +1,18 @@
 package menu.view;
 
+
 public class OutputView {
 
-    //메소드
-    //서비스 시작 문구 출력 기능 ex> 점심 메뉴 추천을 시작합니다.
-    //코치 인원 확인 기능 ex> 코치의 이름을 입력해 주세요. (, 로 구분)
+    public void printStartMessage() {
+        System.out.println(Message.START_MESSAGE.getMessage());
+    }
+
+    public void printNameRequestMessage() {
+        System.out.println(Message.NAME_REQUEST_MESSAGE.getMessage());
+    }
+
     //메뉴 입력 기능 (이)가 못 먹는 메뉴를 입력해 주세요.
     //메뉴 추천 결과 menu machine
+    // 최종 결과를 정해진 형식에 맞춰 출력한다.
     //서비스 종료 문구
 }
