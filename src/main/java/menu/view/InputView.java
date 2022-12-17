28577378132501313f229aad329117d8ea9aec52
은지 <midzy0228@gmail.com>
2022-12-17 14:39:03 +0900

    feat: 입력 받기 위해 안내 문구 출력 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -7,13 +7,17 @@ import java.util.List;
 
 public class InputView {
     private static final String NAME_SEPARATOR = ",";
+    private static final String INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (%s 로 구분)\n";
+    private static final String INPUT_DISLIKE_MENU = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
 
     public List<String> readCoachNames() {
+        System.out.printf(INPUT_COACH_NAME, NAME_SEPARATOR);
         String input = readInput();
         return splitComma(input);
     }
 
-    public List<String> readDislikeMenu() {
+    public List<String> readDislikeMenu(String coachName) {
+        System.out.printf(INPUT_DISLIKE_MENU, coachName);
         String input = readInput();
         return splitComma(input);
     }
