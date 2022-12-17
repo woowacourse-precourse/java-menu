531434e6d5a658860aab526901128136e2b5c5dc
SH <gram753@naver.com>
2022-12-17 14:15:14 +0900

    feat: 생성자 추가

--- src/main/java/menu/model/MenuList.java
+++ src/main/java/menu/model/MenuList.java
@@ -1,15 +1,17 @@
 package menu.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MenuList {
 	private List<Menu> menusCantEat;
 
+	public MenuList() {
+		this.menusCantEat = new ArrayList<>();
+	}
+
 	public List<Menu> getMenusCantEat() {
 		return menusCantEat;
 	}
 
-	public void setMenusCantEat(List<Menu> menusCantEat) {
-		this.menusCantEat = menusCantEat;
-	}
 }
