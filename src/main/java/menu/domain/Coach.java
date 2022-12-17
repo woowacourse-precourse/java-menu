442b347485128dc58ee038482c446c2bc28e0614
pilyang <30036534+pilyang@users.noreply.github.com>
2022-12-17 14:11:09 +0900

    feat: 기능 구현을 위한 클래스 파일 추가
    - Coach: 코치의 이름 정보등 저장
    - MenuRecommender: 메뉴 추천 기능 구현할 클래스

---
+++ src/main/java/menu/domain/Coach.java
@@ -0,0 +1,10 @@
+package menu.domain;
+
+public class Coach {
+
+    private final String name;
+
+    public Coach(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/menu/domain/MenuRecommender.java
@@ -0,0 +1,13 @@
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuRecommender {
+
+    private List<Coach> coaches;
+
+    public MenuRecommender() {
+        coaches = new ArrayList<>();
+    }
+}
