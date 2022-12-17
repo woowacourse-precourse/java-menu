ed6c9a5acb5e87d475f56cf3a5cc10e8e6222137
hojunking96 <junsong96@naver.com>
2022-12-17 16:51:32 +0900

    feats(DayMenu) : 요일 별 카테고리를 저장하는 객체 구현

---
+++ src/main/java/menu/DayMenu.java
@@ -0,0 +1,15 @@
+package menu;
+
+public class DayMenu {
+    private String category;
+
+    public String getCategory() {
+        return this.category;
+    }
+
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+}
