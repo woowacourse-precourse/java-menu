e33cf6c8f55db54ffb68298942d0cf841d1592ea
Woojin Lee <dhkep03@gmail.com>
2022-12-17 13:45:15 +0900

    feat(MenuController): 시작 로직 구현

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,4 +1,36 @@
 package menu.controller;
 
+import menu.domain.MenuService;
+import menu.ui.InputView;
+import menu.ui.OutputView;
+
 public class MenuController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final MenuService menuService;
+
+    public MenuController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.menuService = new MenuService();
+    }
+
+    public void start() {
+        outputView.announceStart();
+        generateCoaches();
+        registerInedibleFoods();
+        announceMenuRecommendResult();
+    }
+
+    private void generateCoaches() {
+
+    }
+
+    private void registerInedibleFoods() {
+
+    }
+
+    private void announceMenuRecommendResult() {
+
+    }
 }
