5c2ef7315c3b1ba16d912898b6efc7f4cac0d33a
khsrla9806 <kimhunsope@naver.com>
2022-12-17 13:42:04 +0900

    feat(OutputView): 메시지 출력에 관련된 View 기능 구현 초안

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,17 @@
+package menu.view;
+
+public class OutputView {
+    public void printStartMessage() {
+        System.out.println("점심 메뉴 추천을 시작합니다.");
+    }
+
+    public void printInputCoachNameMessage() {
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+    }
+
+    public void printFinalResult() {
+        System.out.println("메뉴 추천 결과입니다.");
+        // TODO: 결과 출력
+        System.out.println("추천을 완료했습니다.");
+    }
+}
