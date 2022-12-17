595c025073fa0dbc52565ddd92bc15e466eff0b6
wnsgml7267 <dlwns7267@naver.com>
2022-12-17 13:57:31 +0900

    refactor(view/InputView): OutputView에 있는 줄 바꿈 기능 적용

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -2,6 +2,8 @@ package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static menu.view.OutputView.printBreak;
+
 public class InputView {
     public static final String INPUT_MAIN = "코치의 이름을 입력해 주세요. (, 로 구분)";
     public static final String INPUT_NOT_FOOD = "(이)가 못 먹는 메뉴를 입력해 주세요.";
