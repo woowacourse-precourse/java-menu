70fb3094c55c56b6a7e56772229b28da2ec42e94
thdwoqor <celebarte01@gmail.com>
2022-12-17 13:48:53 +0900

    feat: 메뉴 선택 기능 추가

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,11 @@
 package menu;
 
+import menu.model.Menu;
+
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+//        System.out.println(Menu.selectMenu(Arrays.asList("중식", "양식", "일식", "중식", "일식"),Arrays.asList()));
     }
 }
