4142a33d44010391860b5598ba2275162c2e7024
dclxxi <dclxxi@kakao.com>
2022-12-17 14:11:48 +0900

    refactor: 코치의 이름 길이 검증 함수 분리

--- src/main/java/model/Coach.java
+++ src/main/java/model/Coach.java
@@ -1,8 +1,11 @@
 package model;
 
+import java.util.List;
+
 public class Coach {
     
     private final String name;
+    private List<String> canNotEatMenus;
     
     public Coach(String name) {
         validLength(name);
@@ -10,9 +13,21 @@ public class Coach {
     }
     
     private void validLength(String name) {
+        validLessThanLength(name);
+        validGreaterThanLength(name);
+    }
+    
+    private void validLessThanLength(String name) {
         int nameLength = name.length();
-        if (nameLength < 2 || nameLength > 4) {
-            throw new IllegalArgumentException("코치의 이름은 최소 2글자, 최대 4글자로 입력해야 합니다.");
+        if (nameLength < 2) {
+            throw new IllegalArgumentException("코치의 이름은 최소 2글자로 입력해야 합니다.");
+        }
+    }
+    
+    private void validGreaterThanLength(String name) {
+        int nameLength = name.length();
+        if (nameLength > 4) {
+            throw new IllegalArgumentException("코치의 이름은 최대 4글자로 입력해야 합니다.");
         }
     }
 }
