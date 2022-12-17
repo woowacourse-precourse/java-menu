301a09ab86eb9dd1e95d771153e11c09e5902e26
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 16:25:58 +0900

    style(InputView): 개행 문자 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -6,7 +6,7 @@ import menu.validation.InputValidation;
 import java.util.List;
 
 public class InputView {
-    private static final String INPUT_COACHES_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private static final String INPUT_COACHES_NAME = "\n코치의 이름을 입력해 주세요. (, 로 구분)";
     private static final String INPUT_HATE_MENU_FORMAT = "\n%s(이)가 못 먹는 메뉴를 입력해 주세요.\n";
     private static final String SPACE = " ";
     private static final String NONE = "";
