cfec112096d216335041f2caa47a5d74a7a22502
parksey <parkseyeon99@naver.com>
2022-12-17 13:43:45 +0900

    test(InputExceptionTest): 코치 이름 입력에 대한 예외 테스트 추가

--- src/test/java/menu/view/InputExceptionTest.java
+++ src/test/java/menu/view/InputExceptionTest.java
@@ -1,4 +1,41 @@
 package menu.view;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class InputExceptionTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    public void null_test(String userInput) {
+        assertThatThrownBy(() -> {
+            InputException.nullException(userInput);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    public void notEndFormat_test(String userInput) {
+        assertThatThrownBy(() -> {
+            InputException.notEndFormat(userInput);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,6})
+    public void notMemberSize_test(int size) {
+        assertThatThrownBy(() -> {
+            InputException.notMemberSize(size);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,5})
+    public void notCoachNameSize_test(int size) {
+        assertThatThrownBy(() -> {
+            InputException.notCoachNameSize(size);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
