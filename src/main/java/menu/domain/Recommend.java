2224c3050a3dea595d26d9373ed6a01db0247d04
DongHo Kim <hkim4410@naver.com>
2022-12-17 15:12:37 +0900

    docs: 주요 객체들의 속성과 역할 변경

---
+++ src/main/java/menu/domain/Recommend.java
@@ -0,0 +1,9 @@
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recomm {
+    private final List<Category> categories = new ArrayList<>();
+    private 
+}
--- src/main/java/menu/domain/Result.java
+++
@@ -1,7 +0,0 @@
-package menu.domain;
-
-import java.util.List;
-
-public class Result {
-    private final List<Menu>
-}
