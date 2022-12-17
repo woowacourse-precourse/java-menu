0b07bcc21f12196c639677e138db10f855c129f5
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 17:35:05 +0900

    fix(Application): 잘못된 초기화 수정

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        MainController mainController = MainController.getInstance();
         initCategory();
         initFoodByCategory();
+        MainController mainController = MainController.getInstance();
         mainController.start();
         mainController.registerCoach();
         mainController.registerInedibleFood();
@@ -33,13 +33,13 @@ public class Application {
         CategoryRepository categoryRepository = CategoryRepository.getInstance();
         categoryRepository.addFoodToCategory(FoodCategory.JAPAN.getFoodCategory(),
                 List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼"));
-        categoryRepository.addFoodToCategory(FoodCategory.JAPAN.getFoodCategory(),
+        categoryRepository.addFoodToCategory(FoodCategory.KOREA.getFoodCategory(),
                 List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"));
-        categoryRepository.addFoodToCategory(FoodCategory.JAPAN.getFoodCategory(),
+        categoryRepository.addFoodToCategory(FoodCategory.CHINA.getFoodCategory(),
                 List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채"));
-        categoryRepository.addFoodToCategory(FoodCategory.JAPAN.getFoodCategory(),
+        categoryRepository.addFoodToCategory(FoodCategory.ASIA.getFoodCategory(),
                 List.of("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜"));
-        categoryRepository.addFoodToCategory(FoodCategory.JAPAN.getFoodCategory(),
+        categoryRepository.addFoodToCategory(FoodCategory.WEST.getFoodCategory(),
                 List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
     }
 }
