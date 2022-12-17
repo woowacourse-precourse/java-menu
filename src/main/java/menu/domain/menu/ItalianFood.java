1e35b39edb9f218b3391f33710dd2e1d0686e899
kiarakim <kiarakim413@gmail.com>
2022-12-17 17:34:42 +0900

    feat: 나라 별 음식 enum 추가

---
+++ src/main/java/menu/domain/menu/ItalianFood.java
@@ -0,0 +1,37 @@
+package menu.domain.menu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+public enum ItalianFood {
+    라자냐("라자냐"),
+    그라탱("그라탱"),
+    뇨끼("뇨끼"),
+    끼슈("끼슈"),
+    프렌치_토스트("프렌치 토스트"),
+    바게트("바게트"),
+    스파게티("스파게티"),
+    피자("피자"),
+    파니니("파니니"),
+    any("any");
+
+    private final String name;
+
+    ItalianFood(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getAllFoods() {
+        List<String> foodNames = new ArrayList<>();
+        Arrays.stream(values())
+                .map(i -> foodNames.add(i.getName()))
+                .collect(Collectors.toList());
+        foodNames.remove(any.getName());
+        return foodNames;
+    }
+}
