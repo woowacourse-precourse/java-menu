d5e0e8b66d8b31aeb3c6c4dcbd15b0dc01a2f07f
Hayoon <hy97@sju.ac.kr>
2022-12-17 14:47:40 +0900

    feat(LunchController): 코치별 이름 및 못먹는 음식 저장 메서드 구현

--- src/main/java/menu/controller/LunchController.java
+++ src/main/java/menu/controller/LunchController.java
@@ -4,6 +4,7 @@ import menu.model.domain.Coach;
 import menu.model.domain.Food;
 import menu.model.service.CoachService;
 import menu.model.service.FoodService;
+import menu.util.CoachGenerator;
 import menu.util.CoachValidator;
 import menu.util.Parser;
 import menu.util.Validator;
@@ -15,36 +16,43 @@ import java.util.List;
 
 public class LunchController {
 
-    private final CoachService coachService = new CoachService();
-    private final FoodService foodService = new FoodService();
-    private final LunchController lunchController = new LunchController();
+    public void inputLunchAndRun(CoachService coachService, FoodService foodService) {
+        OutputView.startLunch();
 
-    public String inputLunchAndRun() {
         try {
-            OutputView.startLunch();
-
             String input = InputView.inputCoachesName();
             CoachValidator.isValidCoachName(input);
             List<String> coaches = Parser.splitInfo(input);
-            for(String coach : coaches) {
-                String inputFoods = InputView.inputCantEatFood(coach);
+            //CoachGenerator.parsingCoachList(coaches, coachService);
+
+            for (String coachName : coaches) {
+                String inputFoods = InputView.inputCantEatFood(coachName);
                 List<String> cannotEatFoodsForString = Parser.splitInfo(inputFoods);
-                Coach findCoachByName = coachService.findByName(coach);
+//                Coach findCoachByName = coachService.findByName(coach);
+//                System.out.println("findCoachByName: " + findCoachByName.getName());
                 List<Food> cannotEatFoodsForFood = new ArrayList<Food>();
                 for (String foodName : cannotEatFoodsForString) {
                     Food findFoodByName = foodService.findByName(foodName);
                     cannotEatFoodsForFood.add(findFoodByName);
                 }
-                coachService.saveCannotEatFood(findCoachByName, cannotEatFoodsForFood);
+                coachService.join(new Coach(coachName, cannotEatFoodsForFood));
             }
-
-
-            MenuValidator.isRightMenu(input);
-            return menuController.RunAndReturnCommand(input, pairService, missionService, memberService);
-        } catch (IllegalArgumentException e) {
+            ///
+            List<String> allNames = coachService.findAllNames();
+            for (String name : allNames) {
+                Coach byName = coachService.findByName(name);
+                System.out.println("이름: " + byName.getName());
+                for(Food food : byName.getCannotEatFoods()) {
+                    System.out.println("못먹는거 :" + food.getName());
+                }
+            }
+            ///
+        }
+//            return menuController.RunAndReturnCommand(input, pairService, missionService, memberService);
+        catch (
+                IllegalArgumentException e) {
             OutputView.printErrorMessage(e.getMessage());
-            return inputMenuAndRun();
+            inputLunchAndRun(coachService, foodService);
         }
     }
-
 }
