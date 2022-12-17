20b9a84aead61b7501e982413029f91a557c2e50
wonseok <tkddls23@konkuk.ac.kr>
2022-12-17 13:24:12 +0900

    feat(view: OuputView): 시작과 에러 메시지 출력 구현

--- src/main/java/menu/constant/MessageConstant.java
+++ src/main/java/menu/constant/MessageConstant.java
@@ -1,4 +1,9 @@
 package menu.constant;
 
 public class MessageConstant {
+
+
+    public static final String MESSAGE_OUTPUT_START = "점심 메뉴 추천을 시작합니다.";
+
+
 }
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,4 +1,17 @@
 package menu.view;
 
+import static menu.constant.MessageConstant.MESSAGE_OUTPUT_START;
+
 public class OutputView {
+    /**
+     * 게임 시작 문구를 출력 한다.
+     */
+    public void printStart() {
+        System.out.println(MESSAGE_OUTPUT_START);
+    }
+
+    public void printError(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
 }
