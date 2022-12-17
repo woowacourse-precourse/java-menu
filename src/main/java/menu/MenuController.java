621da69b8cd9e07ed64de1382293d22be62b1ea4
iamjooon2 <iamjooon2@gmail.com>
2022-12-17 16:32:35 +0900

    feat: 랜덤한 카테고리를 고르는 기능

--- src/main/java/menu/MenuController.java
+++ src/main/java/menu/MenuController.java
@@ -1,6 +1,8 @@
 package menu;
 
+import java.util.ArrayList;
 import java.util.List;
+import menu.type.CategoryType;
 import menu.view.InputView;
 
 
@@ -15,6 +17,13 @@ public class MenuController {
         for (int i = 0; i < coachLists.size(); i++) {
             List<String> hateFoods = inputView.getHateList(coachLists.get(i));
             coachHates.updateHateMap(coachLists.get(i), hateFoods);
+        }
+        MenuChoicer menuChoices = new MenuChoicer(coachLists, coachHates);
+        List<CategoryType> chosenCategoryList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            CategoryType chosenCategoryType = menuChoices.chooseRandomCategory();
+            chosenCategoryList.add(chosenCategoryType);
+
         }
 
     }
