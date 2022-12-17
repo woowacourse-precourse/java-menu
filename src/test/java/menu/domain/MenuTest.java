57c7d9ef4f70a5a540c4b9a8a538871caac9bc34
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 16:02:01 +0900

    test: 각 코치의 한주 메뉴가 중복되는지를 검사하는 함수 테스트

--- src/test/java/menu/domain/MenuTest.java
+++ src/test/java/menu/domain/MenuTest.java
@@ -12,10 +12,13 @@ public class MenuTest {
     @DisplayName("못먹는 음식이 포함되어 있는지 확인하는 함수 테스트")
     void checkIsInEdibleMenuTest() {
         List<String> inEdibleMenu = List.of("짬뽕","짜장면");
+        List<String> nowMenu = List.of("마파두부","고추잡채");
         String menu_1 = "짬뽕";
         String menu_2 = "탕수육";
+        String menu_3 = "고추잡채";
 
-        assertThat(Menu.isInEdibleMenu(inEdibleMenu, menu_1)).isEqualTo(true);
-        assertThat(Menu.isInEdibleMenu(inEdibleMenu, menu_2)).isEqualTo(false);
+        assertThat(Menu.isInEdibleMenu(inEdibleMenu, menu_1,nowMenu)).isEqualTo(true);
+        assertThat(Menu.isInEdibleMenu(inEdibleMenu, menu_2,nowMenu)).isEqualTo(false);
+        assertThat(Menu.isInEdibleMenu(inEdibleMenu, menu_3,nowMenu)).isEqualTo(true);
     }
 }
