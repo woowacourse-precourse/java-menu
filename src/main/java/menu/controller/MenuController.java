380def23b8c01c6b56cffb4a8e14cc5148a8bda3
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2022-12-17 17:15:36 +0900

    refactor(menu) : 접근제어자를 private 로 제한

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class MenuController {
-    FoodRecommendation foodRecommendation;
+    private FoodRecommendation foodRecommendation;
 
     public void run() {
         start();
