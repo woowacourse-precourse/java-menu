9815f8d3455bf4a667bd13b9e28fe60323c8b21a
Woojin Lee <dhkep03@gmail.com>
2022-12-17 13:57:29 +0900

    feat(MenuService): 코치 생성

--- src/main/java/menu/domain/MenuService.java
+++ src/main/java/menu/domain/MenuService.java
@@ -1,4 +1,17 @@
 package menu.domain;
 
+import java.util.List;
+
 public class MenuService {
+    private final Coaches coaches;
+    private final WeeklyCategory weeklyCategory;
+
+    public MenuService() {
+        this.coaches = new Coaches();
+        this.weeklyCategory = new WeeklyCategory();
+    }
+
+    public void generateCoaches(List<String> coachNames) {
+        coaches.addCoaches(coachNames);
+    }
 }
