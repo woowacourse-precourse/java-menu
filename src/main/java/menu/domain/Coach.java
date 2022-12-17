0c7541fd77d651c69ad964cec8c4ffadfe4984b3
kim0914 <kosa0914@naver.com>
2022-12-17 15:45:05 +0900

    refactor(Coach): 카테고리 인스턴스 분리

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -5,43 +5,33 @@ import java.util.List;
 
 public class Coach {
 
-    public static final int CATEGORY_MAX_COUNT = 2;
-    public static final int RECOMMEND_MENU_SIZE = 5;
-
     private final String name;
     private final List<Menu> banFoods;
 
     private List<Menu> recommendMenu = new ArrayList<>();
-    private List<Category> recommendCategory = new ArrayList<>();
 
     public Coach(String name, List<Menu> banFoods) {
         this.name = name;
         this.banFoods = banFoods;
     }
 
-    public void addCategory(Category category) {
-        recommendCategory.add(category);
-    }
-
     public void addMenu(Menu menu) {
         recommendMenu.add(menu);
     }
 
-    public boolean canNotEat(Menu menu) {
-        return banFoods.contains(menu);
+    public boolean canEat(Menu menu) {
+        return !banFoods.contains(menu);
     }
 
-    public boolean hasMaxCategoryCount(Category inputCategory) {
-        return recommendCategory.stream()
-                .filter(category -> category.getName().equals(inputCategory.getName()))
-                .count() >= CATEGORY_MAX_COUNT;
+    public boolean hasNotSameMenu(Menu menu) {
+        return !recommendMenu.contains(menu);
     }
 
-    public boolean hasSameMenu(Menu menu) {
-        return recommendMenu.contains(menu);
+    public String getName() {
+        return name;
     }
 
-    public boolean canReceiveMenu() {
-        return recommendMenu.size() < RECOMMEND_MENU_SIZE;
+    public List<Menu> getRecommendMenu() {
+        return recommendMenu;
     }
 }
