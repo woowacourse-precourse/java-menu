1a5acb43b08eb56e2c55df8a148b7f0586560ddb
newh08 <newh08@gmail.com>
2022-12-17 14:26:25 +0900

    feat: implement method to remove the menu that already selected by coach and save the menu randomly selected

--- src/main/java/menu/domain/coach/Coach.java
+++ src/main/java/menu/domain/coach/Coach.java
@@ -1,7 +1,12 @@
 package menu.domain.coach;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Coach {
     private final String name;
+    private final List<String> selectedMenu = new ArrayList<>();
 
     public Coach(String name) {
         this.name = name;
@@ -9,5 +14,11 @@ public class Coach {
 
     public String getName() {
         return name;
+    }
+
+    public void selectMenu(List<String> menus) {
+        menus.removeIf(selectedMenu::contains);
+        String menu = Randoms.shuffle(menus).get(0);
+        selectedMenu.add(menu);
     }
 }
