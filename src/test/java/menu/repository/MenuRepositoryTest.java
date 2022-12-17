20dfd8841dd5ee88f182e5edb4958a825c686b93
woo-chang <clllickme@naver.com>
2022-12-17 17:05:45 +0900

    test(MenuRepository): 음식을 저장하고(중복된 경우, 중복되지 않은 경우) 확인하는 기능 및 저장소를 초기화하는 기능 테스트

---
+++ src/test/java/menu/repository/MenuRepositoryTest.java
@@ -0,0 +1,51 @@
+package menu.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import menu.domain.Category;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MenuRepositoryTest {
+
+    @DisplayName("메뉴를 저장하고 확인한다.")
+    @Test
+    void storeMenu() {
+        Category category = Category.KOREA;
+        String menu = "김치";
+
+        MenuRepository.addMenu(category, menu);
+        List<String> menus = MenuRepository.findByCategory(category);
+
+        assertThat(menus.size()).isEqualTo(1);
+        assertThat(menus).containsExactly(menu);
+    }
+
+    @DisplayName("중복되는 메뉴는 저장되지 않는다.")
+    @Test
+    void storeDuplicationMenu() {
+        Category category = Category.KOREA;
+        String menu = "김치";
+
+        MenuRepository.addMenu(category, menu);
+        MenuRepository.addMenu(category, menu);
+        List<String> menus = MenuRepository.findByCategory(category);
+
+        assertThat(menus.size()).isEqualTo(1);
+        assertThat(menus).containsExactly(menu);
+    }
+
+    @DisplayName("초기화하면 아무것도 남아있지 않는다.")
+    @Test
+    void clear() {
+        Category category = Category.KOREA;
+        String menu = "김치";
+
+        MenuRepository.addMenu(category, menu);
+        MenuRepository.clear();
+        List<String> menus = MenuRepository.findByCategory(category);
+
+        assertThat(menus.size()).isEqualTo(0);
+    }
+}
