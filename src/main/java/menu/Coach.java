929337ff90144c0b58f2b179fe848ca4e80db57f
kevstevie <jianna442@gmail.com>
2022-12-17 13:53:35 +0900

    refactor: Coach 클래스 초기화 환경 수정

--- src/main/java/menu/Coach.java
+++ src/main/java/menu/Coach.java
@@ -1,11 +1,12 @@
 package menu;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Coach {
     private final String name;
     private final List<String> hateFood;
-    private List<String> eatenFood;
+    private List<String> eatenFood = new ArrayList<>();
 
     public Coach(String name, List<String> hateFood) {
         this.name = name;
