d38c7c58b82ec8b337c9a1b1761d053ac1ea3cb3
hectick <chyeon1223@cau.ac.kr>
2022-12-17 13:14:18 +0900

    feat: 서비스 시작 문구를 출력하는 기능 추가

---
+++ src/test/java/menu/constant/Message.java
@@ -0,0 +1,6 @@
+package menu.constant;
+
+public class Message {
+    public static final String SERVICE_START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+
+}
---
+++ src/test/java/menu/view/OutputView.java
@@ -0,0 +1,10 @@
+package menu.view;
+
+import menu.constant.Message;
+
+public class OutputView {
+
+    public static void printServiceStartMessage(){
+        System.out.println(Message.SERVICE_START_MESSAGE);
+    }
+}
