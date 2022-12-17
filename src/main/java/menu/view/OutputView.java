d191997c45e14ec53d10eee406361c278ae7b3ec
SH <gram753@naver.com>
2022-12-17 17:04:45 +0900

    feat: 추천 종료를 출력하는 기능 추가

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -13,6 +13,16 @@ public class OutputView {
 	}
 
 	public static void printMenuInputMessage(String name) {
-		System.out.printf(name + MENU_INPUT_MSG);
+		System.out.printf("\n" + name + MENU_INPUT_MSG);
+	}
+
+	public static void printResult() {
+		System.out.println("메뉴 추천 결과입니다.\n"
+			+ "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]\n"
+			+ "[ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]");
+	}
+
+	public static void printEndMessage() {
+		System.out.println(END_MSG);
 	}
 }
