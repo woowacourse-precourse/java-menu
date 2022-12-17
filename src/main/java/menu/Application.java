aa03d0aec03b9f1578352ef3580254a7c8fc44a3
be-student <presentsong@naver.com>
2022-12-17 14:20:59 +0900

    feat: 시작시 메뉴를 불러오는 기능을 추가한다

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,12 +1,38 @@
 package menu;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import menu.client.MenuClient;
+import menu.di.MenuUseCase;
 import menu.di.MenuUseCaseImpl;
 
 public class Application {
 
     public static void main(String[] args) {
-        MenuClient menuClient = new MenuClient(new MenuUseCaseImpl());
+        MenuClient menuClient = new MenuClient(getMenuUseCase());
         menuClient.run();
     }
+
+    private static MenuUseCase getMenuUseCase() {
+        List<String> category = loadCategory();
+        Map<String, List<String>> menus = loadMenus();
+        return new MenuUseCaseImpl(category, menus);
+    }
+
+    private static List<String> loadCategory() {
+        return List.of("일식", "한식", "중식", "아시안", "양식");
+    }
+
+    private static Map<String, List<String>> loadMenus() {
+        Map<String, List<String>> menus = new HashMap<>();
+        menus.put("일식", List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼"));
+        menus.put("한식", List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"));
+        menus.put("중식", List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채"));
+        menus.put("아시안", List.of("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜"));
+        menus.put("양식", List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
+        return menus;
+    }
+
+
 }
--- src/main/java/menu/di/MenuUseCaseImpl.java
+++ src/main/java/menu/di/MenuUseCaseImpl.java
@@ -1,9 +1,18 @@
 package menu.di;
 
 import java.util.List;
+import java.util.Map;
 import menu.domain.CrewName;
 
 public class MenuUseCaseImpl implements MenuUseCase {
+
+    private final List<String> categories;
+    private final Map<String, List<String>> menus;
+
+    public MenuUseCaseImpl(List<String> categories, Map<String, List<String>> menus) {
+        this.categories = categories;
+        this.menus = menus;
+    }
 
     @Override
     public void validateNames(ValidateNameCommand validateNameCommand) {
