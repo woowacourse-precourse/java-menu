a528b9c6f2865533cf22f479e0f69504836bbf87
2014012606 <2014012606@hanyang.ac.kr>
2022-12-17 14:10:24 +0900

    feat: 코치의 이름 목록을 입력받는 기능 추가

---
+++ src/main/java/menu/view/InputView.java
@@ -0,0 +1,13 @@
+package menu.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String getCoachesName() {
+        System.out.println(getCoachesName());
+        String userInput = Console.readLine();
+
+        // 예외 검사
+        return userInput;
+    }
+}
