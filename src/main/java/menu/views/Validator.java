4c3e273a58d451a0a54931a6f1d885e8a5ab5ead
WhalesBob <ndw8200@naver.com>
2022-12-17 14:01:45 +0900

    feat : 코치님들의 이름 길이 점검하는 기능 추가

--- src/main/java/menu/views/Validator.java
+++ src/main/java/menu/views/Validator.java
@@ -6,6 +6,7 @@ public class Validator {
         boolean isAllRight = true;
         try{
             validateCoachNumber(userInput);
+            validateCoachNameLength(userInput);
         }catch(IllegalArgumentException ex){
             isAllRight = false;
         }
@@ -14,8 +15,18 @@ public class Validator {
 
     private void validateCoachNumber(String[] coaches){
         if(!(2 <= coaches.length && coaches.length <= 5)){
-            String errorMessage = "[ERROR] 입력해 주시는 코치님들의 숫자는 2~5명 사이로 입력해 주십시오.";
+            String errorMessage = "[ERROR] 입력해 주시는 코치님들의 숫자는 2~5명 사이로 다시 입력해 주십시오.";
             errorProcess(errorMessage);
+        }
+    }
+
+    private void validateCoachNameLength(String[] coaches){
+        for (String coach : coaches) {
+            int nameLength = coach.length();
+            if (!(2 <= nameLength && nameLength <= 4)) {
+                String errorMessage = "[ERROR] 코치님들의 이름은 2글자에서 4글자 사이여야 합니다. 다시 입력해주세요.";
+                errorProcess(errorMessage);
+            }
         }
     }
 
