7b35d9f2aea149e5a55004312985ce15cf641d66
HaODay <ycis5013@naver.com>
2022-12-17 15:15:00 +0900

    feat(InputView): 각 코치 별로 못 먹는 메뉴 입력 받는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -2,12 +2,34 @@ package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
+import static menu.domain.CoachRepository.coaches;
+import static menu.util.message.InputMessage.INPUT_CANNOT_EAT_MENU;
+import static menu.util.message.InputMessage.INPUT_COACH_NAME;
+import static menu.valid.InputValidator.validateCannotEatMenu;
 import static menu.valid.InputValidator.validateCoachName;
 
 public class InputView {
+    OutputView outputView = new OutputView();
 
-public void readCoachName(){
-    System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
-    validateCoachName(Console.readLine());
-}
+    public List<String> readCoachName() {
+        try {
+            System.out.println(INPUT_COACH_NAME);
+            return validateCoachName(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return readCoachName();
+        }
+    }
+
+    public List<String> readCannotEatMenu(int index) {
+        try {
+            System.out.printf(INPUT_CANNOT_EAT_MENU, coaches().get(index).getName());
+            return validateCannotEatMenu(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            return readCannotEatMenu(index);
+        }
+    }
 }
