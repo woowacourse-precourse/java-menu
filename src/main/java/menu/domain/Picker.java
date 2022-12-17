052702750704499634bb6ec33b69aeda2c40cc15
be-student <presentsong@naver.com>
2022-12-17 15:29:18 +0900

    feat: shuffle 메서드를 picker에 추가한다

--- src/main/java/menu/domain/Picker.java
+++ src/main/java/menu/domain/Picker.java
@@ -1,6 +1,10 @@
 package menu.domain;
 
+import java.util.List;
+
 public interface Picker {
 
     int pickNumberInRange(int startInclusive, int endInclusive);
+
+    List<String> shuffle(List<String> strings);
 }
--- src/main/java/menu/domain/RandomPicker.java
+++ src/main/java/menu/domain/RandomPicker.java
@@ -1,11 +1,17 @@
 package menu.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 public class RandomPicker implements Picker {
 
     @Override
     public int pickNumberInRange(int startInclusive, int endInclusive) {
         return Randoms.pickNumberInRange(startInclusive, endInclusive);
+    }
+
+    @Override
+    public List<String> shuffle(List<String> strings) {
+        return Randoms.shuffle(strings);
     }
 }
