4418ea9f88f396f76c491e7a9152f6b44fc9f1a8
CAEL0 <kjongmin26@gmail.com>
2022-12-17 15:34:57 +0900

    feat(ViewConstants): 결과 출력 관련 상수 추가

--- src/main/java/menu/view/ViewConstants.java
+++ src/main/java/menu/view/ViewConstants.java
@@ -5,6 +5,8 @@ public class ViewConstants {
     public static final String ASKING_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
     public static final String ASKING_INEDIBLE_MENU = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
     public static final String RESULT = "메뉴 추천 결과입니다.";
+    public static final String DAYS = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]";
+    public static final String CATEGORY = "카테고리";
     public static final String COMPLETION = "추천을 완료했습니다.";
 
     public static final String FRONT = "[ ";
