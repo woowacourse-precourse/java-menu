79c612e387edbd95ffcb446b81b0d0e4d00ad456
MINJU <alswn9938@gmail.com>
2022-12-17 16:17:43 +0900

    refactor : initController를 Enum 사용하게 수정

--- src/main/java/menu/controller/InitController.java
+++ src/main/java/menu/controller/InitController.java
@@ -5,18 +5,8 @@ import menu.domain.category.CategoryRepository;
 import menu.domain.category.CategoryType;
 import menu.domain.menu.MenuRepository;
 
-import java.util.List;
-import java.util.Map;
-
 public class InitController {
-    // 저장순서가 유지된다.
-    private final static Map<String, List<String>> map = Map.of(
-            "일식", List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼"),
-            "한식", List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼구수", "불고기", "떡볶이", "제육볶음"),
-            "중식", List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채"),
-            "아시안", List.of("팟타이", "카오 팟", "나시고랭", "파인애플 볶음밥", "쌀국수", "똠양꿍", "반미", "월남쌈", "분짜"),
-            "양식", List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니")
-    );
+
     MenuRepository menuRepository;
     CategoryRepository categoryRepository;
 
@@ -25,23 +15,24 @@ public class InitController {
         this.categoryRepository = CategoryRepository.getInstance();
     }
 
-    // 간략화 할 수 있는 방법 찾아보기
     public void run() {
         initCategoryAndMenuRepository();
     }
 
     private void initCategoryAndMenuRepository() {
-        for (String categoryName : map.keySet()) {
+        for (String categoryName : CategoryType.names) {
             Category category = new Category(categoryName);
-            int categoryId = CategoryType.getIndexByKorean(categoryName);
+            int categoryId = CategoryType.getIndexByName(categoryName);
             categoryRepository.save(categoryId, category);
+
             initMenuRepository(category);
         }
     }
 
 
     private void initMenuRepository(Category category) {
-        for (String menuNames : map.get(category.getName())) {
+        String name = category.getName();
+        for (String menuNames : CategoryType.getMenusByName(name)) {
             menuRepository.save(category, menuNames);
         }
     }
--- src/main/java/menu/domain/category/CategoryType.java
+++ src/main/java/menu/domain/category/CategoryType.java
@@ -1,40 +1,55 @@
 package menu.domain.category;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum CategoryType {
-    JAPANESE(1,"일식"),
-    KOREAN(2,"한식"),
-    CHINESE(3, "중식"),
-    ASIAN(4,"아시안"),
-    WESTERN(5,"양식");
+    JAPANESE(1,"일식",
+            List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
+    KOREAN(2,"한식",
+            List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼구수", "불고기", "떡볶이", "제육볶음")),
+    CHINESE(3, "중식",
+            List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
+    ASIAN(4,"아시안",
+            List.of("팟타이", "카오 팟", "나시고랭", "파인애플 볶음밥", "쌀국수", "똠양꿍", "반미", "월남쌈", "분짜")),
+    WESTERN(5,"양식",
+            List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
 
     private final int index;
-    private final String korean;
+    private final String name;
+    private final List<String> menus;
 
-    CategoryType(int index, String korean){
+    CategoryType(int index, String name, List<String> menus){
         this.index = index;
-        this.korean = korean;
+        this.name = name;
+        this.menus = menus;
     }
 
-    private static final Map<String, CategoryType> categoryTypeByKorean =
+    public static final List<String> names =
+            Collections.unmodifiableList(Stream.of(CategoryType.values())
+                    .map(CategoryType::getName)
+                    .collect(Collectors.toList()));
+
+
+    private static final Map<String, CategoryType> categoryTypeByName =
             Collections.unmodifiableMap(Stream.of(values())
-                    .collect(Collectors.toMap(CategoryType::getKorean, Function.identity())));
+                    .collect(Collectors.toMap(CategoryType::getName, Function.identity())));
 
-    public String getKorean() {
-        return korean;
+    public String getName() {
+        return name;
     }
 
-    public int getIndex(){
-        return index;
-    }
-
-    public static int getIndexByKorean(String korean){
-        return categoryTypeByKorean.get(korean)
+    public static int getIndexByName(String korean){
+        return categoryTypeByName.get(korean)
                 .index;
+    }
+
+    public static List<String> getMenusByName(String name){
+        return categoryTypeByName.get(name)
+                .menus;
     }
 }
