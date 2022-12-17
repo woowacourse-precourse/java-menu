f5bf98e0b9b0072e7ffbe3d9fa569a018e05d4b9
wnsgml7267 <dlwns7267@naver.com>
2022-12-17 13:46:58 +0900

    feat(view/InputView): 입력 값 받는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,4 +1,21 @@
 package menu.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static final String INPUT_MAIN = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    public static final String INPUT_NOT_FOOD = "(이)가 못 먹는 메뉴를 입력해 주세요.";
+
+    public static String mainInput() {
+        System.out.println(INPUT_MAIN);
+        String input = Console.readLine();
+        printBreak();
+        return input;
+    }
+    public static String notFoodInput() {
+        System.out.println(INPUT_NOT_FOOD);
+        String input = Console.readLine();
+        printBreak();
+        return input;
+    }
 }
