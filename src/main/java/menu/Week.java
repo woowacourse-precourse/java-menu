c244ae7339604af719f601f0049b5f9c51bb2205
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2022-12-17 13:22:16 +0900

    feat(menu) : 요일 정의

---
+++ src/main/java/menu/Week.java
@@ -0,0 +1,15 @@
+package menu;
+
+public enum Week {
+    MONDAY("월요일"),
+    SATURDAY("화요일"),
+    WEDNESDAY("수요일"),
+    THURSDAY("목요일"),
+    FRIDAY("금요일");
+
+    private final String value;
+
+    Week(String value) {
+        this.value = value;
+    }
+}
