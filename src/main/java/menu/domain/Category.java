4127e0f32683244c97f5b10612d030a0c65f8297
sonsoowon <yuwoo1018@gmail.com>
2022-12-17 14:40:09 +0900

    feat(Category): 입력된 이름에 해당하는 카테고리 객체 반환

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -11,6 +11,8 @@ public enum Category {
 
 
     private static final String INVALID_INDEX = "인덱스는 0부터 4사이의 숫자여야 합니다.";
+    private static final String INVALID_NAME = "존재하지 않는 이름입니다.";
+
     private final String name;
     private final int index;
 
@@ -19,11 +21,19 @@ public enum Category {
         this.index = index;
     }
 
-    public static Category getCategory(int index) {
+    public static Category getCategoryByIndex(int index) {
         return Arrays.stream(Category.values())
                 .filter(category -> category.index == index)
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException(INVALID_INDEX));
+    }
+
+    public static Category getCategoryByName(String name) {
+        return Arrays.stream(Category.values())
+                .filter(category -> name.equals(category.name))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_NAME));
+
     }
 
     public String getName() {
