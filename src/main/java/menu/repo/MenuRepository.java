52f8dbdc1eeb76cd381e69042715e448bb7bd3e5
MinjeongKong <kong1301@ajou.ac.kr>
2022-12-17 14:03:45 +0900

    feat: 메뉴 레포지토리

---
+++ src/main/java/menu/repo/MenuRepository.java
@@ -0,0 +1,39 @@
+package menu.repo;
+
+import menu.domain.Category;
+import menu.domain.Menu;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MenuRepository {
+    private Long id = 0L;
+    private final Map<Long, Menu> menus = new HashMap<>();
+
+    public Long save(Menu menu) {
+        menus.put(id, menu);
+        return id++;
+    }
+
+    public Menu findById(Long id) {
+        return menus.get(id);
+    }
+
+    public List<Menu> findAll() {
+        return new ArrayList<>(menus.values());
+    }
+
+    public List<String> findMenusAllByCategory(Category category) {
+        Menu menu = findByCategory(category);
+        return menu.getMenus();
+    }
+
+    private Menu findByCategory(Category category) {
+        Menu menu = menus.values().stream()
+                .filter(o -> o.getCategory().equals(category))
+                .findFirst().orElse(null);
+        return menu;
+    }
+}
