ff884a878df726109b932a08c114042f75548e1c
limheesang <dlagmltkd12@naver.com>
2022-12-17 14:12:36 +0900

    feat: 코치를 저장하고 불러오는 기능

---
+++ src/main/java/menu/domain/repository/CoachRepository.java
@@ -0,0 +1,27 @@
+package menu.domain.repository;
+
+import menu.domain.model.Coach;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CoachRepository {
+
+    private static final List<Coach> coaches;
+
+    static {
+        coaches = new ArrayList<>();
+    }
+
+    private CoachRepository() {
+    }
+
+    public static void add(Coach coach) {
+        coaches.add(coach);
+    }
+
+    public static List<Coach> findAll() {
+        return Collections.unmodifiableList(coaches);
+    }
+}
