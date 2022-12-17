e1d10d79accf8e9dd448d96a0dfb178882f2bca2
be-student <presentsong@naver.com>
2022-12-17 16:03:07 +0900

    test: null 체크가 제대로 작동하는지 검증하는 테스트를 추가한다

---
+++ src/test/java/menu/util/NullValidatorTest.java
@@ -0,0 +1,25 @@
+package menu.util;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("NullValidator 클래스")
+@DisplayNameGeneration(ReplaceUnderscores.class)
+public class NullValidatorTest {
+
+    @Test
+    void throwIfNull_메서드는_null_이_들어오면_IEA_예외를_발생시킨다() {
+        assertThatThrownBy(() -> NullValidator.throwIfNull(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void throwIfNull_메서드는_null_이_아니라면_아무것도_하지_않는다() {
+        assertDoesNotThrow(() -> NullValidator.throwIfNull("not null"));
+    }
+}
