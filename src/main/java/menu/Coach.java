6525c646cc18228a496ec9c52037df05abc60098
youngkyunIm <yee0230@naver.com>
2022-12-17 13:33:00 +0900

    feat(Coach) : 이름 최소 2 ~ 4 글자

--- src/main/java/menu/Coach.java
+++ src/main/java/menu/Coach.java
@@ -5,10 +5,17 @@ public class Coach {
     private final String name;
 
     public Coach(String name) {
+        validName(name);
         this.name = name;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    private void validName(String input) {
+        if (input.length() > 4 || input.length() < 2) {
+            throw new IllegalArgumentException("코치의 이름은 2~4 글자 입니다.");
+        }
     }
 }
---
+++ src/test/java/menu/CoachTest.java
@@ -0,0 +1,29 @@
+package menu;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CoachTest {
+
+    @DisplayName("Normal BridgeSize")
+    @ParameterizedTest(name = "[{index}] input {0} ")
+    @ValueSource(strings = {"토비", "토토토비"})
+    void Validate_BridgeSize_exception(String input) {
+        assertThat(new Coach(input).getName()).isEqualTo(input);
+    }
+
+    @DisplayName("Unvalidated BridgeSize Range")
+    @ParameterizedTest(name = "[{index}] input {0} ")
+    @ValueSource(strings = {"토", "토토토토토"})
+    void Unvalidated_BridgeSize_exception(String input) {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Coach(input);
+        });
+        assertEquals(exception.getMessage(), "코치의 이름은 2~4 글자 입니다.");
+    }
+}
