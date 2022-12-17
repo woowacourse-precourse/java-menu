41f62e46e8c5ec20c5daeaad3398b6c9a2f151cd
jjongwa <troas96@naver.com>
2022-12-17 13:37:49 +0900

    feat: 코치 이름 입력 시 제한사항 예외처리 기능, 예외 발생 시 error 출력 기능

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,10 +1,16 @@
 package menu;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        View view = new View();
-        view.startGame();
-//        System.out.println(view.inputCoachName());
+        try {
+            View view = new View();
+            view.startGame();
+            List<String> coachNameList = view.inputCoachName();
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+        }
     }
 }
--- src/main/java/menu/View.java
+++ src/main/java/menu/View.java
@@ -1,18 +1,32 @@
 package menu;
 
 import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class View {
-    public void startGame(){
+    public void startGame() {
         System.out.println("점심 메뉴 추천을 시작합니다.");
         System.out.println();
     }
 
-    public List<String> inputCoachName(){
+    public List<String> inputCoachName() {
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
         String input = Console.readLine();
         System.out.println(input);
-        return List.of(input.split(","));
+        List<String> coachNameList = List.of(input.split(","));
+        checkCoachName(coachNameList);
+        return coachNameList;
     }
+
+    private void checkCoachName(List<String> coachNameList) {
+        if (coachNameList.size() < 2 || coachNameList.size() > 5)
+            throw new IllegalArgumentException("코치는 최소 2명 최대 5명으로 입력해야 합니다.");
+        for (String coachName : coachNameList) {
+            if (coachName.length() < 2 || coachName.length() > 4)
+                throw new IllegalArgumentException("코치의 이름은 2~4글자 사이로 입력해야 합니다.");
+        }
+    }
+
+
 }
