16f875e55ddcccf9ee548e3b82a67faebac2a3be
Cyma-s <ssol0319@naver.com>
2022-12-17 16:20:53 +0900

    feat(MenuController) : outputView 로직 추가

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,21 +1,29 @@
 package menu.controller;
 
 import java.util.List;
+import menu.CategoryRandomNumberGenerator;
 import menu.domain.Coach;
+import menu.domain.RecommendResult;
 import menu.view.InputView;
 import menu.view.OutputView;
 
 public class MenuController {
     private final InputView inputView;
     private final OutputView outputView;
+    private final CategoryRandomNumberGenerator categoryRandomNumberGenerator;
 
     public MenuController() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+        this.categoryRandomNumberGenerator = new CategoryRandomNumberGenerator();
     }
 
     public void run() {
+        outputView.printServiceStart();
         List<Coach> coaches = getInputs();
+        RecommendResult recommendResult = new RecommendResult(coaches, categoryRandomNumberGenerator);
+        recommendResult.makeRecommendResult();
+        outputView.printRecommendResult(coaches, recommendResult.getCategories());
     }
 
     public List<Coach> getInputs() {
