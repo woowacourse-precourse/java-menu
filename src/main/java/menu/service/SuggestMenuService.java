d0ab6bf0a9f342da0338c01191b65b4844be0f43
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-12-17 15:19:44 +0900

    Feat(SuggestMenuService): 추가 메뉴 추천 서비스 코드

--- src/main/java/menu/service/SuggestMenuService.java
+++ src/main/java/menu/service/SuggestMenuService.java
@@ -1,14 +1,21 @@
 package menu.service;
 
-import menu.MainController;
+import menu.Category;
+import menu.Coach;
 import menu.dto.SuggestMenuResultDto;
+import menu.repository.CoachRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SuggestMenuService {
+
+    private final CoachRepository coachRepository;
 
     private static final SuggestMenuService instance = new SuggestMenuService();
 
     private SuggestMenuService() {
-
+        coachRepository = CoachRepository.getInstance();
     }
 
     public static SuggestMenuService getInstance() {
@@ -16,7 +23,14 @@ public class SuggestMenuService {
     }
 
     public SuggestMenuResultDto suggest() {
+        List<Category> dailyCategoryList = Category.getCategoryOfWeekList();
+        SuggestMenuResultDto resultDto = new SuggestMenuResultDto(dailyCategoryList);
 
-
+        List<Coach> coachList = coachRepository.findAll();
+        for (Coach coach : coachList) {
+            List<String> weeklyMenuList = coach.getWeeklyMenuList(new ArrayList<>(dailyCategoryList));
+            resultDto.addCoachMenuList(coach.getName(), weeklyMenuList);
+        }
+        return resultDto;
     }
 }
