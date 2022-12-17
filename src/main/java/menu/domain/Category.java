d555735c31ca782b314a3c4418426aad97be92f1
EUNJEONGMUN <munej26@gmail.com>
2022-12-17 13:41:30 +0900

    feat: 카테고리 enum 구현

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,4 +1,17 @@
 package menu.domain;
 
 public enum Category {
+    JAPANESE(1, "일식"),
+    KOREAN(2, "한식"),
+    CHINESE(3, "중식"),
+    ASIAN(4, "아시안"),
+    WESTERN(5, "양식");
+
+    private final int number;
+    private final String name;
+
+    Category(int number, String name) {
+        this.number = number;
+        this.name = name;
+    }
 }
