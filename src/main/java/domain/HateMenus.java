7044f1b6d6c2ebb53e42cd3f2e91924ccaa212c9
saint6839 <saint6839@gmail.com>
2022-12-17 13:46:31 +0900

    refactor(domain) : 클래스 이름 변경
    - 입력 요소 갯수에 대한 유효성 검증 메서드 추가

---
+++ src/main/java/domain/HateMenus.java
@@ -0,0 +1,25 @@
+package domain;
+
+import java.util.List;
+
+public class HateMenus {
+
+    private final List<Menu> hateMenusName;
+
+    public HateMenus(List<Menu> hateMenusName) {
+        validateHateMenusNameCount(hateMenusName);
+        this.hateMenusName = hateMenusName;
+    }
+
+    private void validateHateMenusNameCount(List<Menu> hateMenusName) {
+        if (hateMenusName.size() <= 2) {
+            return;
+        }
+        throw new IllegalArgumentException("[ERROR] 못 먹는 메뉴는 최소 0개 이상, 최대 2개 이하로 입력해주세요.");
+    }
+
+    public List<Menu> getHateMenusName() {
+        return hateMenusName;
+    }
+
+}
