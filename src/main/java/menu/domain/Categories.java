1a08a8370af0720ccb1024fd1114132913c98484
kokodak <kokodakadokok@gmail.com>
2022-12-17 16:59:27 +0900

    refactor: 사용하지 않은 import 삭제

--- src/main/java/menu/domain/Categories.java
+++ src/main/java/menu/domain/Categories.java
@@ -2,7 +2,6 @@ package menu.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
