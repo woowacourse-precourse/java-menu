567d8cb73d328420d954d973e5656b1e3664fa39
hyun-seung <ardimento22@naver.com>
2022-12-17 17:10:22 +0900

    [FIX] 코치별 메뉴 설정 방법 변경 - 기존 : 한 코치당 5일씩 메뉴를 설정해주었다. - 변경 : 하루마다 각 코치들에게 메뉴를 설정해주었다.

--- src/main/java/menu/ChoiceMenu.java
+++ src/main/java/menu/ChoiceMenu.java
@@ -1,6 +1,13 @@
 package menu;
 
 import java.util.List;
+import menu.domain.Category;
+import menu.domain.Coach;
+import menu.domain.Days;
+import menu.domain.Menu;
+import menu.domain.PickingCategory;
+import menu.domain.PickingMenu;
+import menu.domain.repository.CoachRepository;
 import menu.service.SettingCoach;
 import menu.service.SettingMenu;
 import menu.view.InputView;
@@ -8,16 +15,20 @@ import menu.view.OutputView;
 
 public class ChoiceMenu {
 
-    SettingMenu settingMenu;
     OutputView output;
     InputView input;
+    SettingMenu settingMenu;
     SettingCoach settingCoach;
+    PickingCategory pickingCategory;
+    PickingMenu pickingMenu;
 
     ChoiceMenu() {
-        settingMenu = new SettingMenu();
         output = new OutputView();
         input = new InputView();
+        settingMenu = new SettingMenu();
         settingCoach = new SettingCoach();
+        pickingCategory = new PickingCategory();
+        pickingMenu = new PickingMenu();
     }
 
     public void start() {
@@ -25,6 +36,7 @@ public class ChoiceMenu {
         output.printServiceStart();
 
         setCoachs();
+        setMenusToCoachs();
         getResult();
     }
 
@@ -36,8 +48,52 @@ public class ChoiceMenu {
         }
     }
 
+    public void setMenusToCoachs() {
+        List<Coach> coachs = CoachRepository.getCoachs();
+        for(int i=0; i< Days.values().length; i++) {
+            setMenuToCoachs(coachs);
+        }
+    }
+
+    public void setMenuToCoachs(List<Coach> coachs) {
+        for (Coach coach : coachs) {
+            setMenuToCoach(coach);
+        }
+    }
+
+    public void setMenuToCoach(Coach coach) {
+        Category category = getCategoryToCoach(coach);
+        Menu menu = getMenuToCoach(coach, category);
+        coach.addMenu(menu);
+    }
+
+    private Category getCategoryToCoach(Coach coach) {
+        try {
+            Category category = pickingCategory.getCategoryByPickingNumber();
+            coach.isFineAboutCategory(category);
+            return category;
+        } catch (IllegalArgumentException e) {
+            return getCategoryToCoach(coach);
+        }
+    }
+
+    private Menu getMenuToCoach(Coach coach, Category category) {
+        try {
+            Menu menu = pickingMenu.getMenuByShuffle(category);
+            coach.isNotDuplicateMenu(menu);
+            return menu;
+        } catch (IllegalArgumentException e) {
+            return getMenuToCoach(coach, category);
+        }
+    }
+
     public void getResult() {
+        List<Coach> coachs = CoachRepository.getCoachs();
         output.printResult();
+        for (Coach coach : coachs) {
+            output.printCoachMenus(coach.getName(), coach.getMenus());
+        }
+        output.printEnding();
     }
 
     public String[] getCoachNames() {
