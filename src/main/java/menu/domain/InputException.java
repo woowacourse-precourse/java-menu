0fbf24778954f63405b12a870dc2fb320bc8644f
Cyma-s <ssol0319@naver.com>
2022-12-17 13:27:08 +0900

    feat(InputException) : 코치 입력 개수 범위 에러 추가

--- src/main/java/menu/domain/InputException.java
+++ src/main/java/menu/domain/InputException.java
@@ -1,7 +1,10 @@
 package menu.domain;
 
 public enum InputException {
-    DUPLICATE_COACH_NAME("중복된 코치 이름입니다.");
+    DUPLICATE_COACH_NAME("중복된 코치 이름입니다."),
+    OUT_OF_COACH_NAME_COUNT_MINIMUM("코치는 최소 2명 이상 입력해야 합니다."),
+    OUT_OF_COACH_NAME_COUNT_MAXIMUM("코치는 최대 5명 이하로 입력해야 합니다.");
+
 
     private final String exceptionMessage;
     private static final String ERROR_HEADER = "[ERROR] ";
