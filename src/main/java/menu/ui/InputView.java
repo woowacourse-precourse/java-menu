60519ec10308f0b75a6907563ea7db4363e029ba
Woojin Lee <dhkep03@gmail.com>
2022-12-17 13:54:47 +0900

    feat(InputView): 코치 이름 목록 입력

--- src/main/java/menu/ui/InputView.java
+++ src/main/java/menu/ui/InputView.java
@@ -1,4 +1,15 @@
 package menu.ui;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+    public List<String> readCoachNames() {
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        List<String> coachNames = Arrays.asList(
+                Console.readLine().split(",")
+        );
+        return coachNames;
+    }
 }
