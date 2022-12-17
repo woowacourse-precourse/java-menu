e46ad55ccad8497d3874b5a816e923fb585008fb
alstjr706 <alstjr706@naver.com>
2022-12-17 14:55:17 +0900

    feat : (점심 메뉴 추천) 코치이름과, 메뉴를 입력 받는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,4 +1,16 @@
 package menu.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public String coachNameInput() {
+        String coach = Console.readLine();
+        return coach;
+    }
+
+    public String menuInput() {
+        String menu = Console.readLine();
+        return menu;
+    }
 }
