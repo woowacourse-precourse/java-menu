706610624b05d2039a09b99fb41c68e4e17f135c
shinD <huipulci1@naver.com>
2022-12-17 17:20:01 +0900

    fix: 시작 시 SelectCategoryUseCase 생성

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -4,14 +4,18 @@ import menu.common.ConsoleLogger;
 import menu.common.Logger;
 import menu.controller.MenuRecommendController;
 import menu.service.MenuRecommend;
+import menu.service.SelectCategory;
 import menu.step.RecommendStartStep;
 import menu.step.Step;
 import menu.usecase.MenuRecommendUseCase;
+import menu.usecase.SelectCategoryUseCase;
 
 public class Application {
     public static void main(String[] args) {
         MenuRecommendUseCase menuRecommend = new MenuRecommend();
-        MenuRecommendController controller = new MenuRecommendController(menuRecommend);
+        SelectCategoryUseCase selectCategoryUseCase = new SelectCategory();
+        MenuRecommendController controller =
+                new MenuRecommendController(menuRecommend, selectCategoryUseCase);
         Logger consoleLogger = new ConsoleLogger();
         Step step = new RecommendStartStep(controller, consoleLogger);
 
