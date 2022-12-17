d13d780e3e45dff60c088188b4becb545a11f378
dev_kong <ahdjd5@gmail.com>
2022-12-17 15:38:36 +0900

    fix(Recommender): change getRandomCategory return type List<Category> -> Category

--- src/main/java/menu/utills/Recommender.java
+++ src/main/java/menu/utills/Recommender.java
@@ -3,34 +3,19 @@ package menu.utills;
 import camp.nextstep.edu.missionutils.Randoms;
 import menu.utills.constants.Category;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Recommender {
-    public static List<Category> getRandomCategory() {
-        List<Integer> randomNumbers = generateRandomNumbers();
-        return randomNumbers.stream()
-                .map(Category::getCategoryByValue)
-                .collect(Collectors.toList());
+    public static Category getRandomCategory () {
+        int randomNumber = generateRandomNumber();
+        return Category.getCategoryByValue(randomNumber);
     }
 
-    public static List<Integer> generateRandomNumbers() {
-        List<Integer> randomNumbers = new ArrayList<>();
-        while (randomNumbers.size() < 5) {
-            int randomNumber = Randoms.pickNumberInRange(1,5);
-            if(checkExistedDouble(randomNumbers, randomNumber)) {
-                continue;
-            }
-            randomNumbers.add(randomNumber);
-        }
-        return randomNumbers;
+    public static String getRandomMenuName(List<String> menuNames) {
+        return Randoms.shuffle(menuNames).get(0);
     }
 
-    private static boolean checkExistedDouble(List<Integer> numbers, int number) {
-        int countDuplicated = (int) numbers.stream()
-                .filter(integer -> integer == number)
-                .count();
-        return countDuplicated >= 2;
+    private static int generateRandomNumber() {
+        return Randoms.pickNumberInRange(1,5);
     }
 }
