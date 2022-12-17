97390de663dca8706476b5b3fe6e74aec00b20a1
kokodak <kokodakadokok@gmail.com>
2022-12-17 13:53:24 +0900

    feat(outputView): 메뉴 추천 완료 문구 출력 메서드 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public static void printStartMessage() {
         System.out.println("점심 메뉴 추천을 시작합니다.");
     }
+
+    public static void printEndMessage() {
+        System.out.println("추천을 완료했습니다.");
+    }
 }
