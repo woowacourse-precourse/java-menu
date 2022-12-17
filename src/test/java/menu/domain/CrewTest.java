f4d148907cea35529aedeb23e3cc777033503111
sosow0212 <sosow0212@naver.com>
2022-12-17 16:39:51 +0900

    test : (CrewTest) 코치 음식 추가시에 이미 있는 음식인지 테스트

---
+++ src/test/java/menu/domain/CrewTest.java
@@ -0,0 +1,21 @@
+package menu.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CrewTest {
+    @Test
+    @DisplayName("코치 음식 추가시에 이미 있는 음식인지 테스트")
+    public void addFoodTest() {
+        // given
+        Crew crew = new Crew("포비");
+
+        // when
+        crew.addFood("초밥");
+
+        // then
+        assertThat(crew.isAlreadyBeenFood("초밥")).isEqualTo(true);
+    }
+}
