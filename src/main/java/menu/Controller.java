552fb8497e2ae277b7139fc32ae0157f479e8ce2
LJW25 <dbjwl@g.skku.edu>
2022-12-17 13:48:05 +0900

    feat: Controller 클래스 정의

--- src/main/java/menu/Controller.java
+++ src/main/java/menu/Controller.java
@@ -5,4 +5,21 @@ public class Controller {
     public void runController(){
 
     }
+
+    private void getData(){
+        //TODO: 시작 문구 출력, 이름 입력받기, 메뉴 입력받기
+    }
+
+    private void getCoachNames(){
+
+    }
+
+    private void getHateMenus(){
+
+    }
+
+    private void endRecommend(){
+
+    }
+
 }
