5f2e7e1bec2f7e8a2f0b38b4428f5975b4641859
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-17 15:58:20 +0900

    feat: 서비스 안내 시작 문구를 출력하는 기능 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public static void printErrorMessage(String message){
         System.out.println(message);
     }
+
+    public void printServiceIntroducingMessage(){
+        System.out.println("점심 메뉴 추천을 시작합니다.");
+    }
 }
