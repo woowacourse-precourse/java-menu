b10acea706f29fb5af3bf9efebe81b42d0600e65
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-12-17 15:49:00 +0900

    test(ValidateType): 코치 이름 사이즈 체크 테스트 생성

--- src/test/java/menu/input/ValidateTypeTest.java
+++ src/test/java/menu/input/ValidateTypeTest.java
@@ -24,4 +24,22 @@ public class ValidateTypeTest {
         boolean canRead = ValidationType.validate(ValidationType.CHECK_COACH_SIZE, input);
         assertThat(canRead).isTrue();
     }
+
+    @DisplayName("코치 이름 길이에 따른 예외 테스트")
+    @ValueSource(strings = {"1", "0", "5"})
+    @ParameterizedTest
+    void checkCoachNameSize_예외(String input) {
+        boolean canRead = ValidationType.validate(ValidationType.CHECK_NAME_SIZE, input);
+        assertThat(canRead).isFalse();
+    }
+
+    @DisplayName("코치 이름 길이에 따른 정상 테스트")
+    @ValueSource(strings = {"2", "3", "4"})
+    @ParameterizedTest
+    void checkCoachNameSize_정상(String input) {
+        boolean canRead = ValidationType.validate(ValidationType.CHECK_NAME_SIZE, input);
+        assertThat(canRead).isTrue();
+    }
+
+
 }
