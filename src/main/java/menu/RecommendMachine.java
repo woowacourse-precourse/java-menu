cb35b82d570e36dfe670d52c685310652588c255
jaeyeon kim <jakind@naver.com>
2022-12-17 15:23:31 +0900

    feat(RecommendMachine): 클래스명 변경 및, 추천 기능 추가

---
+++ src/main/java/menu/RecommendMachine.java
@@ -0,0 +1,23 @@
+package menu;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class RecommendMachine {
+
+    public void recommend(Category category, Coach coach) {
+        boolean decideNotYet = true;
+
+        while (decideNotYet) {
+            List<String> categoryToFoods = Randoms.shuffle(category.getFoods());
+            String selectFood = categoryToFoods.get(0);
+
+            if (!coach.isHateFood(selectFood) && coach.isPossibleRecommendFood(selectFood)) {
+                coach.recommendCategory(category.toString());
+                coach.recommendFood(selectFood);
+                decideNotYet = false;
+            }
+        }
+    }
+}
