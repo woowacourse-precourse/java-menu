5d1a3aef8444b7e21d43c41da4e4aeed766238f5
areyouhun <areyouhun@gmail.com>
2022-12-17 14:52:24 +0900

    feat(coaches): 클래스 coach를 관리하는 일급 컬렉션 제작

---
+++ src/main/java/menu/domain/Coaches.java
@@ -0,0 +1,27 @@
+package menu.domain;
+
+import java.util.List;
+
+public class Coaches {
+    
+    private static final int MINIMUM_COACHES_SIZE = 2;
+    private static final int MAXIMUM_COACHES_SIZE = 5;
+    private static final String INVALID_COACHES_SIZE = "코치는 최소 2명에서 최대 5명까지 입력해야 합니다.";
+    
+    private final List<Coach> coaches;
+    
+    public Coaches(List<Coach> coaches) {
+        validate(coaches);
+        this.coaches = coaches;
+    }
+
+    private void validate(List<Coach> coaches) {
+        if (coaches.size() < MINIMUM_COACHES_SIZE || coaches.size() > MAXIMUM_COACHES_SIZE) {
+            throw new IllegalArgumentException(INVALID_COACHES_SIZE);
+        }
+    }
+
+    public List<Coach> getCoaches() {
+        return coaches;
+    }
+}
