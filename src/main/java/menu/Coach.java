71ff8ec91bce771c12a1d2f8e54f67664fdaa146
LJW25 <dbjwl@g.skku.edu>
2022-12-17 13:40:33 +0900

    feat: Coach 클래스 정의

--- src/main/java/menu/Coach.java
+++ src/main/java/menu/Coach.java
@@ -1,4 +1,16 @@
 package menu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Coach {
+    String name;
+    List<String> hates;
+    List<String> recommended;
+
+    Coach(String name, List<String> hates){
+        this.name = name;
+        this.hates = hates;
+        this.recommended = new ArrayList<>();
+    }
 }
