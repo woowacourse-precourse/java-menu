295da95fc68b21cba2e9546a6c6e4e647c3d6689
saint6839 <saint6839@gmail.com>
2022-12-17 15:24:02 +0900

    feat(view) : 싫어하는 음식을 받는 메서드 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,12 +2,19 @@ package view;
 
 import camp.nextstep.edu.missionutils.Console;
 import domain.Coaches;
+import domain.HateMenus;
 import support.StringParser;
+
+import java.io.IOException;
 
 public final class InputView {
     private InputView() {}
 
     public static Coaches inputCoachNames() {
         return StringParser.parseStringToCoachesByRest(Console.readLine());
+    }
+
+    public static HateMenus inputHateMenus() throws IOException {
+        return StringParser.parseStringToMenusByRest(Console.readLine());
     }
 }
