8a50e350589da23c584ae4a8a5d710820cfe3e8c
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 14:23:14 +0900

    feat(InputValidation): 못 먹는 메뉴 입력 값 검증 기능 추가
    docs: 못 먹는 메뉴 입력 값 검증 완료 체크

--- src/main/java/menu/validation/InputValidation.java
+++ src/main/java/menu/validation/InputValidation.java
@@ -9,12 +9,19 @@ public class InputValidation {
     private static final String COMMA = ",";
     private static final int MINIMUM_NAME_SIZE = 2;
     private static final int MAXIMUM_NAME_SIZE = 5;
+    private static final int MAXIMUM_HATE_MENU_SIZE = 2;
 
     public static List<String> validateCoachesName(String names) {
         isExist(names);
         List<String> coaches = toList(names);
         isSizeBetween2And5(coaches.size());
         return coaches;
+    }
+
+    public static List<String> validateHateMenus(String menus) {
+        List<String> hateMenus = toList(menus);
+        isSizeLessThanTwo(hateMenus.size());
+        return hateMenus;
     }
 
     private static void isExist(String inputValue) {
@@ -42,5 +49,15 @@ public class InputValidation {
 
     private static boolean isMoreThanFive(int size) {
         return size > MAXIMUM_NAME_SIZE;
+    }
+
+    private static void isSizeLessThanTwo(int size) {
+        if (isMoreThanTwo(size)) {
+            ExceptionMessage.MENU_MORE_THAN_TWO.throwException();
+        }
+    }
+
+    private static boolean isMoreThanTwo(int size) {
+        return size > MAXIMUM_HATE_MENU_SIZE;
     }
 }
