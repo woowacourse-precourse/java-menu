b7e342c4650e677d5f454c61c274feeb8d3b804b
kevstevie <jianna442@gmail.com>
2022-12-17 17:07:53 +0900

    refactor: 상수명 MENU에서 CATEGORY로 수정

--- src/main/java/menu/CategorySelector.java
+++ src/main/java/menu/CategorySelector.java
@@ -7,8 +7,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CategorySelector {
-    private static final int MENU_MIN = 1;
-    private static final int MENU_MAX = 5;
+    private static final int CATEGORY_MIN = 1;
+    private static final int CATEGORY_MAX = 5;
     private static final int WEEK_MAX = 2;
     private static final int ZERO = 0;
 
@@ -16,7 +16,7 @@ public class CategorySelector {
     private List<String> categoryRepository = new ArrayList<>();
 
     public String pickCategory() {
-        int random = Randoms.pickNumberInRange(MENU_MIN, MENU_MAX);
+        int random = Randoms.pickNumberInRange(CATEGORY_MIN, CATEGORY_MAX);
         Dish dish = Arrays.stream(Dish.values()).filter(s -> s.number == random)
                 .findFirst().get();
         if (checkAmount(dish)) {
