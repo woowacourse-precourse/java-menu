e1220d56359fc6ffae81aea45c060f7035c3deb9
Gibeom Nam <rlqja0523@naver.com>
2022-12-17 17:26:03 +0900

    feat: 올바른 값을 입력할 때까지 입력을 받는 기능

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,6 +1,9 @@
 package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import menu.exception.CoachException;
+import menu.exception.InputException;
+import menu.exception.NoMenuException;
 import menu.model.Coach;
 import menu.utils.Converter;
 
@@ -14,16 +17,31 @@ public class InputView {
     }
 
     public List<String> getCoachName() {
-        System.out.println("코치의 이름을 입력해 주세요. (,로 구분)");
-        String names = Console.readLine();
+        System.out.println("점심 메뉴 추천을 시작합니다.");
         System.out.println();
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        InputException coachException = new CoachException(converter);
+        String names = verifyInput(coachException);
         return converter.convertToList(names);
     }
 
     public List<String> getNoMenu(Coach coach) {
         System.out.printf("%s(이)가 못 먹는 메뉴를 입력해 주세요.\n", coach.getName());
-        String menu = Console.readLine();
-        System.out.println();
+        InputException noMenuException = new NoMenuException(converter);
+        String menu = verifyInput(noMenuException);
         return converter.convertToList(menu);
+    }
+
+    private String verifyInput(InputException exception) {
+        while (true) {
+            try {
+                String input = Console.readLine().trim();
+                System.out.println();
+                exception.verifyException(input);
+                return input;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
