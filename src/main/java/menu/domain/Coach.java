08efe729b6c95999ddf03755f5de0d94b28b1736
CAEL0 <kjongmin26@gmail.com>
2022-12-17 14:30:37 +0900

    feat(Coach): 코치 클래스에 못 먹는 메뉴 추가

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -2,8 +2,10 @@ package menu.domain;
 
 public class Coach {
     private final String name;
+    private final InedibleMenu inedibleMenu;
 
-    public Coach(String name) {
+    public Coach(String name, String inedibleMenu) {
+        this.inedibleMenu = new InedibleMenu(inedibleMenu);
         this.name = name;
     }
 }
