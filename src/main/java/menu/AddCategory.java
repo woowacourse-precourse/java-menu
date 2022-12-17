2ce6e18f78b4b89b96258c5787cc41ac0fb36703
donghae <donghae1999@gmail.com>
2022-12-17 17:12:20 +0900

    refactor: 사용안하는 코드 삭제 리팩토링

--- src/main/java/menu/AddCategory.java
+++ src/main/java/menu/AddCategory.java
@@ -5,39 +5,39 @@ import java.util.List;
 
 public class AddCategory {
     public List<Menu> add(List<Menu> menu){
-        menu.add(addJapan(menu));
-        menu.add(addKorean(menu));
-        menu.add(addChinese(menu));
-        menu.add(addAsian(menu));
-        menu.add(addAmerican(menu));
+        menu.add(addJapan());
+        menu.add(addKorean());
+        menu.add(addChinese());
+        menu.add(addAsian());
+        menu.add(addAmerican());
         return menu;
     }
 
-    private Menu addAmerican(List<Menu> menu) {
+    private Menu addAmerican() {
         String foodname = "라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니";
-        return addMenu(5,foodname,"양식",menu);
+        return addMenu(5,foodname,"양식");
     }
 
-    private Menu addAsian(List<Menu> menu) {
+    private Menu addAsian() {
         String foodname = "팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜";
-        return addMenu(4,foodname,"아시안",menu);
+        return addMenu(4,foodname,"아시안");
     }
 
-    private Menu addChinese(List<Menu> menu) {
+    private Menu addChinese() {
         String foodname = "깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채";
-        return addMenu(3,foodname,"중식",menu);
+        return addMenu(3,foodname,"중식");
     }
 
-    private Menu addKorean(List<Menu> menu) {
+    private Menu addKorean() {
         String foodname = "김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음";
-        return addMenu(2,foodname,"한식",menu);
+        return addMenu(2,foodname,"한식");
     }
 
-    private Menu addJapan(List<Menu> menu) {
+    private Menu addJapan() {
         String foodname = "규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼";
-        return addMenu(1,foodname,"일식",menu);
+        return addMenu(1,foodname,"일식");
     }
-    private Menu addMenu(int num,String foodName,String category,List<Menu> menu){
+    private Menu addMenu(int num,String foodName,String category){
         List<String> countryFood = new ArrayList<>();
         String[] food = foodName.split(", ");
         for(int i=0; i<food.length; i++)
