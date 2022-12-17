b893f8864800331ae96059e51336a07db2ff6fd0
zeunala <zeunala@naver.com>
2022-12-17 14:58:11 +0900

    feat: 코치이름 입력받기 기능 추가

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,11 @@
 package menu;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("점심 메뉴 추천을 시작합니다.\n");
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        String coachNames = Console.readLine();
     }
 }
