42193e1c47afdbc6f3ca2149ece0f2ec30638072
CAEL0 <kjongmin26@gmail.com>
2022-12-17 14:25:27 +0900

    feat(Menu): 코치 이름 검증 후 저장

--- src/main/java/menu/domain/Menu.java
+++ src/main/java/menu/domain/Menu.java
@@ -1,4 +1,23 @@
 package menu.domain;
 
+import menu.constant.MenuConstants;
+import menu.util.Validator;
+
+import java.util.List;
+
 public class Menu {
+    private final List<String> coachNames;
+    private List<Coach> coaches;
+
+    public Menu(String coachNames) {
+        List<String> splittedCoaches = List.of(coachNames.split(MenuConstants.DELIMITER));
+
+        Validator.validateNumberOfCoaches(splittedCoaches.size());
+        Validator.validateDuplication(splittedCoaches);
+        splittedCoaches.stream()
+                .map(String::length)
+                .forEach(Validator::validateCoachNameLength);
+
+        this.coachNames = splittedCoaches;
+    }
 }
