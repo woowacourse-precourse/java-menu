fdccdaa5839d6f10d4506f9afe58ee061b14d4cc
cookienc <kim76744@naver.com>
2022-12-17 17:30:23 +0900

    refactor: Service 리팩토링

--- src/main/java/menu/service/RecommendService.java
+++ src/main/java/menu/service/RecommendService.java
@@ -13,42 +13,87 @@ import java.util.List;
 
 public class RecommendService {
     private static final int MAX_RECOMMEND_SIZE = 5;
+    private static final int FIRST = 1;
 
     private final MenuRepository menuRepository;
+    private final RecommendSystem recommendSystem;
 
     public RecommendService() {
         this.menuRepository = new MenuRepository();
+        this.recommendSystem = new RecommendSystem();
     }
 
     public List<RecommendResult> findRecommendMenus(final Coaches coaches) {
-        RecommendSystem recommendSystem = new RecommendSystem();
+
         final List<MenuCategory> categories = recommendSystem.pickRandomCategory();
+
         List<RecommendResult> results = new ArrayList<>();
+        doRecommend(coaches, categories, results);
+
+        return results;
+    }
+
+    private void doRecommend(final Coaches coaches, final List<MenuCategory> categories, final List<RecommendResult> results) {
         int count = 0;
         for (MenuCategory category : categories) {
             count++;
-            for (int j = 0; j < coaches.getCoaches().size(); j++) {
-                final Coach coach = coaches.getCoaches().get(j);
-                final List<String> menus = menuRepository.getMenus(category);
-                boolean canNotRecommend = false;
-                String menuName = "";
-                do {
-                    menuName = recommendSystem.pickRandomMenu(menus, coach);
-                    canNotRecommend = coach.validateMenu(menuName);
-                } while (canNotRecommend);
+            recommendSameDay(results, coaches, count, category);
+        }
+    }
 
-                if (count == 1) {
-                    final Menu menu = new Menu(category, menuName);
-                    List<Menu> recommendMenus = new ArrayList<>();
-                    recommendMenus.add(menu);
-                    RecommendResult result = new RecommendResult(coach, recommendMenus);
-                    results.add(result);
-                } else {
-                    results.get(j).addMenu(new Menu(category, menuName));
-                }
-            }
+    private void recommendSameDay(final List<RecommendResult> results, final Coaches coaches, final int count, final MenuCategory category) {
+        final int coachSize = coaches.getCoaches().size();
+
+        for (int index = 0; index < coachSize; index++) {
+            final Coach coach = coaches.getCoaches().get(index);
+            final List<String> menus = menuRepository.getMenus(category);
+            String menuName = recommendUntilSuccess(coach, menus);
+
+            makeResult(results, count, category, index, coach, menuName);
+        }
+    }
+
+    private static void makeResult(
+            final List<RecommendResult> results,
+            final int count,
+            final MenuCategory category,
+            final int index,
+            final Coach coach,
+            final String menuName
+    ) {
+        if (count == FIRST) {
+            makeResultAndAddMenu(results, category, coach, menuName);
+            return;
         }
 
-        return results;
+        addMenu(results, category, index, menuName);
+    }
+
+    private static void makeResultAndAddMenu(final List<RecommendResult> results, final MenuCategory category, final Coach coach, final String menuName) {
+        final Menu menu = new Menu(category, menuName);
+        RecommendResult result = new RecommendResult(coach, makeRecommendMenus(menu));
+
+        results.add(result);
+    }
+
+    private static List<Menu> makeRecommendMenus(final Menu menu) {
+        List<Menu> recommendMenus = new ArrayList<>();
+        recommendMenus.add(menu);
+
+        return recommendMenus;
+    }
+
+    private static void addMenu(final List<RecommendResult> results, final MenuCategory category, final int j, final String menuName) {
+        results.get(j).addMenu(new Menu(category, menuName));
+    }
+
+    private String recommendUntilSuccess(final Coach coach, final List<String> menus) {
+        boolean canNotRecommend = false;
+        String menuName = "";
+        do {
+            menuName = recommendSystem.pickRandomMenu(menus, coach);
+            canNotRecommend = coach.validateMenu(menuName);
+        } while (canNotRecommend);
+        return menuName;
     }
 }
