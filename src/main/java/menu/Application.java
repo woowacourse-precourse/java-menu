c60ee6cda41b898c42b961712b1d35bcda5e9caa
soyunim <togksk123@naver.com>
2022-12-17 13:58:57 +0900

    [feat] 코치 이름과 못 먹는 음식 입력

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,41 @@
 package menu;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static List<List<String>> coaches = new ArrayList<>();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameProcess();
+    }
+    public static void GameProcess() {
+        GetName();
+        System.out.println(coaches);
+    }
+
+    public static void GetName(){
+        List<String> coachNames = new ArrayList();
+        String name = Console.readLine();
+        String[] cNames = name.split(",");
+        for (int i = 0; i < cNames.length; i++) {
+            coachNames.add(cNames[i].trim());
+        }
+        System.out.println("coach name:"+coachNames);
+        for (int i = 0; i < coachNames.size(); i++) {
+            coaches.add(NoFood(coachNames.get(i)));
+        }
+    }
+
+    public static List NoFood(String name){
+        List<String> food = new ArrayList<>();
+        food.add(name);
+        String foodName = Console.readLine();
+        String[] foods = foodName.split(",");
+        for (int i = 0; i < foods.length; i++) {
+            food.add(foods[i].trim());
+        }
+        return food;
     }
 }
