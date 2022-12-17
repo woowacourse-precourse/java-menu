f9a8a63d90c3f4eb243e1fda6c07e8f2a14bf894
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-12-17 15:07:37 +0900

    <feat>(OutputView.java): formatAsTable() 메서드 작성
     - 정해진 형식으로 리스트를 변환하는 메서드 작성

--- src/main/java/menu/OutputView.java
+++ src/main/java/menu/OutputView.java
@@ -1,5 +1,8 @@
 package menu;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class OutputView {
     private static final String SERVICE_START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
     private static final String RESULT_INFORM_MESSAGE = "메뉴 추천 결과입니다.";
@@ -7,12 +10,13 @@ public class OutputView {
     private static final String ERROR_PREFIX = "[ERROR] ";
     private static final String PICKYEATINGS_INFORM_MESSAGE = "(이)가 못 먹는 메뉴를 입력해 주세요.";
     private static final String COACH_NAME_INFORM_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private static final List<String> DAY_OF_THE_WEEKS = Arrays.asList("월요일", "화요일", "수요일", "목요일", "금요일");
 
     public void printStartMessage() {
         System.out.println(SERVICE_START_MESSAGE);
     }
 
-    public void printEndMessage() {
+    private void printEndMessage() {
         System.out.println(SERVICE_END_MESSAGE);
     }
 
@@ -24,11 +28,24 @@ public class OutputView {
         System.out.println(PICKYEATINGS_INFORM_MESSAGE);
     }
 
-    public void printResultInformMessage() {
+    private void printResultInformMessage() {
         System.out.println(RESULT_INFORM_MESSAGE);
     }
 
+    public void printResult() {
+        printResultInformMessage();
 
+        printEndMessage();
+    }
 
+    private String formatAsTable(String title, List<String> contents) {
+        StringBuilder form = new StringBuilder();
+        form.append("[").append(title);
+        for (int i = 0; i < contents.size(); i++) {
+            form.append("|").append(contents.get(i));
+        }
+        form.append("]");
 
+        return form.toString();
+    }
 }
