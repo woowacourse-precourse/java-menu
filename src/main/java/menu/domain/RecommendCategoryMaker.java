3f6d5adf4c3a5551401869584d7d072b7c65c261
kat8813 <ohseo8813@gmail.com>
2022-12-17 14:16:24 +0900

    <feat>(RecommendCategoryMaker.java): 5개의 카테고리를 만드는 클래스 추가

---
+++ src/main/java/menu/domain/RecommendCategoryMaker.java
@@ -0,0 +1,29 @@
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class RecommendCategoryMaker {
+
+    private final static int CATEGORY_COUNT = 5;
+    private final static int MAX_DUPLICATE_CATEGORY_COUNT = 2;
+
+    private final CategoryGenerator categoryGenerator;
+
+    public RecommendCategoryMaker(CategoryGenerator categoryGenerator) {
+        this.categoryGenerator = categoryGenerator;
+    }
+
+    public List<Category> make() {
+        List<Category> categories = new ArrayList<>();
+        while (categories.size() > CATEGORY_COUNT) {
+            Category category = categoryGenerator.generate();
+            if (Collections.frequency(categories, category) < MAX_DUPLICATE_CATEGORY_COUNT) {
+                categories.add(category);
+            }
+        }
+        return categories;
+    }
+}
