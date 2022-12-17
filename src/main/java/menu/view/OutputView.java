859020aa1ddb39396524b7d3cc4ae6c8bbf90474
JunHyeon <dlwnsgus07@naver.com>
2022-12-17 16:20:33 +0900

    styled: 공백 추가

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -12,6 +12,7 @@ public class OutputView {
     static final String RECOMMEND_START = "점심 메뉴 추천을 시작합니다.\n";
     static final String PRINT_MENU = "메뉴 추천 결과입니다.\n";
     static final String FINISH = "추천을 완료했습니다.\n";
+
     public static void recommendStart(){
         System.out.println(RECOMMEND_START);
     }
