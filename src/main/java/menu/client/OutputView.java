31fb64fde7866648b7f4c4ca01a644dd1014524f
be-student <presentsong@naver.com>
2022-12-17 13:44:46 +0900

    feat: 시작 메시지를 출력하는 기능을 추가한

--- src/main/java/menu/client/OutputView.java
+++ src/main/java/menu/client/OutputView.java
@@ -3,6 +3,7 @@ package menu.client;
 public class OutputView {
 
     private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final String START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
 
     private OutputView() {
     }
@@ -11,6 +12,14 @@ public class OutputView {
         println(ERROR_PREFIX + errorMessage);
     }
 
+    public static void printStartMessage() {
+        println(START_MESSAGE);
+        println();
+    }
+
+    private static void println() {
+        System.out.println();
+    }
 
     private static void println(String message) {
         System.out.println(message);
