e7c7f4b3534cf289dcf13486188147b6918569e0
cookienc <kim76744@naver.com>
2022-12-17 13:29:30 +0900

    feat: 음식 카테고리에 순서 지정

--- src/main/java/menu/model/FoodCategory.java
+++ src/main/java/menu/model/FoodCategory.java
@@ -1,15 +1,17 @@
 package menu.model;
 
 public enum FoodCategory {
-    JAPAN("일식"),
-    KOREA("한식"),
-    CHINA("중식"),
-    ASIAN("아시안"),
-    WESTERN("양식");
+    JAPAN("일식", 1),
+    KOREA("한식", 2),
+    CHINA("중식", 3),
+    ASIAN("아시안", 4),
+    WESTERN("양식", 5);
 
     private final String category;
+    private final int number;
 
-    FoodCategory(final String category) {
+    FoodCategory(final String category, final int number) {
         this.category = category;
+        this.number = number;
     }
 }
