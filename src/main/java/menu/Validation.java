82cb27a5759c7adcd35b4122065d87960698deda
yejin <kwonyj1022@naver.com>
2022-12-17 17:15:45 +0900

    feat: validateCoachesName() 코치 인원 제한 추가

--- src/main/java/menu/Validation.java
+++ src/main/java/menu/Validation.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class Validation {
     public void validateCoachesName(List<String> coachesName) {
+        if (coachesName.size() < 2 || coachesName.size() > 5) {
+            throw new IllegalArgumentException("[ERROR] 코치는 최소 2명, 최대 5명까지 식사를 함께 합니다.");
+        }
         for (int i = 0; i < coachesName.size(); i++) {
             if ((coachesName.get(i).length() < 2) || (coachesName.get(i).length() > 4)) {
                 throw new IllegalArgumentException("[ERROR] 코치의 이름은 최소 2글자, 최대 4글자입니다.");
