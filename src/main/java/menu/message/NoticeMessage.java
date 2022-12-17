0578a49873e735772833b5578e84f29184e4af16
yujaemin <woawoa0513@gmail.com>
2022-12-17 15:36:37 +0900

    feat: 메뉴 추천 결과 출력 함수 구현

--- src/main/java/menu/message/NoticeMessage.java
+++ src/main/java/menu/message/NoticeMessage.java
@@ -4,15 +4,9 @@ import menu.domain.Coach;
 
 public class NoticeMessage {
     public static final String SERVICE_START_NOTICE = "점심 메뉴 추천을 시작합니다.";
-    public static final String COACH_NAME_INPUT_NOTICE = "코치의 이름을 입력해 주세요. (, 로 구분)\n";
-    public static final String DISLIKE_FOOD_INPUT_NOTICE = "(이)가 못 먹는 메뉴를 입력해 주세요.\n";
-    public static final String SERVICE_END_NOTICE = String.format(
-        "메뉴 추천 결과입니다.\n"
-                + "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]\n"
-                + "[ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]\n"
-                + "[ 토미 | 쌈밥 | 김치찌개 | 미소시루 | 짜장면 | 팟타이 ]\n"
-                + "[ 제임스 | 된장찌개 | 비빔밥 | 가츠동 | 토마토 달걀볶음 | 파인애플 볶음밥 ]\n"
-                + "[ 포코 | 된장찌개 | 불고기 | 하이라이스 | 탕수육 | 나시고렝 ]\n\n"
-                + "추천을 완료했습니다."
-    );
+    public static final String COACH_NAME_INPUT_NOTICE = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    public static final String DISLIKE_FOOD_INPUT_NOTICE = "(이)가 못 먹는 메뉴를 입력해 주세요.";
+    public static final String SERVICE_END_NOTICE_TOP = "메뉴 추천 결과입니다.\n"
+                + "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]";
+    public static final String SERVICE_END_NOTICE_BOTTOM = "추천을 완료했습니다.";
 }
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,22 +1,53 @@
 package menu.view;
 
+import java.util.List;
+import menu.domain.Coach;
+import menu.domain.Menu;
 import menu.message.NoticeMessage;
 
 public class OutputView {
+    private static final String END_NOTICE_PREFIX = "[ ";
+    private static final String END_NOTICE_SUFFIX = " ]";
+    private static final String END_NOTICE_MIDFIX = " | ";
 
     public void printServiceStartNotice() {
         System.out.println(NoticeMessage.SERVICE_START_NOTICE + '\n');
     }
 
     public void printCoachNameInputNotice() {
-        System.out.println(NoticeMessage.COACH_NAME_INPUT_NOTICE + '\n');
+        System.out.println(NoticeMessage.COACH_NAME_INPUT_NOTICE);
     }
 
     public void printDislikeFoodInputNotice(String coachName) {
-        System.out.println(coachName + NoticeMessage.DISLIKE_FOOD_INPUT_NOTICE + '\n');
+        System.out.println(coachName + NoticeMessage.DISLIKE_FOOD_INPUT_NOTICE);
     }
 
-    public void printServiceEndNotice() {
-        System.out.println(NoticeMessage.SERVICE_END_NOTICE + '\n');
+    public void printServiceEndNotice(List<Coach> coaches) {
+        StringBuilder endNotice = new StringBuilder(NoticeMessage.SERVICE_END_NOTICE_TOP).append('\n');
+
+        for (Coach coach : coaches) {
+            endNotice.append(makeRecommendResult(coach)).append('\n');
+        }
+
+        endNotice.append(NoticeMessage.SERVICE_END_NOTICE_BOTTOM);
+
+        System.out.println(endNotice);
+    }
+
+    private String makeRecommendResult(Coach coach) {
+        StringBuilder result = new StringBuilder();
+        result.append(END_NOTICE_PREFIX);
+
+        result.append(coach.getName());
+
+        List<Menu> recommendedMenus = coach.getRecommendedMenus();
+        for (int day = 0; day < 5; day++) {
+            result.append(END_NOTICE_MIDFIX);
+            result.append(recommendedMenus.get(day));
+        }
+
+        result.append(END_NOTICE_SUFFIX);
+
+        return result.toString();
     }
 }
