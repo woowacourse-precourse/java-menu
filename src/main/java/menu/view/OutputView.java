6fb077dfe4dbb93a192dfa7d9886c035b02c2c4c
pilyang <30036534+pilyang@users.noreply.github.com>
2022-12-17 16:08:19 +0900

    feat: 출력을 위한 table builder 구현
    공통되는 출력 형식을 위한 table builder 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,10 +1,27 @@
 package menu.view;
 
+import java.util.List;
+
 public class OutputView {
 
     private static final String PROGRAM_START_INFO_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    private static final String MENU_RECOMMEND_RESULT_INFO_MESSAGE = "메뉴 추천 결과입니다.";
+    private static final String MENU_RECOMMEND_RESULT_INFO_END_MESSAGE = "추천을 완료했습니다.";
+
+    private static final String TABLE_START_SIGN = "[ ";
+    private static final String TABLE_DIVIDE_SIGN = " | ";
+    private static final String TABLE_END_SIGN = " ]";
 
     public void printProgramStartInfo() {
         System.out.println(PROGRAM_START_INFO_MESSAGE);
+    }
+
+
+    private String tableRowBuilder(List<String> tableComponents) {
+        StringBuilder rowBuilder = new StringBuilder(TABLE_START_SIGN);
+        rowBuilder.append(String.join(TABLE_DIVIDE_SIGN, tableComponents));
+        rowBuilder.append(TABLE_END_SIGN);
+
+        return rowBuilder.toString();
     }
 }
