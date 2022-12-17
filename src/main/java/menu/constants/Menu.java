ca632f38de00c86a2ac81a3ea3211757432f55f3
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 14:45:59 +0900

    feat(Menu): 카테고리 번호에 따른 카테고리 정보 가져오기 기능 추가

--- src/main/java/menu/constants/Menu.java
+++ src/main/java/menu/constants/Menu.java
@@ -3,35 +3,49 @@ package menu.constants;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum Menu {
-    JAPANESE_FOOD("일식",Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리",
+    JAPANESE_FOOD(1, "일식",Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리",
             "하이라이스", "라멘", "오코노미야끼")),
-    KOREAN_FOOD("한식", Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수",
+    KOREAN_FOOD(2, "한식", Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수",
             "불고기", "떡볶이", "제육볶음")),
-    CHINESE_FOOD("중식", Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부",
+    CHINESE_FOOD(3, "중식", Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부",
             "탕수육", "토마토 달걀볶음", "고추잡채")),
-    ASIAN_FOOD("아시안", Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수",
+    ASIAN_FOOD(4, "아시안", Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수",
             "똠얌꿍", "반미", "월남쌈", "분짜")),
-    WESTERN_FOOD("양식", Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트",
+    WESTERN_FOOD(5,"양식", Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트",
             "스파게티", "피자", "파니니"));
 
-    private static final List<String> allMenus = Stream.of(JAPANESE_FOOD.menus, KOREAN_FOOD.menus,
+    private static final List<String> ALL_MENU = Stream.of(JAPANESE_FOOD.menus, KOREAN_FOOD.menus,
                     CHINESE_FOOD.menus, ASIAN_FOOD.menus, WESTERN_FOOD.menus)
             .flatMap(Collection::stream)
             .collect(Collectors.toList());
+    private static final Map<Integer, Menu> CATEGORY_BY_NUMBER = Stream.of(values())
+            .collect(Collectors.toMap(Menu::getCategoryNumber, Function.identity()));
 
+    private final int categoryNumber;
     private final String category;
     private final List<String> menus;
 
-    Menu(String category, List<String> menus) {
+    Menu(int categoryNumber, String category, List<String> menus) {
+        this.categoryNumber = categoryNumber;
         this.category = category;
         this.menus = menus;
     }
 
+    private int getCategoryNumber() {
+        return categoryNumber;
+    }
+
     public static boolean isExistMenu(String menu) {
-        return allMenus.contains(menu);
+        return ALL_MENU.contains(menu);
+    }
+
+    public static Menu getCategoryByNumber(int number) {
+        return CATEGORY_BY_NUMBER.get(number);
     }
 }
