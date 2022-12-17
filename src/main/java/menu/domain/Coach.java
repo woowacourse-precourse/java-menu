068d2bd6c9288687e792a4677c80b8758b9dd353
Miseong Kim <miseongk@naver.com>
2022-12-17 14:51:55 +0900

    feat(Coach): 코치 별로 못먹는 메뉴 확인하는 기능 구현

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,6 +1,10 @@
 package menu.domain;
 
+import menu.view.InputView;
+import menu.view.OutputView;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Coach {
@@ -11,5 +15,13 @@ public class Coach {
     public Coach(String name) {
         this.name = name;
         this.hateMenu = new ArrayList<>();
+    }
+
+    public void checkHateMenu() {
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+
+        outputView.printInputCoachHateMenu(this.name);
+        this.hateMenu.addAll(Arrays.asList(inputView.readHateMenu()));
     }
 }
