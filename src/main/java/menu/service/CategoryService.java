9915d65f53e0e6991d40a7aaaf11d398550da881
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 15:02:53 +0900

    feat(CategoryService): 랜덤 카테고리 뽑기 기능 구현

--- src/main/java/menu/service/CategoryService.java
+++ src/main/java/menu/service/CategoryService.java
@@ -1,13 +1,27 @@
 package menu.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import menu.constant.FoodCategory;
+import menu.domain.Category;
+import menu.repository.CategoryRepository;
+import menu.service.constant.CategoryRange;
+
 public class CategoryService {
     private static class InstanceHolder {
         private static final CategoryService INSTANCE = new CategoryService();
     }
 
+    private final CategoryRepository categoryRepository = CategoryRepository.getInstance();
+
     private CategoryService(){}
 
     public static CategoryService getInstance() {
         return CategoryService.InstanceHolder.INSTANCE;
+    }
+
+    public String pickRandomCategory() {
+        return FoodCategory.findFoodByKey(
+                Randoms.pickNumberInList(FoodCategory.findKeys())
+        );
     }
 }
