cdfdb7f167abae53e8d98257c596c3f63ac73a26
java-saeng <gusdn85554@naver.com>
2022-12-17 17:11:13 +0900

    refactor : ConsoleUtil는 유틸리티 클래스로 InputView, OutputView 관리

--- src/main/java/menu/MenuEngine.java
+++ src/main/java/menu/MenuEngine.java
@@ -3,8 +3,7 @@ package menu;
 import menu.maker.CategoryMaker;
 import menu.maker.MenuMaker;
 import menu.model.Category;
-import menu.view.InputView;
-import menu.view.OutputView;
+import menu.view.ConsoleUtil;
 
 import java.util.List;
 import java.util.Map;
@@ -13,23 +12,18 @@ public class MenuEngine {
 
     public void start() {
         printGameStart();
-        InputView inputView = new InputView();
 
-        List<String> coachNames = inputView.readCoachName();
-
-        Map<String, List<String>> exceptedFoods = inputView.readExceptedFood(coachNames);
+        List<String> coachNames = ConsoleUtil.inputCoachName();
+        Map<String, List<String>> exceptedFoods = ConsoleUtil.inputExceptedFood(coachNames);
 
         Category category = new Category();
-
         CategoryMaker categoryMaker = new CategoryMaker();
-
         categoryMaker.addCategory(category);
 
         MenuMaker menuMaker = new MenuMaker();
-
         Map<String, List<String>> menu = menuMaker.addMenu(category, coachNames, exceptedFoods);
 
-        OutputView.printResult(category, menu);
+        ConsoleUtil.outputGameResult(category, menu);
     }
 
     private static void printGameStart() {
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -9,23 +9,16 @@ import java.util.Map;
 
 public class OutputView {
 
-    public static void printResult(final Category category,
-                                   final Map<String, List<String>> menu) {
-        printDay();
-        printCategory(category);
-        printMenu(menu);
-    }
-
-    private static void printDay() {
+    public static void printDay() {
         System.out.println("메뉴 추천 결과입니다.");
         System.out.println(printMenu(Constants.DAY));
     }
 
-    private static void printCategory(Category category) {
+    public static void printCategory(Category category) {
         System.out.println(printMenu(category.getCategories()));
     }
 
-    private static String printMenu(final List<String> element) {
+    public static String printMenu(final List<String> element) {
         StringBuilder stringBuilder = new StringBuilder("[ ");
 
         stringBuilder.append(String.join(" | ", element));
@@ -35,7 +28,7 @@ public class OutputView {
         return stringBuilder.toString();
     }
 
-    private static void printMenu(final Map<String, List<String>> element) {
+    public static void printMenu(final Map<String, List<String>> element) {
         List<String> printing = new ArrayList<>();
 
         for (String key : element.keySet()) {
