1ecb5440c6fa003da1f33f6a27b251a15bcb8c82
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-17 16:29:22 +0900

    feat: 기능의 흐름대로 컨트롤러 작성

--- src/main/java/menu/controller/FoodRecommendController.java
+++ src/main/java/menu/controller/FoodRecommendController.java
@@ -1,2 +1,35 @@
-package menu.controller;public class FoodRecommendController {
+package menu.controller;
+
+import menu.domain.Coach;
+import menu.domain.FoodPeerCoaches;
+import menu.domain.FoodRecommendMachine;
+import menu.domain.Menu;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+import java.util.List;
+
+public class FoodRecommendController {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+    public void run() {
+        outputView.printServiceIntroducingMessage();
+        FoodRecommendMachine machine = new FoodRecommendMachine(readCoachInformation());
+        machine.makeRecommend();
+        outputView.printResultMessage(machine.getFoodRecommends());
+    }
+
+    private FoodPeerCoaches readCoachInformation() {
+        FoodPeerCoaches foodPeerCoaches = inputView.readCoachPeers();
+        for (Coach coach : foodPeerCoaches.getCoaches()) {
+            enrollHateFoodOfCoach(coach);
+        }
+        return foodPeerCoaches;
+    }
+
+    private void enrollHateFoodOfCoach(Coach coach) {
+        List<Menu> menus = inputView.readHateFoodOf(coach);
+        coach.enrollHateFood(menus);
+    }
 }
