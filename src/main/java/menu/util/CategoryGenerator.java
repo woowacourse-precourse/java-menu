d95c46e04641544215c56189f9bc8e9c05b2a178
sosow0212 <sosow0212@naver.com>
2022-12-17 16:08:11 +0900

    refactor : (CategoryGenerator) 매주 카테고리를 새로 만들어주게끔 리팩토링 진행

--- src/main/java/menu/util/CategoryGenerator.java
+++ src/main/java/menu/util/CategoryGenerator.java
@@ -5,21 +5,19 @@ import menu.domain.Categories;
 import menu.domain.menu.FoodStyle;
 
 public class CategoryGenerator {
-    private Categories categories = new Categories();
 
-    public Categories generateCategories() {
-        categories = new Categories();
-        makeCategory();
-        return categories;
+    public void generateCategories(Categories categories) {
+        makeCategory(categories);
     }
 
-    public void makeCategory() {
-        while (!categories.isDone()) {
+    public void makeCategory(Categories categories) {
+        while (true) {
             FoodStyle foodStyle = FoodStyle.getFoodStyle(Randoms.pickNumberInRange(1, 5));
             if (categories.isFullOfCategory(foodStyle)) {
                 continue;
             }
             categories.addCategory(foodStyle.getStyle());
+            break;
         }
     }
 }
