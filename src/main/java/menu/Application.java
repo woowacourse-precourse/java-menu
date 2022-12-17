a0ec5385a750c7b816b9339f5ba31e480aa6c93f
zeunala <zeunala@naver.com>
2022-12-17 15:06:53 +0900

    feat: 각 고치별 못 먹는 메뉴 입력 기능 추가

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -7,5 +7,11 @@ public class Application {
         System.out.println("점심 메뉴 추천을 시작합니다.\n");
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
         String coachNames = Console.readLine();
+        System.out.println();
+        for (String coachName : coachNames.split(",")) {
+            System.out.printf("%s(이)가 못 먹는 메뉴를 입력해 주세요.\n", coachName);
+            String bannedMenus = Console.readLine().strip();
+            System.out.println();
+        }
     }
 }
