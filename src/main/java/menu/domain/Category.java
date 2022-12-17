500f3b1b1866c75ca81b2c54cc8f301a886d8a27
sonsoowon <yuwoo1018@gmail.com>
2022-12-17 14:34:53 +0900

    feat(Category): 카테고리 클래스 추가

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,4 +1,32 @@
 package menu.domain;
 
+import java.util.Arrays;
+
 public enum Category {
+    JAPAN("일식", 0),
+    KOREA("한식", 1),
+    CHINA("중식", 2),
+    ASIAN("아시안", 3),
+    WESTERN("양식", 4);
+
+
+    private static final String INVALID_INDEX = "인덱스는 0부터 4사이의 숫자여야 합니다.";
+    private final String name;
+    private final int index;
+
+    Category(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+
+    public static Category getCategory(int index) {
+        return Arrays.stream(Category.values())
+                .filter(category -> category.index == index)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_INDEX));
+    }
+
+    public String getName() {
+        return name;
+    }
 }
