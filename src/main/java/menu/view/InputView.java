311bf33d784496353c298de3f7dea371a919754a
woosung1223 <dntjd991223@naver.com>
2022-12-17 17:27:25 +0900

    refactor: 메세지 출력 시 앞에 개행문자가 필요한 경우 삽입

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -5,8 +5,9 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class InputView {
-    private final String READ_NAME_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
-    private final String READ_MENU_CANT_EAT_MESSAGE = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
+    private final String READ_NAME_MESSAGE = System.lineSeparator() + "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private final String READ_MENU_CANT_EAT_MESSAGE =
+            System.lineSeparator() + "%s(이)가 못 먹는 메뉴를 입력해 주세요." + System.lineSeparator();
     private final String DELIMITER = ",";
 
     public List<String> readNames() {
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -5,7 +5,7 @@ import menu.domain.MenuDTO;
 import java.util.stream.Collectors;
 
 public class OutputView {
-    private final String RESULT_MESSAGE = "메뉴 추천 결과입니다.";
+    private final String RESULT_MESSAGE = System.lineSeparator() + "메뉴 추천 결과입니다.";
     private final String WEEK_EXPRESSION = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]";
     private final String SYSTEM_OVER_MESSAGE = System.lineSeparator() + "추천을 완료했습니다.";
 
