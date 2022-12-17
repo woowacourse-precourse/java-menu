a1ce3ca7b3798ce1a799b211754e7e185b6a01e4
WhalesBob <ndw8200@naver.com>
2022-12-17 13:56:20 +0900

    feat : 코치님들의 리스트를 입력받는 기능, 관련 입력 점검기능 추가

--- src/main/java/menu/views/UserInput.java
+++ src/main/java/menu/views/UserInput.java
@@ -1,4 +1,21 @@
 package menu.views;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class UserInput {
+    private final Validator validator = new Validator();
+
+    public List<String> requestCoachName(){
+        String[] userInput;
+        boolean isRightInput;
+        do{
+            userInput = Console.readLine().split(",");
+            isRightInput = validator.validateCoachList(userInput);
+        }while(!isRightInput);
+
+        return Arrays.asList(userInput);
+    }
 }
--- src/main/java/menu/views/Validator.java
+++ src/main/java/menu/views/Validator.java
@@ -1,4 +1,26 @@
 package menu.views;
 
 public class Validator {
+
+    public boolean validateCoachList(String[] userInput){
+        boolean isAllRight = true;
+        try{
+            validateCoachNumber(userInput);
+        }catch(IllegalArgumentException ex){
+            isAllRight = false;
+        }
+        return isAllRight;
+    }
+
+    private void validateCoachNumber(String[] coaches){
+        if(!(2 <= coaches.length && coaches.length <= 5)){
+            String errorMessage = "[ERROR] 입력해 주시는 코치님들의 숫자는 2~5명 사이로 입력해 주십시오.";
+            errorProcess(errorMessage);
+        }
+    }
+
+    private void errorProcess(String errorMessage){
+
+        throw new IllegalArgumentException();
+    }
 }
