6b32ded739eecef082cb986111d0dd2fb59ad977
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 14:19:05 +0900

    feat(InputView): 못 먹는 메뉴 입력하기 기능 추가
    docs: 못 먹는 메뉴 입력하기 완료 체크

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -7,9 +7,15 @@ import java.util.List;
 
 public class InputView {
     private static final String INPUT_COACHES_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private static final String INPUT_HATE_MENU_FORMAT = "\n%s(이)가 못 먹는 메뉴를 입력해 주세요.\n";
 
     public List<String> readCoachesName() {
         System.out.println(INPUT_COACHES_NAME);
         return InputValidation.validateCoachesName(Console.readLine());
+    }
+
+    public String readCoachHateMenu(String name) {
+        System.out.printf(INPUT_HATE_MENU_FORMAT, name);
+        return Console.readLine();
     }
 }
