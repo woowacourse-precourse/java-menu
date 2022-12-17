5230d7caf3e92ff864b7c91fc245348b93fd9311
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 16:04:01 +0900

    feat(CategoryService): 특정 카테고리에서 음식을 뽑는 기능 구현

--- src/main/java/menu/service/CategoryService.java
+++ src/main/java/menu/service/CategoryService.java
@@ -2,12 +2,15 @@ package menu.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import menu.constant.FoodCategory;
+import menu.domain.Category;
 import menu.repository.CategoryRepository;
 
 public class CategoryService {
     private static class InstanceHolder {
         private static final CategoryService INSTANCE = new CategoryService();
     }
+
+    private final int FIRST_ELEMENT = 0;
 
     private final CategoryRepository categoryRepository = CategoryRepository.getInstance();
 
@@ -21,5 +24,10 @@ public class CategoryService {
         return FoodCategory.findFoodByKey(
                 Randoms.pickNumberInList(FoodCategory.findKeys())
         );
+    }
+
+    public String pickRandomFoodInCategory(String categoryName) {
+        Category category = categoryRepository.findCategoryByName(categoryName);
+        return Randoms.shuffle(category.getAllFoods()).get(FIRST_ELEMENT);
     }
 }
