d2a5c4a0b9f7deacc4976efb73b590514c2e589f
Ridealist <cjsn92@gmail.com>
2022-12-17 17:49:36 +0900

    feat: 코치 이름 입력 유효성 검사 로직 추가 구현

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -6,16 +6,28 @@ import java.util.List;
 import menu.repository.FoodRepository;
 
 public class Coach {
-    private final String name;
-    private List<Food> cannotEatFoods = new ArrayList<>();
 
-    public Coach(String name) {
+    private static final int NAME_LENGTH_LOWER_BOUND = 2;
+    private static final int NAME_LENGTH_UPPER_BOUND = 4;
+    private static final String INVALID_NAME_MESSAGE = "코치 이름은 2글자 이상 4글자 이하여야 합니다. 다시 입력해주세요.";
+
+    private final String name;
+    private final List<Food> cannotEatFoods = new ArrayList<>();
+
+    public Coach(String name) throws IllegalArgumentException {
+        validateNameLength(name);
         this.name = name;
     }
 
     public void updateFoods(List<String> foods) {
         for (String name : foods) {
             cannotEatFoods.add(FoodRepository.getByName(name));
+        }
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() < NAME_LENGTH_LOWER_BOUND || name.length() > NAME_LENGTH_UPPER_BOUND) {
+            throw new IllegalArgumentException(INVALID_NAME_MESSAGE);
         }
     }
 
