0e431d61dec0eaca415020a401257a52f68b61b8
apptie <appti@naver.com>
2022-12-17 16:21:13 +0900

    refactor(CoachOutputView): CoachOutputView 싱글톤 패턴 변수명 수정

--- src/main/java/menu/view/coach/CoachOutputView.java
+++ src/main/java/menu/view/coach/CoachOutputView.java
@@ -8,10 +8,10 @@ public class CoachOutputView extends DefaultOutputView {
     }
 
     private static class CoachOutputViewSingletonHelper {
-        private static final CoachOutputView SINGLETON = new CoachOutputView();
+        private static final CoachOutputView COACH_OUTPUT_VIEW = new CoachOutputView();
     }
 
     public static CoachOutputView getInstance() {
-        return CoachOutputViewSingletonHelper.SINGLETON;
+        return CoachOutputViewSingletonHelper.COACH_OUTPUT_VIEW;
     }
 }
