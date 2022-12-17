cf576f8876782bdf06d197a742ae82fb57853ac0
thdwoqor <celebarte01@gmail.com>
2022-12-17 15:35:36 +0900

    feat: 예외 처리를 위한 `Validate` 함수 추가

---
+++ src/main/java/menu/util/Validate.java
@@ -0,0 +1,31 @@
+package menu.util;
+
+import java.util.List;
+
+public class Validate {
+    public static void nameLength(List<String> names, int minInclusive, int maxInclusive) {
+        names.stream().forEach(name -> {
+                    if (name.length() < minInclusive || name.length() > maxInclusive) {
+                        throw new IllegalArgumentException(Message.ERROR_DIFFERENT_NAME_LENGTHS.message);
+                    }
+                }
+        );
+    }
+
+    public static void coachLength(List<String> names, int minInclusive, int maxInclusive) {
+        if (names.size() < minInclusive || names.size() > maxInclusive) {
+            throw new IllegalArgumentException(Message.ERROR_DIFFERENT_COACH_LENGTHS.message);
+        }
+    }
+
+    private enum Message {
+        ERROR_DIFFERENT_NAME_LENGTHS("[ERROR] 코치의 이름은 최소 2글자, 최대 4글자이다."),
+        ERROR_DIFFERENT_COACH_LENGTHS("[ERROR] 코치는 최소 2명, 최대 5명까지 식사를 함께 한다."),
+
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+        }
+}
