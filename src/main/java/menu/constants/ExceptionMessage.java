3404c8b7371f94bd9bbac526d4a44f7b98c5bbdc
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 14:36:23 +0900

    feat(ExceptionMessage): 예외 메시지 상수 추가
    - 메뉴가 존재하지 않는 경우
    - 메뉴 개수가 조건에 맞지 않는 경우에 따른 예외 메시지

--- src/main/java/menu/constants/ExceptionMessage.java
+++ src/main/java/menu/constants/ExceptionMessage.java
@@ -5,7 +5,9 @@ public enum ExceptionMessage {
     NOT_INPUT_NAMES_LESS_THAN_TWO("코치는 최소 2명 이상 입력해야 합니다."),
     NOT_INPUT_NAMES_OVER_FIVE("코치는 최대 5명 이하 입력해야 합니다."),
     NAME_LESS_THAN_TWO("코치 이름은 최소 2자 이상으로 입력해야 합니다."),
-    NAME_MORE_THAN_FOUR("코치 이름은 최대 4자 이하 입력해야 합니다.");
+    NAME_MORE_THAN_FOUR("코치 이름은 최대 4자 이하 입력해야 합니다."),
+    NOT_EXIST_MENU("존재하지 않는 메뉴입니다."),
+    MENU_MORE_THAN_TWO("메뉴는 최대 2개 이하 입력해야 합니다.");
 
     private static final String ERROR_TAG = "[ERROR] ";
     private final String exceptionMessage;
