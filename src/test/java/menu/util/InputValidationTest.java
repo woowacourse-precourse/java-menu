6f8947cc5ce1de167896fc8f0c2692d827804309
jiwon <ljwon77@daum.net>
2022-12-17 17:55:28 +0900

    test: 정상적인 코치 이름과 수, 음식에 대해 아무 일도 발생하지 않는지 테스트 추가

--- src/test/java/menu/util/InputValidationTest.java
+++ src/test/java/menu/util/InputValidationTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static menu.constant.ExceptionMessage.*;
 import static menu.domain.DomainCondition.*;
-import static menu.domain.DomainCondition.NAX_COACH_NAME_LENGTH;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 class InputValidationTest {
 
@@ -47,4 +47,25 @@ class InputValidationTest {
                 .hasMessageContaining(String.format(WRONG_COACH_FOOD_COUNT.getValue(), MIN_FOOD_COUNT.getValue(), MAX_FOOD_COUNT.getValue()));
     }
 
+    @DisplayName("정상적인 코치 이름과 수가 들어왔을 때는 아무 일도 발생하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"포비, 아아, 으으으"} )
+    void 코치_정상_검증_테스트(String userInput) throws Exception {
+        InputValidation inputValidation = new InputValidation();
+
+        assertThatCode(() ->
+                inputValidation.validateCoach(userInput))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("정상적인 음식 이름이 들어왔을 때는 아무 일도 발생하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "우동, 스시"} )
+    void 음식_이름_정상_검증_테스트 (String userInput) throws Exception {
+        InputValidation inputValidation = new InputValidation();
+
+        assertThatCode(() ->
+                inputValidation.validateNotEatFood(userInput))
+                .doesNotThrowAnyException();
+    }
 }
