684e30f960849ea39591cb634bd2fd967ae60720
Cyma-s <ssol0319@naver.com>
2022-12-17 13:42:08 +0900

    feat(Menu) : 메뉴 이름 저장하는 클래스 추가

---
+++ src/main/java/menu/domain/Menu.java
@@ -0,0 +1,13 @@
+package menu.domain;
+
+public class Menu {
+    private final String menuName;
+
+    Menu(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+}
