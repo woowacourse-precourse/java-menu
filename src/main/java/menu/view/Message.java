2d1343e2c6edfd9daaa7e6842a39ccf9f94706af
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-12-17 13:54:39 +0900

    feat : 못먹는 음식 입력시 출력 기능 추가

--- src/main/java/menu/view/Message.java
+++ src/main/java/menu/view/Message.java
@@ -2,7 +2,8 @@ package menu.view;
 
 public enum Message {
 	SERVICE_START("점심 메뉴 추천을 시작합니다."),
-	INPUT_COACH_NAMES("코치의 이름을 입력해 주세요. (, 로 구분)");
+	INPUT_COACH_NAMES("코치의 이름을 입력해 주세요. (, 로 구분)"),
+	INPUT_NOT_EAT_FOOD("(이)가 못 먹는 메뉴를 입력해 주세요.");
 
 	private final String message;
 
@@ -13,4 +14,9 @@ public enum Message {
 	public static void printMessage(Message message) {
 		System.out.println(message.message);
 	}
+
+	public static void printNotEatFoodMessage(String name) {
+		System.out.println(name + INPUT_NOT_EAT_FOOD);
+	}
+
 }
