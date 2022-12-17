ecf4ab002608205820081263067fb107cfcf5c5f
dev_kong <ahdjd5@gmail.com>
2022-12-17 13:44:04 +0900

    feat(utils): add Converter class

---
+++ src/main/java/menu/utills/Converter.java
@@ -0,0 +1,10 @@
+package menu.utills;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Converter {
+    public static List<String> toListByDelimiter(String input) {
+        return Arrays.asList(input.split(","));
+    }
+}
