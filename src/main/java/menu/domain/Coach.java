90d07c90cdff83d37180f0f57fcbdc8d5782127a
jeomxon <jeomxon@gmail.com>
2022-12-17 17:49:04 +0900

    refactor: 하드코딩된 에러 메시지 상수화

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -12,6 +12,11 @@ public class Coach {
     private static final int MINIMUM_LENGTH_OF_NAME = 2;
     private static final int MAXIMUM_LENGTH_OF_NAME = 4;
     private static final int MAXIMUM_LENGTH_OF_CAN_NOT_EAT = 2;
+    private static final int MINIMUM_NUMBER_OF_COACH = 2;
+    private static final int MAXIMUM_NUMBER_OF_COACH = 5;
+    private static final String NUMBER_OF_COACH_ERROR_MESSAGE = "[ERROR] 코치는 최소 2명, 최대 5명까지 입력이 가능합니다.";
+    private static final String LENGTH_OF_COACH_NAME_ERROR_MESSAGE = "[ERROR] 2~4자리의 이름만 입력이 가능합니다.";
+    private static final String CAN_NOT_EAT_FOOD_ERROR_MESSAGE = "[ERROR] 각 코치가 먹지 못하는 메뉴는 최소 0개에서 최대 2개입니다.";
 
     private final Map<String, List<String>> coaches;
     private final Map<String, List<String>> recommendFoods;
@@ -74,20 +79,20 @@ public class Coach {
     }
 
     private void validateNumberOfCoach(String[] names) {
-        if (names.length < 2 || names.length > 5) {
-            throw new IllegalArgumentException("[ERROR] 코치는 최소 2명, 최대 5명까지 입력이 가능합니다.");
+        if (names.length < MINIMUM_NUMBER_OF_COACH || names.length > MAXIMUM_NUMBER_OF_COACH) {
+            throw new IllegalArgumentException(NUMBER_OF_COACH_ERROR_MESSAGE);
         }
     }
 
     private void validateName(String name) {
         if (name.length() < MINIMUM_LENGTH_OF_NAME || name.length() > MAXIMUM_LENGTH_OF_NAME) {
-            throw new IllegalArgumentException("[ERROR] 2~4자리의 이름만 입력이 가능합니다.");
+            throw new IllegalArgumentException(LENGTH_OF_COACH_NAME_ERROR_MESSAGE);
         }
     }
 
     private void validateCanNotEatFood(List<String> canNotEatFoods) {
         if (canNotEatFoods.size() > MAXIMUM_LENGTH_OF_CAN_NOT_EAT) {
-            throw new IllegalArgumentException("[ERROR] 각 코치가 먹지 못하는 메뉴는 최소 0개에서 최대 2개입니다.");
+            throw new IllegalArgumentException(CAN_NOT_EAT_FOOD_ERROR_MESSAGE);
         }
     }
 }
