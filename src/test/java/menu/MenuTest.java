348f5ff91e47ca48979956b5bc07195e1a2bac57
minaver <psmjoshua@gmail.com>
2022-12-17 16:54:53 +0900

    test(Menu): 중복되지_않는_메뉴_추천_테스트 메뉴 종류 변경

--- src/test/java/menu/MenuTest.java
+++ src/test/java/menu/MenuTest.java
@@ -18,12 +18,13 @@ import static org.assertj.core.util.Lists.newArrayList;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class MenuTest {
+
     @DisplayName("각 코치에게 한 주에 중복되지 않는 '메뉴' 를 추천하는지 확인")
     @Test
-    void 중복되지_않는_메뉴_추천() {
+    void 중복되지_않는_메뉴_추천_테스트() {
         // given
         List<String> korFood = Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음");
-        Menu menu = new Menu(FoodCategory.KOR);
+        Menu menu;
 
         Coach coach1 = new Coach("상민",Arrays.asList("김밥", "김치찌개"));
         Coach coach2 = new Coach("상훈",Arrays.asList("김밥", "김치찌개"));
@@ -34,6 +35,7 @@ public class MenuTest {
         // when
         List<CoachWeekMenu> coachWeekMenus = new ArrayList<>();
         for (int i = 0 ; i < Constant.weekNum; i++) {
+            menu = new Menu(FoodCategory.KOR);
             coachWeekMenus = menu.chooseShuffleMenu(Arrays.asList(coachWeekMenu1, coachWeekMenu2));
         }
         CoachWeekMenu coachWeekMenu = coachWeekMenus.get(0);
@@ -42,6 +44,6 @@ public class MenuTest {
 
         // then
         assertThat(coachWeekMenu.getEatMenus().size()).isEqualTo(coachWeekMenuRemoveOverlap.size());
-
     }
+
 }
