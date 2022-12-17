6d4c38d6ef55e0e1e9b5c7eef1308b21ab3b1295
greeng00se <thegreengoose7@gmail.com>
2022-12-17 14:23:03 +0900

    feat(OutputView): 출력 수정

--- src/main/java/menu/dto/MenusDto.java
+++ src/main/java/menu/dto/MenusDto.java
@@ -1,13 +1,20 @@
 package menu.dto;
 
 import java.util.List;
+import menu.domain.Category;
 
 public class MenusDto {
 
+    private final List<Category> categories;
     private final List<MenuDto> menus;
 
-    public MenusDto(List<MenuDto> menus) {
+    public MenusDto(List<Category> categories, List<MenuDto> menus) {
+        this.categories = categories;
         this.menus = menus;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
     }
 
     public List<MenuDto> getMenus() {
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -2,8 +2,10 @@ package menu.view;
 
 import static java.text.MessageFormat.format;
 
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.stream.Collectors;
+import menu.domain.Category;
 import menu.dto.MenuDto;
 import menu.dto.MenusDto;
 
@@ -11,8 +13,8 @@ public class OutputView {
 
     private static final String PRINT_START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
     private static final String EXCEPTION_MESSAGE = "[ERROR] ";
-    private static final String MENU_MESSAGE = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]\n"
-            + "[ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]";
+    private static final String MENU_MESSAGE = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]";
+    private static final String CATEGORY_MESSAGE_FORMAT = "[ 카테고리 | {0} ]";
     private static final String MENU_MESSAGE_FORMAT = "[ {0} | {1} ]";
     private static final String MENU_MESSAGE_DELIMITER = " | ";
     private static final String NEXT_LINE = "\n";
@@ -24,7 +26,15 @@ public class OutputView {
 
     public void printMenuMessage(MenusDto menusDto) {
         System.out.println(MENU_MESSAGE);
+        System.out.println(generateCategoryMessage(menusDto.getCategories()));
         System.out.println(generateMenusMessage(menusDto.getMenus()));
+    }
+
+    private String generateCategoryMessage(List<Category> categories) {
+        String categoryMessage = categories.stream()
+                .map(Category::getName)
+                .collect(Collectors.joining(MENU_MESSAGE_DELIMITER));
+        return MessageFormat.format(CATEGORY_MESSAGE_FORMAT, categoryMessage);
     }
 
     private String generateMenusMessage(List<MenuDto> menus) {
