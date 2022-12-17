a1ab9490d486f8cc2f501b278dd254d6d88479f5
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-12-17 15:53:43 +0900

    feat(InputView): validate 메서드 생성

--- src/main/java/menu/input/InputView.java
+++ src/main/java/menu/input/InputView.java
@@ -1,4 +1,13 @@
 package menu.input;
 
+import menu.type.ValidationType;
+
 public class InputView {
+
+    public void validate(ValidationType validationType, String input) {
+        if (!ValidationType.validate(validationType, input)) {
+            String errorMessage = validationType.getErrorMessage();
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
 }
