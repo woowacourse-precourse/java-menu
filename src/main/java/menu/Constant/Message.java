b2ed64ddb094c13a0250e588fc27d3aeb5b35c6c
LJW25 <dbjwl@g.skku.edu>
2022-12-17 15:01:15 +0900

    fix: message 수정

--- src/main/java/menu/Constant/Message.java
+++ src/main/java/menu/Constant/Message.java
@@ -4,12 +4,12 @@ public class Message {
 
     public static final String MSG_START = "점심 메뉴 추천을 시작합니다.";
     public static final String MSG_GET_NAMES = "코치의 이름을 입력해 주세요. (, 로 구분)";
-    public static final String MSG_GET_HATEMENUS = "가 못 먹는 메뉴를 입력해 주세요.";
+    public static final String MSG_GET_HATEMENUS = "(이)가 못 먹는 메뉴를 입력해 주세요.";
     public static final String MSG_PRINT_RESULT = "메뉴 추천 결과입니다.";
     public static final String MSG_END = "추천을 완료했습니다.";
 
     public static final String ERROR_COACH_NUM = "[ERROR] 코치는 최소 2명, 최대 5명이어야 합니다. 다시 입력해 주세요.";
-    public static final String ERROR_COACH_NAME = "[ERROR] 코치의 이름은 최소 2글자, 최대 4글자여야 합니다. 다시 입력해 주세요.";
+    public static final String ERROR_COACH_LENGTH = "[ERROR] 코치의 이름은 최소 2글자, 최대 4글자여야 합니다. 다시 입력해 주세요.";
     public static final String ERROR_HATE_NUM = "[ERROR] 못 먹는 메뉴는 최소 0개, 최대 2개여야 합니다. 다시 입력해 주세요.";
 
 }
