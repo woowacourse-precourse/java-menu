4711159ef6ac74d8dfd1a9085c31cad0e88e48aa
seokjin8678 <rnrn8678@naver.com>
2022-12-17 13:54:12 +0900

    feat: 초기 메뉴들 추가하는 기능 추가

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,9 +1,25 @@
 package menu.domain;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public enum Category {
-    JAPANESE,
-    KOREAN,
-    CHINESE,
-    WESTERN,
-    ASIAN
+    JAPANESE("일식"),
+    KOREAN("한식"),
+    CHINESE("중식"),
+    WESTERN("양식"),
+    ASIAN("아시안");
+
+    private final String name;
+
+    Category(String name) {
+        this.name = name;
+    }
+
+    public static Category of(String name) {
+        return Arrays.stream(Category.values())
+                .filter(category -> Objects.equals(category.name, name))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당 카테고리를 찾을 수 없습니다."));
+    }
 }
