03088057b9dd0f6a455cf581e08d5bdf454c5fc4
2014012606 <2014012606@hanyang.ac.kr>
2022-12-17 13:49:48 +0900

    카테고리를 추천하는 메소드 추가

--- src/main/java/menu/model/CategoryRecommender.java
+++ src/main/java/menu/model/CategoryRecommender.java
@@ -1,4 +1,24 @@
 package menu.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class CategoryRecommender {
+    private List<Category> recommendedCategories = new ArrayList<>();
+
+    public Category recommendCategory() {
+        List<Category> categories = new ArrayList<>(List.of(Category.values()));
+        Category category;
+        while (true) {
+            category = categories.get(Randoms.pickNumberInRange(1,5));
+            if (Collections.frequency(recommendedCategories, category) > 2) {
+                recommendedCategories.add(category);
+                break;
+            }
+        }
+        return category;
+    }
 }
