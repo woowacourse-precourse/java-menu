1c01aa61579d425328acda213c16fd255c9365c8
ingpyo <juninpyo72@gmail.com>
2022-12-17 14:28:59 +0900

    feat:카테고리 사용횟수 저장소 셋팅

--- src/main/java/menu/Service.java
+++ src/main/java/menu/Service.java
@@ -1,17 +1,33 @@
 package menu;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Service {
+
 	Category category = new Category();
 	Menu menu = new Menu();
 	TotelMenu totelMenu = new TotelMenu();
+	List<String> byDayOfTheWeekSelectCategoryRepository = new ArrayList<>();
+	Map<String, Integer> countCotegory = new HashMap<>();
+
+
 
 	public void menuMapping() {
 		category.settingCategorys();
 		menu.settingMenu();
-		for (int index = 0; index < category.getCategoriesCount(); index++)
+		for (int index = 0; index < category.getCategoriesCount(); index++) {
 			totelMenu.settingTotelMenus(category.getCategory(index), menu.getMenus(index));
+		}
 	}
 
+	public void setCountCotegory() {
+		for (int index = 0; index < category.getCategoriesCount(); index++) {
+			countCotegory.put(category.getCategory(index), 0);
+		}
+	}
 }
