d490ab8bb444e73fa41130eff8a98b2c6760ca85
JunHyeon <dlwnsgus07@naver.com>
2022-12-17 15:10:18 +0900

    feat : 최종 추천 결과를 출력하는 기능 추가

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,52 @@
+package menu.view;
+
+import menu.domain.Category;
+import menu.domain.Coach;
+import menu.domain.Week;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OutputView {
+    static final String RECOMMEND_START = "점심 메뉴 추천을 시작합니다.\n";
+    static final String PRINT_MENU = "메뉴 추천 결과입니다.\n";
+    static final String FINISH = "추천을 완료했습니다.\n";
+    public static void recommendStart(){
+        System.out.println(RECOMMEND_START);
+    }
+
+    public static void printResult(List<Coach> coaches){
+        System.out.println(PRINT_MENU);
+        printWeek();
+        printCategory();
+        printCoachRecommendMenu(coaches);
+    }
+
+    private static void printCoachRecommendMenu(List<Coach> coaches) {
+        for(Coach coach : coaches){
+            List<String> eatenMenu = coach.getEatenMenu();
+            eatenMenu.add(0, coach.getName());
+            String menu = eatenMenu.stream().collect(Collectors.joining(" | ", "[ ", " ]"));
+            System.out.println(menu);
+        }
+    }
+
+    private static void printCategory() {
+        String category = Arrays.stream(Category.values())
+                .map(Category::getName)
+                .collect(Collectors.joining(" | ", "[ ", " ]"));
+        System.out.println(category);
+    }
+
+    private static void printWeek() {
+        String week = Arrays.stream(Week.values())
+                .map(Week::getDay)
+                .collect(Collectors.joining(" | ", "[ ", " ]"));
+        System.out.println(week);
+    }
+
+    public static void quit(){
+        System.out.println(FINISH);
+    }
+}
