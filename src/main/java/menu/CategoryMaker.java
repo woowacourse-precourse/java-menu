b608a6a068d9846bdf6ddb486c2df44f5d1dd9bc
신채원 <cw121@naver.com>
2022-12-17 14:43:51 +0900

    feat: 카테고리 저장 변수 수정

--- src/main/java/menu/CategoryMaker.java
+++ src/main/java/menu/CategoryMaker.java
@@ -11,30 +11,22 @@ public class CategoryMaker {
         this.categoryGenerator = categoryGenerator;
     }
 
-    public List<String> makeCategories() {
-        List<String> randomCategory = new ArrayList<>();
+    public List<Integer> makeCategories() {
+        List<Integer> randomCategory = new ArrayList<>();
 
         while(randomCategory.size()<5){
-            String category = findCategory(categoryGenerator.generate());
-            System.out.println(isDuplicate(randomCategory,category));
+            int category = categoryGenerator.generate();
             if(!isDuplicate(randomCategory,category)){
-
                 randomCategory.add(category);
             }
         }
         return randomCategory;
     }
 
-    private String findCategory(int randomNumber) {
-        Categories categories = Categories.of(randomNumber);
-        String category = categories.getCategory();
-        return category;
-    }
-
-    public boolean isDuplicate(List<String> randomCategory,String category){
+    public boolean isDuplicate(List<Integer> randomCategory,int category){
         int count=0;
-        for(String random : randomCategory){
-            if(random.equals(category)){
+        for(int random : randomCategory){
+            if(random==category){
                 count++;
             }
         }
