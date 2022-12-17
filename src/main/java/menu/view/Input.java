491207a5eda0c9393cd47a2ecf901dd8e80f5613
JeonghoPark <pjhpjh0805@naver.com>
2022-12-17 17:12:45 +0900

    refactor: input 클래스 하드코딩 상수로 대체

--- src/main/java/menu/view/Input.java
+++ src/main/java/menu/view/Input.java
@@ -9,25 +9,29 @@ import java.util.List;
 
 public class Input {
 
+    private static final String COMMA = ",";
+
+    private static final String BLANK = "";
+
     public List<String> readCoachNames() {
         String names = Console.readLine();
         Validator.checkCoachNames(names);
         System.out.println();
-        return Arrays.asList(names.split(","));
+        return Arrays.asList(names.split(COMMA));
     }
 
     public List<String> readHatingMenus() {
         String menus = Console.readLine();
         System.out.println();
-        if (menus.equals("")) {
+        if (menus.equals(BLANK)) {
             return new ArrayList<>();
         }
-        if (!menus.contains(",")) {
+        if (!menus.contains(COMMA)) {
             Validator.checkExistMenu(menus);
             return getAloneMenuList(menus);
         }
         Validator.checkHatingMenus(menus);
-        return Arrays.asList(menus.split(","));
+        return Arrays.asList(menus.split(COMMA));
     }
 
     private List<String> getAloneMenuList(String menus) {
