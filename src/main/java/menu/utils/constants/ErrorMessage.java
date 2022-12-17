d8e985f4d9fab9e2dc20a9e1bd6c716dd7fc34ac
wnsgml7267 <dlwns7267@naver.com>
2022-12-17 17:46:27 +0900

    feat(ErrorMessage) :ErrorMessage클래스 생성

--- src/main/java/menu/utils/constants/ErrorMessage.java
+++ src/main/java/menu/utils/constants/ErrorMessage.java
@@ -1,4 +1,18 @@
-package menu.utils.constants;
+package menu.utills.constants;
 
-public class ErrorMessage {
+public enum ErrorMessage {
+    OUT_OF_RANGE("%d이상 %d이하의 갯수만 입력 가능합니다."),
+    LENGTH_ERROR("%d이상, %d이하 글자수의 이름만 입력가능 합니다."),
+    DUPLICATED("중복되지 않은 값만 입력가능 합니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
 }
