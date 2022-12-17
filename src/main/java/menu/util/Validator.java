dd919586adbcdf1aa9f34ce7c63a053241bdab68
bjk1649 <qhwns1649@naver.com>
2022-12-17 16:28:04 +0900

    refactor: 검증을 위한 숫자들 상수로 변경

--- src/main/java/menu/util/Validator.java
+++ src/main/java/menu/util/Validator.java
@@ -6,13 +6,17 @@ import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Validator {
-    private static final String COACH_NAME_ERROR_MESSAGE = "[ERROR] 코치의 이름은 ,을 기준으로 한글 2~4글자이며, 2~5명을 입력하여야합니다. ";
-    private static final String MENU_ERROR_MESSAGE = "[ERROR] 메뉴 이름은 0~2개만 입력 가능합니다.";
+    private static final String COACH_NAME_ERROR_MESSAGE = "[ERROR] 코치의 이름은 한글 2~4글자이며 ','을 기준으로 2~5명을 입력하여야합니다. ";
+    private static final String MENU_ERROR_MESSAGE = "[ERROR] 메뉴 이름은 0~2개만 입력이 가능합니다.";
     private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 입력의 중복은 불가능합니다.";
+    private static final String NAME_INPUT_REGEX = "^[가-힣]{2,4}$";
+    private static final int MINIMUM_COACH_NUMBER = 2;
+    private static final int MAXIMUM_COACH_NUMBER = 5;
+    private static final int MAXIMUM_CANNOT_EATS_NUMBER = 2;
 
     public static void validateCoachNames(String input, String command) throws IllegalArgumentException {
         List<String> names = StringParser.makeListByCommand(input, command);
-        if (names.size() < 2 || names.size() > 5) {
+        if (names.size() < MINIMUM_COACH_NUMBER || names.size() > MAXIMUM_COACH_NUMBER) {
             throw new IllegalArgumentException(COACH_NAME_ERROR_MESSAGE);
         }
         checkDuplicate(names);
@@ -21,15 +25,14 @@ public class Validator {
 
     public static void validateMenuNum(String input, String command) throws IllegalArgumentException {
         List<String> menus = StringParser.makeListByCommand(input, command);
-        if (menus.size() > 2) {
+        if (menus.size() > MAXIMUM_CANNOT_EATS_NUMBER) {
             throw new IllegalArgumentException(MENU_ERROR_MESSAGE);
         }
         checkDuplicate(menus);
     }
 
     private static void validateName(String name) {
-        String condition = "^[가-힣]{2,4}$";
-        if (!Pattern.matches(condition, name)) {
+        if (!Pattern.matches(NAME_INPUT_REGEX, name)) {
             throw new IllegalArgumentException(COACH_NAME_ERROR_MESSAGE);
         }
     }
