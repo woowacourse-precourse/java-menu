2e256169f997ca8d29cb2cf88602d8808620bcb5
JunHyeon <dlwnsgus07@naver.com>
2022-12-17 14:20:38 +0900

    feat : 랜덤으로 카테고리를 선정하는 기능 추가

--- src/main/java/menu/service/MenuService.java
+++ src/main/java/menu/service/MenuService.java
@@ -1,14 +1,31 @@
 package menu.service;
 
+import menu.domain.Category;
 import menu.domain.Coach;
+import menu.generator.CategoryGenerator;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class MenuService {
-    public MenuService(List<Coach> coaches) {
-        this.coaches = coaches;
-    }
-
+    private CategoryGenerator categoryGenerator;
+    private HashMap<Category, Integer> categoryCounter;
     private final List<Coach> coaches;
 
+    public MenuService(List<Coach> coaches) {
+        this.coaches = coaches;
+        categoryGenerator = new CategoryGenerator();
+        for(Category category : Category.values()){
+            categoryCounter.put(category, 0);
+        }
+    }
+    public void recommend(){
+        Category category = getRandomCategory(categoryCounter);
+        Integer count = categoryCounter.get(category);
+        categoryCounter.put(category, count + 1);
+    }
+
+    public Category getRandomCategory(HashMap<Category, Integer> categoryCounter){
+        return categoryGenerator.pick(categoryCounter);
+    }
 }
