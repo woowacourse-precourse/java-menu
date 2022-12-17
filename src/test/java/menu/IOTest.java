bd904b65831f0034b88d20d1a2845e58d7e91507
jjongwa <troas96@naver.com>
2022-12-17 17:03:13 +0900

    test: 추가 테스트 위한 setting

---
+++ src/test/java/menu/IOTest.java
@@ -0,0 +1,9 @@
+package menu;
+
+import java.io.ByteArrayInputStream;
+
+public abstract class IOTest{
+    protected void systemIn(String input){
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+}
