09dda5a091e226a2480b3828c0895c28603d7d5d
CAEL0 <kjongmin26@gmail.com>
2022-12-17 13:32:49 +0900

    refactor(ViewConstants): 변수명 ASKING_INEDIBLE_MENU로 변경

--- src/main/java/menu/view/ViewConstants.java
+++ src/main/java/menu/view/ViewConstants.java
@@ -3,7 +3,7 @@ package menu.view;
 public class ViewConstants {
     public static final String STARTING_RECOMMENDATION = "점심 메뉴 추천을 시작합니다.";
     public static final String ASKING_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
-    public static final String ASKING_MENU = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
+    public static final String ASKING_INEDIBLE_MENU = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
     public static final String RESULT = "메뉴 추천 결과입니다.";
     public static final String COMPLETION = "추천을 완료했습니다.";
 
