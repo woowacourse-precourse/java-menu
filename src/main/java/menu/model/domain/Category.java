b21a350a85e656abcd1893f41bbeaddbdc81eb8b
Hayoon <hy97@sju.ac.kr>
2022-12-17 15:48:10 +0900

    feat(Category): 요일 인덱싱 변수 추가

--- src/main/java/menu/model/domain/Category.java
+++ src/main/java/menu/model/domain/Category.java
@@ -1,16 +1,34 @@
 package menu.model.domain;
 
+import java.util.Arrays;
+
 public enum Category {
 
-    JAPAN("일식"),
-    KOREA("한식"),
-    CHINA("중식"),
-    ASIA("아시안"),
-    AMERICA("양식");
+    JAPAN("일식", 1),
+    KOREA("한식", 2),
+    CHINA("중식", 3),
+    ASIA("아시안", 4),
+    AMERICA("양식", 5);
 
     private String category;
+    private Integer index;
 
-    Category(String category) {
+    Category(String category, Integer index) {
         this.category = category;
+        this.index = index;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public static Category getValueOf(Integer inputIndex) {
+        return Arrays.stream(Category.values())
+                .filter(category -> category.getIndex().equals(inputIndex))
+                .findFirst().orElse(null);
     }
 }
