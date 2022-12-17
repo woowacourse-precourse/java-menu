e4905b4cc41393d9de195f0b084996781232171d
limheesang <dlagmltkd12@naver.com>
2022-12-17 14:04:13 +0900

    feat: 코치 이름은 최소 2글자, 최대 4글자가 아니면 예외 발생

--- src/main/java/menu/domain/model/Coach.java
+++ src/main/java/menu/domain/model/Coach.java
@@ -4,12 +4,23 @@ import java.util.List;
 
 public class Coach {
 
+    private static final String ERROR_INVALID_NAME_LENGTH = "[ERROR] 이름은 최소 2글자, 최대 4글지 이어야합니다.";
+    private static final int MINIMUM_NAME_LENGTH = 2;
+    private static final int MAXIMUM_NAME_LENGTH = 4;
+
     private final String name;
     private final List<Menu> canNotEatFoods;
 
     public Coach(String name, List<Menu> canNotEatFoods) {
+        validateNameLength(name);
         this.name = name;
         this.canNotEatFoods = canNotEatFoods;
+    }
+
+    private static void validateNameLength(String name) {
+        if (name.length() < MINIMUM_NAME_LENGTH || name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(ERROR_INVALID_NAME_LENGTH);
+        }
     }
 
     public boolean canEat(Menu menu) {
--- src/test/java/menu/domain/model/CoachTest.java
+++ src/test/java/menu/domain/model/CoachTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CoachTest {
@@ -23,6 +24,16 @@ class CoachTest {
     @Test
     void 못먹는_메뉴인지_판단하는기능_테스트() {
         boolean result = coach.canEat(new Menu(Category.JAPAN_FOOD, "우동"));
-        Assertions.assertThat(result).isFalse();
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void 이름_길이가_2_4가아니면_예외발생() {
+        List<Menu> menus = List.of(new Menu(Category.JAPAN_FOOD, "우동"),
+                new Menu(Category.JAPAN_FOOD, "스시")
+        );
+        assertThatThrownBy(() -> coach = new Coach("항", menus))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름은 최소 2글자, 최대 4글지 이어야합니다.");
     }
 }
