06a6d6929856540e1d4370f6ad79b32e836a078f
SproutMJ <mjk54565@gmail.com>
2022-12-17 17:11:05 +0900

    feat: 사용자가 2-5명이 아니면 재입력받도록 변경 	이전에 추가한 CustomIllegalAlgumentException을 사용하도록 변경

--- src/main/java/menu/IO/InputView.java
+++ src/main/java/menu/IO/InputView.java
@@ -1,6 +1,7 @@
 package menu.IO;
 
 import camp.nextstep.edu.missionutils.Console;
+import menu.exception.CustomIllegalArgumentException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,19 +12,21 @@ public class InputView {
     final int MAX_NAME_LENGTH = 4;
     final int MIN_DONT_ENT_MENU_NUM = 0;
     final int MAX_DONT_ENT_MENU_NUM = 2;
+    final int MIN_COACH_NUM = 2;
+    final int MAX_COACH_NUM = 5;
 
     public List<String> readCoachNames() {
         String names = Console.readLine();
         StringTokenizer tokenizer = new StringTokenizer(names, ",");
-
         List<String> coachNames = new ArrayList<>();
         while (tokenizer.hasMoreTokens()) {
             String coachName = tokenizer.nextToken();
             if (!(MIN_NAME_LENGTH <= coachName.length() && coachName.length() <= MAX_NAME_LENGTH))
-                throw new IllegalArgumentException("[ERROR] 사용자 이름은 2~4글자여야 합니다.");
+                throw new CustomIllegalArgumentException("사용자 이름은 2~4글자여야 합니다.");
             coachNames.add(coachName);
         }
-
+        if(!(MIN_COACH_NUM<=coachNames.size()&& coachNames.size()<=MAX_COACH_NUM))
+            throw new CustomIllegalArgumentException("사용자는 2~5명이어야 합니다.");
         return coachNames;
     }
 
@@ -38,7 +41,7 @@ public class InputView {
         }
 
         if (!(MIN_DONT_ENT_MENU_NUM <= dontEatMenus.size() && dontEatMenus.size() <= MAX_DONT_ENT_MENU_NUM))
-            throw new IllegalArgumentException("[ERROR] 못먹는 메뉴는 0-2 사이입니다.");
+            throw new CustomIllegalArgumentException("못먹는 메뉴는 0-2 사이입니다.");
 
         return dontEatMenus;
     }
--- src/main/java/menu/exception/CustomIllegalArgumentException.java
+++ src/main/java/menu/exception/CustomIllegalArgumentException.java
@@ -2,6 +2,6 @@ package menu.exception;
 
 public class CustomIllegalArgumentException extends IllegalArgumentException {
     public CustomIllegalArgumentException(String message){
-        super("[ERROR]" + message);
+        super("[ERROR] " + message);
     }
 }
