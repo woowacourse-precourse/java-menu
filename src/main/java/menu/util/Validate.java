8d1565933400f13f7367f3da1a71ca29b488b18d
thdwoqor <celebarte01@gmail.com>
2022-12-17 15:59:15 +0900

    feat: 못먹는 음식 중복 테스트 추가

--- src/main/java/menu/util/Validate.java
+++ src/main/java/menu/util/Validate.java
@@ -1,6 +1,8 @@
 package menu.util;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validate {
     public static void nameLength(List<String> names, int minInclusive, int maxInclusive) {
@@ -23,16 +25,23 @@ public class Validate {
             throw new IllegalArgumentException(Message.ERROR_DIFFERENT_CANT_EAT_LENGTHS.message);
         }
     }
+    public static void duplication(List<String> numbers) {
+        Set<String> sameNumber = new HashSet<>(numbers);
+        if (numbers.size() != sameNumber.size()) {
+            throw new IllegalArgumentException(Message.ERROR_DUPLICATE_VALUE.message);
+        }
+    }
 
     private enum Message {
         ERROR_DIFFERENT_NAME_LENGTHS("[ERROR] 코치의 이름은 최소 2글자, 최대 4글자이다."),
         ERROR_DIFFERENT_COACH_LENGTHS("[ERROR] 코치는 최소 2명, 최대 5명까지 식사를 함께 한다."),
-        ERROR_DIFFERENT_CANT_EAT_LENGTHS("[ERROR] 각 코치는 최소 0개, 최대 2개의 못 먹는 메뉴가 있다.");
+        ERROR_DIFFERENT_CANT_EAT_LENGTHS("[ERROR] 각 코치는 최소 0개, 최대 2개의 못 먹는 메뉴가 있다."),
+        ERROR_DUPLICATE_VALUE("[ERROR] 중복 값이 포함되어 있습니다.");
 
         private final String message;
 
         Message(String message) {
             this.message = message;
         }
-        }
+    }
 }
