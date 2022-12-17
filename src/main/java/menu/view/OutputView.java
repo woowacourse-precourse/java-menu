1837d4db13f68505fbe0d4cee5cba914c656610b
yujaemin <woawoa0513@gmail.com>
2022-12-17 13:41:24 +0900

    feat: 서비스 시작 안내문 출력 함수 구현

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,10 @@
+package menu.view;
+
+import menu.message.NoticeMessage;
+
+public class OutputView {
+
+    public void printServiceStartNotice() {
+        System.out.println(NoticeMessage.SERVICE_START_NOTICE + '\n');
+    }
+}
