13bd4c7810c25441e1c7f464cd292feda71458b3
euna <christinejung10@daum.net>
2022-12-17 15:13:53 +0900

    add(Coach): add getter, setter

--- src/main/java/menu/coach/Coach.java
+++ src/main/java/menu/coach/Coach.java
@@ -7,7 +7,7 @@ public class Coach {
 
     private String name;
     private List<String> menusCannotEats = new ArrayList<>();
-    private List<String> menusRecommended = new ArrayList<>();
+    private List<String> menusRecommended = new ArrayList<>(); // 순서 : 월, 화, 수, 목, 금
 
     public Coach(String name) {
         this.name = name;
@@ -21,12 +21,16 @@ public class Coach {
         return menusCannotEats;
     }
 
-    public void addMenusCannotEats(String cannotEat) {
-        this.menusCannotEats.add(cannotEat);
+    public List<String> getMenusRecommended() {
+        return menusRecommended;
     }
 
-    public void setRecommendedMenu(List<String> recommendedMenu) {
+    public void setMenusRecommended(List<String> recommendedMenu) {
         this.menusRecommended = recommendedMenu;
+    }
+
+    public void addMenusCannotEats(String cannotEat) {
+        this.menusCannotEats.add(cannotEat);
     }
 
 }
