1b4a3c2b76c8166da86bf5074d1e9b91397319e3
LJW25 <dbjwl@g.skku.edu>
2022-12-17 16:57:06 +0900

    test: 코치 별 결과 출력 테스트 추가

--- src/test/java/menu/OutputViewTest.java
+++ src/test/java/menu/OutputViewTest.java
@@ -6,6 +6,7 @@ import menu.View.OutputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,6 +23,32 @@ public class OutputViewTest extends NsTest {
         outputView.printCategory(categories);
         assertThat(output()).contains(
                 "[ 카테고리 | 아시안 | 중식 | 한식 | 아시안 | 양식 ]"
+        );
+    }
+
+    @DisplayName("코치 별 추천 결과 출력 테스트")
+    @Test
+    void printEachResults() {
+
+        Coach coach1 = new Coach("제임스", Arrays.asList(""));
+        coach1.addRecommend("된장찌개");
+        coach1.addRecommend("비빔밥");
+        coach1.addRecommend("가츠동");
+        coach1.addRecommend("토마토 달걀볶음");
+        coach1.addRecommend("파인애플 볶음밥");
+        Coach coach2 = new Coach("포코", Arrays.asList(""));
+        coach2.addRecommend("된장찌개");
+        coach2.addRecommend("불고기");
+        coach2.addRecommend("하이라이스");
+        coach2.addRecommend("탕수육");
+        coach2.addRecommend("나시고렝");
+
+        List<Coach> coaches = new ArrayList<>(Arrays.asList(coach1, coach2));
+
+        outputView.printCoachesResult(coaches);
+        assertThat(output()).contains(
+                "[ 제임스 | 된장찌개 | 비빔밥 | 가츠동 | 토마토 달걀볶음 | 파인애플 볶음밥 ]",
+                "[ 포코 | 된장찌개 | 불고기 | 하이라이스 | 탕수육 | 나시고렝 ]"
         );
     }
 
