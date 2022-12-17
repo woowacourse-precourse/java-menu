d3d8f85e55acf4ad3232e926ff70787d2677076c
yenawee <yaena0319@naver.com>
2022-12-17 16:47:06 +0900

    chore : optimize imports

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,6 +1,5 @@
 package menu;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
--- src/main/java/menu/RandomNumGenerator.java
+++ src/main/java/menu/RandomNumGenerator.java
@@ -2,8 +2,6 @@ package menu;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.List;
-
 public class RandomNumGenerator {
     public static int makeCategoryNum (){
         int num = Randoms.pickNumberInRange(1,5);
