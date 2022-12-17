cf497766e0e77063c4bd1e0c008a3dff53887917
seokhwan-an <asok369258@naver.com>
2022-12-17 14:06:17 +0900

    feat : 게임 시작 문구를 출력하는 기능 추가

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -2,7 +2,14 @@ package menu.view;
 
 public class OutputView {
 
+    private static final String START_PHRASE = "점심 메뉴 추천을 시작합니다.+\n";
+
     public static void printPhrase(String phrase) {
         System.out.println(phrase);
+    }
+
+    // 시작 문구 출력 기능
+    public static void printStart() {
+        System.out.println(START_PHRASE);
     }
 }
