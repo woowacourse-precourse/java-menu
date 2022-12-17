696de476915bd0f813556c4c19ccb814ec90b7dc
jiwon <ljwon77@daum.net>
2022-12-17 17:26:15 +0900

    feat: 결과 출력 기능 추가

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,8 +1,20 @@
 package menu.view;
 
+import menu.domain.DayWeek;
+
+import static menu.constant.InputMessage.RECOMMEND_RESULT;
+import static menu.constant.InputMessage.STOP_RECOMMEND;
+
 public class OutputView {
 
     public void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public void printRecommendResult(String recommendResult) {
+        System.out.println(RECOMMEND_RESULT.getValue());
+        System.out.println(DayWeek.getDayWeekInfo());
+        System.out.println(recommendResult);
+        System.out.println(STOP_RECOMMEND.getValue());
     }
 }
