468063df05528ce7c0dd69ab8d64d5fabbbb1a03
anneu <b.imnuj@gmail.com>
2022-12-17 16:08:26 +0900

    feat: 못 먹는 메뉴 개수가 0개 ~ 2개가 아닐 경우 예외 처리 기능 구현

--- src/main/java/menu/domain/CantEatMenus.java
+++ src/main/java/menu/domain/CantEatMenus.java
@@ -6,6 +6,17 @@ public class CantEatMenus {
     private final List<Menu> menus;
 
     public CantEatMenus(List<Menu> menus) {
+        validateSize(menus);
         this.menus = menus;
+    }
+
+    private void validateSize(List<Menu> menus) {
+        if (!isValidSize(menus.size())) {
+            throw new IllegalArgumentException("[ERROR] 못 먹는 메뉴는 0개 ~ 2개까지 가능합니다.");
+        }
+    }
+
+    private static boolean isValidSize(int size) {
+        return 0 <= size && size <= 2;
     }
 }
