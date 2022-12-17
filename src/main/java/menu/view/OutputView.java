a48dcdb53ea054d713601225b938f561405f901f
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 13:35:39 +0900

    feat: 코치의 이름 입력을 알리는 출력문 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -6,4 +6,8 @@ public class OutputView {
     public static void printStartMsg() {
         System.out.println("점심 메뉴 추천을 시작합니다.\n");
     }
+
+    public static void printInputCoaches() {
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+    }
 }
