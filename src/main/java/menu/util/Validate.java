a957383f1a0eee579d1a0965f5be11bc9450790a
= <=>
2022-12-17 16:53:32 +0900

    feat: 코치 이름 및 코치 인원수 예외처리 추가

--- src/main/java/menu/util/Validate.java
+++ src/main/java/menu/util/Validate.java
@@ -10,13 +10,31 @@ import java.util.List;
  */
 public class Validate {
     private static final MenuService menuService = new MenuService();
-    public static void validateCoachNames(List<String> coachName) {
-        if (coachName.size() < 2) {
-            throw new IllegalArgumentException(IllegalArgumentExceptionMessage.INPUT_COACH_LENGTH.getMessage());
+    public static void validateCoachNames(List<String> coachNames) {
+        validateNumberOfCoach(coachNames);
+        validateLengthOfCoachName(coachNames);
+    }
+
+    private static void validateLengthOfCoachName(List<String> coachNames) {
+        if (coachNames.stream().anyMatch(coachName -> coachNames.size() < Command.COACH_NAME_LENGTH_MIN.getData())) {
+            throw new IllegalArgumentException(IllegalArgumentExceptionMessage.INPUT_COACH_NAME_LENGTH_MIN.getMessage());
+        } else if (coachNames.stream().anyMatch(coachName -> coachNames.size() > Command.COACH_NAME_LENGTH_MAX.getData())) {
+            throw new IllegalArgumentException(IllegalArgumentExceptionMessage.INPUT_COACH_NAME_LENGTH_MAX.getMessage());
+        }
+    }
+
+    private static void validateNumberOfCoach(List<String> coachNames) {
+        if (coachNames.size() < Command.NUMBER_OF_COACH_MIN.getData()) {
+            throw new IllegalArgumentException(IllegalArgumentExceptionMessage.INPUT_COACH_LENGTH_MIN.getMessage());
+        } else if(coachNames.size() > Command.NUMBER_OF_COACH_MAX.getData()) {
+            throw new IllegalArgumentException(IllegalArgumentExceptionMessage.INPUT_COACH_LENGTH_MAX.getMessage());
         }
     }
 
     public static void validateHateMenu(List<String> hateMenus) {
+        if(hateMenus.size() > 2) {
+            throw new IllegalArgumentException(IllegalArgumentExceptionMessage.HATE_MENU_SIZE.getMessage());
+        }
         for (String hateMenu : hateMenus) {
             menuService.findByName(hateMenu);
         }
