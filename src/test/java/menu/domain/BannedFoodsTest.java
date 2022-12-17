af61823b4cf5b2186bec43195b58b490311338b8
sosow0212 <sosow0212@naver.com>
2022-12-17 17:41:53 +0900

    refactor : (BannedFoodsTest) 외부에서 사용하던 기능을 내부 메서드로 변환하면서 테스트 리팩토링

--- src/test/java/menu/domain/BannedFoodsTest.java
+++ src/test/java/menu/domain/BannedFoodsTest.java
@@ -1,6 +1,7 @@
 package menu.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.in;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,5 +19,19 @@ public class BannedFoodsTest {
 
         // then
         assertThat(bannedFoods.getBannedFoods().contains(bannedFoodName)).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("금지 음식 리스트 추가 테스트")
+    public void addBannedFoodsTest() {
+        // given
+        BannedFoods bannedFoods = new BannedFoods();
+        String input = "우동,스시";
+
+        // when
+        bannedFoods.addBannedFoods(input);
+
+        // then
+        assertThat(bannedFoods.getBannedFoods().size()).isEqualTo(2);
     }
 }
