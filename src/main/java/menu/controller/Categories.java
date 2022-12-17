fd3ef408d13f0f1762860268e73d32e8f83e99bf
YunDaeHyeok <bmong4mong0318@gmail.com>
2022-12-17 16:03:21 +0900

    feat: 카테고리 저장을 위한 클래스 생성

---
+++ src/main/java/menu/controller/Categories.java
@@ -0,0 +1,32 @@
+package menu.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Categories {
+    public final List<Category> categories;
+
+    public Categories() {
+        this.categories = new ArrayList<>();
+    }
+
+    public void add(Category category) {
+        categories.add(category);
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[ 카테고리 | ");
+
+        for (int i = 0; i < categories.size(); i++){
+            stringBuilder.append(categories.get(i).getCategory());
+            if (i != categories.size() - 1){
+                stringBuilder.append(" | ");
+            }
+        }
+        stringBuilder.append(" ]");
+        return stringBuilder.toString();
+    }
+}
