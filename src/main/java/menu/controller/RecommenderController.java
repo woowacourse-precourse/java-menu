7d8dfcd461741d4f884a1c0044d5c51d2d79d683
2014012606 <2014012606@hanyang.ac.kr>
2022-12-17 14:19:31 +0900

    feat: 메뉴 추천 프로그램을 관리하는 recommenderController 클래스를 추가

---
+++ src/main/java/menu/controller/RecommenderController.java
@@ -0,0 +1,13 @@
+package menu.controller;
+
+import menu.model.Coach;
+
+import java.util.List;
+
+public class RecommenderController {
+    private List<Coach> coaches;
+
+    public void run() {
+
+    }
+}
