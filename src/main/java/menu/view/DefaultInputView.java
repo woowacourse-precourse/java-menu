0712cb00b35ea91efa50e2a9524880389ba66b46
apptie <appti@naver.com>
2022-12-17 16:03:57 +0900

    feat(DefaultInputView): InputView의 공통 기능을 가지고 있는 추상 클래스 추가

---
+++ src/main/java/menu/view/DefaultInputView.java
@@ -0,0 +1,14 @@
+package menu.view;
+
+import java.util.Objects;
+
+public abstract class DefaultInputView {
+
+    private static final String INPUT_EXCEPTION_MESSAGE = "입력은 공백을 수 없습니다.";
+
+    protected void validateInput(String input) {
+        if (Objects.isNull(input) || input.isBlank()) {
+            throw new IllegalArgumentException(INPUT_EXCEPTION_MESSAGE);
+        }
+    }
+}
