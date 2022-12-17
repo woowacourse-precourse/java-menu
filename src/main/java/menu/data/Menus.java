52ea610f19eedb8eb102937a5707546f3805ba27
JunDonghyuk <ehdgur4814@naver.com>
2022-12-17 16:37:43 +0900

    update(Menus): 사전 제공된 메뉴를 이용한 Map field 추가

--- src/main/java/menu/data/Menus.java
+++ src/main/java/menu/data/Menus.java
@@ -1,13 +1,28 @@
 package menu.data;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import menu.model.Category;
+
+import java.util.*;
 
 public class Menus {
-    List<String> japanMenus = new ArrayList<>(Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼"));
-    List<String> koreaMenus = new ArrayList<>(Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"));
-    List<String> chinaMenus = new ArrayList<>(Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채"));
-    List<String> asiaMenus = new ArrayList<>(Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜"));
-    List<String> westMenus = new ArrayList<>(Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
+    private Map<Integer, List<String>> menus;
+
+    private List<String> japanMenus = new ArrayList<>(Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼"));
+    private List<String> koreaMenus = new ArrayList<>(Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"));
+    private List<String> chinaMenus = new ArrayList<>(Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채"));
+    private List<String> asiaMenus = new ArrayList<>(Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜"));
+    private List<String> westMenus = new ArrayList<>(Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
+    public Menus() {
+        this.menus = new HashMap<>();
+        menus.put(Category.JAPAN.getCountry(), japanMenus);
+        menus.put(Category.KOREA.getCountry(), koreaMenus);
+        menus.put(Category.CHINA.getCountry(), chinaMenus);
+        menus.put(Category.ASIA.getCountry(), asiaMenus);
+        menus.put(Category.WEST.getCountry(), westMenus);
+    }
+
+    public List<String> giveCountryMenus(int country) {
+        return menus.get(country);
+    }
+
 }
