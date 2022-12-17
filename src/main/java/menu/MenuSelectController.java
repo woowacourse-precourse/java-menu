2258a4243bf1f725a6f0c05d607b8d81bfac0e45
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-12-17 16:16:49 +0900

    Refactor : 예외처리를 위해 프로그램의 흐름을 함수로 나누어줌

--- src/main/java/menu/MenuSelectController.java
+++ src/main/java/menu/MenuSelectController.java
@@ -7,16 +7,38 @@ public class MenuSelectController {
 
     public void start() {
         outputDriver.printStartMessage();
-        coachRepo.setCoachList(inputDriver.scanCoachName());
+        scanCoachName();
         setCoachCannotEatMenu();
         MenuSelectListMaker menuSelectListMaker = new MenuSelectListMaker(coachRepo);
         outputDriver.printResult(menuSelectListMaker.menuListStringReturn());
     }
 
+    private void scanCoachName() {
+        outputDriver.printScanCoachName();
+        try{
+            coachRepo.setCoachList(inputDriver.scanCoachName());
+        }
+        catch(IllegalArgumentException e){
+            outputDriver.printErrorForScanName();
+            System.out.println();
+            scanCoachName();
+        }
+    }
+
     private void setCoachCannotEatMenu() {
         for (Coach coach : coachRepo.coachList){
-            outputDriver.printScanCannotEatMenuMessage(coach.name);
+            setCoachCannotEatMenuForOneCoach(coach);
+        }
+    }
+
+    private void setCoachCannotEatMenuForOneCoach(Coach coach) {
+        outputDriver.printScanCannotEatMenuMessage(coach.name);
+        try{
             coachRepo.setCoachCannotEatMenu(coach.name,inputDriver.scanCannotEatMenu());
         }
+        catch (IllegalArgumentException e){
+            setCoachCannotEatMenuForOneCoach(coach);
+        }
+
     }
 }
