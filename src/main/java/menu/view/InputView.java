f30b4e02f607675ee1c29546443b715ed6271370
LeeJejune <leejejune0625@gmail.com>
2022-12-17 17:02:59 +0900

    feat: 코치의 이름과 메뉴를 받아오는 인풋뷰 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -6,6 +6,8 @@ import menu.service.dto.CoachNameDto;
 
 public class InputView {
 
+    private static final String MENU_MSG = "(이)가 못 먹는 메뉴를 입력해 주세요.";
+    private static final String COACH_NAME_MSG = "코치의 이름을 입력해 주세요. (, 로 구분)";
 
     private InputView() {
     }
@@ -18,5 +20,17 @@ public class InputView {
         System.out.println(message);
     }
 
+    public static CoachNameDto getNames() {
+        println();
+        println(COACH_NAME_MSG);
+        final String userInput = Console.readLine();
+        return new CoachNameDto(userInput);
+    }
+
+    public static MenuDto getMenus(final String name) {
+        println();
+        println(name + MENU_MSG);
+        final String userInput = Console.readLine();
+        return new MenuDto(userInput);
     }
 }
