56fa7dfa3a3aeb25be3d3b0ef86b22f939308e77
khsrla9806 <kimhunsope@naver.com>
2022-12-17 13:22:02 +0900

    feat(InputView): 코치 이름을 입력받는 메서드 구현

---
+++ src/main/java/menu/view/InputView.java
@@ -0,0 +1,22 @@
+package menu.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Console;
+import menu.exception.InputException;
+
+public class InputView extends InputException {
+    public List<String> readCoachName() {
+        String inputData = Console.readLine();
+        List<String> coachName = Arrays.stream(inputData.split(",")).collect(Collectors.toList());
+        try {
+            validateCoachName(coachName);
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            readCoachName();
+        }
+        return coachName;
+    }
+}
