268d0a852f6cb4c94853331dca01ee89d2ea1bbf
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 16:07:38 +0900

    feat(OutputView): 메뉴 추천 결과 출력하기 기능 추가
    docs: 메뉴 추천 결과 출력하기 완료 체크

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,9 +1,63 @@
 package menu.view;
 
+import menu.constants.DayOfWeek;
+import menu.model.Coach;
+import menu.model.Coaches;
+import menu.model.SelectCategory;
+
+import java.util.List;
+
 public class OutputView {
     private static final String START_MENU_RECOMMEND = "점심 메뉴 추천을 시작합니다.";
+    private static final String MENU_RECOMMEND_RESULT = "\n메뉴 추천 결과입니다.";
+    private static final String START_RESULT = "[ ";
+    private static final String END_RESULT = " ]";
+    private static final String SEPARATOR = " | ";
+    private static final String DAY_OF_WEEK_TITLE = "구분";
 
     public void printStartProgram() {
         System.out.println(START_MENU_RECOMMEND);
+    }
+
+    public void printResultMenuRecommend(SelectCategory selectCategory, Coaches coaches) {
+        System.out.println(MENU_RECOMMEND_RESULT);
+        generateResult(selectCategory, coaches);
+    }
+
+    private void generateResult(SelectCategory selectCategory, Coaches coaches) {
+        printDayOfWeek();
+        printCategory(selectCategory);
+        printCoachesRecommendMenu(coaches);
+    }
+
+    private void printDayOfWeek() {
+        List<String> dayOfWeek = DayOfWeek.getWeekOfDayList();
+        dayOfWeek.add(0, DAY_OF_WEEK_TITLE);
+        System.out.println(setFormat(dayOfWeek));
+    }
+
+    private void printCategory(SelectCategory selectCategory) {
+        System.out.println(setFormat(selectCategory.getSelectCategories()));
+    }
+
+    private void printCoachesRecommendMenu(Coaches coaches) {
+        for (Coach coach : coaches.getCoaches()) {
+            System.out.println(getRecommendMenu(coach));
+        }
+    }
+
+    private String getRecommendMenu(Coach coach) {
+        List<String> dayOfWeek = coach.getSelectMenu();
+        dayOfWeek.add(0, DAY_OF_WEEK_TITLE);
+        return setFormat(dayOfWeek);
+    }
+
+    private String setFormat(List<String> values) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(START_RESULT);
+        stringBuilder.append(String.join(SEPARATOR, values));
+        stringBuilder.append(END_RESULT);
+        stringBuilder.append(System.lineSeparator());
+        return stringBuilder.toString();
     }
 }
