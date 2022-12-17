3d052f306ba68e049c1d60f30b23e509c3e244f2
LeeJejune <leejejune0625@gmail.com>
2022-12-17 17:04:55 +0900

    feat: 코치와 카테고리를 보여주는 output뷰 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -7,6 +7,9 @@ public class OutputView {
 
     private static final String ERROR_PREFIX = "[ERROR] ";
     private static final String START_MSG = "점심 메뉴 추천을 시작합니다.";
+    private static final String LINE = " | ";
+    private static final String START = "[ ";
+    private static final String END = " ]";
 
     private OutputView() {
     }
@@ -17,6 +20,18 @@ public class OutputView {
 
     public static void printStartMessage() {
         println(START_MSG);
+    }
+
+    private static void printCoach(final List<String> coachMenu) {
+        print(START);
+        print(String.join(LINE, coachMenu));
+        println(END);
+    }
+
+    private static void printCategory(final List<String> categories) {
+        print(CATEGORY_MSG);
+        print(String.join(LINE, categories));
+        println(END);
     }
 
     private static void print(final String message) {
