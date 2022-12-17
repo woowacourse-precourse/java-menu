54364d1a9506a7b82238a8ca8c8a9a18c8df2b3a
woo-chang <clllickme@naver.com>
2022-12-17 14:02:42 +0900

    feat(InputView): 못 먹는 음식을 입력하는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import menu.domain.Coach;
 import menu.utils.CoachesConverter;
+import menu.utils.MenusConverter;
 
 public class InputView {
 
@@ -12,6 +13,17 @@ public class InputView {
             try {
                 String input = Console.readLine();
                 return CoachesConverter.convert(input);
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    public List<String> readImpossilbeToEat() {
+        while (true) {
+            try {
+                String input = Console.readLine();
+                return MenusConverter.convert(input);
             } catch (IllegalArgumentException e) {
                 OutputView.printErrorMessage(e.getMessage());
             }
