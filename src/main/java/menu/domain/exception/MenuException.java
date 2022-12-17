25fd1b1cb936cbe363b4226cb27e0ff94801920d
Cyma-s <ssol0319@naver.com>
2022-12-17 15:40:45 +0900

    feat(MenuException) : 코치가 먹을 수 없는 메뉴 에러 추가

--- src/main/java/menu/domain/exception/MenuException.java
+++ src/main/java/menu/domain/exception/MenuException.java
@@ -1,7 +1,8 @@
 package menu.domain.exception;
 
 public enum MenuException {
-    DUPLICATE_MENU("중복된 메뉴입니다.");
+    DUPLICATE_MENU("중복된 메뉴입니다."),
+    CAN_NOT_MENU("코치가 먹을 수 없는 메뉴입니다.");
 
     private final String exceptionMessage;
 
