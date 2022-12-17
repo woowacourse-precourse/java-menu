812b0b53247e545a73e1c273eceea69fa08de965
Reznov <reznov03048@gamil.com>
2022-12-17 14:47:25 +0900

    feat: 코치가 못먹는 메뉴를 입력하라는 메시지를 출력하는 기능 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -3,6 +3,7 @@ package menu.view;
 public class OutputView {
     private static final String START_COMMENT = "점심 메뉴 추천을 시작합니다.";
     private static final String COACH_NAMES_COMMENT = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private static final String COACH_FORBIDDEN_FOOD_COMMENT = "(이)가 못 먹는 메뉴를 입력해 주세요.";
 
     public static void printStartComment() {
         System.out.println(START_COMMENT);
@@ -10,5 +11,9 @@ public class OutputView {
 
     public static void printCoachNameComment() {
         System.out.println(COACH_NAMES_COMMENT);
+    }
+
+    public static void printCoachForbiddenFoodComment(String name) {
+        System.out.printf("%s" + COACH_FORBIDDEN_FOOD_COMMENT, name);
     }
 }
