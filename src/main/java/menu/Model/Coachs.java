0d3c105709c02598ed73b19a5421263f9f7584c4
Eun-chan Cho <eunchan1001@gmail.com>
2022-12-17 16:20:40 +0900

    feat(Coachs): add validation to the number of coachs

--- src/main/java/menu/Model/Coachs.java
+++ src/main/java/menu/Model/Coachs.java
@@ -3,13 +3,26 @@ package menu.Model;
 import java.util.List;
 
 public class Coachs {
-    List<Coach> coachs;
+    private List<Coach> coachs;
+    private final int MIN_NUM_COACH = 2;
+    private final int MAX_NUM_COACH = 5;
+    private final String COACHS_UNDER_RANGE = "코치는 최소 2명 이상 입력해야 합니다.";
+    private final String COACHS_OVER_RANGE = "코치는 최대 5명 이하 입력해야 합니다.";
 
     public Coachs(String input) {
-        String[] names = input.split(",");
+        List<String> names = List.of(input.split(","));
         for (String name : names) {
             Coach coach = new Coach(name);
             this.coachs.add(coach);
+        }
+    }
+
+    private void validateCoachs(List<String> names) {
+        if (names.size() < MIN_NUM_COACH) {
+            throw new IllegalArgumentException(COACHS_UNDER_RANGE);
+        }
+        if (names.size() > MAX_NUM_COACH) {
+            throw new IllegalArgumentException(COACHS_OVER_RANGE);
         }
     }
 
