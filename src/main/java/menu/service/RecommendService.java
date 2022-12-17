bfbdde9b5c01e2037d13ab978e08486393d14185
jiwon <ljwon77@daum.net>
2022-12-17 16:42:16 +0900

    feat: 날짜별 및 코치별 메뉴 추천 로직 추가

--- src/main/java/menu/service/RecommendService.java
+++ src/main/java/menu/service/RecommendService.java
@@ -1,28 +1,41 @@
 package menu.service;
 
 import menu.domain.Category;
+import menu.domain.Coach;
+import menu.domain.DayWeek;
+
+import java.util.List;
 
 public class RecommendService {
-
-    //     - [] x요일에 추천할 카테고리를 무작위로 정한다.
-    //      - [] 만약, 해당 카테고리가 해당 주에 3번 이상 추천된다면 다시 추천할 카테고리를 선정한다.
-    //    - 각 코치가 x요일에 먹을 메뉴를 추천한다.
-    //      - [] 해당 카테고리에 있는 메뉴를 임의의 순서로 섞어서, 첫 번째 값을 리턴한다.
-    //        - [] 해당 메뉴가 해당 코치가 못 먹는 메뉴라면 다시 값을 섞어서 반환환다.
-    //        - [] 해당 메뉴가 해당 주에 이미 추천이 됐다면 다시 값을 섞어서 반환환다.
-
-    private final MenuService menuService = new MenuService();
+    private final MenuService menuService;
     private final CategoryService categoryService = new CategoryService();
 
+    private final RecommendResultService recommendResultService = new RecommendResultService();
 
-    public void recommend() {
-        Category findCategory = categoryService.getRecommendCategory();
-        while(!categoryService.isSaveCategoryInfo(findCategory)) {
-            findCategory = categoryService.getRecommendCategory();
-        }
-        String recommendMenu = menuService.getRecommendMenu(findCategory);
-        
-
+    public RecommendService(MenuService menuService) {
+        this.menuService = menuService;
     }
 
+    public void recommend(DayWeek dayWeek, List<Coach> coaches) {
+        for (Coach coach : coaches) {
+            Category findCategory = categoryService.getRecommendCategory();
+            while(!categoryService.isSaveCategoryInfo(findCategory)) {
+                findCategory = categoryService.getRecommendCategory();
+            }
+            recommendResultService.saveCategoryResult(dayWeek, findCategory);
+            String recommendMenu = menuService.getRecommendMenu(findCategory);
+            boolean isAlreadyRecommend = menuService.isAlreadyRecommend(coach, recommendMenu);
+            boolean isNotEatFood = coach.isNotEatFood(recommendMenu);
+            while(isAlreadyRecommend || isNotEatFood) {
+                recommendMenu = menuService.getRecommendMenu(findCategory);
+                isAlreadyRecommend = menuService.isAlreadyRecommend(coach, recommendMenu);
+                isNotEatFood = coach.isNotEatFood(recommendMenu);
+            }
+            recommendResultService.createAndSaveResult(coach, dayWeek, recommendMenu);
+        }
+    }
+
+    public void getRecommendResult(List<Coach> coaches) {
+        recommendResultService.getRecommendResult(coaches);
+    }
 }
