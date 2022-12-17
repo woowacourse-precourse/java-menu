f4eb964f975b2daff257e0e66d42eac5c037bcb2
ingpyo <juninpyo72@gmail.com>
2022-12-17 13:47:17 +0900

    feat:카테고리를 전달해주는 기능

--- src/main/java/menu/Category.java
+++ src/main/java/menu/Category.java
@@ -4,13 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Category {
-	List<String> categorys = new ArrayList<>();
+	List<String> categories = new ArrayList<>();
 
 	public void settingCategorys() {
-		categorys.add("일식");
-		categorys.add("한식");
-		categorys.add("중식");
-		categorys.add("아시안");
-		categorys.add("양식");
+		categories.add("일식");
+		categories.add("한식");
+		categories.add("중식");
+		categories.add("아시안");
+		categories.add("양식");
 	}
+
+	public String getCategory(int index) {
+		return categories.get(index);
+	}
+
 }
