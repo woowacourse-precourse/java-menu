3b09a9c88ec0a9cc125731a0d9008d3eb128c5c6
woo-chang <clllickme@naver.com>
2022-12-17 14:05:09 +0900

    feat(OutputView): 코치 이름 입력에 대해 출력하는 기능 구현

--- src/main/java/menu/view/Message.java
+++ src/main/java/menu/view/Message.java
@@ -3,7 +3,8 @@ package menu.view;
 public enum Message {
 
     ERROR("[ERROR] %s"),
-    SERVICE_START("점심 메뉴 추천을 시작합니다.");
+    SERVICE_START("점심 메뉴 추천을 시작합니다."),
+    INPUT_COACH("코치의 이름을 입력해 주세요. (, 로 구분)");
 
     Message(String message) {
         this.message = message;
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -6,6 +6,10 @@ public class OutputView {
         System.out.println(Message.SERVICE_START.getMessage());
     }
 
+    public void printInputCoach() {
+        System.out.println(Message.INPUT_COACH.getMessage());
+    }
+
     public static void printErrorMessage(String message) {
         System.out.println(String.format(Message.ERROR.getMessage(), message));
     }
