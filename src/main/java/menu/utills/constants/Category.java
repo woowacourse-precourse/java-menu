4ce8b0f817e2df7871eca5c402686129cb389432
dev_kong <ahdjd5@gmail.com>
2022-12-17 13:19:40 +0900

    feat(constants): add Category enum
    value를 통해 category를 찾는 getCategoryByLabel method

---
+++ src/main/java/menu/utills/constants/Category.java
@@ -0,0 +1,30 @@
+package menu.utills.constants;
+
+import java.util.Arrays;
+
+public enum Category {
+    JAPAN("일식",1),
+    KOREA("한식",2),
+    CHINA("중식",3),
+    ASIA("아사인",4),
+    WESTERN("양식",5);
+
+    private final String label;
+    private final int value;
+
+    Category(String label,int value) {
+        this.label = label;
+        this.value = value;
+    }
+
+    public boolean isEqual(int value) {
+        return this.value == value;
+    }
+
+    public static Category getCategoryByValue(int value) {
+        return Arrays.stream(Category.values())
+                .filter(category -> category.isEqual(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("보기에있는 값만 입력가능합니다."));
+    }
+}
