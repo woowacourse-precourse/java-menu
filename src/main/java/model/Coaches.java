385572676ddaeee64dbfae3baf72707fe8420452
dclxxi <dclxxi@kakao.com>
2022-12-17 14:12:31 +0900

    refactor: 함수명의 Length을 Size로 변경

--- src/main/java/model/Coaches.java
+++ src/main/java/model/Coaches.java
@@ -7,22 +7,26 @@ public class Coaches {
     private final List<Coach> coaches;
     
     public Coaches(List<Coach> coaches) {
-        validLessThanLength(coaches);
-        validGreaterThanLength(coaches);
+        validSize(coaches);
         this.coaches = coaches;
     }
     
-    private void validLessThanLength(List<Coach> coaches) {
+    private void validSize(List<Coach> coaches) {
+        validLessThanSize(coaches);
+        validGreaterThanSize(coaches);
+    }
+    
+    private void validLessThanSize(List<Coach> coaches) {
         int coachesSize = coaches.size();
         if (coachesSize < 2) {
             throw new IllegalArgumentException("코치는 최소 2명 이상 입력해야 합니다.");
         }
     }
     
-    private void validGreaterThanLength(List<Coach> coaches) {
+    private void validGreaterThanSize(List<Coach> coaches) {
         int coachesSize = coaches.size();
         if (coachesSize > 5) {
-            throw new IllegalArgumentException("코치는 최소 2명 이상 입력해야 합니다.");
+            throw new IllegalArgumentException("코치는 최대 5명 까지 입력해야 합니다.");
         }
     }
 }
