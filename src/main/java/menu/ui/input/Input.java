d76ae8b705e109ebf27d156f6aef6a9aa416ba95
KSH <tkdgns543@gmail.com>
2022-12-17 15:56:26 +0900

    feat(ui): 코치 이름 입력 기능구현

--- src/main/java/menu/ui/input/Input.java
+++ src/main/java/menu/ui/input/Input.java
@@ -1,4 +1,18 @@
 package menu.ui.input;
 
+import menu.ui.input.exceptions.CoachList;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Input {
+    private static final String COACH_NAME_INPUT_NOTIFICATION = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private static final String COMMA = ",";
+
+    public List<String> receiveCoaches() {
+        Repetitor input = new CoachList();
+        String coachNames = input.getResult(COACH_NAME_INPUT_NOTIFICATION);
+
+        return Arrays.asList(coachNames.split(COMMA));
+    }
 }
