7881ed9d734f0cb23823d18d50022a04ac421651
HaODay <ycis5013@naver.com>
2022-12-17 17:35:07 +0900

    feat(NumberProperty): 숫자 속성 관련 열거형 생성

---
+++ src/main/java/menu/util/NumberProperty.java
@@ -0,0 +1,9 @@
+package menu.util;
+
+public class NumberProperty {
+    public static final int MIN_COACH_NUMBER = 2;
+    public static final int MAX_COACH_NUMBER = 5;
+    public static final int MIN_COACH_NAME_LENGTH = 2;
+    public static final int MAX_COACH_NAME_LENGTH = 4;
+    public static final int MAX_CANNOT_EAT_MENU_NUMBER = 2;
+}
