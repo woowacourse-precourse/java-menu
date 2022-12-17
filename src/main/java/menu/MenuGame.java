03bec497a41b573293d91006afc8979dfb10d200
Amenable-C <duswo0624@naver.com>
2022-12-17 14:18:24 +0900

    feat : 서비스 시작 문구 출력

---
+++ src/main/java/menu/MenuGame.java
@@ -0,0 +1,9 @@
+package menu;
+
+import menu.ui.OutputView;
+
+public class MenuGame {
+    public void start() {
+        OutputView.printMenuGameStart();
+    }
+}
---
+++ src/main/java/menu/ui/OutputView.java
@@ -0,0 +1,9 @@
+package menu.ui;
+
+public class OutputView {
+
+    private static final String GAME_START = "점심 메뉴 추천을 시작합니다.";
+    public static void printMenuGameStart() {
+        System.out.println(GAME_START);
+    }
+}
