6c2799056ca901e0f0e1ac26c68ee2045727e0f3
WhalesBob <ndw8200@naver.com>
2022-12-17 14:12:22 +0900

    feat : SuggestProgram 에서 초기 동작 기능 통합

--- src/main/java/menu/SuggestProgram.java
+++ src/main/java/menu/SuggestProgram.java
@@ -1,8 +1,18 @@
 package menu;
 
+import menu.views.ProgramOutput;
+import menu.views.UserInput;
+
+import java.util.List;
+
 public class SuggestProgram {
 
-    public void startProgram(){
+    private final UserInput userInput = new UserInput();
+    private final ProgramOutput output = new ProgramOutput();
 
+    public void startProgram(){
+        output.suggestProgramStart();
+        output.printRequestCoachNames();
+        List<String> coaches = userInput.requestCoachName();
     }
 }
