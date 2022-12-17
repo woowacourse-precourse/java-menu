7f26959a85c45bd1030c1e77ded91fe002ff13b2
Woojin Lee <dhkep03@gmail.com>
2022-12-17 16:44:12 +0900

    refactor(OutputView): 반복되는 문자열 상수화

--- src/main/java/menu/ui/OutputView.java
+++ src/main/java/menu/ui/OutputView.java
@@ -7,6 +7,7 @@ import menu.domain.constant.Day;
 import menu.domain.vo.CoachMenu;
 
 public class OutputView {
+    private final String SEPARATE_LINE_PREFIX = " | ";
 
     public void announceStart() {
         System.out.println("점심 메뉴 추천을 시작합니다.");
@@ -24,15 +25,19 @@ public class OutputView {
 
     public void printTopOfTable(List<String> categoriesOfWeek) {
         System.out.println("메뉴 추천 결과입니다.");
-        System.out.println("[ 구분 | " + String.join(" | ", Arrays.stream(Day.values()).map(Day::getDayInKorean).collect(
-                Collectors.toList())) + " ]");
-        System.out.println("[ 카테고리 | " + String.join(" | ", categoriesOfWeek) + " ]");
+        System.out.println("[ 구분" + SEPARATE_LINE_PREFIX + String.join(SEPARATE_LINE_PREFIX,
+                Arrays.stream(Day.values()).map(Day::getDayInKorean).collect(
+                        Collectors.toList())) + " ]");
+        System.out.println(
+                "[ 카테고리" + SEPARATE_LINE_PREFIX + String.join(SEPARATE_LINE_PREFIX, categoriesOfWeek) + " ]");
     }
 
     public void printMenuRecommendResult(List<CoachMenu> coachMenus) {
         coachMenus.forEach(coachMenu -> {
             System.out.println(
-                    "[ " + coachMenu.getCoachName() + " | " + String.join(" | ", coachMenu.getMenus()) + " ]");
+                    "[ " + coachMenu.getCoachName() + SEPARATE_LINE_PREFIX + String.join(SEPARATE_LINE_PREFIX,
+                            coachMenu.getMenus())
+                            + " ]");
         });
         addEmptyLine();
         System.out.print("추천을 완료했습니다.");
