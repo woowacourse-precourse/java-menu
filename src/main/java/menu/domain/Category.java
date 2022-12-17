b297d7ac3cd4429997c3eea733639e57d47353f1
greeng00se <thegreengoose7@gmail.com>
2022-12-17 14:23:24 +0900

    feat(Category): 각 카테고리별 이름 추가

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,15 +1,32 @@
 package menu.domain;
 
+import java.util.Arrays;
+
 public enum Category {
-    JAPAN(1),
-    KOREA(2),
-    CHINA(3),
-    ASIAN(4),
-    WESTERN(5);
+    JAPAN(1, "일식"),
+    KOREA(2, "한식"),
+    CHINA(3, "중식"),
+    ASIAN(4, "아시안"),
+    WESTERN(5, "양식");
+
+    private static final String INVALID_CATEGORY_MESSAGE = "존재하지 않는 카테고리입니다.";
 
     private final Integer command;
+    private final String name;
 
-    Category(Integer command) {
+    Category(Integer command, String name) {
         this.command = command;
+        this.name = name;
+    }
+
+    public static Category of(Integer number) {
+        return Arrays.stream(Category.values())
+                .filter(category -> category.command == number)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_CATEGORY_MESSAGE));
+    }
+
+    public String getName() {
+        return name;
     }
 }
