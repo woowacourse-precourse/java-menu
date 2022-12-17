79369bbd6346d8e11caa53b70cfb1cbf0e422e65
JUHA <khabh@naver.com>
2022-12-17 15:30:29 +0900

    refactor(InputView): 중복 코드 제거

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -9,22 +9,24 @@ import java.util.List;
 import static java.text.MessageFormat.format;
 
 public class InputView {
-    private static final String COACH = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private static final String REQUEST_COACH_NAMES = "코치의 이름을 입력해 주세요. (, 로 구분)";
     private static final String DO_NOT_RECOMMEND = "{0}(이)가 못 먹는 메뉴를 입력해 주세요.";
-    private final OutputView outputView = new OutputView();
-
 
     public List<String> getCoachNames() {
-        System.out.println(COACH);
-        String input = Console.readLine();
+        String input = getInput(REQUEST_COACH_NAMES);
 
         return CoachNameParser.parseName(input);
     }
 
     public List<String> readDoNotRecommendFoodsOf(String name) {
-        System.out.println(format(DO_NOT_RECOMMEND, name));
-        String input = Console.readLine();
+        String input = getInput(format(DO_NOT_RECOMMEND, name));
 
         return FoodsInputParser.parser(input);
+    }
+
+    private String getInput(String message) {
+        System.out.println(message);
+
+        return Console.readLine();
     }
 }
