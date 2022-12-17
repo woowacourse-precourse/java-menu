c45979098807bb86799a81ff6e8517f8b5cc67a8
HaODay <ycis5013@naver.com>
2022-12-17 17:30:17 +0900

    feat(Coach): 각 코치의 메뉴 검증하는 기능 구현
    - 못 먹는 메뉴 제외
    - 다른 요일과 중복되지 않는 메뉴

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -2,7 +2,6 @@ package menu.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class Coach {
     String name;
@@ -11,27 +10,31 @@ public class Coach {
 
     public Coach(String name) {
         this.name = name;
+        cannotEatMenu = new ArrayList<>();
+        alreadyEatMenu = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
 
+    public List<String> getAlreadyEatMenu() {
+        return alreadyEatMenu;
+    }
+
     public void initCannotEatMenu(List<String> cannotEatMenu) {
         this.cannotEatMenu = new ArrayList<>(cannotEatMenu);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Coach coach = (Coach) o;
-        return Objects.equals(name, coach.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
+    public boolean cannotRecommendMenu(String menu) {
+        if (cannotEatMenu.contains(menu)) {
+            return true;
+        }
+        if (alreadyEatMenu.contains(menu)) {
+            return true;
+        }
+        alreadyEatMenu.add(menu);
+        return false;
     }
 }
 
