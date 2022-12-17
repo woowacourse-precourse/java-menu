d92fd07e2111ee16ffc0e0b53869bd20c3035c06
HiiWee <wpdlzhf159@gmail.com>
2022-12-17 14:11:08 +0900

    feat: 코치 도메인 정의

---
+++ src/main/java/menu/domain/Coach.java
@@ -0,0 +1,10 @@
+package menu.domain;
+
+public class Coach {
+    private final String name;
+    private ExcludeMenu excludeMenu;
+
+    public Coach(final String name) {
+        this.name = name;
+    }
+}
