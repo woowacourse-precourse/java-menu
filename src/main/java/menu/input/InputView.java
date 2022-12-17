8237bc89db72cef6a72edf0925612cf59af91fa0
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-12-17 16:06:01 +0900

    feat(InputView): readCoaches 메서드 생성

--- src/main/java/menu/input/InputView.java
+++ src/main/java/menu/input/InputView.java
@@ -1,8 +1,23 @@
 package menu.input;
 
+import camp.nextstep.edu.missionutils.Console;
 import menu.type.ValidationType;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
+
+    public List<String> readCoaches() {
+        String input = Console.readLine();
+        String[] coaches = input.split(",");
+        validate(ValidationType.CHECK_COACH_SIZE, String.valueOf(coaches.length));
+        for (String coach: coaches) {
+            validate(ValidationType.CHECK_NAME_SIZE, String.valueOf(coach.length()));
+        }
+        return Arrays.stream(coaches).collect(Collectors.toList());
+    }
 
     public void validate(ValidationType validationType, String input) {
         if (!ValidationType.validate(validationType, input)) {
