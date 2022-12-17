fb958501fcfeee5b00057b0e5c696f3bfee299c7
신채원 <cw121@naver.com>
2022-12-17 16:45:01 +0900

    feat: 카테고리 출력 구현

--- src/main/java/menu/OutputView.java
+++ src/main/java/menu/OutputView.java
@@ -7,7 +7,25 @@ public class OutputView {
     public void printResult(List<String> categories,List<String> names, List<List<String>> results) {
         System.out.println("메뉴 추천 결과입니다.");
         System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
-
+        printCategories(categories);
+        System.out.println("\n추천을 완료했습니다.");
     }
 
+    public void printCategories(List<String> categories) {
+        categories.add(0,"카테고리");
+        System.out.println(makeResultString(categories));
+    }
+
+    public String makeResultString(List<String> values) {
+        StringBuilder string = new StringBuilder();
+        string.append("[ ");
+        for(int i=0;i<values.size();i++){
+            string.append(values.get(i));
+            if (i != (values.size() - 1)) {
+                string.append(" | ");
+            }
+        }
+        string.append(" ]");
+        return string.toString();
+    }
 }
