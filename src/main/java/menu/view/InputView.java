8f33fefe3482c9f4ef84453473bbe2a7880c4c9d
iamjooon2 <iamjooon2@gmail.com>
2022-12-17 13:40:28 +0900

    feat: 코치가 못먹는 메뉴를 입력받는 기능

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,8 +1,11 @@
 package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InputView {
     OutputView outputView = new OutputView();
@@ -12,6 +15,15 @@ public class InputView {
         String coachString = Console.readLine();
         List<String> coachList = Arrays.asList(coachString.split(","));
         return coachList;
+    }
+
+    public Map<String, List<String>> getHateList(String name){
+        Map<String, List<String>> hateMap = new HashMap<>();
+        outputView.askCoachHate(name);
+        String hateString = Console.readLine();
+        List<String> hateList = Arrays.asList(hateString.split(","));
+        hateMap.put(name, hateList);
+        return hateMap;
     }
 
 }
