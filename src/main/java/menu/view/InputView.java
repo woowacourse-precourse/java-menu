6fd99c88616ff04cea8b99431922c8c47e06c319
hjx66 <hjx66@g.cnu.ac.kr>
2022-12-17 13:55:54 +0900

    feat: 특정 코치가 못 먹는 메뉴 입력 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -19,6 +19,24 @@ public class InputView {
         return coachs;
     }
 
+    public static List<String> readUnableMenu(String coach){
+        OutputView.printUnableMenu(coach);
+        List<String> unableMenus = Arrays.asList(Console.readLine().split(","));
+        try {
+            validateUnableMenuCount(unableMenus);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            unableMenus = readUnableMenu(coach);
+        }
+        return unableMenus;
+    }
+
+    public static void validateUnableMenuCount(List<String> unableMenus){
+        if(unableMenus.size() > 2){
+            throw new IllegalArgumentException("[ERROR] 먹지 못하는 음식은 최대 2개여야 합니다. ");
+        }
+    }
+
     public static void validateCoachNameLength(List<String> coachs) {
         for (String coach:coachs) {
             if(coach.length() < 2 || coach.length() > 4){
@@ -32,5 +50,6 @@ public class InputView {
             throw new IllegalArgumentException("[ERROR] 코치는 최소 2명 이상, 최대 5명 이하로 입력해야 합니다.");
         }
     }
+
 
 }
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -6,4 +6,8 @@ public class OutputView {
         System.out.println("점심 메뉴 추천을 시작합니다.\n\n코치의 이름을 입력해 주세요. (, 로 구분)");
     }
 
+    public static void printUnableMenu(String coach){
+        System.out.println(coach + "(이)가 못 먹는 메뉴를 입력해 주세요.");
+    }
+
 }
