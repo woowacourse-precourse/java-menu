0fead68e23495f9e2d30730968008a368f6a74eb
Hayoon <hy97@sju.ac.kr>
2022-12-17 17:58:43 +0900

    feat(LunchController): 요일별 카테고리 실행 메서드 추가

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -26,6 +26,12 @@ public class OutputView {
         System.out.println("메뉴 추천 결과입니다.");
         System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
         System.out.print("[ 카테고리");
+        List<String> categoryList = new ArrayList<>();
+
+        for (String category : categoryList) {
+            System.out.print(" | " + category);
+        }
+        System.out.println(" ]");
 
         List<String> allNames = coachService.findAllNames();
         for (String name : allNames) {
