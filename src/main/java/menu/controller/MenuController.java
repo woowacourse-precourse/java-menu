8b8a4ac7d9d19a9e126547f3d42e03a3fd22dfc5
yujaemin <woawoa0513@gmail.com>
2022-12-17 14:33:47 +0900

    feat: MenuController 클래스 생성

---
+++ src/main/java/menu/controller/MenuController.java
@@ -0,0 +1,17 @@
+package menu.controller;
+
+import java.util.List;
+import menu.domain.CategoryHistory;
+import menu.domain.Coach;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+public class MenuController {
+
+    private InputView inputView;
+    private OutputView outputView;
+    private CategoryHistory categoryHistory;
+    private List<Coach> coaches;
+
+
+}
