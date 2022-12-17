c17a0df960fba1d4fb7c63ddf72894bd6fda7451
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 15:48:46 +0900

    feat(DayOfWeek): 요일 값을 리스트로 가져오는 기능 추가

--- src/main/java/menu/constants/DayOfWeek.java
+++ src/main/java/menu/constants/DayOfWeek.java
@@ -1,5 +1,9 @@
 package menu.constants;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum DayOfWeek {
     MONDAY("월요일"),
     TUESDAY("화요일"),
@@ -7,9 +11,20 @@ public enum DayOfWeek {
     THURSDAY("목요일"),
     FRIDAY("금요일");
 
+    private static final List<String> dayOfWeek = Stream.of(values())
+            .map(DayOfWeek::getWeekOfDay)
+            .collect(Collectors.toList());
     private final String weekOfDay;
 
     DayOfWeek(String weekOfDay) {
         this.weekOfDay = weekOfDay;
+    }
+
+    private String getWeekOfDay() {
+        return weekOfDay;
+    }
+
+    public List<String> getWeekOfDayList() {
+        return dayOfWeek;
     }
 }
