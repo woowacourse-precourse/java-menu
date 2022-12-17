e0c6b4d0bc3e87028a234b611f949f5e1e639a0d
yubin <dbqls2821@gmail.com>
2022-12-17 17:31:32 +0900

    feat: recommendationService 내부 구현

--- src/main/java/menu/service/RecommendationService.java
+++ src/main/java/menu/service/RecommendationService.java
@@ -17,43 +17,61 @@ public class RecommendationService {
   private final int dayOfWeekNum = 5;
 
   private List<Integer> categoryResult = new ArrayList<>();
-  private List<List<String>> menuResult = new ArrayList<>();
+  private String[][] result;
 
   public String getRecommendationResult(List<Coach> coaches) {
     makeRandomMenu(coaches);
     StringBuilder builder = new StringBuilder();
     List<String> categoriesInKorean = categoryResult.stream().map(Category::getCategoryInKorean).collect(Collectors.toList());
     builder.append("[ 카테고리 | ").append(String.join(" | ", categoriesInKorean)).append(" ]\n");
+    String[][] printResult = new String[coaches.size()][dayOfWeekNum];
     for (int i = 0; i < coaches.size(); i++) {
-      builder.append("[ ").append(coaches.get(i).getName()).append(" | ").append(String.join(" | ", menuResult.get(i))).append(" ]\n");
+      for (int j = 0; j < dayOfWeekNum; j++) {
+        printResult[i][j] = result[j][coaches.size() - 1 - i];
+      }
+    }
+    for (int i = printResult.length-1; i > -1; i--) {
+      builder.append("[ ").append(coaches.get(i).getName()).append(" | ").append(String.join(" | ", printResult[i])).append(" ]\n");
     }
     return builder.toString();
   }
 
-  public List<List<String>> makeRandomMenu(List<Coach> coaches) {
+  public void makeRandomMenu(List<Coach> coaches) {
     makeRandomCategory();
-    initializeMenuResult();
-    for (int i = 0; i < coaches.size(); i++) {
-      List<String> coachHateFood = coaches.get(i).getHateFood();
-      // 카테고리마다 랜덤으로 메뉴를 불러와서 그걸 셔플을 돌려서 0번쨰 인자 가져와
-      // 만약 싫어하는 음식이면 다시 돌려
-      menuResult.add(generateRandomMenu(coachHateFood));
+    result = new String[dayOfWeekNum][coaches.size()];
+    for (int i = 0; i < dayOfWeekNum; i++) {
+      for (int j = 0; j < coaches.size(); j++) {
+        List<String> hateFood = coaches.get(j).getHateFood();
+        String menu = Randoms.shuffle(Menu.getMenus(Category.getCategory(categoryResult.get(i)))).get(0);
+        boolean isExist = false;
+        for (int k = 0; k < result[i].length; k++) {
+          if(result[i][j] == null) break;
+          if (result[i][j].equals(menu) || hateFood.contains(result[i][j])) {
+            isExist = true;
+          }
+        }
+        if (isExist) {
+          --j;
+          continue;
+        }
+        result[i][j] = menu;
+      }
     }
-    return menuResult;
+
   }
 
-  private List<String> generateRandomMenu(List<String> coachHateFood) {
-    List<String> menus = new ArrayList<>();
-    for (int j = 0; j < categoryResult.size(); j++) {
-      String randomMenu = Randoms.shuffle(Menu.getMenus(Category.getCategory(categoryResult.get(j)))).get(0);
-      if (menus.contains(randomMenu) || coachHateFood.contains(randomMenu)) {
-        --j;
-        continue;
-      }
-      menus.add(randomMenu);
-    }
-    return menus;
-  }
+//  private List<String> generateRandomMenu(List<String> coachHateFood) {
+//    List<String> menus = new ArrayList<>();
+//    for (int j = 0; j < categoryResult.size(); j++) {
+//      String randomMenu = Randoms.shuffle(Menu.getMenus(Category.getCategory(categoryResult.get(j)))).get(0);
+//      if (menus.contains(randomMenu) || coachHateFood.contains(randomMenu)) {
+//        --j;
+//        continue;
+//      }
+//      menus.add(randomMenu);
+//    }
+//    return menus;
+//  }
 
   private List<Integer> makeRandomCategory() {
     List<Integer> categoryCount = new ArrayList<>();
@@ -78,9 +96,4 @@ public class RecommendationService {
     }
   }
 
-  private void initializeMenuResult() {
-    for (int i = 0; i < menuResult.size(); i++) {
-      menuResult.add(new ArrayList<>());
-    }
-  }
 }
