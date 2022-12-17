f978912601db8b4d9d0ffd146a45f8bffff84c8e
parksey <parkseyeon99@naver.com>
2022-12-17 13:28:46 +0900

    feat(Menu): 메뉴 리스트들을 담은 메뉴 클래스 추가

---
+++ src/main/java/menu/domain/Menu.java
@@ -0,0 +1,21 @@
+package menu.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Menu {
+    public Map<String, List<String>> menus;
+
+    public Menu() {
+        menus = new HashMap<>();
+    }
+
+    public void addMenu(String name, List<String> menuList) {
+        menus.put(name, menuList);
+    }
+
+    public Map<String, List<String>> getMenus() {
+        return menus;
+    }
+}
