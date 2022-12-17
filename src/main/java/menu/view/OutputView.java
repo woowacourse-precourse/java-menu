f845989e94d16a41e0c4d63b8c42e390fe55b16e
iamjooon2 <iamjooon2@gmail.com>
2022-12-17 16:07:14 +0900

    feat: 추천 결과를 출력하는 기능

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,5 +1,7 @@
 package menu.view;
 
+import java.util.List;
+
 public class OutputView {
 
     public void initMessage() {
@@ -12,4 +14,25 @@ public class OutputView {
         System.out.printf("%s(이)가 못 먹는 메뉴를 입력해 주세요.\n", name);
     }
 
+    public void printResultInit() {
+        System.out.println("메뉴 추천 결과입니다.\n"
+                + "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
+    }
+
+    public void printCategory(List<String> categories) {
+        String message = "[ 카테고리 | ";
+        for (int i = 0; i < categories.size(); i++) {
+            message += categories.get(i) + " | ";
+        }
+        System.out.println(message.substring(0, message.length() - 2) + "]");
+    }
+
+    public void printCoachMenu(String coachName, List<String> chosenMenu) {
+        String message = "[ ";
+        message += coachName + " | ";
+        for (int i = 0; i < chosenMenu.size(); i++) {
+            message += chosenMenu.get(i) + " | ";
+        }
+        System.out.println(message.substring(0, message.length() - 2) + "]");
+    }
 }
