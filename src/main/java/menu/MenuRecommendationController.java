24194e59f6843931375127cd7f0b2a61de3a1f7e
cpot5620 <=cpot5620@naver.com>
2022-12-17 15:41:58 +0900

    feat(controller): create class to manage program

--- src/main/java/menu/MenuRecommendationController.java
+++ src/main/java/menu/MenuRecommendationController.java
@@ -1,2 +1,24 @@
-package menu;public class MenuRecommendationController {
+package menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static menu.View.*;
+
+public class MenuRecommendationController {
+    MenuRecommendation recommendation;
+    List<Coach> coaches = new ArrayList<>();
+    List<Category> categories = new ArrayList<>();
+
+    public MenuRecommendationController() {
+        recommendation = new MenuRecommendation();
+    }
+
+    private void makeCoaches(List<String> coachNames) {
+        for (int i = 0; i < coachNames.size(); i++) {
+            Coach coach = new Coach(coachNames.get(i));
+            coaches.add(coach);
+        }
+    }
+
 }
