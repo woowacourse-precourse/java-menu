1fdd6c1ca3635dd4333303192aa7f598954ec94b
kimdahee0815 <diana815@naver.com>
2022-12-17 15:28:27 +0900

    feat(pickRandomFood) : 무작위하게 음식을 뽑는 과정에서 인원수만큼 각각 뽑음
    뽑는 과정에서 각 카테고리 별로 음식을 한사람당 월,화,수,목,금 5번 뽑고, 다음 사람도 계속 해서 뽑아감. (pickRandomFoodForEachCategoryForPerson, findCategoryIndex)
    하지만, 그 사람이 먹을 수 없는 음식은 제외해야 함(checkIfNotEatingFood)

--- src/test/java/menu/RecommendMenu.java
+++ src/test/java/menu/RecommendMenu.java
@@ -9,6 +9,7 @@ public class RecommendMenu {
 	ArrayList<String> coachName;
 	LinkedHashMap<String, ArrayList<String>> notEatingMenuForEachCoach;
 	ArrayList<String> categoryForWeek = new ArrayList<>();
+	ArrayList<String[]> menu = new ArrayList<>();
 	
 	enum categories {
 		JAPANESE("일식", 1), KOREAN("한식", 2), CHINESE("중식", 3), ASIAN("아시안", 4), WESTERN("양식", 5);
@@ -31,6 +32,12 @@ public class RecommendMenu {
 	public RecommendMenu(ArrayList<String> coachName, LinkedHashMap<String, ArrayList<String>> notEatingMenuForEachCoach ) {
 		this.coachName = coachName;
 		this.notEatingMenuForEachCoach = notEatingMenuForEachCoach;
+		menu.add(new String[0]);
+		menu.add(new String[] {"규동","우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼"});
+		menu.add(new String[] { "김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"});
+		menu.add(new String[] { "깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채"});
+		menu.add(new String[] { "팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜"});
+		menu.add(new String[] { "라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"});
 	}
 	
 	
@@ -82,5 +89,38 @@ public class RecommendMenu {
 		return category;
 	}
 	
+	public void pickRandomFood() {
+		for(int person = 0; person < coachName.size(); person++) {
+			
+		}
+	}
+	
+	public void pickRandomFoodForEachCategoryForPerson() {
+		String category = "";
+		int indexOfCategory = 0;
+		int randFoodIndex = 0;
+		for(int i = 0; i < categoryForWeek.size(); i++) {
+			category = categoryForWeek.get(i);
+			indexOfCategory = findCategoryIndex(category);
+			randFoodIndex = Randoms.pickNumberInRange(0, menu.get(indexOfCategory).length - 1);
+			
+		}
+	}
+	
+	public int findCategoryIndex(String category) {
+		int categoryindex = 0;
+		for(categories c : categories.values()) {
+			if(c.getCategory().equals(category)) {
+				categoryindex = c.getValue();
+			}
+		}
+		return categoryindex;
+	}
+	
+	public void checkIfNotEatingFood(int person, int indexOfCategory, int randFoodIndex) {
+		String food = "";
+		food = menu.get(indexOfCategory)[randFoodIndex];
+		
+	}
 	
 }
