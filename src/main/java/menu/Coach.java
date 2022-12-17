e1e626a2f97cb2e6254e6aa1bf300d965e3d1dd4
jaeyeon kim <jakind@naver.com>
2022-12-17 14:06:03 +0900

    feat(Coach): 코치 클래스에 예외 처리 로직 및 추천 메뉴 등록 기능 추가

--- src/main/java/menu/Coach.java
+++ src/main/java/menu/Coach.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Coach {
+    private final String DUPLICATE_HATE_FOOD = "이미 등록된 싫어하는 음식입니다.";
     private final String name;
     private Set<String> hateFood;
     private Set<String> recommendedCategory;
@@ -16,15 +17,33 @@ public class Coach {
         recommendedFood = new HashSet<>();
     }
 
+    public void setHateFood(String food) {
+        if (hateFood.contains(food)) {
+            hateFood.clear(); // 잘 못 입력 받는 경우 이전에 받은 것들도 다 날려야하기 때문에, clear 를 해준다.
+            throw new IllegalArgumentException(SystemConstant.ERROR + DUPLICATE_HATE_FOOD);
+        }
+    }
+
     public boolean isHateFood(String food) {
         return hateFood.contains(food); // 있으면 hate food 임
     }
 
-    public boolean isRecommendedFood(String food) {
-        return recommendedFood.contains(food);
+    public boolean isPossibleRecommendCategory(String category) {
+        Set<String> tempRecommendCategory = new HashSet<>(recommendedCategory);
+        tempRecommendCategory.add(category);
+
+        return tempRecommendCategory.size() <= 2; // 이러면 추천 가능한 것
     }
 
-    public boolean isRecommendedCategory(String category) {
-        return recommendedCategory.contains(category);
+    public boolean isPossibleRecommendFood(String food) {
+        return !recommendedFood.contains(food); // true 이면 추천 가능한 것
+    }
+
+    public void recommendCategory(String category) {
+        recommendedCategory.add(category);
+    }
+
+    public void recommendFood(String food) {
+        recommendedFood.add(food);
     }
 }
