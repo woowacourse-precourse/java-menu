522dcf9eb3c1bfe5047786c667b8dea15eb9a6d3
youngkyunIm <yee0230@naver.com>
2022-12-17 13:39:13 +0900

    feat(Coaches) : 최소 2명, 최대 5명 함께 식사

--- src/main/java/menu/Coaches.java
+++ src/main/java/menu/Coaches.java
@@ -1,16 +1,32 @@
 package menu;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Coaches {
 
     private final List<Coach> coaches;
 
-    public Coaches(List<Coach> coaches) {
-        this.coaches = coaches;
+    public Coaches(List<String> coaches) {
+        validCoaches(coaches);
+        this.coaches = listToCoaches(coaches);
     }
 
     public List<Coach> getCoaches() {
         return this.coaches;
+    }
+
+    private void validCoaches(List<String> coaches) {
+        if (coaches.size() < 2 || coaches.size() > 5) {
+            throw new IllegalArgumentException("코치 맴버는 2명 ~ 5명을 입력해야 합니다.");
+        }
+    }
+
+    private List<Coach> listToCoaches(List<String> coaches) {
+        List<Coach> tmpCoaches = new ArrayList<>();
+        for (String name : coaches) {
+            tmpCoaches.add(new Coach(name));
+        }
+        return tmpCoaches;
     }
 }
