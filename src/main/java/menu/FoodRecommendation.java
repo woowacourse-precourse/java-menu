e7df7a93a5b5cccd4045d347cb4463dce8fe654e
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2022-12-17 16:14:19 +0900

    feat(menu) : 한 주에 같은 카테고리를 2번으로 제한

--- src/main/java/menu/FoodRecommendation.java
+++ src/main/java/menu/FoodRecommendation.java
@@ -10,10 +10,23 @@ public class FoodRecommendation {
 
     public FoodRecommendation(List<Coach> coaches) {
         this.coaches = coaches;
-        for(Week week : Week.values()) {
-            Category category = Category.getCategoryByOrder(Randoms.pickNumberInRange(1, 5));
-            categoryByWeek.put(week, category);
+        for (Week week : Week.values()) {
+            categoryByWeek.put(week, getRandomCategory());
         }
+    }
+
+    private int countContains(Category c) {
+        return (int) categoryByWeek.values().stream()
+                .filter(category -> category.getCategoryName().equals(c.getCategoryName()))
+                .count();
+    }
+
+    private Category getRandomCategory() {
+        Category category = Category.getCategoryByOrder(Randoms.pickNumberInRange(1, 5));
+        while (countContains(category) > 1) {
+            category = Category.getCategoryByOrder(Randoms.pickNumberInRange(1, 5));
+        }
+        return category;
     }
 
     public void recommend() {
@@ -23,7 +36,7 @@ public class FoodRecommendation {
     }
 
     private void recommendByWeek(Week week) {
-        for(Coach coach : coaches) {
+        for (Coach coach : coaches) {
             Category category = categoryByWeek.get(week);
             String food = category.getRandomFood();
             coach.recommendFood(food);
