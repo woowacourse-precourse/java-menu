ba0673e5210c8dd1f79ee0aea102652af19ec9a1
은지 <midzy0228@gmail.com>
2022-12-17 15:33:03 +0900

    feat: 코치는 최소 2명까지 최대5명까지 제한 기능 구현

--- src/main/java/menu/model/CoachName.java
+++ src/main/java/menu/model/CoachName.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 public class CoachName {
 
-    private static final Integer MIN_NAME_SIZE = 2;
-    private static final Integer MAX_NAME_SIZE = 4;
+    private static final Integer MIN_COACH_SIZE = 2;
+    private static final Integer MAX_COACH_SIZE = 5;
+    private static final Integer MIN_NAME_LENGTH = 2;
+    private static final Integer MAX_NAME_LENGTH = 4;
 
     private final List<String> coachNames;
 
@@ -19,6 +21,10 @@ public class CoachName {
     }
 
     private void validate(List<String> coachNames) {
+        if (!isValidCoachSize(coachNames)) {
+            throw new IllegalArgumentException("코치는 최소 2명에서 5명까지 식사가 가능합니다.");
+        }
+
         for (String coachName : coachNames) {
             if (!isValidCoachNameLength(coachName)) {
                 throw new IllegalArgumentException("코치의 이름은 최소 2글자, 최대 4글자 입니다.");
@@ -27,6 +33,10 @@ public class CoachName {
     }
 
     private boolean isValidCoachNameLength(String name) {
-        return MIN_NAME_SIZE <= name.length() && name.length() <= MAX_NAME_SIZE;
+        return MIN_NAME_LENGTH <= name.length() && name.length() <= MAX_NAME_LENGTH;
+    }
+
+    private boolean isValidCoachSize(List<String> coachNames) {
+        return MIN_COACH_SIZE <= coachNames.size() && coachNames.size() <= MAX_COACH_SIZE;
     }
 }
---
+++ src/main/java/menu/model/RecommendMachine.java
@@ -0,0 +1,2 @@
+package menu.model;public class RecommendMachine {
+}
