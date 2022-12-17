8e2ad0da8555ecc716de075a3db992962151816e
jyeost <pig9619@naver.com>
2022-12-17 17:55:07 +0900

    test : 테스트 코드 에러 해결 못함

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -14,7 +14,7 @@ public class OutputView implements Output {
 
     @Override
     public void printStart() {
-        System.out.println("점심 메뉴 추천을 시작합니다." + System.lineSeparator());
+        System.out.println("점심 메뉴 추천을 시작합니다.");
     }
 
     @Override
