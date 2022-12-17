92a70298497bce2a3f26c792659f0d4498d064ff
euna <christinejung10@daum.net>
2022-12-17 15:00:53 +0900

    fix(CategoryService): add field recommendCategoryNumbers and fix methods

--- src/main/java/menu/category/CategoryService.java
+++ src/main/java/menu/category/CategoryService.java
@@ -12,20 +12,21 @@ import java.util.Set;
 
 public class CategoryService {
 
+    private static List<Integer> recommendCategoryNumbers = new ArrayList<>(); // 주별 카테고리 추천 (숫자로 저장)
+
     // 못 먹는 메뉴 기반 랜덤 추천
     public static void recommendMenu() {
         List<Coach> coaches = CoachService.getCoaches();
+        recommendCategories();
         for (Coach coach : coaches) {
-            List<Integer> recommendCategoriesToCoach = setRecommendCategoriesToCoach(coach);
-            List<String> recommendMenusToCoach = setRecommendMenusToCoach(coach, recommendCategoriesToCoach);
+            List<String> recommendMenusToCoach = setRecommendMenusToCoach(coach);
             coach.setRecommendedMenu(recommendMenusToCoach);
         }
         OutputView.printRecommendResult(coaches);
     }
 
-    // 코치별 카테고리 랜덤 추천
-    private static List<Integer> setRecommendCategoriesToCoach(Coach coach) {
-        // 카테고리 추천 (한 주에 같은 카테고리는 최대 2회)
+    // 카테고리 랜덤 추천 (한 주에 같은 카테고리는 최대 2회)
+    private static void recommendCategories() {
         List<Integer> recommendCategories = new ArrayList<>();
         while (true) {
             for (int i = 0; i < 5; i++) {
@@ -35,16 +36,16 @@ public class CategoryService {
             if (recommendCategoryNumbers.size() > 3) break; // 추천 카테고리 숫자들에 대한 set 개수가 3 이하 -> 3회 이상 중복된 것
             recommendCategories = new ArrayList<>(); // break 하지 않을 경우 초기화
         }
-        return recommendCategories;
+        recommendCategoryNumbers = recommendCategories;
     }
 
     // 코치별 메뉴 랜덤 추천 (중복되지 않도록)
-    private static List<String> setRecommendMenusToCoach(Coach coach, List<Integer> recommendCategoriesToCoach) {
+    private static List<String> setRecommendMenusToCoach(Coach coach) {
         List<String> recommendMenus = new ArrayList<>();
         List<String> cannotEats = coach.getMenusCannotEats();
 
-        for (Integer recommendCategory : recommendCategoriesToCoach) {
-            List<String> menusOfCategory = Category.getCategoryMenusByNumber(recommendCategory);
+        for (Integer recommendCategoryNumber : recommendCategoryNumbers) {
+            List<String> menusOfCategory = Category.getCategoryMenusByNumber(recommendCategoryNumber);
             String randomMenu;
             while (true) {
                 int randomNumber = Randoms.pickNumberInRange(1, menusOfCategory.size() - 1);
