0ad66d0da8f7a9d362c76e911bc14d4e9362c58d
hectick <chyeon1223@cau.ac.kr>
2022-12-17 14:13:44 +0900

    fix: 자료형 잘못된거 수정

--- src/test/java/menu/constant/Day.java
+++ src/test/java/menu/constant/Day.java
@@ -3,24 +3,30 @@ package menu.constant;
 import java.util.List;
 
 public enum Day {
-    MONDAY("월요일"),
-    TUESDAY("화요일"),
-    WEDNESDAY("수요일"),
-    THURSDAY("목요일"),
-    FRIDAY("금요일");
+    MONDAY("월요일", 0),
+    TUESDAY("화요일", 1),
+    WEDNESDAY("수요일", 2),
+    THURSDAY("목요일", 3),
+    FRIDAY("금요일", 4);
 
     private String name;
-    private static final Category[] days = Category.values();
+    private int order;
+    private static final Day[] days = Day.values();
 
-    Day(String name) {
+    Day(String name, int order) {
         this.name = name;
+        this.order = order;
     }
 
     public String getName(){
         return name;
     }
 
-    public static Category get(int index){
+    public int getOrder(){
+
+    }
+
+    public static Day get(int index){
         return days[index];
     }
 
