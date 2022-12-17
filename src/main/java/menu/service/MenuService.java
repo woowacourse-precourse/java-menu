9a789217d6f60782b2a9e496577add8c2d83e30a
yujaemin <woawoa0513@gmail.com>
2022-12-17 15:15:10 +0900

    feat: MenuService 클래스 생성

---
+++ src/main/java/menu/service/MenuService.java
@@ -0,0 +1,12 @@
+package menu.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import menu.domain.Category;
+import menu.domain.Coach;
+import menu.domain.Menu;
+
+public class MenuService {
+
+
+}
