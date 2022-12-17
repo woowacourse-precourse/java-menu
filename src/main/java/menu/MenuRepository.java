18aa1a85e8be47913105de2d3e3ea5c5b403c78f
hodomaroo <enghemdwkd@gmail.com>
2022-12-17 17:12:38 +0900

    (fear/MenuRepository) 메뉴 저장소 역할을 하는 MenuRepository 구현

--- src/main/java/menu/MenuRepository.java
+++ src/main/java/menu/MenuRepository.java
@@ -3,18 +3,28 @@ package menu;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import menu.domain.Category;
 import menu.domain.Menu;
+import net.bytebuddy.asm.Advice.OffsetMapping.ForInstrumentedMethod;
 
 public class MenuRepository {
 
-    private static Map<Category, List<Menu>> categoryMenuMap = new HashMap<>();
+    private static Map<String, Menu> menuMap = new HashMap<>();
+
+    public static Menu getMenusByName(String menu) {
+        if(!menuMap.containsKey(menu)){
+            throw new IllegalArgumentException(ErrorFormat.INVALID_MENU_NAME.getMessage());
+        }
+        return menuMap.get(menu);
+    }
 
     public static List<Menu> getMenusByCategory(Category category) {
-        return categoryMenuMap.get(category);
+        return menuMap.values().stream().filter((menu) -> menu.getCategory().equals(category))
+                .collect(Collectors.toUnmodifiableList());
     }
 
     public static void addMenu(Menu menu) {
-        categoryMenuMap.get(menu.getCategory()).add(menu);
+        menuMap.put(menu.getName(), menu);
     }
 }
