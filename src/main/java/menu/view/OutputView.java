7a61627eef51b60da356dffb1590a959e78f3bf6
kokodak <kokodakadokok@gmail.com>
2022-12-17 15:33:50 +0900

    feat(outputView): 메뉴 추천 결과 메서드 일부 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,5 +1,7 @@
 package menu.view;
 
+import menu.domain.Coaches;
+
 public class OutputView {
 
     public static void printStartMessage() {
@@ -8,5 +10,11 @@ public class OutputView {
 
     public static void printEndMessage() {
         System.out.println("추천을 완료했습니다.");
+    }
+
+    public static void printRecommendedResult(Coaches coaches) {
+        System.out.println("메뉴 추천 결과입니다.\n"
+                + "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
+
     }
 }
