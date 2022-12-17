3623204546f99e48cf861d01814bf7413fbf37a9
Woojin Lee <dhkep03@gmail.com>
2022-12-17 14:04:09 +0900

    feat(Coach): 코치 생성

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,4 +1,19 @@
 package menu.domain;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import menu.domain.constant.Day;
+
 public class Coach {
+    private final String name;
+    private final List<String> inedibleFoods;
+    private final Map<Day, String> weeklyFoods;
+
+    public Coach(String name) {
+        this.name = name;
+        this.inedibleFoods = new ArrayList<>();
+        this.weeklyFoods = new LinkedHashMap<>();
+    }
 }
