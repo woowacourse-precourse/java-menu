5ff289b0f445a5ec08a75640127b15b3d3312b33
sosow0212 <sosow0212@naver.com>
2022-12-17 16:37:31 +0900

    test : (CategoriesTest) 카테고리 3개 이상 있을 때 오류 테스트

--- src/test/java/menu/domain/CategoriesTest.java
+++ src/test/java/menu/domain/CategoriesTest.java
@@ -3,6 +3,7 @@ package menu.domain;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.List;
+import menu.domain.menu.FoodStyle;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -24,4 +25,20 @@ public class CategoriesTest {
         assertThat(categories.getCategories()).isEqualTo(List.of(category1, category2));
     }
 
+    @Test
+    @DisplayName("카테고리 3개 이상 있을 시에 오류 테스트")
+    public void isFullOfCategory() {
+        // given
+        Categories categories = new Categories();
+        String category1 = "한식";
+        String category2 = "한식";
+        categories.addCategory(category1);
+        categories.addCategory(category2);
+
+        // when
+        boolean result = categories.isFullOfCategory(FoodStyle.getFoodStyle(2)); // 한식
+
+        // then
+        assertThat(result).isEqualTo(true);
+    }
 }
