4037c8c5ba1deeac7fe967407fb21e3603e26719
JunH <tjdtls690@gmail.com>
2022-12-17 13:33:33 +0900

    :sparkles: feat(Category) : 번호에 따른 존재하는 카테고리 가져오기 기능 구현

--- src/main/java/menu/domain/category/Category.java
+++ src/main/java/menu/domain/category/Category.java
@@ -1,5 +1,7 @@
 package menu.domain.category;
 
+import java.util.Arrays;
+
 public enum Category {
     JAPANESE_FOOD(1),
     KOREAN_FOOD(2),
@@ -11,5 +13,16 @@ public enum Category {
     
     Category(int categoryNumber) {
         this.categoryNumber = categoryNumber;
+    }
+    
+    public static Category findByCategoryNumber(int categoryNumber) {
+        return Arrays.stream(values())
+                .filter(category -> category.isSameCategoryNumber(categoryNumber))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 존재하지 않는 카테고리입니다."));
+    }
+    
+    private boolean isSameCategoryNumber(int categoryNumber) {
+        return this.categoryNumber == categoryNumber;
     }
 }
---
+++ src/test/java/menu/domain/category/CategoryTest.java
@@ -0,0 +1,15 @@
+package menu.domain.category;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+    
+
+}
