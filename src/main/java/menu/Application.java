50e8e282337616205d3475286c3112c0ef2e41af
Go-Jaecheol <gojaech@naver.com>
2022-12-17 15:27:25 +0900

    refactor: initializeBan 메서드에서 getBanMenu 메서드 분리

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,12 +1,13 @@
 package menu;
 
 import menu.controller.MenuController;
+import menu.service.BanService;
 import menu.service.CoachService;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        MenuController menuController = new MenuController(new CoachService());
+        MenuController menuController = new MenuController(new CoachService(), new BanService());
         menuController.start();
     }
 }
--- src/main/java/menu/service/BanService.java
+++ src/main/java/menu/service/BanService.java
@@ -2,6 +2,7 @@ package menu.service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import menu.domain.Ban;
 import menu.domain.Coach;
@@ -12,9 +13,13 @@ public class BanService {
     public Ban initializeBan(Coach coach) {
         Map<String, ArrayList<String>> banResults = new HashMap<>();
         for (String coachName : coach.getCoachNames()) {
-            OutputView.messageBanInput(coachName);
-            banResults.put(coachName, (ArrayList<String>) InputView.readBanMenu());
+            banResults.put(coachName, (ArrayList<String>) getBanMenu(coachName));
         }
         return new Ban(banResults);
+    }
+
+    private List<String> getBanMenu(String coachName) {
+        OutputView.messageBanInput(coachName);
+        return InputView.readBanMenu();
     }
 }
