bd255a47f56bc463e5e0340b986a6adb05108232
thdwoqor <celebarte01@gmail.com>
2022-12-17 14:36:03 +0900

    feat: 'MainController' 생성

---
+++ src/main/java/menu/controller/MainController.java
@@ -0,0 +1,29 @@
+package menu.controller;
+
+import menu.model.Coach;
+import menu.model.Menu;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public MainController(InputView inputView, OutputView outputview) {
+        this.inputView = inputView;
+        this.outputView = outputview;
+    }
+
+    public void service() {
+        List<Coach> coachs= new ArrayList<>();
+        outputView.startMessage();
+        inputView.readCoachName().stream()
+                .forEach(e -> coachs.add(new Coach(e, inputView.readCantEats(e))));
+        outputView.resultMessage();
+        List<String> categorys = Menu.selectCategory();
+        outputView.categoryMessage(categorys);
+    }
+}
