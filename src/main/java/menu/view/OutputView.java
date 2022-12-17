71994b8e922ce2017768d92297cc516c28585a3b
euna <christinejung10@daum.net>
2022-12-17 13:27:00 +0900

    feat(OutputView): methods to print start, end message

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -2,10 +2,15 @@ package menu.view;
 
 public class OutputView {
 
+    private static final String START_MESSAGE = "점심 메뉴 추천을 시작합니다.\n";
+    private static final String END_MESSAGE = "추천을 완료했습니다.\n";
+
     public static void printStartMessage() {
+        System.out.println(START_MESSAGE);
     }
 
     public static void printEndMessage() {
+        System.out.println(END_MESSAGE);
     }
 
 }
