a44fd4f42f525f951d0ce1d903cda2012e2574ae
CAEL0 <kjongmin26@gmail.com>
2022-12-17 14:20:00 +0900

    feat(Coach): 코치 이름 길이에 대한 검증 삭제

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,12 +1,9 @@
 package menu.domain;
 
-import menu.util.Validator;
-
 public class Coach {
     private final String name;
 
     public Coach(String name) {
-        Validator.validateCoachNameLength(name.length());
         this.name = name;
     }
 }
