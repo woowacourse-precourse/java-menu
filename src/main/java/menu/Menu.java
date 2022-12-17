5ad17d4922182e640c9704554e0c25fa42a700c2
ingpyo <juninpyo72@gmail.com>
2022-12-17 13:46:45 +0900

    feat:메뉴를 전달해주는 기능

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -16,4 +16,8 @@ public class Menu {
 		menus.add(Arrays.asList("팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜"));
 		menus.add(Arrays.asList("라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니"));
 	}
+
+	public List<String> getMenus(int index) {
+		return menus.get(index);
+	}
 }
