494f331910c68a65d3bccd747c37f3dc859b0889
ksh-beginner <ohk9134@naver.com>
2022-12-17 14:36:04 +0900

    feat(OutputView) : 코치 이름 입력 문구 출력 기능 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -3,9 +3,15 @@ package menu.view;
 public class OutputView {
 
     private static final String LUNCH_MENU_RECOMMEND_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    private static final String READ_COACH_NAME_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
 
     public void printLunchMenuRecommendMessage() {
         printMessage(LUNCH_MENU_RECOMMEND_MESSAGE);
+    }
+
+    public void printReadCoachNameMessage() {
+        printBlankLine();
+        printMessage(READ_COACH_NAME_MESSAGE);
     }
 
     private void printBlankLine() {
