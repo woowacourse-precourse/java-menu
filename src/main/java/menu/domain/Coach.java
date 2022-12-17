92d867f20e5967b2a55d159bcab77c825404b4c5
오승윤 <>
2022-12-17 17:20:22 +0900

    feat (Coach) : 먹을 수 없는 메뉴를 중복으로 입력하지 않도록 검증하는 로직 추가

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -11,6 +11,7 @@ public class Coach {
     private static final String RANGE_ERROR_MSG = "이름은 2-4 글자여야 합니다";
     private static final String INEDIBLES_RANGE_ERROR_MSG = "최대 2개의 메뉴만 입력 가능합니다";
     private static final String INEDIBLES_NOT_EXIST_MENU_ERROR_MSG = "없는 메뉴입니다";
+    private static final String DOUPLICATE__MENU_ERROR_MSG = "중복된 메뉴를 입력했습니다";
     private static final Integer MIN_SIZE_NAME = 2;
     private static final Integer MAX_SIZE_NAME = 4;
 
@@ -55,6 +56,7 @@ public class Coach {
 
         validateInediblesExist(input_inedibles);
         validateInediblesSize(input_inedibles);
+        validateDuplicate(input_inedibles);
 
         inedibles = input_inedibles;
     }
@@ -69,6 +71,12 @@ public class Coach {
     private void validateInediblesSize(List<String> input_inedibles_List){
         if(input_inedibles_List.size()>MAX_SIZE_INEDIBLES)
             throw new IllegalArgumentException(INEDIBLES_RANGE_ERROR_MSG);
+    }
+
+    private void validateDuplicate(List<String> input_inedibles){
+        int origin_size = input_inedibles.size();
+        if(input_inedibles.stream().distinct().count() != origin_size)
+            throw new IllegalArgumentException(DOUPLICATE__MENU_ERROR_MSG);
     }
 
     public List<String> getInedibles() {
