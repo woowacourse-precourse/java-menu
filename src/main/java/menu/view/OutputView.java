d75acd2d1c591fb316bf3541c60317ae17ccd3b7
kat8813 <ohseo8813@gmail.com>
2022-12-17 14:45:33 +0900

    <feat>(OutputView.java): 각 코치들의 이번주 추천 메뉴를 출력

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,8 +1,31 @@
 package menu.view;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import menu.dto.CoachesWeeklyMenu;
+
 public class OutputView {
 
     public void printAppRunMessage() {
         System.out.println("점심 메뉴 추천을 시작합니다.");
+    }
+
+    public void printWeeklyRecommendMenu(CoachesWeeklyMenu coachesWeeklyMenu) {
+        System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
+        printWeeklyMenuCategories(coachesWeeklyMenu);
+        coachesWeeklyMenu.getEachCoachesMenu()
+                .entrySet()
+                .forEach(this::printEachCoachesWeeklyMenu);
+    }
+
+    private void printEachCoachesWeeklyMenu(Entry<String, List<String>> entry) {
+        String menus = String.join(" | ", entry.getValue());
+        System.out.println("[ "+ entry.getKey() +" |" + menus + " ]");
+    }
+
+    private void printWeeklyMenuCategories(CoachesWeeklyMenu coachesWeeklyMenu) {
+        String categories = String.join(" | ", coachesWeeklyMenu.getCategoriesNames());
+        System.out.println("[ 카테고리" + categories + " ]");
     }
 }
