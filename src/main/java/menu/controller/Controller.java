e6eacd48d58dbe39ac909ea3b40c891fa9beee75
0chil <0@chll.it>
2022-12-17 15:15:30 +0900

    feat(Controller): 코치 이름과 못먹는 음식으로 코치들을 만든다

---
+++ src/main/java/menu/controller/Controller.java
@@ -0,0 +1,37 @@
+package menu.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import menu.constant.Food;
+import menu.domain.Coach;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+public class Controller {
+
+    private static InputView INPUT_VIEW = new InputView();
+    private static OutputView OUTPUT_VIEW = new OutputView();
+
+    public void run() {
+        readCoaches();
+    }
+
+    private List<Coach> readCoaches() {
+        List<String> coachNames = INPUT_VIEW.readNames();
+        List<Coach> coaches = new ArrayList<>();
+        for (String coachName : coachNames) {
+            List<Food> inedibles = readInedibles(coachName);
+            coaches.add(new Coach(coachName, inedibles));
+        }
+        return coaches;
+    }
+
+    private List<Food> readInedibles(String coachName) {
+        return INPUT_VIEW.readInedibleOf(coachName).stream()
+                .map(Food::from)
+                .collect(Collectors.toList());
+    }
+}
