4341976c1a8ea48c8610d0a408f99465243f1c44
CAEL0 <kjongmin26@gmail.com>
2022-12-17 14:41:49 +0900

    feat(Coach): 이미 먹은 메뉴 추가

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -3,6 +3,7 @@ package menu.domain;
 public class Coach {
     private final String name;
     private final InedibleMenu inedibleMenu;
+    private final EatenMenu eatenMenu = new EatenMenu();
 
     public Coach(String name, String inedibleMenu) {
         this.inedibleMenu = new InedibleMenu(inedibleMenu);
