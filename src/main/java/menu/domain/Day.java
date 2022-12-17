49696c620b8d8082fbff29458179dbb293d98f18
sonsoowon <yuwoo1018@gmail.com>
2022-12-17 16:01:40 +0900

    feat(Day): 요일 관리 클래스 추가

--- src/main/java/menu/domain/Day.java
+++ src/main/java/menu/domain/Day.java
@@ -1,4 +1,24 @@
 package menu.domain;
 
+import java.util.List;
+
 public enum Day {
+
+    MONDAY("월요일", 0),
+    TUESDAY("화요일", 1),
+    WEDNESDAY("수요일", 2),
+    THURSDAY("목요일", 3),
+    FRIDAY("금요일", 4);
+
+    private final String name;
+    private final int index;
+
+    Day(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
