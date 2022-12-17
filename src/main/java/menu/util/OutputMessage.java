9fa80200dec94c2fd61ef13d58866e7c8fc95d16
HaODay <ycis5013@naver.com>
2022-12-17 13:37:50 +0900

    feat(OutputMessage): 출력 메시지 상수를 위한 클래스 생성

---
+++ src/main/java/menu/util/OutputMessage.java
@@ -0,0 +1,8 @@
+package menu.util;
+
+public class OutputMessage {
+
+    public static final String START_PROGRAM="점심 메뉴 추천을 시작합니다.";
+    public static final String RECOMMEND_RESULT="메뉴 추천 결과입니다.";
+    public static final String FINISH_RECOMMEND="추천을 완료했습니다.";
+}
