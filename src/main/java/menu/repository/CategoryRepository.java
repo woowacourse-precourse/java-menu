ddc43e88c10a5f762e27d31f6751809c3e3e6022
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 13:47:15 +0900

    feat(CategoryRepositoy): 필드 생성자 추가

--- src/main/java/menu/repository/CategoryRepository.java
+++ src/main/java/menu/repository/CategoryRepository.java
@@ -1,11 +1,20 @@
 package menu.repository;
 
+import menu.domain.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryRepository {
     private static class InstanceHolder {
         private static final CategoryRepository INSTANCE = new CategoryRepository();
     }
 
-    private CategoryRepository(){}
+    private List<Category> categories;
+
+    private CategoryRepository() {
+        categories = new ArrayList<>();
+    }
 
     public static CategoryRepository getInstance() {
         return CategoryRepository.InstanceHolder.INSTANCE;
