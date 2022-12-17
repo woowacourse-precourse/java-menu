febc2e8516e2a1ebb54d20bd31687040481f8342
YunDaeHyeok <bmong4mong0318@gmail.com>
2022-12-17 15:54:05 +0900

    feat: 점심 추천 결과 출력을 위한 클래스 생성

---
+++ src/main/java/menu/controller/LunchSuggestion.java
@@ -0,0 +1,51 @@
+package menu.controller;
+
+import java.util.ArrayList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LunchSuggestion {
+
+    private final Map<Coach, List<String>> lunchSuggestion;
+
+    public LunchSuggestion() {
+        this.lunchSuggestion = new HashMap<>();
+
+    }
+
+    public void addSuggestion(Coach coach, String food) {
+        List<String> foods = null;
+        if (!lunchSuggestion.containsKey(coach)) {
+            foods = new ArrayList<>();
+            foods.add(food);
+        }
+        if (lunchSuggestion.containsKey(coach)) {
+            foods = lunchSuggestion.get(coach);
+            foods.add(food);
+        }
+        lunchSuggestion.put(coach, foods);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Coach coach : lunchSuggestion.keySet()) {
+            stringBuilder.append("[ ");
+            stringBuilder.append(coach.getName());
+            stringBuilder.append(" | ");
+
+            List<String> foods = lunchSuggestion.get(coach);
+            for (int i = 0; i < foods.size(); i++){
+                stringBuilder.append(foods.get(i));
+                if (i != foods.size() - 1){
+                    stringBuilder.append(" | ");
+                }
+            }
+            stringBuilder.append(" ]\n");
+        }
+        return stringBuilder.toString();
+    }
+}
