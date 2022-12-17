159ea8969de7552cd8201d3799e3d1a31beb8fc3
yejin <kwonyj1022@naver.com>
2022-12-17 16:21:55 +0900

    feat: Coach 클래스 구현 완료

---
+++ src/main/java/menu/Coach.java
@@ -0,0 +1,47 @@
+package menu;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coach {
+    private String name = "";
+    private List<String> hateFood = new ArrayList<>();
+    private List<String> weekMenu = new ArrayList<>();
+
+    public void selectMenu(List<String> menus) {
+        String menu = Randoms.shuffle(menus).get(0);
+        if (!weekMenu.contains(menu)) {
+            weekMenu.add(menu);
+        } else {
+            selectMenu(menus);
+        }
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getHateFood() {
+        return hateFood;
+    }
+
+    public void setHateFood(List<String> hateFood) {
+        this.hateFood = hateFood;
+    }
+
+    public List<String> getWeekMenu() {
+        return weekMenu;
+    }
+
+    public void setWeekMenu(List<String> weekMenu) {
+        this.weekMenu = weekMenu;
+    }
+
+}
