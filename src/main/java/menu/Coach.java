53ab6d5b2278b7088e837b2cdc396e74c0a3a2fa
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-12-17 15:18:58 +0900

    Feat(Coach): 추가 주간 메뉴 랜덤 생성 기능

--- src/main/java/menu/Coach.java
+++ src/main/java/menu/Coach.java
@@ -1,7 +1,11 @@
 package menu;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Coach {
     private String name;
@@ -15,7 +19,39 @@ public class Coach {
         return name;
     }
 
-    public void addHateMenus(List<String> hateMenus){
+    public void addHateMenus(List<String> hateMenus) {
         this.hateMenus = new ArrayList<>(hateMenus);
+    }
+
+    public List<String> getWeeklyMenuList(List<Category> categories) {
+        List<String> weeklyMenuList = new ArrayList<>();
+
+        Set<String> menuAlreadyPicked = new HashSet<>();
+
+        for (Category category : categories) {//5 times
+            final List<String> menuList = category.getMenuList();
+            String menu;
+            while (true) {
+
+                menu = Randoms.shuffle(menuList).get(0);
+
+                if (!(isDuplicateMenu(menuAlreadyPicked, menu) || isHateMenu(menu))) {
+                    menuAlreadyPicked.add(menu);
+                    break;
+                }
+            }
+
+            weeklyMenuList.add(menu);
+        }
+
+        return weeklyMenuList;
+    }
+
+    private boolean isHateMenu(String menu) {
+        return hateMenus.contains(menu);
+    }
+
+    private boolean isDuplicateMenu(Set<String> menuAlreadyPicked, String menu) {
+        return menuAlreadyPicked.contains(menu);
     }
 }
