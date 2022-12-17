2588dd5553a62e49b380d887ed3a9c4b80e355fd
seokhwan-an <asok369258@naver.com>
2022-12-17 14:02:22 +0900

    feat : 각 코치가 못 먹는 메뉴 입력하는 기능 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -9,10 +9,11 @@ import menu.validation.Validation;
 public class InputView {
 
     private static final String COACH_NAME_INPUT = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private static final String CAN_NOT_EAT_FOOD_INPUT = "(이)가 못 먹는 메뉴를 입력해 주세요.";
     private static final String SPLIT = ",";
 
     // 코치를 입력 받는 기능
-    public static List<String> inputCoachName () {
+    public static List<String> inputCoachName() {
         OutputView.printPhrase(COACH_NAME_INPUT);
         List<String> names = Arrays.asList(Console.readLine().split(SPLIT));
         Validation.validateCoachsLength(names);
@@ -20,4 +21,15 @@ public class InputView {
         return names;
     }
 
+    public static List<String> inputCanNotEatFood(List<String> names) {
+        List<String> canNotEatFood = new ArrayList<>();
+        for (String name : names) {
+            OutputView.printPhrase(name + CAN_NOT_EAT_FOOD_INPUT);
+            String food = Console.readLine();
+            canNotEatFood.add(food);
+            System.out.println();
+        }
+        Validation.validateCoachCanNotEatFoodLength(canNotEatFood);
+        return canNotEatFood;
+    }
 }
