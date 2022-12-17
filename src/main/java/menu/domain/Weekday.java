fa71575255b4a56f44b83d2be30cb6886169652c
Ridealist <cjsn92@gmail.com>
2022-12-17 14:18:26 +0900

    feat(Weekday): 요일 정보 관리하는 Enum 클래스 구현

--- src/main/java/menu/domain/Weekday.java
+++ src/main/java/menu/domain/Weekday.java
@@ -1,4 +1,18 @@
 package menu.domain;
 
 public enum Weekday {
+
+    MONDAY(1, "월요일"),
+    TUESDAY(2, "화요일"),
+    WEDNESDAY(3, "수요일"),
+    THURSDAY(4, "목요일"),
+    FRIDAY(5, "금요일");
+
+    public int weekNumber;
+    public String name;
+
+    Weekday(int weekNumber, String name) {
+        this.weekNumber = weekNumber;
+        this.name = name;
+    }
 }
