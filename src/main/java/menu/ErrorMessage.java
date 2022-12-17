afd30aec1f41d0fd44b9b0d811ba06cefbb53651
kiarakim <kiarakim413@gmail.com>
2022-12-17 13:54:25 +0900

    feat: 에러 메세지 추가

---
+++ src/main/java/menu/ErrorMessage.java
@@ -0,0 +1,8 @@
+package menu;
+
+public class ErrorMessage {
+    String PREFIX = "[ERROR] ";
+    String INVALID_COACH_MIN ="코치는 최소 2명과 식사를 합니다.";
+    String INVALID_COACH_NAX = "코치는 최대 5명과 식사를 합니다.";
+    String INVALID_INEDIBLE_EACH = "각 코치는 최대 2개의 못 먹는 메뉴가 있습니다.";
+}
