133b50a99d4cc5ffd974d0d735757d4585fc86fc
ingpyo <juninpyo72@gmail.com>
2022-12-17 17:37:36 +0900

    fix:코치 이름 입력 예외사항 기능 추가

--- src/main/java/menu/InputView.java
+++ src/main/java/menu/InputView.java
@@ -7,9 +7,11 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+	RecommendException recommendException = new RecommendException();
 	public List<String> readInputCoachs() {
 		List<String> splitCoachs = new ArrayList<>();
 		String inputCoachs = Console.readLine();
+		checkinputCoachs(inputCoachs);
 		splitCoachs.addAll(List.of(inputCoachs.split(",")));
 		return splitCoachs;
 	}
@@ -19,5 +21,12 @@ public class InputView {
 		String inputNotEatFoods = Console.readLine();
 		splitFoods.addAll(Arrays.asList(inputNotEatFoods.split(",")));
 		return splitFoods;
+	}
+
+	public void checkinputCoachs(String inputCoachs) {
+		String[] split = inputCoachs.split(",");
+		recommendException.checkCoachsSize(split.length);
+		recommendException.checkCoachsNameSize(split);
+		recommendException.checkMissMatch(split);
 	}
 }
---
+++ src/main/java/menu/RecommendException.java
@@ -0,0 +1,27 @@
+package menu;
+
+import java.util.regex.Pattern;
+
+public class RecommendException {
+	public void checkCoachsSize(int size) throws IllegalArgumentException {
+		if (size < 2 || size > 5) {
+			throw new IllegalArgumentException("[ERROR] 인원수는 2~5명만 입력해주세요.");
+		}
+	}
+
+	public void checkCoachsNameSize(String[] splitCoach) {
+		for (String coach : splitCoach) {
+			if (coach.length() < 2 || coach.length() > 4) {
+				throw new IllegalArgumentException("[ERROR] 이름의 길이를 2~4사이로 정해주세요.");
+			}
+		}
+	}
+	public void checkMissMatch(String[] splitCoach) throws IllegalArgumentException {
+		for (String coach : splitCoach) {
+			if (!Pattern.matches("^[가-힣]*$", coach)) {
+				throw new IllegalArgumentException("[ERROR] 한글로 이루어져야합니다.");
+			}
+		}
+
+	}
+}
