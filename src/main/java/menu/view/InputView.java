eead50e738609cf8afa03ce9a1d9b09ea8970da2
shinD <huipulci1@naver.com>
2022-12-17 15:42:06 +0900

    feat: View의 출력을 테스트코드에 맞게 수정

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -5,8 +5,8 @@ import menu.domain.Coach;
 
 public class InputView {
 
-    private static final String INPUT_COACH_NAME = "\n코치의 이름을 입력해 주세요. (, 로 구분)";
-    private static final String INPUT_COACH_INEDIBLE_FOOD_FORMAT = "\n%s(이)가 못 먹는 메뉴를 입력해 주세요.";
+    private static final String INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private static final String INPUT_COACH_INEDIBLE_FOOD_FORMAT = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
 
     /**
      * 코치 이름 입력
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,36 +1,44 @@
 package menu.view;
 
-import menu.domain.WeekRecommends;
+import menu.domain.Coach;
 
 import java.util.List;
+
+import static java.lang.String.format;
+import static java.util.stream.Collectors.joining;
 
 public class OutputView {
 
     private static final String START_RECOMMEND = "점심 메뉴 추천을 시작합니다.";
     private static final String BLANK = "";
+    private static final String BAR = " | ";
     private static final String RECOMMEND_RESULT = "메뉴 추천 결과입니다.";
     private static final String END_RECOMMEND = "추천을 완료했습니다.";
 
     // TODO Enum 을 통해 형식으로 출력하도록 변경
     private static final String DIVISION = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]";
-    private static final String CATEGORY = "[ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]";
+    private static final String CATEGORY = "[ 카테고리 | 한식 | 양식 | 일식 | 중식 | 아시안 ]";
     private static final String COACH_RECOMMENDED_FOOD_FORMAT = "[ %s | %s ]";
 
     public static void printRecommendStart() {
         System.out.println(START_RECOMMEND);
-        System.out.println(BLANK);
     }
 
-    public static void printRecommendResult(List<WeekRecommends> recommend) {
+    public static void printRecommendResult(List<Coach> coaches) {
         System.out.println(RECOMMEND_RESULT);
-        System.out.println(BLANK);
-        printRecommends();
+        System.out.println(DIVISION);
+        System.out.println(CATEGORY);
+        printRecommends(coaches);
         System.out.println(END_RECOMMEND);
     }
 
-    private static void printRecommends() {
-        System.out.println(DIVISION);
-        System.out.println(CATEGORY);
-        System.out.println(COACH_RECOMMENDED_FOOD_FORMAT);
+    private static void printRecommends(List<Coach> coaches) {
+        for (Coach coach : coaches) {
+            String menus = coach.recommends().stream()
+                    .map(it -> it.food().name()).
+                    collect(joining(BAR));
+            String message = format(COACH_RECOMMENDED_FOOD_FORMAT, coach.name(), menus);
+            System.out.println(message);
+        }
     }
 }
