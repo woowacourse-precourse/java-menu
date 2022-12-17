aece6e54e8a1f0032d1b8938044893c2a1b136bf
Cyma-s <ssol0319@naver.com>
2022-12-17 15:09:51 +0900

    feat(Category) : 랜덤 숫자로 Category 생성하는 함수 추가

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -5,23 +5,33 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum Category {
-    KOREAN("한식", MenuList.KOREAN_MENU),
-    JAPAN("일식", MenuList.JAPAN_MENU),
-    CHINA("중식", MenuList.CHINA_MENU),
-    ASIAN("아시안", MenuList.ASIAN_MENU),
-    WESTERN("양식", MenuList.WESTERN_MENU),
-    INVALID_CATEGORY(null, "");
+    JAPAN(1, "일식", MenuList.JAPAN_MENU),
+    KOREAN(2, "한식", MenuList.KOREAN_MENU),
+    CHINA(3, "중식", MenuList.CHINA_MENU),
+    ASIAN(4, "아시안", MenuList.ASIAN_MENU),
+    WESTERN(5, "양식", MenuList.WESTERN_MENU),
+    INVALID_CATEGORY(-1, null, "");
 
+    private final int categoryNumber;
     private final String categoryName;
     private final List<Menu> menuList = new ArrayList<>();
 
-    Category(String categoryName, String menuList) {
+    Category(int categoryNumber, String categoryName, String menuList) {
+        this.categoryNumber = categoryNumber;
         this.categoryName = categoryName;
         List<String> menus = List.of(menuList.replace(" ", "").split(","));
         for(String menuName: menus) {
             Menu menu = new Menu(menuName);
             this.menuList.add(menu);
         }
+    }
+
+    public static Category makeCategory(int number) {
+        Category category = Arrays.stream(values())
+                .filter(value -> value.categoryNumber == number)
+                .findAny()
+                .orElse(INVALID_CATEGORY);
+        return category;
     }
 
     public static Category validateMenu(String menuName) {
