68622bc10dac6650126257a77949b096e7a6f285
yujaemin <woawoa0513@gmail.com>
2022-12-17 13:43:59 +0900

    feat: 서비스 종료 안내문 출력 함수 구현

--- src/main/java/menu/message/NoticeMessage.java
+++ src/main/java/menu/message/NoticeMessage.java
@@ -3,4 +3,13 @@ package menu.message;
 public class NoticeMessage {
     public static final String SERVICE_START_NOTICE = "점심 메뉴 추천을 시작합니다.";
 
+    public static final String SERVICE_END_NOTICE = String.format(
+        "메뉴 추천 결과입니다.\n"
+                + "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]\n"
+                + "[ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]\n"
+                + "[ 토미 | 쌈밥 | 김치찌개 | 미소시루 | 짜장면 | 팟타이 ]\n"
+                + "[ 제임스 | 된장찌개 | 비빔밥 | 가츠동 | 토마토 달걀볶음 | 파인애플 볶음밥 ]\n"
+                + "[ 포코 | 된장찌개 | 불고기 | 하이라이스 | 탕수육 | 나시고렝 ]\n\n"
+                + "추천을 완료했습니다."
+    );
 }
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -7,4 +7,8 @@ public class OutputView {
     public void printServiceStartNotice() {
         System.out.println(NoticeMessage.SERVICE_START_NOTICE + '\n');
     }
+
+    public void printServiceEndNotice() {
+        System.out.println(NoticeMessage.SERVICE_END_NOTICE + '\n');
+    }
 }
