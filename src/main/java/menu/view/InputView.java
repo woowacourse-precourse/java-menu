3cfb74e61b11576f491d3fbd394b6751cfb0c59c
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 14:37:29 +0900

    fix(InputView): 띄어쓰기로 발생하는 문제 해결

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -8,14 +8,16 @@ import java.util.List;
 public class InputView {
     private static final String INPUT_COACHES_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
     private static final String INPUT_HATE_MENU_FORMAT = "\n%s(이)가 못 먹는 메뉴를 입력해 주세요.\n";
+    private static final String SPACE = " ";
+    private static final String NONE = "";
 
     public List<String> readCoachesName() {
         System.out.println(INPUT_COACHES_NAME);
-        return InputValidation.validateCoachesName(Console.readLine());
+        return InputValidation.validateCoachesName(Console.readLine().replace(SPACE, NONE));
     }
 
     public List<String> readCoachHateMenu(String name) {
         System.out.printf(INPUT_HATE_MENU_FORMAT, name);
-        return InputValidation.validateHateMenus(Console.readLine());
+        return InputValidation.validateHateMenus(Console.readLine().replace(SPACE, NONE));
     }
 }
