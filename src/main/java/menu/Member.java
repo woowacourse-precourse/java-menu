5733eadc8294b208819f1da84f9c4cc7bc9d9cdf
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-12-17 13:21:03 +0900

    <feat>(Member.java): member 클래스 멤버변수 선언
     - 이름, 추천된 식사, 못먹는 음식

---
+++ src/main/java/menu/Member.java
@@ -0,0 +1,11 @@
+package menu;
+
+import java.util.List;
+
+public class Member {
+    private String name;
+    private List<String> pickyEatings;
+    private List<String> recommendedMeal;
+
+    
+}
