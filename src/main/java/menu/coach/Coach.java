d421c55749e8af187242e549f68e68387c018adc
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-12-17 13:33:00 +0900

    feat(Coach): 코치 클래스 멤버 변수 및 생성자 정의
    - 주의 사항 목록 추가(입력 요구사항)
    - 리팩토링 시 변수명 변경 고려

--- src/main/java/menu/coach/Coach.java
+++ src/main/java/menu/coach/Coach.java
@@ -1,4 +1,16 @@
 package menu.coach;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Coach {
+    String name;
+    List<String> foodNotEat;
+    List<String> foodEaten;
+
+    Coach(String name) {
+        this.name = name;
+        this.foodNotEat = new ArrayList<>();
+        this.foodEaten = new ArrayList<>();
+    }
 }
