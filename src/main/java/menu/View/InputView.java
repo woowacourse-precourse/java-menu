c5f574ca45d51825b5311455f1d1ea2178bff02c
Eun-chan Cho <eunchan1001@gmail.com>
2022-12-17 14:35:49 +0900

    feat(InputView): implement InputView with methods

---
+++ src/main/java/menu/View/InputView.java
@@ -0,0 +1,34 @@
+package menu.View;
+
+import menu.Model.Coach;
+import menu.Model.Coachs;
+import menu.Model.Category;
+
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private final String READ_COACH_NAMES = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private final String READ_INEDIBLES = "%s가 못 먹는 메뉴를 입력해 주세요.";
+
+    private final String INVALID_FOOD = "음식이 메뉴에 없습니다.";
+
+    public Coachs readCoachNames() {
+        System.out.println(READ_COACH_NAMES);
+        String input = Console.readLine();
+        return new Coachs(input);
+    }
+
+    public List<String> readInedibles(Coach coach) {
+        System.out.println(String.format(READ_INEDIBLES, coach.getName()));
+        String input = Console.readLine();
+        List<String> inedibles = List.of(input.split(","));
+        for (String food : inedibles) {
+            if (!Category.checkFoodInMenus(food)) {
+                throw new IllegalArgumentException(INVALID_FOOD);
+            }
+        }
+        return inedibles;
+    }
+}
