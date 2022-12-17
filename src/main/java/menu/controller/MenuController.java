de8d7fccc3cb445239ae9ea04f0e3e3a3d4556b4
greeng00se <thegreengoose7@gmail.com>
2022-12-17 15:05:31 +0900

    feat(MenuController): 코치님 저장, 메뉴 추천 기능 추가

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,5 +1,11 @@
 package menu.controller;
 
+import java.util.List;
+import menu.domain.Coach;
+import menu.dto.CoachNameDto;
+import menu.dto.InvalidMenusDto;
+import menu.dto.MenusDto;
+import menu.service.CoachService;
 import menu.service.MenuService;
 import menu.util.Retry;
 import menu.view.InputView;
@@ -11,15 +17,32 @@ public class MenuController {
     private final OutputView outputView = new OutputView();
 
     private final MenuService menuService;
+    private final CoachService coachService;
 
-    public MenuController(MenuService menuService) {
+    public MenuController(MenuService menuService, CoachService coachService) {
         this.menuService = menuService;
+        this.coachService = coachService;
     }
 
     public void run() {
         outputView.printStart();
-        String coachNames = Retry.execute(inputView::readCoachNames);
-        menuService.saveCoach(coachNames);
+        saveCoach();
+        recommendMenu();
+        outputView.printEnd();
+    }
 
+    private void saveCoach() {
+        CoachNameDto coachNameDto = Retry.execute(inputView::readCoachNames);
+        coachService.saveCoach(coachNameDto);
+    }
+
+    private void recommendMenu() {
+        List<Coach> coaches = coachService.findAll();
+        for (Coach coach : coaches) {
+            InvalidMenusDto invalidMenusDto = inputView.readMenuNames(coach.getName());
+            coach.addInvalidMenus(invalidMenusDto.getMenus());
+        }
+        MenusDto result = menuService.findMenu(coaches);
+        outputView.printMenuMessage(result);
     }
 }
