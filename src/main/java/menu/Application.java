24f427dad7763a499e7ddcd4dd1cb9734ab04d5c
anneu <b.imnuj@gmail.com>
2022-12-17 13:35:45 +0900

    feat: 초기 뼈대 코드 구현

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,10 @@
 package menu;
 
+import menu.controller.MenuDecideController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        MenuDecideController controller = new MenuDecideController();
+        controller.run();
     }
 }
---
+++ src/main/java/menu/controller/MenuDecideController.java
@@ -0,0 +1,15 @@
+package menu.controller;
+
+import menu.domain.MenueDecideService;
+import menu.view.Input;
+import menu.view.Output;
+
+public class MenuDecideController {
+    private final Input input = new Input();
+    private final Output output = new Output();
+    private final MenueDecideService service = new MenueDecideService();
+
+    public void run() {
+
+    }
+}
