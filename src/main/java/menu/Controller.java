66f13301902b53bca4c4b32f21d65210d00cd563
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-12-17 15:32:43 +0900

    <feat>(Controller.java): controller 클래스, run 메서드 작성
     - 컨트롤러 클래스와, 프로그램을 실행하는 run 메서드 작성
     - 리팩토링 필요

--- src/main/java/menu/Controller.java
+++ src/main/java/menu/Controller.java
@@ -1,2 +1,41 @@
-package menu;public class Controller {
+package menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+    private static InputView inputView = new InputView();
+    private static OutputView outputView = new OutputView();
+    private static MenuRecommender menuRecommender = new MenuRecommender();
+
+    public static void run() {
+
+        //게임 시작
+        outputView.printStartMessage();
+
+        //코치 이름 입력받기
+        outputView.printCoachNameInformMessage();
+        List<String> coaches = inputView.readCoachNames();
+        List<Member> members = new ArrayList<>();
+
+        //편식 음식 입력받기
+        for (String coachName : coaches) {
+            outputView.printPickEatingsInformMessage(coachName);
+            List<String> pickyEatings = inputView.readPickyEatings();
+
+            members.add(new Member(coachName, pickyEatings));
+        }
+
+        menuRecommender.makeRecommends();
+
+        outputView.printResult(menuRecommender);
+
+        return;
+    }
+    
+
+
+    public static void main(String[] args) {
+        run();
+    }
 }
