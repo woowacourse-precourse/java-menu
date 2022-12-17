658532f9a593f03050a81eb70d7e0d5ff81805e3
Gibeom Nam <rlqja0523@naver.com>
2022-12-17 14:22:46 +0900

    feat: 1 ~ 5에 따라 카테고리를 선택하는 기능

--- src/main/java/menu/service/MenuService.java
+++ src/main/java/menu/service/MenuService.java
@@ -1,5 +1,6 @@
 package menu.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import menu.model.Category;
 
 import java.util.ArrayList;
@@ -7,6 +8,22 @@ import java.util.List;
 
 public class MenuService {
     private final List<Category> pickedCategories = new ArrayList<>();
+
+    private void pickCategories() {
+        while(pickedCategories.size() < 5) {
+            int order = Randoms.pickNumberInRange(1, 5);
+            for (Category category : Category.values()) {
+                addCategories(category, order);
+            }
+
+        }
+    }
+
+    private void addCategories(Category category, int order) {
+        if (category.getOrder() == order && !checkCategoryPickedTwice(category)) {
+            pickedCategories.add(category);
+        }
+    }
 
     private boolean checkCategoryPickedTwice(Category category) {
         int count = 0;
