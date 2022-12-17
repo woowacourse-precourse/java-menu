1938831ec670338d90dacd9bcf53b56e48007722
이건회 <rg970604@naver.com>
2022-12-17 13:37:28 +0900

    feat(OutputView.java) : 코치이름 입력, 금지음식 입력시 출력문구

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -6,11 +6,23 @@ public class OutputView {
 
     private static final String SERVICE_START = "점심 메뉴 추천을 시작합니다.";
     private static final String SERVICE_DONE = "추천을 완료했습니다.";
+    private static final String INPUT_COACHNAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
 
-    public static void printServiceStart(){
+    private static final String INPUT_PROHIBITED_FOOD = "(이)가 못 먹는 메뉴를 입력해 주세요.";
+
+    public static void printServiceStart() {
         out.println(SERVICE_START);
     }
-    public static void printServiceDone(){
+
+    public static void printInputCoachnames() {
+        out.println(INPUT_COACHNAME);
+    }
+
+    public static void printInputProhibitedFoods(String name) {
+        out.println(name + INPUT_PROHIBITED_FOOD);
+    }
+
+    public static void printServiceDone() {
         out.println(SERVICE_DONE);
     }
 
