f00b9b12e4f3fe32d9f6e1ad07e857aafbe03238
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-12-17 13:37:45 +0900

    Feat : 코치 각각의 객체

---
+++ src/main/java/menu/Coach.java
@@ -0,0 +1,12 @@
+package menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coach {
+    List<String> cannotEatMenuList = new ArrayList<>();
+    String name;
+    public Coach (String name){
+        this.name = name;
+    }
+}
