d17f96740d8b86f5e4573c783d56c188ec95a730
limheesang <dlagmltkd12@naver.com>
2022-12-17 17:12:15 +0900

    feat: 코치들 이름 입력 기능

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,4 +1,19 @@
 package menu.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+
+    private InputView() {
+    }
+
+    public static List<String> inputCoachNames() {
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        String names = Console.readLine();
+        String[] splitNames = names.split(",");
+        return Arrays.asList(splitNames);
+    }
 }
