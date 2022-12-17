bedc9ca88c81ec078ec09fbfee47173c6c29fd1d
Miseong Kim <miseongk@naver.com>
2022-12-17 14:31:00 +0900

    feat(Category): 추천할 카테고리 Enum

---
+++ src/main/java/menu/domain/Category.java
@@ -0,0 +1,20 @@
+package menu.domain;
+
+public enum Category {
+
+    JAPANESE("일식"),
+    KOREAN("한식"),
+    CHINESE("중식"),
+    ASIAN("아시안"),
+    WESTERN("양식");
+
+    private final String name;
+
+    Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
