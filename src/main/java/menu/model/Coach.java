546221fbd8cbefe7d98ae38aece7e83d5afcd99a
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2022-12-17 14:31:58 +0900

    style(Coach): modify 메서드 위치 수정

--- src/main/java/menu/model/Coach.java
+++ src/main/java/menu/model/Coach.java
@@ -1,11 +1,20 @@
 package menu.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Coach {
 
     private final String name;
     private final List<String> hateFoods;
+    private List<String> choicedMenu;
+
+    public Coach(String name, List<String> hateFoods) {
+        this.name = name;
+        this.hateFoods = hateFoods;
+        this.choicedMenu = new ArrayList<>();
+        CoachRepository.addCoach(this);
+    }
 
     public String getName() {
         return this.name;
@@ -13,11 +22,5 @@ public class Coach {
 
     public List<String> getHateFoods() {
         return this.hateFoods;
-    }
-
-    public Coach(String name, List<String> hateFoods) {
-        this.name = name;
-        this.hateFoods = hateFoods;
-        CoachRepository.addCoach(this);
     }
 }
