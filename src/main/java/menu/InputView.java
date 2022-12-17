6d59b2bab1d1ef9ef1e5b26e95c00055c49806d5
ingpyo <juninpyo72@gmail.com>
2022-12-17 13:53:05 +0900

    feat:코치 이름 입력 기능

---
+++ src/main/java/menu/InputView.java
@@ -0,0 +1,15 @@
+package menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+	public List<String> ReadinputCoachs() {
+		List<String> splitCoachs = new ArrayList<>();
+		String inputCoachs = Console.readLine();
+		splitCoachs.addAll(List.of(inputCoachs.split(",")));
+		return splitCoachs;
+	}
+}
