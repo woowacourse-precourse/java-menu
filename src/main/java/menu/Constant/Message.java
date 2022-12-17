d215b61eb76c6515c8c1f7a5930dadb6ba37e0eb
LJW25 <dbjwl@g.skku.edu>
2022-12-17 14:05:22 +0900

    fix: Message staic 추가

--- src/main/java/menu/Constant/Message.java
+++ src/main/java/menu/Constant/Message.java
@@ -2,14 +2,14 @@ package menu.Constant;
 
 public class Message {
 
-    public final String MSG_START = "점심 메뉴 추천을 시작합니다.";
-    public final String MSG_GET_NAMES = "코치의 이름을 입력해 주세요. (, 로 구분)";
-    public final String MSG_GET_HATEMENUS = "가 못 먹는 메뉴를 입력해 주세요.";
-    public final String MSG_PRINT_RESULT = "메뉴 추천 결과입니다.";
-    public final String MSG_END = "추천을 완료했습니다.";
+    public static final String MSG_START = "점심 메뉴 추천을 시작합니다.";
+    public static final String MSG_GET_NAMES = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    public static final String MSG_GET_HATEMENUS = "가 못 먹는 메뉴를 입력해 주세요.";
+    public static final String MSG_PRINT_RESULT = "메뉴 추천 결과입니다.";
+    public static final String MSG_END = "추천을 완료했습니다.";
 
-    public final String ERROR_COACH_NUM = "[ERROR] 코치는 최소 2명, 최대 5명이어야 합니다. 다시 입력해 주세요.";
-    public final String ERROR_COACH_NAME = "[ERROR] 코치의 이름은 최소 2글자, 최대 4글자여야 합니다. 다시 입력해 주세요.";
-    public final String ERROR_HATE_NUM = "[ERROR] 못 먹는 메뉴는 최소 0개, 최대 2개여야 합니다. 다시 입력해 주세요.";
+    public static final String ERROR_COACH_NUM = "[ERROR] 코치는 최소 2명, 최대 5명이어야 합니다. 다시 입력해 주세요.";
+    public static final String ERROR_COACH_NAME = "[ERROR] 코치의 이름은 최소 2글자, 최대 4글자여야 합니다. 다시 입력해 주세요.";
+    public static final String ERROR_HATE_NUM = "[ERROR] 못 먹는 메뉴는 최소 0개, 최대 2개여야 합니다. 다시 입력해 주세요.";
 
 }
