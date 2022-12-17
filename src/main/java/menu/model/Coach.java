1ae77c0c83987a905a53d42c95e4b83809923692
Kayoung <rkdkdudjd@gmail.com>
2022-12-17 13:48:02 +0900

    feat(Coach): 못먹는 메뉴 추가 기능
    + 중복인 메뉴 들어올 시 예외 처리

--- src/main/java/menu/model/Coach.java
+++ src/main/java/menu/model/Coach.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Coach {
     private static final String INVALID_NAME = "코치의 이름이 올바르지 않습니다.";
+    public static final String ALREADY_REGISTERED = "해당 코치가 못 먹는 메뉴로 이미 등록돼있습니다.";
     private String name;
     private List<String> menuCannotEat = new ArrayList<>();
 
@@ -17,5 +18,16 @@ public class Coach {
         if (name.length() < 2 || name.length() > 4) {
             throw new IllegalArgumentException(INVALID_NAME);
         }
+    }
+
+    public void addMenuCannotEat(String menu) {
+        if(isAlreadyRegistered(menu)) {
+            throw new IllegalArgumentException(ALREADY_REGISTERED);
+        }
+
+    }
+
+    private boolean isAlreadyRegistered(String menu) {
+        return menuCannotEat.contains(menu);
     }
 }
--- src/test/java/menu/model/CoachTest.java
+++ src/test/java/menu/model/CoachTest.java
@@ -1,7 +1,6 @@
 package menu.model;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,9 +9,20 @@ class CoachTest {
     @Test
     @DisplayName("코치 이름 검증")
     public void validateName() {
-        assertThatIllegalArgumentException().isThrownBy(()-> {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
             new Coach("안녕하세요");
         }).withMessageStartingWith("코치의 이름이 올바르지 않습니다.");
+
+    }
+
+    @Test
+    @DisplayName("이미 등록된 메뉴를 다시 등록할 경우 예외처리한다.")
+    public void throwExceptionForDuplicateMenu() {
+        Coach hi = new Coach("포비");
+        hi.addMenuCannotEat("안뇽");
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            hi.addMenuCannotEat("안뇽");
+        }).withMessageStartingWith("해당 코치가 못 먹는 메뉴로 이미 등록돼있습니다.");
 
     }
 }
