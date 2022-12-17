d140805f590fc4768e8d979926f2b02b6a75450f
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 13:54:51 +0900

    feat: 코치들의 이름을 입력받는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,2 +1,23 @@
-package menu.view;public class InputView {
+package menu.view;
+
+import menu.domain.Validator;
+import java.util.Arrays;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public InputView(){};
+
+    public static List<String> inputCoaches() {
+        OutputView.printInputCoaches();
+        try {
+            List<String> coaches = Arrays.asList(Console.readLine().split(","));
+            Validator.validateCoachesLength(coaches);
+            Validator.validateCoachName(coaches);
+            return coaches;
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return inputCoaches();
+        }
+    }
 }
