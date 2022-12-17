9ab985ac2320d5c662fdf160caf11601d7628e9a
EUNJEONGMUN <munej26@gmail.com>
2022-12-17 13:24:30 +0900

    feat: 코치 CoachName 생성 및 예외처리

--- src/main/java/menu/domain/CoachName.java
+++ src/main/java/menu/domain/CoachName.java
@@ -1,4 +1,24 @@
 package menu.domain;
 
 public class CoachName {
+    private static final int NAME_LENGTH_MIN = 2;
+    private static final int NAME_LENGTH_MAX = 4;
+
+    private final String name;
+
+
+    private CoachName(String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    public static CoachName from(String name) {
+        return new CoachName(name);
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length()<NAME_LENGTH_MIN || name.length()>NAME_LENGTH_MAX) {
+            throw new IllegalArgumentException("코치의 이름은 최소 2글자, 최대 4글자입니다.");
+        }
+    }
 }
