2e874a259153e9bb9b5a52638ca4dfe29f92b12d
watterricecake <fruturum@nate.com>
2022-12-17 14:07:46 +0900

    feat: 결과 출력 view

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,9 +1,23 @@
 package menu.view;
 
 public class OutputView {
-    public static void startRecommend(){
+    public static void startRecommendView(){
         System.out.println("점심 메뉴 추천을 시작합니다.");
     }
-
-
+    public static void resultView(){
+        System.out.println("메뉴 추천 결과입니다.");
+    }
+    public static void weekView(){
+        System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
+    }
+    public static void categoryView(){
+        System.out.println("[ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]");
+    }
+    public static void coachMenuView(String name, String[] foods){
+        System.out.print("[ " + name);
+        for(String food:foods){
+            System.out.print(" | " + food);
+        }
+        System.out.println();
+    }
 }
