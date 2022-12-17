b1e58029ccea5e540c82501f595fd6e20ccdda0e
kimdahee0815 <diana815@naver.com>
2022-12-17 14:27:09 +0900

    feat(printRecommendInstruction) : 메뉴 추천 결과를 보여주기 전 문구를 출력한다

---
+++ src/test/java/menu/RecommendMenu.java
@@ -0,0 +1,9 @@
+package menu;
+
+public class RecommendMenu {
+	public void printRecommendInstruction() {
+		System.out.println("메뉴 추천 결과입니다.");
+		System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
+		System.out.println("[ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]");
+	}
+}
