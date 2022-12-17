d04795d6cb5089f985a3d577cc80d090709bdbdf
JunHyeon <dlwnsgus07@naver.com>
2022-12-17 16:54:44 +0900

    test : MenuList 이넘클래스 테스트코드 작성

---
+++ src/test/java/menu/domain/MenuListTest.java
@@ -0,0 +1,27 @@
+package menu.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuListTest {
+
+    @DisplayName("카테고리를 주었을때 정상적으로 해당 카테고리 메뉴를 가져오는지 확인")
+    @Test
+    void getFiled() {
+        List<String> menuList = MenuList.getFiled(Category.JAPAN);
+        Assertions.assertThat(menuList).isEqualTo(MenuList.JAPAN.getMenus());
+    }
+
+    @DisplayName("메뉴의 이름이 주어졌을때 존재하는 메뉴인지 아닌지 파악하는 기능")
+    @Test
+    void find() {
+        String menu = "스시";
+        Assertions.assertThat(MenuList.find(menu)).isTrue();
+    }
+}
