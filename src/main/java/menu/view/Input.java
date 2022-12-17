3d33c34aa6900041ceaf060a7330000c351f9d6c
K-jun98 <alap_u@naver.com>
2022-12-17 13:49:47 +0900

    못먹는 음식은 최소 0개 최대 2개의 못 먹는 메뉴가 있다.

--- src/main/java/menu/view/Input.java
+++ src/main/java/menu/view/Input.java
@@ -1,25 +1,27 @@
 package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class Input {
 
     public List<String> inputCoachNames() {
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
-        return validateCoachNumber(inputSplitLineToList());
+        return validateListSize(inputSplitLineToList(), 2, 5);
     }
 
     public List<String> inputMenus(String name) {
         System.out.println(name + "(이)가 못 먹는 메뉴를 입력해 주세요.");
-        return inputSplitLineToList();
+        return validateListSize(inputSplitLineToList(), 0, 2);
     }
 
     private static List<String> inputSplitLineToList() {
         return Arrays.asList(Console.readLine().split(","));
     }
 
-    private List<String> validateCoachNumber(List<String> names) {
-        if (names.size() < 2 || 5 < names.size()) {
+    private List<String> validateListSize(List<String> names, int from, int to) {
+        if (names.size() < from || to < names.size()) {
             throw new IllegalArgumentException("코치의 인원은 최소 2명, 최대 5명까지 식사를 함께 할 수 있습니다.");
         }
         return names;
