ec447ea824edec68c4afeafbe52e8a0b0d5baf9a
euna <christinejung10@daum.net>
2022-12-17 16:34:01 +0900

    add(InputViewTest): tests for method setCoachesCannotEats

--- src/test/java/menu/view/InputViewTest.java
+++ src/test/java/menu/view/InputViewTest.java
@@ -34,12 +34,34 @@ public class InputViewTest {
     }
 
     @DisplayName("코치 이름 입력에 대한 예외처리")
-    @ValueSource(strings = {"A", "", " ", "앍", "가가,나나,다다,라라,마마,바바"})
+    @ValueSource(strings = {"A", "", " ", "앍", "가가,나나,다다,라라,마마,바바", "니냐니냐니냐뇨"})
     @ParameterizedTest
     void setCoachesName_예외테스트(String coachesName_input) {
         putSystemInput(coachesName_input);
         assertSimpleTest(() ->
                 assertThatThrownBy(InputView::setCoachesName)
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    /**
+     * setCoachesCannotEats
+     */
+    @DisplayName("코치가 못 먹는 메뉴 입력")
+    @ValueSource(strings = {"규동,김밥", "깐풍기,팟타이", "라자냐", ""})
+    @ParameterizedTest
+    void setCoachesCannotEats_테스트(String coachCannotEats_input) {
+        putSystemInput(coachCannotEats_input);
+        String[] coachCannotEats = coachCannotEats_input.split(",");
+        assertThat(Arrays.asList(coachCannotEats)).containsExactlyElementsOf(Arrays.asList(InputView.setCoachesCannotEats()));
+    }
+
+    @DisplayName("코치가 못 먹는 메뉴 입력에 대한 예외처리")
+    @ValueSource(strings = {"김피탕,하와안피자", "볶음면,동파육,뇨끼"})
+    @ParameterizedTest
+    void setCoachesCannotEats_예외테스트(String coachCannotEats_input) {
+        putSystemInput(coachCannotEats_input);
+        assertSimpleTest(() ->
+                assertThatThrownBy(InputView::setCoachesCannotEats)
                         .isInstanceOf(IllegalArgumentException.class));
     }
 
