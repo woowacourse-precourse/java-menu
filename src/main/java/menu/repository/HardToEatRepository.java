784aa4d32f0a3c8186cc80130829d76fef3f364f
woo-chang <clllickme@naver.com>
2022-12-17 14:11:42 +0900

    feat(HardToEatRepository): 코치가 못 먹는 메뉴 정보를 담고 있는 저장소 구현

---
+++ src/main/java/menu/repository/HardToEatRepository.java
@@ -0,0 +1,36 @@
+package menu.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import menu.domain.Coach;
+
+public class HardToEatRepository {
+
+    private static final Map<Coach, List<String>> menus = new HashMap<>();
+
+    public static void addMenus(Coach coach, String menu) {
+        processExistCoach(coach, menu);
+        processNotExistCoach(coach, menu);
+    }
+
+    private static void processExistCoach(Coach coach, String menu) {
+        if (menus.containsKey(coach)) {
+            List<String> menuNames = menus.get(coach);
+            storeMenu(menu, menuNames);
+        }
+    }
+
+    private static void storeMenu(String menu, List<String> menuNames) {
+        if (!menuNames.contains(menu)) {
+            menuNames.add(menu);
+        }
+    }
+
+    private static void processNotExistCoach(Coach coach, String menu) {
+        if (!menus.containsKey(coach)) {
+            menus.put(coach, new ArrayList<>(List.of(menu)));
+        }
+    }
+}
