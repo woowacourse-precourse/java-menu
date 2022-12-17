47b29fd3ed5665af4b6a6246ecb434d27f547d68
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2022-12-17 16:43:25 +0900

    feat(InputValidator): add 입력받은 값에 중복있는지 검증

--- src/main/java/menu/message/ErrorMsg.java
+++ src/main/java/menu/message/ErrorMsg.java
@@ -1,6 +1,7 @@
 package menu.message;
 
 public enum ErrorMsg {
+    ERROR_MSG_DUPLICATED_ELEMENT_IN_LIST("중복되지 않는 값으로 입력해야 합니다."),
     ERROR_MSG_SIZE_OF_COACHES_NAME_IS_UNDER_TWO("코치는 최소 2명 이상으로 입력해야 합니다."),
     ERROR_MSG_SIZE_OF_COACHES_NAME_IS_OVER_FIVE("코치는 최대 5명 이하로 입력해야 합니다.");
 
--- src/main/java/menu/view/InputValidator.java
+++ src/main/java/menu/view/InputValidator.java
@@ -1,12 +1,14 @@
 package menu.view;
 
+import menu.module.NotModule;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static menu.message.ErrorMsg.*;
 
-public class InputValidator {
+public class InputValidator extends NotModule {
 
     private static final String COMMA = ",";
     private static final int LOWER_COACH_NUMBER_INCLUSIVE = 2;
@@ -19,11 +21,20 @@ public class InputValidator {
     }
 
     public void validateCoachesName(List<String> coachesName) {
+        if (isDuplicatedList(coachesName)) {
+            throw new IllegalArgumentException(ERROR_MSG_DUPLICATED_ELEMENT_IN_LIST.get());
+        }
         if (coachesName.size() < LOWER_COACH_NUMBER_INCLUSIVE) {
             throw new IllegalArgumentException(ERROR_MSG_SIZE_OF_COACHES_NAME_IS_UNDER_TWO.get());
         }
         if (coachesName.size() > UPPER_COACH_NUMBER_INCLUSIVE) {
             throw new IllegalArgumentException(ERROR_MSG_SIZE_OF_COACHES_NAME_IS_OVER_FIVE.get());
         }
+    }
+
+    private boolean isDuplicatedList(List<String> data) {
+        return not(data.size() == data.stream()
+                .distinct()
+                .count());
     }
 }
