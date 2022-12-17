559948b42a2f5f59a135e95587f7110113b09ac7
kevstevie <jianna442@gmail.com>
2022-12-17 15:30:36 +0900

    feat: 이름 길이에 대한 유효성 검사 케이스 추가

--- src/test/java/menu/InputTest.java
+++ src/test/java/menu/InputTest.java
@@ -4,12 +4,22 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 class InputTest {
 
     @Test
-    @DisplayName("싫어하는 메뉴 입력 유효성 검증")
+    @DisplayName("코치 이름 유효성")
+    void name() {
+        assertThatThrownBy(() -> Input.validateCoach("11,2,333"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @Test
+    @DisplayName("없는 메뉴 입력 유효성 검증")
     void menu() {
-        Assertions.assertThatThrownBy(() -> Input.validateHate("자장면"))
+        assertThatThrownBy(() -> Input.validateHate("자장면"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
