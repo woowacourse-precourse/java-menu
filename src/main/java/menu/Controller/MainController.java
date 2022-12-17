a94f66a35e524972f9ed0cbae4f01c2a7c5a025b
hojunking96 <junsong96@naver.com>
2022-12-17 17:28:16 +0900

    refactor() : 코드 분리

--- src/main/java/menu/Controller/MainController.java
+++ src/main/java/menu/Controller/MainController.java
@@ -1,47 +1,24 @@
 package menu.Controller;
 
-import menu.Model.Coach;
 import menu.Model.Coaches;
-import menu.View.InputView;
 import menu.View.OutputView;
 
-import java.util.List;
 
 public class MainController {
     private final Coaches coaches;
     private final RecommendController recommend;
+    private final InitController initController;
 
     public MainController() {
         coaches = new Coaches();
         recommend = new RecommendController(coaches);
+        initController = new InitController(coaches);
     }
 
     public void run() {
         OutputView.printRecommendStart();
-        initCoaches();
-        initNotPreferMenu();
+        initController.initCoaches();
+        initController.initNotPreferMenus();
         recommend.recommendStart();
-
     }
-
-
-    private void initCoaches() {
-        String input = InputView.requestCoachName();
-        List<String> coachNames = List.of(input.split(","));
-        for (String e : coachNames) {
-            Coach coach = new Coach(e);
-            this.coaches.addCoach(coach);
-        }
-    }
-
-    private void initNotPreferMenu() {
-        for (Coach coach : coaches.getCoaches()) {
-            String input = InputView.requestNotPreferMenu(coach.getName());
-            List<String> notPreferMenuNames = List.of(input.split(","));
-            for (String e : notPreferMenuNames) {
-                coach.addNotPreferMenu(e);
-            }
-        }
-    }
-
 }
