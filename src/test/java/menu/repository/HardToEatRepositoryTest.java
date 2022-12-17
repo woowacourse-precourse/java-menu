ddef54de384ccc0d52916e8aece48ce1f1f7127c
woo-chang <clllickme@naver.com>
2022-12-17 16:59:55 +0900

    test(HardToEatRepository): 중복되는 경우와 중복되지 않은 경우에 대해 음식을 저장하고 확인하는 기능 테스트

---
+++ src/test/java/menu/repository/HardToEatRepositoryTest.java
@@ -0,0 +1,39 @@
+package menu.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import menu.domain.Coach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HardToEatRepositoryTest {
+
+    @DisplayName("못 먹는 음식을 저장하고 확인한다.")
+    @Test
+    void storeMenu() {
+        Coach coach = new Coach("홍길동");
+        String menu = "김밥";
+
+        HardToEatRepository.addMenus(coach, menu);
+        List<String> menus = HardToEatRepository.findByCoach(coach);
+
+        assertThat(menus.size()).isEqualTo(1);
+        assertThat(menus).containsExactly(menu);
+    }
+
+    @DisplayName("중복된 음식은 저장되지 않는다.")
+    @Test
+    void storeDuplicationMenu() {
+        Coach coach = new Coach("홍길동");
+        String menu1 = "김밥";
+        String menu2 = "김밥";
+
+        HardToEatRepository.addMenus(coach, menu1);
+        HardToEatRepository.addMenus(coach, menu2);
+        List<String> menus = HardToEatRepository.findByCoach(coach);
+
+        assertThat(menus.size()).isEqualTo(1);
+        assertThat(menus).containsExactly(menu1);
+    }
+}
