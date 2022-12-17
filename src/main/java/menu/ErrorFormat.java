12ceff675171785cf5f3aa8eaab802163ce5c8e4
hodomaroo <enghemdwkd@gmail.com>
2022-12-17 17:08:12 +0900

    (feat/ErrorFormat) 에러 메시지 출력 형식 추가

--- src/main/java/menu/ErrorFormat.java
+++ src/main/java/menu/ErrorFormat.java
@@ -2,7 +2,8 @@ package menu;
 
 public enum ErrorFormat {
 
-    NEED_MORE_COUCH("코치는 최소 2명 이상 입력해야 합니다");
+    NEED_MORE_COUCH("코치는 최소 2명 이상 입력해야 합니다"),
+    INVALID_MENU_NAME("유효하지 않은 메뉴 이름입니다");
     private final String message;
 
     ErrorFormat(String message) {
