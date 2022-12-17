edb000785c5486c3e41bbcb6d883556f8a0da290
minaver <psmjoshua@gmail.com>
2022-12-17 15:34:40 +0900

    feat(Application): main 함수에 메뉴 추천 기능 추가

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,16 +1,10 @@
 package menu;
 
-import menu.domain.Menu;
-import menu.dto.CoachWeekMenu;
-import menu.enums.FoodCategory;
-import menu.vo.Coach;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import menu.domain.Recommend;
 
 public class Application {
     public static void main(String[] args) {
-        
+        Controller controller = new Controller(new Recommend());
+        controller.recommendMenu();
     }
 }
