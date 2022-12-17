266a4cd4b9f1ea1a55f65d553013a5c08f19bac3
kiarakim <kiarakim413@gmail.com>
2022-12-17 13:54:37 +0900

    feat: 가이드 메세지 추가

---
+++ src/main/java/menu/GuidanceMessage.java
@@ -0,0 +1,10 @@
+package menu;
+
+public interface GuidanceMessage {
+    String SERVICE_INTRO = "점심 메뉴 추천을 시작합니다.";
+    String INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    String INPUT_INEDIBLE = "(이)가 못 먹는 메뉴를 입력해 주세요.";
+    String SERVICE_RESULT = "메뉴 추천 결과입니다.";
+    String SERVICE_OUTRO = "추천을 완료했습니다.";
+
+}
