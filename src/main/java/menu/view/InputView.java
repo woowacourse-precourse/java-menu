905ff6f4ea1e41d12b74f360d3e29f8afebe39bf
Cyma-s <ssol0319@naver.com>
2022-12-17 13:20:36 +0900

    feat(InputView) : 코치 이름 입력 후 중복 확인 함수 추가

---
+++ src/main/java/menu/view/InputView.java
@@ -0,0 +1,25 @@
+package menu.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import menu.domain.InputException;
+
+public class InputView {
+    private final static String INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)\n";
+
+    public void getCoachName() {
+        System.out.println(INPUT_COACH_NAME);
+        String userCoachName = Console.readLine();
+        validateCoachName(userCoachName);
+    }
+
+    private void validateCoachName(String uncheckedCoachNames) {
+        List<String> coachNames = List.of(uncheckedCoachNames.split(","));
+        Set<String> uniqueCoachNames = new HashSet<>(coachNames);
+        if(coachNames.size() != uniqueCoachNames.size()) {
+            throw new IllegalArgumentException(InputException.DUPLICATE_COACH_NAME.getExceptionMessage());
+        }
+    }
+}
