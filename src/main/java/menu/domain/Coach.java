31e0b7da779cedd58b23a5512501944129c1094b
moonstal1506 <moonstal1506@gmail.com>
2022-12-17 15:50:26 +0900

    refactor: 못먹는 메뉴 추가 방식 변경

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -3,24 +3,32 @@ package menu.domain;
 public class Coach {
 
     private final String name;
-    private Menus couldNotEatMenus;
+    private Menus canNotEatMenus ;
 
-    public Coach(String name, Menus CouldNotEatMenus) {
+    public Coach(String name) {
         if(!isValidName(name)){
             throw new IllegalArgumentException("코치의 이름은 최소 2글자, 최대 4글자입니다.");
         }
-        if(!isValidMenuSize(couldNotEatMenus)){
-            throw new IllegalArgumentException("코치가 못 먹는 메뉴의 개수는 최소 0개, 최대 2개입니다.");
-        }
         this.name = name;
-        this.couldNotEatMenus = CouldNotEatMenus;
+        this.canNotEatMenus = new Menus("");
     }
 
     private static boolean isValidName(String name) {
         return 2 <= name.length() && name.length() <= 4;
     }
 
-    private boolean isValidMenuSize(Menus couldNotEatMenus) {
-        return couldNotEatMenus.size() <= 2;
+    public void addCanNotEatMenus(Menus canNotEatMenus) {
+        if(!isValidMenuSize(canNotEatMenus)){
+            throw new IllegalArgumentException("코치가 못 먹는 메뉴의 개수는 최소 0개, 최대 2개입니다.");
+        }
+        this.canNotEatMenus = canNotEatMenus;
+    }
+
+    private boolean isValidMenuSize(Menus canNotEatMenus) {
+        return canNotEatMenus.size() <= 2;
+    }
+
+    public String getName() {
+        return name;
     }
 }
