24de5d37808f7fa896b4ee1f80700a56dd80ee1e
zeunala <zeunala@naver.com>
2022-12-17 14:23:17 +0900

    feat: 최대 횟수 이상 추천하지 않도록 하는 기능 추가

--- src/main/java/menu/service/CategoryRecommendService.java
+++ src/main/java/menu/service/CategoryRecommendService.java
@@ -1,17 +1,45 @@
 package menu.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CategoryRecommendService {
+    /**
+     * 카테고리별 몇번 추천되었는지 횟수를 제공한다.
+     */
+    private static final Map<String, Integer> recommendCount = new HashMap<>();
+    public static final int MAXIMUM_COUNT_LIMIT = 2;
+
     public static String recommendCategory() {
-        String selectedCategory = pickCategory();
+        String selectedCategory;
+        do {
+            selectedCategory = pickCategory();
+        } while (isMaximumLimit(selectedCategory));
         return selectedCategory;
     }
 
     private static String pickCategory() {
         int categoryNumber = Randoms.pickNumberInRange(1, 5);
         return Arrays.asList("일식", "한식", "중식", "아시안", "양식").get(categoryNumber - 1);
+    }
+
+    /**
+     * 카테고리에 대해 최대 횟수 이상 추천시 false 반환하고, 그렇지 않으면 추천횟수 증가 후 true 반환
+     *
+     * @param category 판단 원하는 카테고리
+     * @return 최대 횟수 이상 추천여부
+     */
+    private static boolean isMaximumLimit(String category) {
+        if (!recommendCount.containsKey(category)) {
+            recommendCount.put(category, 0);
+        }
+
+        if (recommendCount.get(category) >= MAXIMUM_COUNT_LIMIT) {
+            return true;
+        }
+        recommendCount.put(category, recommendCount.get(category) + 1);
+        return false;
     }
 }
