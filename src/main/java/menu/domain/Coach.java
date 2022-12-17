6c5cfe4398df3a11da8f100b4c2e6f2e0d1e283f
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 14:34:53 +0900

    feat(Coach): 음식이 추천 리스트에 있는지 확인 기능 구현

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -26,6 +26,10 @@ public class Coach {
         return inedibleFoods.stream().noneMatch(foodName::equals);
     }
 
+    public boolean isRecommended(String foodName) {
+        return recommendation.values().stream().anyMatch(foodName::equals);
+    }
+
     // TODO: 코치 이름 2글자에서 4글자 검증 기능 구현하기
 
     public String getName() {
