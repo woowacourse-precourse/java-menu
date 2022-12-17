f6d5536f233d6abf06d9e984efd0900d4628f87e
K-jun98 <alap_u@naver.com>
2022-12-17 13:17:06 +0900

    함께 식사할 코치들의 인원은 최소2명 최대 5명으로 제한함

--- src/main/java/menu/view/Input.java
+++ src/main/java/menu/view/Input.java
@@ -6,7 +6,14 @@ public class Input {
 
     public String[] inputCoachNames() {
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
-        return Console.readLine().split(",");
+        return validateCoachNumber(Console.readLine().split(","));
+    }
+
+    private String[] validateCoachNumber(String[] names) {
+        if (names.length < 2 || 5 < names.length) {
+            throw new IllegalArgumentException("코치의 인원은 최소 2명, 최대 5명까지 식사를 함께 할 수 있습니다.");
+        }
+        return names;
     }
 
 }
