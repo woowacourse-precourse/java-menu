f67c902b46d561306576a7ce8f5f0e8022bea12a
K-jun98 <alap_u@naver.com>
2022-12-17 13:11:05 +0900

    메뉴 추천을 받을 코치의 명단을 입력 받는다.

---
+++ src/main/java/menu/view/Input.java
@@ -0,0 +1,12 @@
+package menu.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public String[] inputCoachNames() {
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        return Console.readLine().split(",");
+    }
+
+}
