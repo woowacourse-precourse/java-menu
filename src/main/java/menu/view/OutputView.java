18ce312aecf2bb036c706143254d61bcf5f37c43
Cyma-s <ssol0319@naver.com>
2022-12-17 14:50:26 +0900

    feat(OutputView) : 서비스 시작 문구 출력 함수 추가

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,4 +1,14 @@
 package menu.view;
 
 public class OutputView {
+    private final static String SERVICE_START = "점심 메뉴 추천을 시작합니다.\n";
+    private final static String RECOMMEND_RESULT_DAY_WEEK = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]";
+    private final static String RECOMMEND_RESULT_CATEGORY = "[ 카테고리 | ";
+    private final static String RECOMMEND_RESULT_PARTITION = " | ";
+    private final static String RECOMMEND_RESULT_START = "[ ";
+    private final static String RECOMMEND_RESULT_END = " ]";
+
+    public void printServiceStart() {
+        System.out.println(SERVICE_START);
+    }
 }
