9e1150415bbf13c03f2e7173c8ea240e5f2d2934
JunDonghyuk <ehdgur4814@naver.com>
2022-12-17 15:00:58 +0900

    feat(MenusConstant): 메뉴 선정에 필요한 상수 추가

---
+++ src/main/java/menu/model/MenuConstant.java
@@ -0,0 +1,5 @@
+package menu.model;
+
+public class MenuConstant {
+    public static final int MAX_CATEGORY_NUMBER = 2;
+}
--- src/main/java/menu/output/MenuConstant.java
+++ src/main/java/menu/output/MenuConstant.java
@@ -1,5 +1,9 @@
 package menu.output;
 
 public class MenuConstant {
-
+    public static final String JAPAN_FOOD = "일식";
+    public static final String KOREA_FOOD = "한식";
+    public static final String CHINA_FOOD = "중식";
+    public static final String ASIA_FOOD = "아시안";
+    public static final String WEST_FOOD = "양식";
 }
