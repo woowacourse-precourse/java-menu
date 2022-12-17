5da73c87b4f2912af98c84e6f49d71fbc270051a
CAEL0 <kjongmin26@gmail.com>
2022-12-17 16:30:24 +0900

    refactor(InputView): 입력을 받는 기능을 빼고 나머지 부분들을 제거

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -4,17 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String readCoachNames() {
-        System.out.println(ViewConstants.ASKING_NAME);
-        String input = Console.readLine();
-        System.out.println();
-        return input;
-    }
-
-    public String readInedibleMenu(String coachName) {
-        System.out.printf(ViewConstants.ASKING_INEDIBLE_MENU, coachName);
-        System.out.println();
-
+    public String readLine() {
         String input = Console.readLine();
         System.out.println();
         return input;
