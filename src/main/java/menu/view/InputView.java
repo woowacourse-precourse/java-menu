af3d76bc240f5ef7d93b11281d58de8fed95ee2c
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2022-12-17 16:46:14 +0900

    feat(menu) : 코치의 숫자가 잘못 됐을 경우 에러를 발생시키고 다시 입력을 받음

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,6 +1,7 @@
 package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import menu.constants.CoachRules;
 
 import java.util.Arrays;
 import java.util.List;
@@ -8,8 +9,24 @@ import java.util.stream.Collectors;
 
 public class InputView {
     public static List<String> readCoachName() {
-        return Arrays.stream(Console.readLine().split(","))
-                .collect(Collectors.toList());
+        try{
+            List<String> coaches = Arrays.stream(Console.readLine().split(","))
+                    .collect(Collectors.toList());
+            validate(coaches);
+            return coaches;
+        } catch(IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readCoachName();
+        }
+    }
+
+    private static void validate(List<String> coaches) {
+        if (coaches.size() < CoachRules.MIN_NUMBER_OF_COACHES.getValue()) {
+            throw new IllegalArgumentException(ErrorMessages.COACH_NUMBER_MIN.getMessage());
+        }
+        if (coaches.size() > CoachRules.MAX_NUMBER_OF_COACHES.getValue()) {
+            throw new IllegalArgumentException(ErrorMessages.COACH_NUMBER_MAX.getMessage());
+        }
     }
 
     public static List<String> readInedibleFoods() {
