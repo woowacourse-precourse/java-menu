7a0c5731e226fdf147ec3e3ca75164572e0f64f3
kiarakim <kiarakim413@gmail.com>
2022-12-17 17:36:37 +0900

    feat: 입력 화면 기능

---
+++ src/main/java/menu/view/InputView.java
@@ -0,0 +1,33 @@
+package menu.view;
+
+import static menu.view.OutputView.printError;
+
+import camp.nextstep.edu.missionutils.Console;
+import menu.exception.InputException;
+
+public class InputView {
+
+    private final InputException inputException = new InputException();
+
+    public String readCrewsName() {
+        try {
+            String input = Console.readLine();
+            inputException.validateCrews(input);
+            return input;
+        } catch (IllegalArgumentException exception) {
+            printError(exception.getMessage());
+            return readCrewsName();
+        }
+    }
+
+    public String readBannedFoods() {
+        try {
+            String input = Console.readLine();
+            return input;
+        } catch (IllegalArgumentException exception) {
+            printError(exception.getMessage());
+            return readBannedFoods();
+        }
+    }
+
+}
