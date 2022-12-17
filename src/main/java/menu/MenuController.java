e66a717f96d946a564b1d2aa3117838beea0c789
iamjooon2 <iamjooon2@gmail.com>
2022-12-17 16:29:50 +0900

    feat: 코치가 먹지 못하는 메뉴를 저장하는 기능

--- src/main/java/menu/MenuController.java
+++ src/main/java/menu/MenuController.java
@@ -1,10 +1,21 @@
 package menu;
 
+import java.util.List;
+import menu.view.InputView;
+
 
 public class MenuController {
 
+    InputView inputView = new InputView();
+
 
     public void run() {
+        List<String> coachLists = inputView.getCoachList();
+        CoachHates coachHates = new CoachHates();
+        for (int i = 0; i < coachLists.size(); i++) {
+            List<String> hateFoods = inputView.getHateList(coachLists.get(i));
+            coachHates.updateHateMap(coachLists.get(i), hateFoods);
+        }
 
     }
 
