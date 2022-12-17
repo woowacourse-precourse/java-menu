be5e6973c5a7e6c43fe881e66fc19918227e2e97
jojaeng2 <ds4ouj@naver.com>
2022-12-17 17:02:33 +0900

    test : 존재하는 코치 목록을 관리하는 기능 테스트

---
+++ src/test/java/menu/service/CoachServiceTest.java
@@ -0,0 +1,48 @@
+package menu.service;
+
+import menu.domain.Coach;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoachServiceTest {
+
+    private static final Integer COACH_LIST_LENGTH = 10;
+
+    @Test
+    void 코치목록조회() {
+        // given
+        List<Coach> coaches = constructorCoaches();
+        final CoachService coachService = new CoachService(coaches);
+
+        // when
+
+        // then
+        Assertions.assertThat(coachService.getCoaches().size()).isEqualTo(COACH_LIST_LENGTH);
+    }
+
+    @Test
+    void 없는코치에거절메뉴추가() {
+        // given
+        List<Coach> coaches = constructorCoaches();
+        final CoachService coachService = new CoachService(coaches);
+        Coach coach = new Coach("test");
+        List<String> refuseMenus = new ArrayList<>();
+
+        // when
+        coachService.addRefuseMenuToCoach(coach, refuseMenus);
+        // then
+    }
+
+    private List<Coach> constructorCoaches() {
+        List<Coach> coaches = new ArrayList<>();
+        for (int i=0; i<COACH_LIST_LENGTH; i++) {
+            Coach coach = new Coach(String.valueOf(i));
+            coaches.add(coach);
+        }
+        return coaches;
+    }
+}
