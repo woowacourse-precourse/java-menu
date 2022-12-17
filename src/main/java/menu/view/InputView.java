b088be87d51c99eecc25f44152c2fbcee7d7e024
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-12-17 14:45:50 +0900

    feat(InputView): 코치 입력을 받는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,1 +1,1 @@
-package menu.view;public class InputView {}
+package menu.view;import camp.nextstep.edu.missionutils.Console;public class InputView {    private static final String INPUT_COACH_MESSAGE= "코치의 이름을 입력해 주세요. (, 로 구분)";    public String readCoachInput() {        System.out.println(INPUT_COACH_MESSAGE);        return Console.readLine();    }}
