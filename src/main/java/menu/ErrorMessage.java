5a5ff72ca3102585553003c771a6f762cf564a39
kiarakim <kiarakim413@gmail.com>
2022-12-17 14:18:07 +0900

    feat: 코치 이름 글자 에러 추가

--- src/main/java/menu/ErrorMessage.java
+++ src/main/java/menu/ErrorMessage.java
@@ -2,6 +2,7 @@ package menu;
 
 public interface ErrorMessage {
     String PREFIX = "[ERROR] ";
+    String INVALID_COACH_NAME = "코치의 이름은 최소 2글자, 최대 4글자입니다.";
     String INVALID_COACH_MIN ="코치는 최소 2명과 식사를 합니다.";
     String INVALID_COACH_NAX = "코치는 최대 5명과 식사를 합니다.";
     String INVALID_INEDIBLE_EACH = "각 코치는 최대 2개의 못 먹는 메뉴가 있습니다.";
