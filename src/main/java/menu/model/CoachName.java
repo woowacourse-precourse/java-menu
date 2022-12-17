571e823920cba0f07c0f6d1a10202eb5637f7d5b
은지 <midzy0228@gmail.com>
2022-12-17 13:33:48 +0900

    feat: 코치의 이름길이 검증 기능 구현

---
+++ src/main/java/menu/model/CoachName.java
@@ -0,0 +1,28 @@
+package menu.model;
+
+import java.util.List;
+
+public class CoachName {
+
+    private static final Integer MIN_NAME_SIZE = 2;
+    private static final Integer MAX_NAME_SIZE = 4;
+
+    private final List<String> coachNames;
+
+    public CoachName(List<String> coachNames) {
+        validate(coachNames);
+        this.coachNames = coachNames;
+    }
+
+    private void validate(List<String> coachNames) {
+        for (String coachName : coachNames) {
+            if (!isValidCoachNameLength(coachName)) {
+                throw new IllegalArgumentException("코치의 이름은 최소 2글자, 최대 4글자 입니다.");
+            }
+        }
+    }
+
+    private boolean isValidCoachNameLength(String name) {
+        return MIN_NAME_SIZE <= name.length() && name.length() <= MAX_NAME_SIZE;
+    }
+}
