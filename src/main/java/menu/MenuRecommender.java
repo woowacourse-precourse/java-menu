b482ff60463ef83cb8e69b13d0eaac83357434ce
Egenieee <hhgg0925@naver.com>
2022-12-17 15:33:39 +0900

    fix: 코치마다 카테고리를 뽑는 것이 아닌, 전체 코치를 위한 카테고리를 뽑는 것으로 로직 수정

--- src/main/java/menu/MenuRecommender.java
+++ src/main/java/menu/MenuRecommender.java
@@ -2,14 +2,14 @@ package menu;
 
 import utils.RandomCategoryGenerator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class MenuRecommender {
     private static final String DELIMITER = ",";
     private static final List<Coach> coaches = new ArrayList<>();
+    private static List<String> categories;
+    private static Map<String, Integer> categoriesCounter = initCounter();
 
     public static List<String> splitter(String coachNames) {
         return Arrays.stream(coachNames.split(DELIMITER))
@@ -27,27 +27,45 @@ public class MenuRecommender {
     }
 
     public static void getCoachRecommendFoods() {
+        categories = getRandomCategories();
+        System.out.println(categories);
+
         for (Coach coach : coaches) {
-            getEachCoachRecommendFoods(coach);
-            System.out.println(coach);
+            coach.setCategories(categories);
         }
     }
 
-    private static void getEachCoachRecommendFoods(Coach coach) {
-        List<String> categories = getRandomCategories(coach);
-        coach.setCategories(categories);
-    }
-
-    private static List<String> getRandomCategories(Coach coach) {
+    private static List<String> getRandomCategories() {
         List<String> categories = new ArrayList<>();
 
         for (int i = 0 ; i < 5 ; i++) {
             String category = RandomCategoryGenerator.getRandomCategory();
-            if (coach.isValidCategory(category)) {
+            if (isValidCategory(category)) {
                 categories.add(category);
             }
         }
 
         return categories;
+    }
+
+    public static boolean isValidCategory(String category) {
+        if (categoriesCounter.get(category) + 1 < 3) {
+            categoriesCounter.put(category,categoriesCounter.getOrDefault(category, 0) + 1);
+            return true;
+        }
+
+        return false;
+    }
+
+    private static Map<String, Integer> initCounter() {
+        Map<String, Integer> counter = new HashMap<>();
+
+        counter.put(Category.getCategories(1), 0);
+        counter.put(Category.getCategories(2), 0);
+        counter.put(Category.getCategories(3), 0);
+        counter.put(Category.getCategories(4), 0);
+        counter.put(Category.getCategories(5), 0);
+
+        return counter;
     }
 }
