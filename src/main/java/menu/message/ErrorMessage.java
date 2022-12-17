afdc3fa08092266db84f01ac7cb996f978ec7a43
HiiWee <wpdlzhf159@gmail.com>
2022-12-17 13:42:59 +0900

    feat: 코치는 2명부터 최대 5명까지 입력할 수 있는 예외 처리 추가

--- src/main/java/menu/message/ErrorMessage.java
+++ src/main/java/menu/message/ErrorMessage.java
@@ -2,6 +2,7 @@ package menu.message;
 
 public class ErrorMessage {
     public static final String INVALID_COACH_NAME_LENGTH = "[ERROR] 코치의 이름은 2~4글자여야 합니다.";
+    public static final String INVALID_COACH_COUNT = "[ERROR] 코치는 최소 2명에서 최대 5명까지 입력할 수 있습니다.";
 
     private ErrorMessage() {
     }
--- src/main/java/menu/util/InputValidator.java
+++ src/main/java/menu/util/InputValidator.java
@@ -6,11 +6,20 @@ import java.util.stream.Collectors;
 import menu.message.ErrorMessage;
 
 public class InputValidator {
+
+    public static final int MIN_COACH = 2;
+    public static final int MAX_COACH = 5;
+    public static final int MIN_NAME = 2;
+    public static final int MAX_NAME = 4;
+
     private InputValidator() {
     }
 
 
     public static List<String> validateCoach(final String inputCoach) {
+        if (!isValidCoachCount(inputCoach)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_COACH_COUNT);
+        }
         if (!isValidLength(inputCoach)) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_COACH_NAME_LENGTH);
         }
@@ -18,9 +27,15 @@ public class InputValidator {
                 .collect(Collectors.toList());
     }
 
+    private static boolean isValidCoachCount(final String inputCoach) {
+        int count = (int) Arrays.stream(inputCoach.split(","))
+                .count();
+        return count >= MIN_COACH && count <= MAX_COACH;
+    }
+
     private static boolean isValidLength(final String inputCoach) {
         return Arrays.stream(inputCoach.split(","))
                 .map(String::length)
-                .allMatch(length -> length >= 2 && length <= 4);
+                .allMatch(length -> length >= MIN_NAME && length <= MAX_NAME);
     }
 }
