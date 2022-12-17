5e3ca47bf3d4f2f30e7945b6c868158d42513ed0
KSH <tkdgns543@gmail.com>
2022-12-17 14:04:10 +0900

    feat(domain): 카테고리를 추천하고 3개보다 많이 나오지 않았는지 확인하는 기능 구현

--- src/main/java/menu/domain/MenuRecommender.java
+++ src/main/java/menu/domain/MenuRecommender.java
@@ -1,11 +1,22 @@
 package menu.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MenuRecommender {
     private List<Category> categoryHistory = new ArrayList<>();
     private List<String> coaches = new ArrayList<>();
     private static final List<Category> CATEGORIES = Arrays.asList(Category.values());
+
+    private Category pickupCategory() {
+        return CATEGORIES.get(Randoms.pickNumberInRange(1, 5));
+    }
+
+    private boolean validCategory(Category category) {
+        return Collections.frequency(categoryHistory, category) == 3;
+    }
 }
