cf5a191c34f7b79e690292576c3524766c230d7a
신채원 <cw121@naver.com>
2022-12-17 13:44:25 +0900

    feat: 코치의 이름들을 입력받는 기능 구현

---
+++ src/main/java/menu/inputView.java
@@ -0,0 +1,12 @@
+package menu;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class inputView {
+
+    public String readNames() {
+        System.out.println("점심 메뉴 추천을 시작합니다.");
+        System.out.println("\n코치의 이름을 입력해 주세요. (, 로 구분)");
+        return Console.readLine();
+    }
+}
