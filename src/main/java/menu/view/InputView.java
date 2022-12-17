123e118f47b70afd367d47b8df950c571485a0f7
This2sho <zx00018@naver.com>
2022-12-17 13:29:06 +0900

    feat(InputView): 코치가 못먹는 메뉴 입력받는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -7,18 +7,19 @@ import java.util.stream.Collectors;
 
 public class InputView {
 
-    public static final String NAME_DELIMITER = ",";
+    public static final String INPUT_DELIMITER = ",";
+    public static final int MAX_FOODS = 2;
 
     public List<String> readCoachNames() {
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
         String input = Console.readLine();
-        List<String> names = getNames(input);
+        List<String> names = toList(input);
         validateCoachNames(names);
         return names;
     }
 
-    private List<String> getNames(String input) {
-        return Arrays.stream(input.split(NAME_DELIMITER))
+    private List<String> toList(String input) {
+        return Arrays.stream(input.split(INPUT_DELIMITER))
             .collect(Collectors.toList());
     }
 
@@ -34,6 +35,20 @@ public class InputView {
     private void validateNameLength(String name) {
         if (name.length() < 2 || name.length() > 4) {
             throw new IllegalArgumentException("이름은 최소 2글자, 최대 4글자여야 합니다.");
+        }
+    }
+
+    public List<String> readNotEatFoods(String coachName) {
+        System.out.printf("%s(이)가 못 먹는 메뉴를 입력해 주세요.\n", coachName);
+        String input = Console.readLine();
+        List<String> foods = toList(input);
+        validateNotEatFoods(foods);
+        return foods;
+    }
+
+    private void validateNotEatFoods(List<String> foods) {
+        if (foods.size() > MAX_FOODS) {
+            throw new IllegalArgumentException("못 먹는 메뉴는 최대 2개만 입력 가능합니다.");
         }
     }
 }
