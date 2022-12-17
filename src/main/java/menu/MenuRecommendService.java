417b6810812efc9c73376fe824d636696433e0bf
hum02 <ehdgma02@gmail.com>
2022-12-17 17:00:52 +0900

    fix: 테스트 통과하도록 요일별로 메뉴 선택하게 수정

--- src/main/java/menu/MenuRecommendService.java
+++ src/main/java/menu/MenuRecommendService.java
@@ -7,39 +7,42 @@ import menu.enums.Category;
 import menu.repository.MenuRepository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static camp.nextstep.edu.missionutils.Randoms.shuffle;
 
 public class MenuRecommendService {
 
     public List<WeekMenus> recommendMenus(List<Coach> coaches, List<Category> weekCategories) {
-        //못 먹는 음식 고려
-        //한 주에 먹었던 음식 또 안됨
         List<WeekMenus> weekMenus = new ArrayList<>();
+        Map<Coach, List<Menu>> coachMenu = new HashMap<>();
 
-        for (Coach coach : coaches) {
-            List<Menu> coachMenu = pickMenusInCategory(weekCategories, coach);
-            weekMenus.add(new WeekMenus(coach, coachMenu));
+        for (Category category : weekCategories) {
+            for (Coach coach : coaches) {
+                pickMenusInCategory(coachMenu, category, coach);
+            }
         }
+
+        for (Map.Entry<Coach, List<Menu>> menus : coachMenu.entrySet()) {
+            weekMenus.add(new WeekMenus(menus.getKey(), menus.getValue()));
+        }
+
         return weekMenus;
     }
 
-    private List<Menu> pickMenusInCategory(List<Category> categories, Coach coach) {
-        List<Menu> coachMenu = new ArrayList<>();
-
-        for (Category category : categories) {
-            String menuName = shuffle(MenuRepository.getMenusByCategory(category)).get(0);
-            if ((isAlreadyExist(coachMenu, menuName)) || coach.isExcludeMenu(menuName)) {
-                menuName = shuffle(MenuRepository.getMenusByCategory(category)).get(0);
-            }
-            coachMenu.add(new Menu(category, menuName));
-
+    private void pickMenusInCategory(Map<Coach, List<Menu>> coachMenu, Category category, Coach coach) {
+        String menuName = shuffle(MenuRepository.getMenusByCategory(category)).get(0);
+        if ((isAlreadyExist(coachMenu, coach, menuName)) || coach.isExcludeMenu(menuName)) {
+            menuName = shuffle(MenuRepository.getMenusByCategory(category)).get(0);
         }
-        return coachMenu;
+        List<Menu> toAdd = coachMenu.getOrDefault(coach, new ArrayList<>());
+        toAdd.add(new Menu(category, menuName));
+        coachMenu.put(coach, toAdd);
     }
 
-    private boolean isAlreadyExist(List<Menu> coachMenu, String menuName) {
-        return coachMenu.stream().anyMatch(m -> m.isSameMenu(menuName));
+    private boolean isAlreadyExist(Map<Coach, List<Menu>> coachMenu, Coach coach, String menuName) {
+        return coachMenu.getOrDefault(coach, new ArrayList<>()).stream().anyMatch(menu -> menu.isSameMenu(menuName));
     }
 }
