53ee4adffe4513361b1295ba240e872db3b99e5d
Reznov <reznov03048@gamil.com>
2022-12-17 14:19:30 +0900

    feat: 입력받은 코치 이름으로 Coach 객체 만들어 CoachRepository에 저장하는 기능 구현

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,16 +1,24 @@
 package menu.controller;
 
+import menu.domain.Coach;
+import menu.repository.CoachRepository;
 import menu.view.InputView;
 import menu.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MenuController {
-
+    private CoachRepository coachRepository;
+    public MenuController() {
+        coachRepository = new CoachRepository();
+    }
     public void start() {
         OutputView.printStartComment();
-
+        // 코치들 입력받음
         List<String> names = inputCoachNames();
+        List<Coach> coaches = nameToCoaches(names);
+        coachRepository.setCoaches(coaches);
     }
 
     private List<String> inputCoachNames() {
@@ -18,5 +26,13 @@ public class MenuController {
         List<String> names = InputView.readNames();
 
         return names;
+    }
+
+    private List<Coach> nameToCoaches(List<String> names) {
+        List<Coach> coaches = new ArrayList<>();
+        for(String name : names) {
+            coaches.add(new Coach(name));
+        }
+        return coaches;
     }
 }
