32a867f085395c658432ce1763162871121ce0c2
SH <gram753@naver.com>
2022-12-17 14:00:26 +0900

    feat: 전체적인 로직을 담당하는 클래스 구현

---
+++ src/main/java/menu/Controller.java
@@ -0,0 +1,40 @@
+package menu;
+
+import java.util.List;
+
+import menu.model.Coaches;
+import menu.model.Menu;
+import menu.model.MenuList;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+public class Controller {
+
+	private Coaches coaches;
+	private Menu menu;
+	private MenuList menusCantEat;
+	private InputView inputView;
+
+
+	public void logic() {
+		OutputView.printStartMessage();
+		coaches = setCoachesName();
+		menusCantEat = setMenusCantEat();
+		// OutputView.printResult();
+	}
+
+	private Coaches setCoachesName() {
+		OutputView.printCoachNameInputMessage();
+		List<String> names = inputView.inputCoaches();
+		return new Coaches(names);
+	}
+
+	private MenuList setMenusCantEat() {
+		for (int i = 0; i < coaches.getNames().size(); i++) {
+			OutputView.printMenuInputMessage(coaches.getNames().get(i));
+			List<String> menuNames = inputView.inputMenuCantEat();
+			menusCantEat.getMenusCantEat().add(new Menu(menuNames));
+		}
+		return menusCantEat;
+	}
+}
