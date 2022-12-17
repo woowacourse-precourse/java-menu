cc2762874eb5820d920cf286c8c740fe92841131
LHS-11 <ghktjq1119@naver.com>
2022-12-17 13:41:17 +0900

    [Docs] : 기능 구현 목록에서 코치 이름 입력 예외사항 추가

---
+++ src/main/java/menu/model/Coach.java
@@ -0,0 +1,13 @@
+package menu.model;
+
+import java.util.List;
+
+public class Coach {
+
+    private String name;
+
+    private List<Menu> menus;
+
+
+
+}
---
+++ src/main/java/menu/model/CoachGroup.java
@@ -0,0 +1,10 @@
+package menu.model;
+
+import java.util.List;
+
+public class CoachGroup {
+
+    private List<Coach> coaches;
+
+
+}
