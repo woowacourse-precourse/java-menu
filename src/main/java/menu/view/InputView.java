36b93989ebffe0f0934e64509b8d25bfd9c16c43
SH <gram753@naver.com>
2022-12-17 14:57:27 +0900

    feat: 예외처리 및 다시 입력받을 수 있는 기능 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,5 +1,7 @@
 package menu.view;
 
+import static menu.Constant.*;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -8,20 +10,45 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 	public List<String> inputCoaches() {
-		String input = Console.readLine();
-		String[] names = input.split(",");
-		// isStringValidate();
-		// isNameLengthValidate();
-		return Stream.of(names)
-			.collect(Collectors.toList());
+		while (true) {
+			try {
+				String input = Console.readLine();
+				String[] names = input.replace(" ", "").split(",");
+				isNameLengthValidate(names);
+				isArrayLengthValidate(names);
+				return Stream.of(names)
+					.collect(Collectors.toList());
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			}
+		}
 	}
 
 	public List<String> inputMenuCantEat() {
-		String input = Console.readLine();
-		String[] menus = input.split(",");
-		// isString();
-		// isMenuLengthValidate();
-		return Stream.of(menus)
-			.collect(Collectors.toList());
+		while (true) {
+			try {
+				String input = Console.readLine();
+				String[] menus = input.replace(" ", "").split(",");
+				// isMenuLengthValidate();
+				return Stream.of(menus)
+					.collect(Collectors.toList());
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	private void isArrayLengthValidate(String[] names) {
+		if (names.length > 5 || names.length < 2) {
+			throw new IllegalArgumentException(WRONG_MEMBER_MSG);
+		}
+	}
+
+	private void isNameLengthValidate(String[] names) {
+		for (String name : names) {
+			if (name.length() > 4 || name.length() < 2) {
+				throw new IllegalArgumentException(WRONG_NAME_MSG);
+			}
+		}
 	}
 }
