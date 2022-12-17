0933f70e79b6a3330a7905390a1365f15d8dc1c6
kwYoohae <gocksdl7979@gmail.com>
2022-12-17 13:15:08 +0900

    feat: 시작 메세지 출력 기능 작성

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,10 @@
+package menu.view;
+
+public class OutputView {
+
+    private final String FIRST_RUN_MESSAGE = "점심 메뉴 추천을 시작합니다.\n";
+
+    public void startMessage() {
+        System.out.println(FIRST_RUN_MESSAGE);
+    }
+}
