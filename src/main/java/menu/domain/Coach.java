23028f2f52901b84e80efc5bf9433e91c45ef2fc
MoonJeWoong <munjin0201@naver.com>
2022-12-17 14:04:13 +0900

    feat(domain): 코치 정보를 저장하고 관리한다.
    세부 구현 사항
      - 결정된 메뉴를 순서대로 저장한다.
      - 못 먹는 메뉴 정보를 저장한다.

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,2 +1,36 @@
-package menu.domain;public class Coach {
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coach {
+
+    private static final String NAME_LENGTH_ERROR = "코치 이름은 2~4글자 이내여야 합니다.";
+    private final String name;
+    private final List<String> selectedMenus = new ArrayList<>();
+    private final List<String> inedibleMenus = new ArrayList<>();
+
+    public Coach(String name){
+        validateCoachName(name);
+        this.name = name;
+    }
+
+    private void validateCoachName(String name){
+        if(name.length()<2 || name.length()>4){
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR);
+        }
+    }
+
+    public void addSelectedMenus(String menu){
+        selectedMenus.add(menu);
+    }
+
+    public boolean containsSelectedMenu(String menu){
+        return selectedMenus.contains(menu);
+    }
+
+    public boolean isInedibleMenu(String menu){
+        return inedibleMenus.contains(menu);
+    }
+
 }
