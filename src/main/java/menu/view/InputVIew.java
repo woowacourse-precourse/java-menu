2a2a3e7373f34e931fbea4b54b027b5a01751576
Ridealist <cjsn92@gmail.com>
2022-12-17 17:49:16 +0900

    feat: 코치 인원 수 및 못먹는 메뉴 최대 가지수 유효성 검사 로직 추가 구현

--- src/main/java/menu/view/InputVIew.java
+++ src/main/java/menu/view/InputVIew.java
@@ -18,24 +18,45 @@ public class InputVIew {
         }
     }
 
+    private static final int COACH_SIZE_LOWER_BOUND = 2;
+    private static final int COACH_SIZE_UPPER_BOUND = 5;
+    private static final int CANNOT_EAT_MENU_UPPER_BOUND = 2;
+    private static final String COACH_SIZE_INVALID_MESSAGE = "코치는 최소 2명, 최대 5명까지 식사를 함께 합니다.";
+    private static final String MENU_SIZE_INVALID_MESSAGE = "못먹는 메뉴는 최소 0개, 최대 2개까지 입력할 수 있습니다.";
     private static final String DELIMITER = ",";
 
-    public static List<String> readCoachNames() {
+    public static List<String> readCoachNames() throws IllegalArgumentException {
         System.out.println(Message.REQUEST_COACH_NAME.explanation);
         String input = readInputAddOneLineBreak(Console::readLine);
-        return Arrays.asList(input.split(DELIMITER));
+        List<String> coaches = Arrays.asList(input.split(DELIMITER));
+        validateCoachSize(coaches);
+        return coaches;
     }
 
     public static List<String> readCannotEatMenus(Coach coach) {
         System.out.printf(Message.REQUEST_CANNOT_EAT_MENUS.explanation, coach.getName());
         System.out.println();
         String input = readInputAddOneLineBreak(Console::readLine);
-        return Arrays.asList(input.split(DELIMITER));
+        List<String> menus = Arrays.asList(input.split(DELIMITER));
+        validateCannotEatMenusSize(menus);
+        return menus;
     }
 
     public static <T> T readInputAddOneLineBreak(Supplier<T> inputReader) {
         T input = inputReader.get();
         System.out.println();
         return input;
+    }
+
+    private static void validateCoachSize(List<String> coaches) {
+        if (coaches.size() < COACH_SIZE_LOWER_BOUND || coaches.size() > COACH_SIZE_UPPER_BOUND) {
+            throw new IllegalArgumentException(COACH_SIZE_INVALID_MESSAGE);
+        }
+    }
+
+    private static void validateCannotEatMenusSize(List<String> menus) {
+        if (menus.size() > CANNOT_EAT_MENU_UPPER_BOUND) {
+            throw new IllegalArgumentException(MENU_SIZE_INVALID_MESSAGE);
+        }
     }
 }
