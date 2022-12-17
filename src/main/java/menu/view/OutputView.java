3b9bc95f374c6ea832e98fc76bdbc645207d6d4e
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 13:27:10 +0900

    feat(OutputView): 추천 프로그램 시작 메시지 출력하기 기능 추가
    docs: 추천 프로그램 시작 메시지 출력하기 완료 체크

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,9 @@
+package menu.view;
+
+public class OutputView {
+    private static final String START_MENU_RECOMMEND = "점심 메뉴 추천을 시작합니다.";
+
+    public void printStartProgram() {
+        System.out.println(START_MENU_RECOMMEND);
+    }
+}
