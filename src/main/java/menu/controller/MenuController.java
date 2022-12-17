b88b6a61b854a5459952bc3786039e42daf63c80
kokodak <kokodakadokok@gmail.com>
2022-12-17 16:19:12 +0900

    feat(menuController): run 메서드 구현

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -49,6 +49,7 @@ public class MenuController {
         recommendedCategories.stream().forEach(category -> {
             coaches.getCoaches().stream().forEach(coach -> coach.addEatMenu(category));
         });
+        OutputView.printRecommendedResult(recommendedCategories, coaches);
         OutputView.printEndMessage();
     }
 }
