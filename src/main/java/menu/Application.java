4495777c8620c437d89f6aae49f39f849e979530
khsrla9806 <kimhunsope@naver.com>
2022-12-17 16:14:17 +0900

    refactor(Application): main 메서드 내부로 MenuController 이동

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -3,8 +3,8 @@ package menu;
 import menu.controller.MenuController;
 
 public class Application {
-    private static MenuController menuController = new MenuController();
     public static void main(String[] args) {
+        MenuController menuController = new MenuController();
         menuController.startProgram();
     }
 }
