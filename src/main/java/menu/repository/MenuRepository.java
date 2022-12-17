c5b8090f75434ce423fc4c9d87139433a0d4b248
greeng00se <thegreengoose7@gmail.com>
2022-12-17 14:54:23 +0900

    feat(MenuRepository): 카테고리에 해당하는 메뉴명을 모두 가져오는 메서드 추가

--- src/main/java/menu/repository/MenuRepository.java
+++ src/main/java/menu/repository/MenuRepository.java
@@ -1,6 +1,7 @@
 package menu.repository;
 
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 import menu.domain.Category;
 import menu.domain.Menus;
@@ -9,10 +10,12 @@ public class MenuRepository {
 
     private static final Map<Category, Menus> repository = new EnumMap<>(Category.class);
 
-    public MenuRepository() {
-    }
-
     public void save(Category category, Menus menus) {
         repository.put(category, menus);
+    }
+
+    public List<String> findMenuNames(Category category) {
+        Menus menus = repository.get(category);
+        return menus.getNames();
     }
 }
