5327465b530a1f4bbb266b53078483357aab3171
이건회 <rg970604@naver.com>
2022-12-17 13:38:10 +0900

    feat(InputView.java) : 코치이름, 금지음식 입력시 출력문구

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -8,15 +8,17 @@ public class InputView {
 
     private static final String DELIMITER = ",";
 
-    public static List<String> inputCoachNames(){
+    public static List<String> inputCoachNames() {
+        OutputView.printInputCoachnames();
         String input = Console.readLine();
-        String [] names = input.split(DELIMITER);
+        String[] names = input.split(DELIMITER);
         return Arrays.asList(names);
     }
 
-    public static List<String> inputProhibitedFoods(){
+    public static List<String> inputProhibitedFoods(String name) {
+        OutputView.printInputProhibitedFoods(name);
         String input = Console.readLine();
-        String [] foods = input.split(DELIMITER);
+        String[] foods = input.split(DELIMITER);
         return Arrays.asList(foods);
     }
 
