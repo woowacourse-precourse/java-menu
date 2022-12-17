046f04c538a691a1e1943bb05ff73284ab0ea130
2014012606 <2014012606@hanyang.ac.kr>
2022-12-17 13:37:15 +0900

    feat: 사용자 입출력에 필요한 상수문장 추가

---
+++ src/main/java/menu/view/ViewConstants.java
@@ -0,0 +1,10 @@
+package menu.view;
+
+public class ViewConstants {
+    public static String error = "[ERROR] ";
+    public static String startLunchRecommender = "점심 메뉴 추천을 시작합니다.\n";
+    public static String getCoachesName = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    public static String getCannotEatManus = "(이)가 못 먹는 메뉴를 입력해 주세요.";
+    public static String showRecommendResult = "메뉴 추천 결과입니다.";
+    public static String endLunchRecommender = "추천을 완료했습니다.";
+}
