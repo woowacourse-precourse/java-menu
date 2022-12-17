442473992b79976191141425d3345949edf58659
kat8813 <ohseo8813@gmail.com>
2022-12-17 14:00:59 +0900

    <feat>(RandomCategoryGenerator.java): 랜덤한 카테고리를 생성하는 클래스 추가

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,5 +1,7 @@
 package menu.domain;
 
+import java.util.Arrays;
+
 public enum Category {
 
     JAPANESE(1),
@@ -12,5 +14,13 @@ public enum Category {
 
     Category(int order) {
         this.order = order;
+    }
+
+
+    public static Category of(int input) {
+        return Arrays.stream(values())
+                .filter(category -> category.order == input)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 카테고리 순서입니다. 입력값 : " + input));
     }
 }
