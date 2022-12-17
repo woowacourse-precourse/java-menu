34ef5c3ed3ea0b44944df35e7eefec47564fa7c7
Yejin Shin <shinyejin0212@gmail.com>
2022-12-17 13:45:17 +0900

    feat(InputView): 입력값 받기

---
+++ src/main/java/menu/SystemMessage.java
@@ -0,0 +1,6 @@
+package menu;
+
+public class SystemMessage {
+    public static final String COACH_NAME_INPUT_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    public static final String DISLIKE_MENU_INPUT_MESSAGE = "(이)가 못 먹는 메뉴를 입력해 주세요.";
+}
---
+++ src/main/java/menu/view/InputView.java
@@ -0,0 +1,26 @@
+package menu.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import menu.SystemMessage;
+
+public class InputView {
+
+    public static List<String> readCoachName() {
+        System.out.println(SystemMessage.COACH_NAME_INPUT_MESSAGE);
+        String names = Console.readLine();
+        List<String> coachNames = new ArrayList<>();
+        coachNames = Arrays.asList(names.split(","));
+        return coachNames;
+    }
+
+    public static List<String> readDislikeMenu() {
+        System.out.println(SystemMessage.COACH_NAME_INPUT_MESSAGE);
+        String names = Console.readLine();
+        List<String> dislikeMenus = new ArrayList<>();
+        dislikeMenus = Arrays.asList(names.split(","));
+        return dislikeMenus;
+    }
+}
