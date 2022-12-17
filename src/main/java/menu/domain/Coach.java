8310ab68fe11e5c617bde4d3bdc9ef4a0b5a624e
woosung1223 <dntjd991223@naver.com>
2022-12-17 13:41:26 +0900

    feat: 각 코치별로 못먹는 메뉴가 존재하도록 구성

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -5,16 +5,24 @@ import java.util.List;
 public class Coach {
     private final String name;
     private final List<String> menus;
+    private final List<String> cannotEatMenus;
 
-    public Coach(String name, List<String> menus) {
+    public Coach(String name, List<String> menus, List<String> cannotEatMenus) {
         validateName(name);
         this.name = name;
         this.menus = menus;
+        this.cannotEatMenus = cannotEatMenus;
     }
 
     private void validateName(String name) {
         if (name.length() < 2 || name.length() > 5) {
             throw new IllegalArgumentException("[ERROR] 코치의 이름은 2글자에서 4글자만 가능합니다.");
+        }
+    }
+
+    private void validateCannotEatMenus(List<String> menus) {
+        if (menus.size() > 2) {
+            throw new IllegalArgumentException("[ERROR] 코치가 못 먹는 음식은 2개 이상이면 안됩니다.");
         }
     }
 }
