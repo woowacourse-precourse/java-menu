e7c50fe56447669bcdc8ef4f92fc5b7b348086e1
sonsoowon <yuwoo1018@gmail.com>
2022-12-17 16:24:39 +0900

    feat(MenuMachine): 무작위로 메뉴를 추천하는 클래스 추가

--- src/main/java/menu/domain/MenuMachine.java
+++ src/main/java/menu/domain/MenuMachine.java
@@ -1,4 +1,27 @@
 package menu.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MenuMachine {
+    private static final MenuMachine menuMachine = new MenuMachine();
+
+    private MenuMachine() {
+    }
+
+    public static MenuMachine getInstance() {
+        return menuMachine;
+    }
+
+    public Menu recommendMenu() {
+        List<String> menus = Menu.getAllMenus()
+                .stream().map(Menu::getName)
+                .collect(Collectors.toUnmodifiableList());
+
+        String menu = Randoms.shuffle(menus).get(0);
+        return Menu.valueOf(menu);
+    }
+
 }
