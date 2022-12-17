d2c97a9de72863085ad3fc1302a1b5bad44105fe
kim0914 <kosa0914@naver.com>
2022-12-17 14:21:33 +0900

    feat(Coach): 이름, 못먹는 음식, 추천음식, 추천한 카테고리를 가지는 코치 객체 추가

---
+++ src/main/java/menu/domain/Coach.java
@@ -0,0 +1,20 @@
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coach {
+
+    public static final int CATEGORY_MAX_COUNT = 2;
+
+    private final String name;
+    private final List<Menu> banFoods;
+
+    private List<Menu> recommendFoods = new ArrayList<>();
+    private List<Category> categories = new ArrayList<>();
+
+    public Coach(String name, List<Menu> banFoods) {
+        this.name = name;
+        this.banFoods = banFoods;
+    }
+}
