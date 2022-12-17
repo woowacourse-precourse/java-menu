5283e43b70f47b8fa58922bd59ae4bab7ffbefd9
ksh-beginner <ohk9134@naver.com>
2022-12-17 13:40:47 +0900

    feat(InputView) : 코치의 이름 입력 기능 구현

---
+++ src/main/java/menu/view/InputView.java
@@ -0,0 +1,11 @@
+package menu.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String readCoachNames() {
+        String coachNames = Console.readLine();
+        return coachNames;
+    }
+}
