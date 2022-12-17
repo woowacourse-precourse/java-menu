a619543568f857d59ec4ad8088bab47a44e7bdb3
thdwoqor <celebarte01@gmail.com>
2022-12-17 17:36:48 +0900

    feat: 한국어 이름에는 한국어만 가능하고 영문 이름일경우 공백 영어만 가능하도록 추가

--- src/main/java/menu/util/Validate.java
+++ src/main/java/menu/util/Validate.java
@@ -5,15 +5,17 @@ import java.util.List;
 import java.util.Set;
 
 public class Validate {
-    public static void inNumber(String name) {
-        if (name.matches(".*[0-9].*")) {
-            throw new IllegalArgumentException(Message.ERROR_NUMBER.message);
+    public static void isName(String name) {
+        if (!name.matches("[가-힣]*$")) {
+            if (!name.matches("[a-zA-Z ]*$")) {
+                throw new IllegalArgumentException(Message.ERROR_NOT_NAME.message);
+            }
         }
     }
 
     public static void nameLength(List<String> names, int minInclusive, int maxInclusive) {
         names.stream().forEach(name -> {
-                    inNumber(name);
+                    isName(name);
                     if (name.length() < minInclusive || name.length() > maxInclusive) {
                         throw new IllegalArgumentException(Message.ERROR_DIFFERENT_NAME_LENGTHS.message);
                     }
@@ -45,7 +47,7 @@ public class Validate {
         ERROR_DIFFERENT_COACH_LENGTHS("[ERROR] 코치는 최소 2명, 최대 5명까지 식사를 함께 한다."),
         ERROR_DIFFERENT_CANT_EAT_LENGTHS("[ERROR] 각 코치는 최소 0개, 최대 2개의 못 먹는 메뉴가 있다."),
         ERROR_DUPLICATE_VALUE("[ERROR] 중복 값이 포함되어 있습니다."),
-        ERROR_NUMBER("[ERROR] 숫자 값이 포함되어 있습니다.");
+        ERROR_NOT_NAME("[ERROR] 이름이 아닙니다.");
 
         private final String message;
 
