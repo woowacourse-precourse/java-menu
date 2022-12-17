8bc7af4942fa3d5a038b9e88b50732969fc13b21
minaver <psmjoshua@gmail.com>
2022-12-17 13:36:14 +0900

    feat(Recommend): Recommend field 및 초기화 메서드 추가

--- src/main/java/menu/domain/Recommend.java
+++ src/main/java/menu/domain/Recommend.java
@@ -1,4 +1,29 @@
 package menu.domain;
 
+import menu.enums.FoodCategory;
+import menu.vo.Coach;
+import menu.vo.Menu;
+
+import java.util.List;
+import java.util.Map;
+
 public class Recommend {
+    private Map<FoodCategory, Integer>  weekCategoryChoose;
+    private Map<Coach,List<Menu>> coachEatenMenu;
+
+    public Recommend(List<Coach> coaches) {
+        
+    }
+
+    private void initWeekCategoryChoose() {
+        weekCategoryChoose.put(FoodCategory.KOR,0);
+        weekCategoryChoose.put(FoodCategory.JAP,0);
+        weekCategoryChoose.put(FoodCategory.CHI,0);
+        weekCategoryChoose.put(FoodCategory.ASI,0);
+        weekCategoryChoose.put(FoodCategory.WES,0);
+    }
+
+    private void initCoachEatenMenu() {
+
+    }
 }
