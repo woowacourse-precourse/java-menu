a0721dd6413dc31f792f2522992ab8f892c24823
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 13:44:25 +0900

    feat(ExceptionMessage): 예외 메시지 상수 추가
    - 입력된 코치 이름 개수에 맞지 않는 경우에 따른 예외 메시지
    - 이름의 길이가 조건에 맞지 않는 경우에 따른 예외 메시지

--- src/main/java/menu/constants/ExceptionMessage.java
+++ src/main/java/menu/constants/ExceptionMessage.java
@@ -1,8 +1,11 @@
 package menu.constants;
 
 public enum ExceptionMessage {
-    NONE_INPUT("값을 입력해 주셔야 합니다."),
-    NOT_INPUT_NAMES_BETWEEN_2AND5("이름은 2명 이상, 5명 이하로 작성해 주어야 합니다.");
+    NONE_INPUT("값을 입력해야 합니다."),
+    NOT_INPUT_NAMES_LESS_THAN_TWO("코치는 최소 2명 이상 입력해야 합니다."),
+    NOT_INPUT_NAMES_OVER_FIVE("코치는 최대 5명 이하 입력해야 합니다."),
+    NAME_LESS_THAN_TWO("코치 이름은 최소 2자 이상으로 입력해야 합니다."),
+    NAME_MORE_THAN_FOUR("코치 이름은 최대 4자 이하 입력해야 합니다.");
 
     private static final String ERROR_TAG = "[ERROR] ";
     private final String exceptionMessage;
