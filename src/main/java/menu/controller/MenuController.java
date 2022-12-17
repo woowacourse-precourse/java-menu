a69b4e4529c3d90142fb40c5da90fc1efcfbc7ca
dooboocookie <bkh3030@gmail.com>
2022-12-17 14:39:59 +0900

    feat: 컨트롤러 뼈대 작성

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,12 +1,36 @@
 package menu.controller;
 
+import menu.service.MenuService;
+import menu.view.InputView;
+import menu.view.OutputView;
+
 public class MenuController {
 
-    public MenuController() {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final MenuService menuService;
 
+    public MenuController(InputView inputView, OutputView outputView, MenuService menuService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.menuService = menuService;
     }
 
     public void run() {
-        
+        inputCoachName();
+        inputUnavailableMenu();
+        recommendMenus();
+    }
+
+    private void inputCoachName() {
+
+    }
+
+    private void inputUnavailableMenu() {
+
+    }
+
+    private void recommendMenus() {
+
     }
 }
