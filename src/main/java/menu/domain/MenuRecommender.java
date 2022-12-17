21729d7010576b79ad4d79005259a51a11fe4e03
KSH <tkdgns543@gmail.com>
2022-12-17 14:00:42 +0900

    feat(domain): 추천 클래스에 가능한 카테고리 목록 구현, 이제까지 추천된 카테고리 제너릭스 그에 맞게 수정

--- src/main/java/menu/domain/MenuRecommender.java
+++ src/main/java/menu/domain/MenuRecommender.java
@@ -1,9 +1,11 @@
 package menu.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MenuRecommender {
-    private List<String> categoryHistory = new ArrayList<>();
+    private List<Category> categoryHistory = new ArrayList<>();
     private List<String> coaches = new ArrayList<>();
+    private static final List<Category> CATEGORIES = Arrays.asList(Category.values());
 }
