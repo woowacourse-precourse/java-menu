d386d917a0eff4ff8b0f50e967dfbbb37ac4126d
sonsoowon <yuwoo1018@gmail.com>
2022-12-17 16:11:26 +0900

    feat(HateMenu): 못먹는 메뉴 관리 클래스 추가

--- src/main/java/menu/domain/HateMenu.java
+++ src/main/java/menu/domain/HateMenu.java
@@ -1,4 +1,25 @@
 package menu.domain;
 
+import java.util.List;
+
 public class HateMenu {
+    private static final int MAX_SIZE = 2;
+    private static final String INVALID_SIZE_MESSAGE = String.format("못먹는 메뉴는 최대 %d개여야 합니다.", MAX_SIZE);
+
+    private final List<Menu> hateMenus;
+
+    public HateMenu(List<Menu> hateMenus) {
+        validate(hateMenus);
+        this.hateMenus = hateMenus;
+    }
+
+    private static void validate(List<Menu> hateMenus) {
+        if(hateMenus.size() > MAX_SIZE) {
+            throw new IllegalArgumentException(INVALID_SIZE_MESSAGE);
+        }
+    }
+
+    public boolean contains(Menu menu) {
+        return hateMenus.contains(menu);
+    }
 }
