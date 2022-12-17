f2fb07bd5ebda2143bc942e6304c9f0e9d50ffbb
Woojin Lee <dhkep03@gmail.com>
2022-12-17 14:16:11 +0900

    feat(InputView): 못 먹는 메뉴 목록 입력

--- src/main/java/menu/ui/InputView.java
+++ src/main/java/menu/ui/InputView.java
@@ -13,6 +13,15 @@ public class InputView {
         return coachNames;
     }
 
+    public List<String> readInedibleFoodsOfCoach(String coachName) {
+        System.out.printf("%s(이)가 못 먹는 메뉴를 입력해 주세요.", coachName);
+        addEmptyLine();
+        List<String> inedibleFoods = Arrays.asList(
+                Console.readLine().split(",")
+        );
+        return inedibleFoods;
+    }
+
     private void addEmptyLine() {
         System.out.println();
     }
