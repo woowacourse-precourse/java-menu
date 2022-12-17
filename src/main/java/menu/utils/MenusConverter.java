cc87494a6595802ea13628a2214ff717440d11c2
woo-chang <clllickme@naver.com>
2022-12-17 15:42:11 +0900

    refactor(MenusConverter): 못 먹는 음식은 추천할 수 있는 메뉴에 존재하지 않아도 가능하도록 수정

--- src/main/java/menu/utils/MenusConverter.java
+++ src/main/java/menu/utils/MenusConverter.java
@@ -3,8 +3,6 @@ package menu.utils;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import menu.domain.Category;
-import menu.repository.MenuRepository;
 
 public class MenusConverter {
 
@@ -25,22 +23,6 @@ public class MenusConverter {
     }
 
     private static void validate(List<String> menus) {
-        for (Category category : Category.values()) {
-            List<String> existMenus = MenuRepository.findByCategory(category);
-            validateExistMenu(menus, existMenus);
-        }
-        validateSize(menus);
-    }
-
-    private static void validateExistMenu(List<String> menus, List<String> existMenus) {
-        for (String menu : menus) {
-            if (!existMenus.contains(menu)) {
-                throw new IllegalArgumentException("존재하지 않는 메뉴입니다.");
-            }
-        }
-    }
-
-    private static void validateSize(List<String> menus) {
         if (menus.size() < 0 || menus.size() > 2) {
             throw new IllegalArgumentException("각 코치는 최소 0개, 최대 2개의 못 먹는 메뉴가 있다.");
         }
