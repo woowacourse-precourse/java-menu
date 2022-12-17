d62c750c0c12f51b627086dbd6a31de43034e654
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 17:13:11 +0900

    style(RandomMenuValueGenerator): 매직 넘버로 수정

--- src/main/java/menu/util/RandomMenuValueGenerator.java
+++ src/main/java/menu/util/RandomMenuValueGenerator.java
@@ -4,9 +4,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 import menu.constants.Category;
 
 public class RandomMenuValueGenerator implements RandomMenuGenerator {
+    private static final int FIRST_INDEX = 0;
 
     @Override
     public String generate(Category category) {
-        return Randoms.shuffle(category.getMenus()).get(0);
+        return Randoms.shuffle(category.getMenus()).get(FIRST_INDEX);
     }
 }
