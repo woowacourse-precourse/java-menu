3dbb7ff5d0528002adeb8299e523062be6d784ac
hoin <apicad0307@gmail.com>
2022-12-17 17:16:59 +0900

    fix(controller/GameController.java): 필요없는 주석 제거

--- src/main/java/menu/controller/GameController.java
+++ src/main/java/menu/controller/GameController.java
@@ -1,11 +1,8 @@
 package menu.controller;
 
-import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-import com.sun.source.tree.Tree;
 import menu.domain.Category;
 import menu.domain.CategoryMaker;
-import menu.input.InputString;
 import menu.input.InputView;
 import menu.output.OutputView;
 
@@ -18,15 +15,6 @@ public class GameController {
     Map<String, Set<String>> coachCannotEat = new TreeMap<>();
     Map<String, Set<String>> candidateMenu = new TreeMap<>();
 
-    public void play(){
-        outputView.GAME_START_OUTPUT();
-        List<Category> categories = CategoryMaker.makeCategories();
-        String[] coaches = inputView.INPUT_COACH_NAME();
-        fillCannotEatTable(coachCannotEat, candidateMenu, coaches);
-        fillMenuTable(categories, coachCannotEat, candidateMenu, coaches);
-        outputView.PRINT_RESULT(categories, candidateMenu);
-    }
-
     private static void fillMenuTable(List<Category> categories, Map<String, Set<String>> coachCannotEat, Map<String, Set<String>> candidateMenu, String[] coaches) {
         for (Category category : categories) {
             for (String coach : coaches) {
@@ -35,24 +23,33 @@ public class GameController {
         }
     }
 
-    private void fillCannotEatTable(Map<String, Set<String>> coachCannotEat, Map<String, Set<String>> candidateMenu, String[] coaches) {
-        for(int i = 0; i< coaches.length; i++){
-            String[] menus = inputView.CANNOT_EAT_FOOD(coaches[i]);
-            coachCannotEat.put(coaches[i], Arrays.stream(menus).collect(Collectors.toSet()));
-            candidateMenu.put(coaches[i], new LinkedHashSet<>());
-        }
-    }
-
     private static void candidate(Map<String, Set<String>> coachCannotEat, Map<String, Set<String>> candidateMenu, Category category, String coach) {
-        while(true){
+        while (true) {
             String menu = Randoms.shuffle(category.getMenus()).get(0);
             Set<String> strings = candidateMenu.get(coach);
-            if(strings.contains(menu)) continue;
+            if (strings.contains(menu)) continue;
             Set<String> strings1 = coachCannotEat.get(coach);
-            if(strings1.contains(menu)) continue;
+            if (strings1.contains(menu)) continue;
             strings.add(menu);
             candidateMenu.put(coach, strings);
             break;
+        }
+    }
+
+    public void play() {
+        outputView.GAME_START_OUTPUT();
+        List<Category> categories = CategoryMaker.makeCategories();
+        String[] coaches = inputView.INPUT_COACH_NAME();
+        fillCannotEatTable(coachCannotEat, candidateMenu, coaches);
+        fillMenuTable(categories, coachCannotEat, candidateMenu, coaches);
+        outputView.PRINT_RESULT(categories, candidateMenu);
+    }
+
+    private void fillCannotEatTable(Map<String, Set<String>> coachCannotEat, Map<String, Set<String>> candidateMenu, String[] coaches) {
+        for (int i = 0; i < coaches.length; i++) {
+            String[] menus = inputView.CANNOT_EAT_FOOD(coaches[i]);
+            coachCannotEat.put(coaches[i], Arrays.stream(menus).collect(Collectors.toSet()));
+            candidateMenu.put(coaches[i], new LinkedHashSet<>());
         }
     }
 
