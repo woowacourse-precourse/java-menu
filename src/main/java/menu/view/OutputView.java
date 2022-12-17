c19d95643eb7e761f327062560444b8d5da6f0a9
sosow0212 <sosow0212@naver.com>
2022-12-17 15:33:46 +0900

    feat : (OutputView) 카테고리 출력 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,5 +1,9 @@
 package menu.view;
 
+import menu.domain.Categories;
+import menu.domain.Crew;
+import menu.domain.Crews;
+
 public class OutputView {
     private final static String ERROR = "[ERROR] ";
 
@@ -15,10 +19,20 @@ public class OutputView {
         System.out.println("\n" + crewName + "(이)가 못 먹는 메뉴를 입력해 주세요.");
     }
 
-    public void printResult() {
+    public void printResult(Categories categories, Crews crews) {
         System.out.println("\n메뉴 추천 결과입니다.");
         System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
-        // todo : 추후에 결과 출력을 위해 Crews와 Category 받기
+        printCategories(categories);
+        printFoodOfCrews(crews);
+    }
+
+    public void printCategories(Categories categories) {
+        String printCategoryMap = "[ 카테고리 " + categories.toString() + " ]";
+        System.out.println(printCategoryMap);
+    }
+
+    public void printFoodOfCrews(Crews crews) {
+
     }
 
     public static void printError(String message) {
