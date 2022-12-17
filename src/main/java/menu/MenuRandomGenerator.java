2a1d0a380ead489886e19632e3b26d586fb67323
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-12-17 14:12:14 +0900

    <fix>(MenuRandomGenerator.java): 메서드 이름 변경
     - 메서드의 기능을 더 잘 표현하는 메서드명으로 변경

--- src/main/java/menu/MenuRandomGenerator.java
+++ src/main/java/menu/MenuRandomGenerator.java
@@ -5,12 +5,12 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class MenuRandomGenerator {
-    public static String categoryRandomSelector() {
+    public static String selectRandomCategory() {
         String category = Category.findByNumber(Randoms.pickNumberInRange(1, 5)).getCategoryName();
         return category;
     }
 
-    public static String menuRandomSelector(String categoryName) {
+    public static String selectRandomMenu(String categoryName) {
         List<String> menus = Category.findByName(categoryName).getMenus();
         String menu = Randoms.shuffle(menus).get(0);
         return menu;
