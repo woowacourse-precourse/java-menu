7a4432f1c566e53f371bc2069a87ca5c61987349
yubin <dbqls2821@gmail.com>
2022-12-17 13:52:54 +0900

    feat: Output을 담당하는 OutputView 클래스 추가 & 내부 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,8 +1,30 @@
 package menu.view;
 
+import menu.GuideMessage;
+
 public class OutputView {
 
   public void printStart() {
+    System.out.println(GuideMessage.START.getMessage());
+    System.out.println();
+  }
 
+  public void printInputCoachName() {
+    System.out.println(GuideMessage.INPUT_COACH.getMessage());
+  }
+
+  public void printInputFoodCantEat(String coach) {
+    System.out.println();
+    System.out.println(GuideMessage.INPUT_FOOD_CANT_EAT.print(coach));
+  }
+
+  public void printRecommendationResult() {
+    System.out.println();
+    System.out.println(GuideMessage.OUTPUT_RECOMMENDATION_RESULT.getMessage());
+  }
+
+  public void printEnd() {
+    System.out.println();
+    System.out.println(GuideMessage.OUTPUT_COMPLETE_RECOMMENDATION.getMessage());
   }
 }
