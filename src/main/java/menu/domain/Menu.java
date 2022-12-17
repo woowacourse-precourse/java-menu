1bdc40eb2b6a491d020fded7bb9b52f124d971d7
jeomxon <jeomxon@gmail.com>
2022-12-17 17:27:30 +0900

    refactor: enum에 order을 추가하여 순서를 명확히 함

--- src/main/java/menu/domain/Menu.java
+++ src/main/java/menu/domain/Menu.java
@@ -5,16 +5,18 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public enum Menu {
-    JAPAN("일식", List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
-    KOREA("한식", List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")),
-    CHINA("중식", List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
-    ASIAN("아시안", List.of("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")),
-    WESTERN("양식", List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
+    JAPAN(1, "일식", List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
+    KOREA(2, "한식", List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")),
+    CHINA(3, "중식", List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
+    ASIAN(4, "아시안", List.of("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")),
+    WESTERN(5, "양식", List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
 
+    private final int order;
     private final String category;
     private final List<String> menus;
 
-    Menu(String category, List<String> menus) {
+    Menu(int order, String category, List<String> menus) {
+        this.order = order;
         this.category = category;
         this.menus = menus;
     }
@@ -32,7 +34,7 @@ public enum Menu {
         int selectedNumber = Randoms.pickNumberInRange(1, 5);
 
         for (Menu menu : Menu.values()) {
-            if ((menu.ordinal() + 1) == selectedNumber) {
+            if (menu.order == selectedNumber) {
                 selectedCategory += menu.category;
                 break;
             }
