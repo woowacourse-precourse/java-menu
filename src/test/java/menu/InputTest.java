2c1a90458c37d34049c8822b9583af22dc35f36a
Cyma-s <ssol0319@naver.com>
2022-12-17 17:25:52 +0900

    test(InputTest) : 코치 이름 길이 테스트

--- src/test/java/menu/InputTest.java
+++ src/test/java/menu/InputTest.java
@@ -20,7 +20,7 @@ public class InputTest extends NsTest {
         });
     }
 
-    @DisplayName("코치 이름 개수가 올바르지 않은 입력 테스트")
+    @DisplayName("코치 이름 개수가 5보다 많은 입력 테스트")
     @ValueSource(strings = {"톰톰,하하,하하하,호호,히히,후후", "하하하,오오오,우우우,오와,우우,후후"})
     @ParameterizedTest
     void getMoreCoachName(String input) {
@@ -30,6 +30,25 @@ public class InputTest extends NsTest {
         });
     }
 
+    @DisplayName("코치 이름 길이가 2글자 보다 작은 경우")
+    @ValueSource(strings = {"톰,하하하", "호후, "})
+    @ParameterizedTest
+    void getLessCoachNameLength(String input) {
+        assertSimpleTest(() -> {
+            runException(input);
+            assertThat(output()).contains(InputException.OUT_OF_COACH_NAME_LENGTH_MINIMUM.getExceptionMessage());
+        });
+    }
+
+    @DisplayName("코치 이름 길이가 4글자 보다 큰 경우")
+    @ValueSource(strings = {"톰톰톰톰톰톰톰톰,하하하", "김김김,이이이이이"})
+    @ParameterizedTest
+    void getMoreCoachNameLength(String input) {
+        assertSimpleTest(() -> {
+            runException(input);
+            assertThat(output()).contains(InputException.OUT_OF_COACH_NAME_LENGTH_MAXIMUM.getExceptionMessage());
+        });
+    }
 
     @Override
     protected void runMain() {
