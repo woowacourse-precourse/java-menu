8a055146460310ea428d0045823c2bb30bd72ddd
SH <gram753@naver.com>
2022-12-17 14:14:48 +0900

    chore: 메세지 수정

--- src/main/java/menu/Constant.java
+++ src/main/java/menu/Constant.java
@@ -4,5 +4,5 @@ public class Constant {
 	public static final String START_MSG = "점심 메뉴 추천을 시작합니다.\n";
 	public static final String COACH_INPUT_MSG = "코치의 이름을 입력해 주세요. (, 로 구분)";
 
-	public static final String MENU_INPUT_MSG = "%f(이)가 못 먹는 메뉴를 입력해 주세요.\n";
+	public static final String MENU_INPUT_MSG = "(이)가 못 먹는 메뉴를 입력해 주세요.\n";
 }
