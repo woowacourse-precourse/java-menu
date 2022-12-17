3f9afc7f038288f9ffb039a98f106fb167c80d4a
Choi-JJunho <junho5336@gmail.com>
2022-12-17 15:11:08 +0900

    refactor: add validate couch name

--- src/main/java/menu/domain/Couch.java
+++ src/main/java/menu/domain/Couch.java
@@ -1,5 +1,7 @@
 package menu.domain;
 
+import menu.utils.ErrorMessage;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -11,7 +13,19 @@ public class Couch {
     private final List<Food> recommend = new ArrayList<>();
 
     private Couch(String name) {
-        this.name = name;
+        String refined = name.trim();
+        validateName(refined);
+        this.name = refined;
+    }
+
+    private void validateName(String name) {
+        if (isOutOfRange(name)) {
+            throw new IllegalArgumentException(ErrorMessage.COUCH_NAME_OUT_OF_RANGE.getMessage());
+        }
+    }
+
+    private boolean isOutOfRange(String name) {
+        return name.length() < 2 || name.length() > 4;
     }
 
     public static Couch from(String name) {
---
+++ src/main/java/menu/utils/ErrorMessage.java
@@ -0,0 +1,17 @@
+package menu.utils;
+
+public enum ErrorMessage {
+    COUCH_NAME_OUT_OF_RANGE("코치의 이름은 최소 2글자, 최대 4글자입니다."),
+    COUCH_GROUP_OUT_OF_RANGE("코치는 최소 2명, 최대 5명까지만 식사를 함께 합니다.");
+
+    private final String message;
+    private static final String PREFIX = "[ERROR]";
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return String.join(" ", PREFIX, message);
+    }
+}
