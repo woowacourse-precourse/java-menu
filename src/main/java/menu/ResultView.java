a8da7ce251d0b41bb85cee5123521e3ed1aacab6
kevstevie <jianna442@gmail.com>
2022-12-17 16:53:34 +0900

    refactor: 0을 FIRST_PLACE로 리팩터

--- src/main/java/menu/ResultView.java
+++ src/main/java/menu/ResultView.java
@@ -10,11 +10,12 @@ public class ResultView {
     private static final String LEFT = "[ ";
     private static final String OLD_RIGHT = "]";
     private static final String RIGHT = " ]";
+    private static final int FIRST_PLACE = 0;
 
     private ResultView() {}
 
     public static String print(String name, List<String> result) {
-        result.add(0, name);
+        result.add(FIRST_PLACE, name);
         return result.toString().replace(OLD_LEFT, LEFT).replace(OLD_RIGHT, RIGHT).replace(OLD_DELIMITER, DELIMITER);
     }
 }
