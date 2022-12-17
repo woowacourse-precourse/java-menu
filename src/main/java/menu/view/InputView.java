d8a3361b8e37d23318ca6fa391b48776ef3de896
CAEL0 <kjongmin26@gmail.com>
2022-12-17 15:59:12 +0900

    refactor(InputView): readName -> readCoachNames로 변경

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String readName() {
+    public String readCoachNames() {
         System.out.println(ViewConstants.ASKING_NAME);
         String input = Console.readLine();
         System.out.println();
