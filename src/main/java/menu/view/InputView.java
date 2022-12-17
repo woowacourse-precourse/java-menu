4eddae97e4b835a698011bda0f42b16bec071780
newh08 <newh08@gmail.com>
2022-12-17 16:58:26 +0900

    fix : fix the method that validate the coach name

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -25,39 +25,48 @@ public class InputView {
 
 
     public List<String> inputForbiddenFoods() {
-        System.out.println(tempCoachName+"(이)가 못 먹는 메뉴를 입력해 주세요.");
+        System.out.println(tempCoachName + "(이)가 못 먹는 메뉴를 입력해 주세요.");
         String input = Console.readLine();
         List<String> forbiddenFood = validateSeparatorForForbiddenFood(input);
         return forbiddenFood;
     }
 
     private List<String> validateInputCoachName(String input) {
-        if (checkInputSizeOverFive(input)) {
-            if (!validateSeparator(input)) {
-                throw new IllegalArgumentException("[ERROR] 코치 이름은 ','로 구분해주세요.");
-            }
+        if (checkInputSizeMoreThanFive(input)) { // input 크기가 5 이상이면 구분자 확인
+            validateSeparator(input);
         }
-        if (checkInputContainOnlyKorean(input)) {
-            throw new IllegalArgumentException("[ERROR] 코치 이름은 ','로 구분해주세요.");
+        if (input.contains(",")) { // input 크기가 5 미만이면서 옳바른 구분자를 가지고 있으면 코치 이름 길이 오류이므로 길이 확인
+            validateCoachNameLength(input);
         }
-        List<String> coaches = List.of(input.split(","));
-        validateCoachNameLength(coaches);
-        return coaches;
+        if (checkInputSizeUnderFive(input)) { // input 크기가 5 미만이면서 이상한 구분자를 사용한 경우 구분자 오류 메세지 출력 위해 구분자 확인
+            checkInputContainOnlyKorean(input);
+        }
+        validateCoachNameLength(input);
+        return List.of(input.split(","));
     }
 
-    private boolean checkInputSizeOverFive(String input) {
+    private boolean checkInputSizeMoreThanFive(String input) {
         return input.length() >= 5;
     }
 
-    private boolean checkInputContainOnlyKorean(String input) {
-        return input.matches(".*[^ㄱ-ㅎㅏ-ㅣ가-힣]+.*");
+    private boolean checkInputSizeUnderFive(String input) {
+        return input.length() < 5;
     }
 
-    private boolean validateSeparator(String input) {
-        return input.contains(",");
+    private void checkInputContainOnlyKorean(String input) {
+        if (input.matches(".*[^ㄱ-ㅎㅏ-ㅣ가-힣]+.*")) {
+            throw new IllegalArgumentException("[ERROR] 코치 이름은 ','로 구분해주세요.");
+        }
     }
 
-    private void validateCoachNameLength(List<String> coaches) {
+    private void validateSeparator(String input) {
+        if (!input.contains(",")) {
+            throw new IllegalArgumentException("[ERROR] 코치 이름은 ','로 구분해주세요.");
+        }
+    }
+
+    private void validateCoachNameLength(String input) {
+        List<String> coaches = List.of(input.split(","));
         for (String coach : coaches) {
             if (coach.length() < 2 || coach.length() > 4) {
                 throw new IllegalArgumentException("[ERROR] 코치의 이름은 2자 이상, 4자 이하여야 합니다.");
