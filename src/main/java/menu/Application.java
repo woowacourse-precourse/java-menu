aa46f3be2d48c5e6d38cb9ac42e7fc9143e62004
jojaeng2 <ds4ouj@naver.com>
2022-12-17 17:20:17 +0900

    feat : menuController에 interface를 사용해 추상화 적용

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,11 +1,12 @@
 package menu;
 
 import menu.controller.MenuController;
+import menu.controller.MenuControllerImpl;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        MenuController menuController = new MenuController();
+        MenuController menuController = new MenuControllerImpl();
         menuController.run();
     }
 }
