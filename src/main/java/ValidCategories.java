fae2c23a7968ea3eaf5d46f6e84b171fc0f4e71c
splitube <voicesplit@daum.net>
2022-12-17 13:41:35 +0900

    feat(기본 카테고리): 가능한 카테고리 저장

---
+++ src/main/java/ValidCategories.java
@@ -0,0 +1,13 @@
+public enum ValidCategories {
+    JAPANESE("일식"),
+    KOREAN("한식"),
+    CHINESE("중식"),
+    ASIAN("아시안"),
+    WESTERN("양식");
+
+    private final String korean;
+
+    ValidCategories(String korean) {
+        this.korean = korean;
+    }
+}
