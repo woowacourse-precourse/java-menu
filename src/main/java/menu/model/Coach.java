1b80a67decc7eef478ccaeb30dedc95e19d2e567
thdwoqor <celebarte01@gmail.com>
2022-12-17 15:05:45 +0900

    feat: 코치 메뉴 추가

--- src/main/java/menu/model/Coach.java
+++ src/main/java/menu/model/Coach.java
@@ -1,14 +1,24 @@
 package menu.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Coach {
     private String name;
     private List<String> cantEats;
+    private List<String> menu = new ArrayList<>();
 
-    public Coach(String name,List<String> cantEats){
-        this.name=name;
-        this.cantEats=cantEats;
+    public Coach(String name, List<String> cantEats) {
+        this.name = name;
+        this.cantEats = cantEats;
+    }
+
+    public List<String> getMenu() {
+        return menu;
+    }
+
+    public void addMenu(String name) {
+        this.menu.add(name);
     }
 
     public String getName() {
