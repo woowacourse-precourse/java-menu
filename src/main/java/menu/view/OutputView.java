f0a3654dde10a2709539c3725b3a1a5b2bb3758c
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2022-12-17 14:25:22 +0900

    feat(menu) : 결과 출력 괄호와 구분자 추가

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -30,26 +30,29 @@ public class OutputView {
     }
 
     private static void printDivision() {
-        StringBuilder division = new StringBuilder("구분");
+        StringBuilder division = new StringBuilder("[ 구분 | ");
         for (Week week : Week.values()) {
             division.append(week.getValue()).append(" | ");
         }
+        division.replace(division.length() - 2, division.length() - 1, "]");
         System.out.println(division);
     }
 
     private static void printCategory() {
-        StringBuilder category = new StringBuilder("카테고리");
+        StringBuilder category = new StringBuilder("[ 카테고리 | ");
         for (Category c : Category.values()) {
             category.append(c.getCategoryName()).append(" | ");
         }
+        category.replace(category.length() - 2, category.length() - 1, "]");
         System.out.println(category);
     }
 
     private static void printCoachAndFoods(Coach coach, List<String> foods) {
-        StringBuilder sb = new StringBuilder(coach.getName());
+        StringBuilder sb = new StringBuilder("[ " + coach.getName() + " | ");
         for (String food : foods) {
             sb.append(food).append(" | ");
         }
+        sb.replace(sb.length() - 2, sb.length()-1, "]");
         System.out.println(sb);
     }
 }
