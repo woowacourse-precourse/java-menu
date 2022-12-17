53cfd57472c65e9da7f35f2ea894181d5ed7dec0
hodomaroo <enghemdwkd@gmail.com>
2022-12-17 17:09:56 +0900

    (feat/OutputView) 출력 메서드 추가

--- src/main/java/menu/outputView.java
+++ src/main/java/menu/outputView.java
@@ -1,28 +1,28 @@
 package menu;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import menu.domain.Day;
 import menu.domain.DataRows;
 
-public class outputView {
-
+public class OutputView {
+    public void printStartComment(){
+        System.out.println(MessageFormat.START_COMMENT.getMessage());
+    }
     public void printError(String errorMessage) {
-        System.out.println(MessageFormat.ERROR_MESSAGE_FORMAT.getMessage() + errorMessage);
+        System.out.println(String.format(MessageFormat.ERROR_MESSAGE_FORMAT.getMessage(),errorMessage));
     }
 
     public void printRecommendResult(DataRows dataRows) {
         System.out.println(MessageFormat.RESULT_HEADER_COMMENT.getMessage());
-        dataRows.getMenusForCouches().forEach((dataRow) -> getFormattedRow(dataRow.getHeader(), dataRow.getValues()));
-        System.out.println(MessageFormat.RESULT_END_COMMENT.getMessage());
+        dataRows.getMenusForCouches().forEach((dataRow) -> System.out.println(getFormattedRow(dataRow.getHeader(), dataRow.getValues())));
 
+        System.out.println();
+        System.out.println(MessageFormat.RESULT_END_COMMENT.getMessage());
     }
 
     private String getFormattedRow(String header, List<String> values) {
         List<String> row = new ArrayList<>(List.of(header));
-        values.forEach((value) -> row.add(value));
+        row.addAll(values);
 
         return String.format(MessageFormat.RESULT_DATA_FORMAT.getMessage(),
                 String.join(MessageFormat.DATA_PARSER.getMessage(), row));
