afcbff007855d5badac888c4ee3e80675fb2ef26
sangeun-b <baek0007@algonquinlive.com>
2022-12-17 13:38:17 +0900

    feat: 음식 카테고리 enum 생성

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,4 +1,26 @@
 package menu.domain;
 
 public enum Category {
+    JAPAN(1, "일식"),
+    KOREA(2,"한식"),
+    CHINA(3, "중식"),
+    ASIAN(4, "아시안"),
+    WESTERN (5, "양식");
+
+    private final int foodNumber;
+    public final String foodType;
+
+
+    Category(int foodNumber, String foodType) {
+        this.foodNumber = foodNumber;
+        this.foodType = foodType;
+    }
+
+    public int getFoodNumber() {
+        return foodNumber;
+    }
+
+    public String getFoodType() {
+        return foodType;
+    }
 }
--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,4 +1,18 @@
 package menu.domain;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
 public class Coach {
+
+    private Map<String, List<String>> coaches = new HashMap<>();
+    private String coachName;
+    private List<String> notFood;
+
+    public Map<String, List<String>> makeCoachInfo(String coach, List<String> hateFoods){
+        return null;
+    }
+
 }
