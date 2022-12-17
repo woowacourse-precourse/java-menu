36b4e2134af0cbbc8b384c3a76e17cdbd373a10b
Reznov <reznov03048@gamil.com>
2022-12-17 14:49:36 +0900

    feat: 코치마다 못먹는 음식을 입력받고 CoachRepository에 저장하는 기능 구현

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -6,19 +6,33 @@ import menu.view.InputView;
 import menu.view.OutputView;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MenuController {
     private CoachRepository coachRepository;
+
     public MenuController() {
         coachRepository = new CoachRepository();
     }
+
     public void start() {
         OutputView.printStartComment();
         // 코치들 입력받음
         List<String> names = inputCoachNames();
-        List<Coach> coaches = nameToCoaches(names);
-        coachRepository.setCoaches(coaches);
+
+        // 코치들별로 못먹는 음식 입력받기 & repository에 저장
+        List<Coach> coaches = new ArrayList<>();
+        for (int i = 0; i < names.size(); i++) {
+            String name = names.get(i);
+            OutputView.printCoachForbiddenFoodComment(name);
+            // 해당 코치의 못먹는 음식 리스트 입력받음
+            List<String> foods = InputView.readForbiddenFoods();
+
+            coaches.add(new Coach(name, foods));
+        }
+
+
     }
 
     private List<String> inputCoachNames() {
@@ -26,13 +40,5 @@ public class MenuController {
         List<String> names = InputView.readNames();
 
         return names;
-    }
-
-    private List<Coach> nameToCoaches(List<String> names) {
-        List<Coach> coaches = new ArrayList<>();
-        for(String name : names) {
-            coaches.add(new Coach(name));
-        }
-        return coaches;
     }
 }
