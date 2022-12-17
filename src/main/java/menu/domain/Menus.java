583fc3fb725dd39eb4d3bbbcfaced498851017d9
moonstal1506 <moonstal1506@gmail.com>
2022-12-17 15:33:19 +0900

    refactor: 메뉴 추가하는 로직 변경

--- src/main/java/menu/domain/Menus.java
+++ src/main/java/menu/domain/Menus.java
@@ -1,21 +1,21 @@
 package menu.domain;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Menus {
 
     private final List<String> menus = new ArrayList<>();
 
     public Menus(String menus) {
-        this.menus.addAll(splitMenu(menus));
+        splitMenus(menus);
     }
 
-    private List<String> splitMenu(String menus) {
-        return Arrays.stream(menus.split(","))
-                .collect(Collectors.toList());
+    private void splitMenus(String menus) {
+        String[] splitMenus = menus.split(",");
+        for (String splitMenu : splitMenus) {
+            this.menus.add(splitMenu);
+        }
     }
 
     public int size() {
