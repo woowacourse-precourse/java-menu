1862a1fbca939cb3d99d7002011886c7802e1460
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-12-17 13:20:34 +0900

    feat(Menu): 메뉴가 카테고리 정보를 가지고 있도록 구현

--- src/main/java/menu/domain/Menu.java
+++ src/main/java/menu/domain/Menu.java
@@ -1,1 +1,1 @@
-package menu.domain;public class Menu {}
+package menu.domain;public class Menu {    private final String name;    private final Category category;    public Menu(String name, Category category) {        this.name = name;        this.category = category;    }}
