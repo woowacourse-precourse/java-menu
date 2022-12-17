a224828fe33855d48ba10de6c401da8f888fb13a
zeunala <zeunala@naver.com>
2022-12-17 15:30:39 +0900

    feat: 코치별 추천 메뉴 요청 기능 추가

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -4,17 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 import menu.service.CategoryRecommendService;
 import menu.service.MenuRecommendService;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("점심 메뉴 추천을 시작합니다.\n");
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
-        String coachNames = Console.readLine();
+        String[] coachNames = Console.readLine().split(",");
         System.out.println();
-        for (String coachName : coachNames.split(",")) {
+        for (String coachName : coachNames) {
             System.out.printf("%s(이)가 못 먹는 메뉴를 입력해 주세요.\n", coachName);
             String bannedMenus = Console.readLine().strip();
             System.out.println();
@@ -30,6 +28,14 @@ public class Application {
         List<String> recommendedCategories = new ArrayList<>();
         for (int i = 0; i < totalDay.size(); i++) {
             recommendedCategories.add(CategoryRecommendService.recommendCategory());
+        }
+        Map<String, List<String>> recommendMenusByCoach = new HashMap<>();
+        for (String coachName : coachNames) {
+            recommendMenusByCoach.put(coachName, new ArrayList<>());
+            for (int i = 0; i < totalDay.size(); i++) {
+                recommendMenusByCoach.get(coachName)
+                        .add(MenuRecommendService.recommendMenu(coachName, recommendedCategories.get(i)));
+            }
         }
     }
 }
