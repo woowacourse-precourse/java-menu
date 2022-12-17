c3adeae0c22c3e333a1ef1ef65f90fb43fcf8773
LJW25 <dbjwl@g.skku.edu>
2022-12-17 15:38:29 +0900

    feat: 카테고리 랜덤 선택 기능 완료

--- src/main/java/menu/Recommender.java
+++ src/main/java/menu/Recommender.java
@@ -1,6 +1,9 @@
 package menu;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Recommender {
@@ -15,6 +18,14 @@ public class Recommender {
 
     private List<Integer> makeRandomCategories(){
         List<Integer> randomCategory = new ArrayList<>();
+
+        while(randomCategory.size() < 5){
+            int category = Randoms.pickNumberInRange(1, 5);
+            if(Collections.frequency(randomCategory, category) < 2){
+                randomCategory.add(category);
+            }
+        }
+        //System.out.println(""+randomCategory);
         return randomCategory;
     }
 
