c1527ac2e0c783c62c2c58df911a21822dd5937b
eunbii0213 <eunbii0213@naver.com>
2022-12-17 16:17:47 +0900

    feat: 잘못된 입력일 경우 IllegalArgumentException 발생 및 다시 입력

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,20 +1,21 @@
 package menu;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import input.Input;
+import validation.Validation;
 import view.View;
 
 public class Application {
 	//코치들 담는 List
 	//코치 : 못먹는 음식, 추천 음식
 	public static String[] coachNamesArr; //이거 리스트로 바꿔 그리고 이넘 사용 고려해봐
-	public static List<List<String>> coachesHateFood;
+	public static String[] hateFoodsArr;
+	public static List<String> coachesHateFood;
 	public static List<List<String>> menuRecommendResult;
-	public static List<String> menuRecommend;
+	public static String recommendMenu;
 	public static List<Integer> categoryCount;
 	public static List<String> weeklyCategory;
 
@@ -25,21 +26,27 @@ public class Application {
 
 		//코치의 이름을 입력해 주세요. (, 로 구분)
 		View.showCoachNameGuideMessage();
-		String coachNames = Input.input();
+		String coachNames = Validation.validateCoachInputException(Input.input());
 		coachNamesArr = coachNames.split(",");
+
+		for (int i = 0; i < coachNamesArr.length; i++) {
+			menuRecommendResult.add(new ArrayList<>());
+		}
 
 		//토미(이)가 못 먹는 메뉴를 입력해 주세요.
 		for (int i = 0; i < coachNamesArr.length; i++) {
 			View.showHateFoodGuideMessage(coachNamesArr[i]);
-			String hateFoodInput = Input.input();
-			String[] hateFoodsArr = hateFoodInput.split(",");
-			coachesHateFood.add(Arrays.asList(hateFoodsArr));
+			String hateFoodInput = Validation.validateHateFoodInputException(Input.input(), coachNamesArr[i]);
+			hateFoodsArr = hateFoodInput.split(",");
+
+			for (int j = 0; j < hateFoodsArr.length; j++) {
+				coachesHateFood.add(hateFoodsArr[j]);
+			}
+			menuRecommendResult.get(i).add(coachNamesArr[i]);
 		}
 
-		//메뉴 추천하기
-
 		//카테고리 정하기
-		for(int i=0;i<5;i++) {
+		for (int i = 0; i < 5; i++) {
 			int categoryNumber = Randoms.pickNumberInRange(1, 5);
 
 			//한 주에 같은 카테고리는 최대 2회까지만 고를 수 있다.
@@ -50,36 +57,47 @@ public class Application {
 			categoryCount.set(categoryNumber, categoryCount.get(categoryNumber) + 1);
 			//1이면 일식, 2면 한식, 3이면 중식, 4면 아시안, 5면 양식
 			weeklyCategory.add(Category.getCategoryName(categoryNumber));
-			List<String> menus = Category.getMenus(categoryNumber);
+
 		}
 
-		for(String category : weeklyCategory){
-			System.out.println(category);
+		//카테고리 주어지면 거기 안에서 n명의 코치가 메뉴 선정 (이걸 주 5회니까 총 5번 해야하는 것)
+		for (int j = 0; j < 5; j++) {
+			String category = weeklyCategory.get(j);
+
+			for (int i = 0; i < coachNamesArr.length; i++) {
+				//메뉴 정하기
+				menuRecommendResult.get(i).add(selectMenu(coachesHateFood, i, category));
+
+			}
 		}
-
-
-
-
-		//메뉴 선택하기
 
 		//메뉴 추천 결과 출력
-		//View.showRecommendResult(menuRecommendResult);
+		View.showRecommendResult(weeklyCategory, menuRecommendResult);
 
 		//메뉴 추천 완료 메세지 출력
-		//View.showRecommendClearGuideMessage();
+		View.showRecommendClearGuideMessage();
+	}
+
+	private static String selectMenu(List<String> coachesHateFood, int coachIndex, String category) {
+		recommendMenu = Randoms.shuffle(Category.getMenus(category)).get(0);
+
+		while (coachesHateFood.contains(recommendMenu) || menuRecommendResult.get(coachIndex).contains(recommendMenu)) {
+			recommendMenu = Randoms.shuffle(Category.getMenus(category)).get(0);
+		}
+
+		return recommendMenu;
 	}
 
 	private static void init() {
 		coachesHateFood = new ArrayList<>();
 		weeklyCategory = new ArrayList<>();
+		weeklyCategory.add("카테고리");
+
+		menuRecommendResult = new ArrayList<>();
 
 		categoryCount = new ArrayList<>();
 		for (int i = 0; i < 6; i++) {
 			categoryCount.add(0);
 		}
-	}
-
-	public static void recommendMenu() {
-
 	}
 }
---
+++ src/main/java/validation/Validation.java
@@ -0,0 +1,69 @@
+package validation;
+
+import input.Input;
+import view.View;
+
+public class Validation {
+	public static String validateCoachInputException(String coachInput) {
+		while (isCoachInputException(coachInput)) {
+			View.showCoachNameGuideMessage();
+			coachInput = Input.input();
+		}
+
+		return coachInput;
+	}
+
+	public static boolean isCoachInputException(String coachInput) {
+		String[] coaches = coachInput.split(",");
+		try {
+			validateCoachNames(coaches);
+			validateCoachCount(coaches);
+		} catch (IllegalArgumentException exception) {
+			System.out.println(exception.getMessage());
+			return true;
+		}
+		return false;
+	}
+
+	private static void validateCoachCount(String[] coaches) {
+		if (coaches.length > 5 || coaches.length < 2) {
+			throw new IllegalArgumentException("[ERROR] 코치의 인원은 2명 이상, 5명 이하로 입력해주세요. ");
+		}
+	}
+
+	private static void validateCoachNames(String[] coaches) {
+		for (int i = 0; i < coaches.length; i++) {
+			if (coaches[i].length() < 2 || coaches[i].length() > 4) {
+				throw new IllegalArgumentException("[ERROR] 코치의 이름은 2글자 이상, 최대 4글자 이하로 입력해주세요. ");
+			}
+		}
+	}
+
+	public static String validateHateFoodInputException(String hateFoodInput , String coachName) {
+		while (isHateFoodInputException(hateFoodInput)) {
+			View.showHateFoodGuideMessage(coachName);
+			hateFoodInput = Input.input();
+		}
+
+		return hateFoodInput;
+	}
+
+	public static boolean isHateFoodInputException(String hateFoodInput) {
+		try {
+			validateHateFoodInput(hateFoodInput);
+		} catch (IllegalArgumentException exception) {
+			System.out.println(exception.getMessage());
+			return true;
+		}
+		return false;
+	}
+
+
+	public static void validateHateFoodInput(String hateFoodInput) {
+		String[] hateFoods = hateFoodInput.split(",");
+
+		if (hateFoods.length > 2) {
+			throw new IllegalArgumentException("[ERROR] 못 먹는 음식은 2개까지만 입력해주세요. ");
+		}
+	}
+}
