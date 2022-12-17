267c40d6f3da59413b7ee8700e25d85967c257bf
woosung1223 <dntjd991223@naver.com>
2022-12-17 14:24:21 +0900

    feat: 기능 제어 컨트롤러 생성

---
+++ src/main/java/menu/controller/MenuController.java
@@ -0,0 +1,27 @@
+package menu.controller;
+
+import menu.domain.Coach;
+import menu.domain.Group;
+import menu.view.InputView;
+
+import java.util.List;
+
+public class MenuController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final Group group = new Group();
+
+    public void run() {
+        System.out.println("점심 메뉴 추천을 시작합니다");
+        List<String> coaches = inputView.readNames();
+
+        for (String coach : coaches) {
+            List<String> cannotEatMenus = inputView.readMenuCantEat(coach);
+            group.addCoach(coach, cannotEatMenus);
+        }
+
+        group.makeMenus();
+
+        outputView.renderResult(group.getCoaches());
+    }
+}
