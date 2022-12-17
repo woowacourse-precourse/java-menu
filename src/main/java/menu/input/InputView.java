8c4e3993f70b8f5b567e887d2ecd0a31ae6b8db3
hoin <apicad0307@gmail.com>
2022-12-17 17:18:13 +0900

    feat(InputView.java, OutputView.java): 코치의 이름과 명수에 대한 예외처리 구현

--- src/main/java/menu/input/InputView.java
+++ src/main/java/menu/input/InputView.java
@@ -1,20 +1,47 @@
 package menu.input;
 
 import camp.nextstep.edu.missionutils.Console;
-import menu.output.OutputString;
 
 public class InputView {
 
-    public String[] INPUT_COACH_NAME(){
+    public String[] INPUT_COACH_NAME() {
         System.out.println(InputString.COACH_NAME_INPUT);
-        String names = Console.readLine();
-        return names.split(",");
+        try {
+            try {
+                String names = Console.readLine();
+                String[] coaches = names.split(",");
+                if (coaches.length < 2 || coaches.length > 5)
+                    throw new IllegalArgumentException("[ERROR] 코치는 2명 이상 5명 이하입니다.");
+                for (String coach : coaches) {
+                    if (coach.length() < 2 || coach.length() > 4)
+                        throw new IllegalArgumentException("[ERROR] 코치의 이름은 최소 2글자, 최대 4글자 입니다.");
+                }
+                return names.split(",");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                return INPUT_COACH_NAME();
+            } catch (Exception e) {
+                throw new IllegalArgumentException("[ERROR] 입력에 오류가 있습니다.");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return INPUT_COACH_NAME();
+        }
     }
 
-    public String[] CANNOT_EAT_FOOD(String name){
+    public String[] CANNOT_EAT_FOOD(String name) {
         System.out.println(name + InputString.CANNOT_EAT_FOOD);
-        String m = Console.readLine();
-        return m.split(",");
+        try {
+            try {
+                String m = Console.readLine();
+                return m.split(",");
+            } catch (Exception e) {
+                throw new IllegalArgumentException("[ERROR]");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return CANNOT_EAT_FOOD(name);
+        }
     }
 
 }
--- src/main/java/menu/output/OutputView.java
+++ src/main/java/menu/output/OutputView.java
@@ -9,29 +9,31 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class OutputView {
-    public void GAME_START_OUTPUT(){
+    public void GAME_START_OUTPUT() {
         System.out.println(OutputString.GAME_START_OUTPUT);
     }
 
-    public void format(List<String> lists, String prefix){
+    public void format(List<String> lists, String prefix) {
         StringBuilder sb = new StringBuilder("[ " + prefix + " ");
 
         for (String list : lists) {
-            sb.append("| "+ list +" ");
+            sb.append("| " + list + " ");
         }
         sb.append("]");
         System.out.println(sb);
     }
-    public void format(Set<String> lists, String prefix){
-        StringBuilder sb = new StringBuilder(" [ " + prefix +" ");
+
+    public void format(Set<String> lists, String prefix) {
+        StringBuilder sb = new StringBuilder("[ " + prefix + " ");
 
         for (String list : lists) {
-            sb.append("| "+ list +" ");
+            sb.append("| " + list + " ");
         }
         sb.append("]");
         System.out.println(sb);
     }
-    public void PRINT_RESULT(List<Category> categories, Map<String, Set<String>> coaches){
+
+    public void PRINT_RESULT(List<Category> categories, Map<String, Set<String>> coaches) {
         List<String> day = Arrays.asList("월요일", "화요일", "수요일", "목요일", "금요일");
         List<String> categoryList = categories.stream().map(c -> c.getCategory()).collect(Collectors.toList());
 
