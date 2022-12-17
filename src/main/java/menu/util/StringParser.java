ef65213e6874bfc849f753527c696c212f0b2dc3
bjk1649 <qhwns1649@naver.com>
2022-12-17 13:43:20 +0900

    fix: 코치 이름 입력받는 기능 로직 수정
    앞뒤 공백은 제거하고 받음, 중복되는 이름 예외처리

--- src/main/java/menu/util/StringParser.java
+++ src/main/java/menu/util/StringParser.java
@@ -2,9 +2,10 @@ package menu.util;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringParser {
     public static List<String> parseByCommand(String input, String command) {
-        return Arrays.asList(input.split(command));
+        return Arrays.stream(input.split(command)).map(String::trim).collect(Collectors.toList());
     }
 }
--- src/main/java/menu/util/Validator.java
+++ src/main/java/menu/util/Validator.java
@@ -1,22 +1,32 @@
 package menu.util;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Validator {
-    private static final String COACH_NAME_ERROR_MESSAGE = "[ERROR] 코치의 이름은 ,을 기준으로 한글 2~4글자이며, 2~5명을 입력하여야합니다.";
+    private static final String COACH_NAME_ERROR_MESSAGE = "[ERROR] 코치의 이름은 ,을 기준으로 한글 2~4글자이며, 2~5명을 입력하여야합니다. 중복은 불가능합니다.";
 
     public static void validateCoachNames(String input, String command) throws IllegalArgumentException {
         List<String> names = StringParser.parseByCommand(input, command);
         if (names.size() < 2 || names.size() > 5) {
             throw new IllegalArgumentException(COACH_NAME_ERROR_MESSAGE);
         }
+        checkDuplicate(names);
         names.forEach(Validator::validateName);
     }
 
     private static void validateName(String name) {
         String condition = "^[가-힣]{2,4}$";
         if (!Pattern.matches(condition, name)) {
+            throw new IllegalArgumentException(COACH_NAME_ERROR_MESSAGE);
+        }
+    }
+
+    private static void checkDuplicate(List<String> input) {
+        Set<String> numSet = new HashSet<>(input);
+        if(numSet.size()!= input.size()){
             throw new IllegalArgumentException(COACH_NAME_ERROR_MESSAGE);
         }
     }
