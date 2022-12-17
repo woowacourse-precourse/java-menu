85623125751fdd0d2472beda1b1fad4efdd03afb
starwook <star.wookk@gmail.com>
2022-12-17 17:29:28 +0900

    test: Add 카테고리_분류 test

--- src/test/java/menu/MenuRecommendTest.java
+++ src/test/java/menu/MenuRecommendTest.java
@@ -1,17 +1,24 @@
 package menu;
 
+import menu.domain.Food;
+import menu.resource.Category;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MenuRecommendTest {
     MenuRecommend menuRecommend = new MenuRecommend();
-
+    MenuGame menuGame = new MenuGame();
+    List<Food> foods = menuGame.getFoods();
     @Test
-    void 카테고리_중복(){
-//        for(int i=0;i<5;i++){
-//            menuRecommend.chooseCategory();
-//        }
+    void 카테고리_분류(){
+        List<Food> categoryFood = menuRecommend.getSpecificCategoryFood(foods, Category.ASIAN);
+        for(int i=0;i<categoryFood.size();i++){
+            Assertions.assertThat(categoryFood.get(i).getCategory()).isEqualTo(Category.ASIAN);
+        }
     }
 
 }
