69c636e154dd610460ce5d263d24ec4fad22952f
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-12-17 17:51:16 +0900

    Refactor : 이름에 대한 공백 정리 기능 추가

--- src/main/java/menu/InputDriver.java
+++ src/main/java/menu/InputDriver.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class InputDriver {
     OutputDriver outputDriver = new OutputDriver();
+
     public String scanCoachName() {
         return checkIfValidStringForNameList(Console.readLine());
     }
@@ -29,29 +30,40 @@ public class InputDriver {
     }
 
     private void checkIfSplitValid(String readLine) {
-        if (List.of(readLine.split(",")).contains("")){
+        if (List.of(readLine.split(",")).contains("")) {
             outputDriver.printErrorForScanSpliting();
             throw new IllegalArgumentException();
         }
     }
 
     private void checkIfCoachNumberValid(String readLine) {
-        if (List.of(readLine.split(",")).size() < 2){
+        if (List.of(readLine.split(",")).size() < 2) {
             outputDriver.printErrorForScanTooLessCoach();
             throw new IllegalArgumentException();
         }
-        if (List.of(readLine.split(",")).size() > 5){
+        if (List.of(readLine.split(",")).size() > 5) {
             outputDriver.printErrorForScanTooManyCoach();
             throw new IllegalArgumentException();
         }
     }
 
     private void checkIfNameLengthValid(String readLine) {
-        for (String name: List.of(readLine.split(","))){
-            if (name.length() < 2 || name.length() > 4){
+        for (String name : List.of(readLine.split(","))) {
+            name = refreshString(name);
+            if (name.length() < 2 || name.length() > 4) {
                 throw new IllegalArgumentException();
             }
         }
+    }
+
+    private String refreshString(String s) {
+        while(s.charAt(0) == ' '){
+            s=s.substring(1);
+        }
+        while(s.charAt(s.length()-1) == ' ') {
+            s = s.substring(0, s.length() - 1);
+        }
+        return s;
     }
 
     public String scanCannotEatMenu() {
@@ -60,12 +72,13 @@ public class InputDriver {
     }
 
     public String checkIfValidStringForMenuList(String readLine) {
+        checkIfSplitValid(readLine);
         checkIfMenuNumberValid(readLine);
         return readLine;
     }
 
     private void checkIfMenuNumberValid(String readLine) {
-        if (List.of(readLine.split(",")).size() > 2 ){
+        if (List.of(readLine.split(",")).size() > 2) {
             outputDriver.printErrorForScanMenuNumber();
             throw new IllegalArgumentException();
         }
