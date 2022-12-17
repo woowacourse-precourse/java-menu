0470f217c00de34e6b1aaf3ae267f34d37a616c7
be-student <presentsong@naver.com>
2022-12-17 16:47:10 +0900

    feat: 셔플 메서드를 테스트하는 코드를 추가한다

--- src/test/java/menu/domain/RandomPickerTest.java
+++ src/test/java/menu/domain/RandomPickerTest.java
@@ -2,6 +2,7 @@ package menu.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -14,5 +15,14 @@ class RandomPickerTest {
     @Test
     void pickRandomInRange_메서드는_시작범위와_끝범위_사이의_수가_반환된다() {
         assertThat(new RandomPicker().pickNumberInRange(1, 5)).isBetween(1, 5);
+    }
+
+    @Test
+    void shuffle_메서드는_섞고_나도_모든_원소가_제거되거나_추가되지_않는다() {
+        List<String> before = List.of("헬프", "테스트", "케이스");
+        assertThat(new RandomPicker().shuffle(before))
+                .containsAll(before)
+                .hasSize(before.size());
+
     }
 }
