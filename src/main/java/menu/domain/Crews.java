bc6665ab8f4bbf3a9965ad52afb25977f359f969
sosow0212 <sosow0212@naver.com>
2022-12-17 13:48:31 +0900

    refactor : (Crews) 생성자로 리스트 받도록 리팩토링

--- src/main/java/menu/domain/Crews.java
+++ src/main/java/menu/domain/Crews.java
@@ -6,11 +6,8 @@ import java.util.List;
 public class Crews {
     private List<Crew> crews;
 
-    public Crews() {
-        crews = new ArrayList<>();
+    public Crews(List<Crew> crews) {
+        this.crews = crews;
     }
 
-    public void addCrew(Crew crew) {
-        this.crews.add(crew);
-    }
 }
