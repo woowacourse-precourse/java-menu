c1312bee118f8f744183eb53ef8953da2002c82f
thdwoqor <celebarte01@gmail.com>
2022-12-17 16:12:43 +0900

    feat: 이름에 숫자 포함 여부 테스트

--- src/main/java/menu/util/Validate.java
+++ src/main/java/menu/util/Validate.java
@@ -5,8 +5,15 @@ import java.util.List;
 import java.util.Set;
 
 public class Validate {
+    public static void inNumber(String name) {
+        if (name.matches(".*[0-9].*")) {
+            throw new IllegalArgumentException(Message.ERROR_NUMBER.message);
+        }
+    }
+
     public static void nameLength(List<String> names, int minInclusive, int maxInclusive) {
         names.stream().forEach(name -> {
+                    inNumber(name);
                     if (name.length() < minInclusive || name.length() > maxInclusive) {
                         throw new IllegalArgumentException(Message.ERROR_DIFFERENT_NAME_LENGTHS.message);
                     }
@@ -25,6 +32,7 @@ public class Validate {
             throw new IllegalArgumentException(Message.ERROR_DIFFERENT_CANT_EAT_LENGTHS.message);
         }
     }
+
     public static void duplication(List<String> numbers) {
         Set<String> sameNumber = new HashSet<>(numbers);
         if (numbers.size() != sameNumber.size()) {
@@ -36,7 +44,8 @@ public class Validate {
         ERROR_DIFFERENT_NAME_LENGTHS("[ERROR] 코치의 이름은 최소 2글자, 최대 4글자이다."),
         ERROR_DIFFERENT_COACH_LENGTHS("[ERROR] 코치는 최소 2명, 최대 5명까지 식사를 함께 한다."),
         ERROR_DIFFERENT_CANT_EAT_LENGTHS("[ERROR] 각 코치는 최소 0개, 최대 2개의 못 먹는 메뉴가 있다."),
-        ERROR_DUPLICATE_VALUE("[ERROR] 중복 값이 포함되어 있습니다.");
+        ERROR_DUPLICATE_VALUE("[ERROR] 중복 값이 포함되어 있습니다."),
+        ERROR_NUMBER("[ERROR] 숫자 값이 포함되어 있습니다.");
 
         private final String message;
 
