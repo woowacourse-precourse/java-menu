34ace15ab8004dd1bb2c5d489013095afb9c84f2
Eun-chan Cho <eunchan1001@gmail.com>
2022-12-17 16:03:57 +0900

    feat(OutputView): add other printing methods

--- src/main/java/menu/View/Outputview.java
+++ src/main/java/menu/View/Outputview.java
@@ -1,3 +1,29 @@
+package menu.View;
+
+import java.util.List;
+
+import menu.Model.Category;
+import menu.Model.Coach;
+import menu.Model.Coachs;
+
+public class Outputview {
+    private final List<String> WEEKDAYS = List.of("월요일", "화요일", "수요일", "목요일", "금요일");
+    private final String START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    private final String RESULT_MESSAGE = "메뉴 추천 결과입니다.";
+    private final String END_MESSAGE = "추천을 완료했습니다.";
+
+    private void printBlank() {
+        System.out.println();
+    }
+
+    public void printStart() {
+        System.out.println(START_MESSAGE);
+        this.printBlank();
+    }
+
+    public void printEnd() {
+        System.out.println(END_MESSAGE);
+    }
 
     public void printResult(Coachs coachs) {
         System.out.println(RESULT_MESSAGE);
