a7132232bfefb3538018ece4954b39c9ea1ee682
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-12-17 13:22:06 +0900

    <feat>(Member.java): getter/setter,  생성자 작성

--- src/main/java/menu/Member.java
+++ src/main/java/menu/Member.java
@@ -7,5 +7,32 @@ public class Member {
     private List<String> pickyEatings;
     private List<String> recommendedMeal;
 
-    
+    public Member(String name, List<String> pickyEatings) {
+        this.name = name;
+        this.pickyEatings = pickyEatings;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPickyEatings() {
+        return pickyEatings;
+    }
+
+    public void setPickyEatings(List<String> pickyEatings) {
+        this.pickyEatings = pickyEatings;
+    }
+
+    public List<String> getRecommendedMeal() {
+        return recommendedMeal;
+    }
+
+    public void setRecommendedMeal(List<String> recommendedMeal) {
+        this.recommendedMeal = recommendedMeal;
+    }
 }
