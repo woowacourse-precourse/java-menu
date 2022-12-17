2260bdb5d201ad44f077e4cbfa13d7632f08336d
anneu <b.imnuj@gmail.com>
2022-12-17 13:48:10 +0900

    style: 서비스 클래스명 오탈자 수정

--- src/main/java/menu/controller/MenuDecideController.java
+++ src/main/java/menu/controller/MenuDecideController.java
@@ -1,13 +1,13 @@
 package menu.controller;
 
-import menu.domain.MenueDecideService;
+import menu.domain.MenuDecideService;
 import menu.view.Input;
 import menu.view.Output;
 
 public class MenuDecideController {
     private final Input input = new Input();
     private final Output output = new Output();
-    private final MenueDecideService service = new MenueDecideService();
+    private final MenuDecideService service = new MenuDecideService();
 
     public void run() {
         output.printStart();
