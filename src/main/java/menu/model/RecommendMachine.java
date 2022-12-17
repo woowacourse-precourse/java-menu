b8d4bfd18767e6a32e0fce3eb246b2b7a58d1e72
은지 <midzy0228@gmail.com>
2022-12-17 16:06:08 +0900

    fix: 추천 카테고리 최대 갯수 3개 버그 수정

--- src/main/java/menu/model/RecommendMachine.java
+++ src/main/java/menu/model/RecommendMachine.java
@@ -9,7 +9,7 @@ public class RecommendMachine {
     private static final Integer DAY_OF_SIZE = 5;
     public List<String> recommendCategory() {
         List<String> result = new ArrayList<>();
-        while (result.size() <= DAY_OF_SIZE) {
+        while (result.size() < DAY_OF_SIZE) {
             String recommending = Category.recommendCategory();
             if (canRecommend(result, recommending)) {
                 result.add(recommending);
@@ -23,6 +23,6 @@ public class RecommendMachine {
     }
 
     private boolean canRecommend(List<String> category, String recommend) {
-        return Collections.frequency(category, recommend) <= MAX_RECOMMEND_CATEGORY_COUNT;
+        return Collections.frequency(category, recommend) < MAX_RECOMMEND_CATEGORY_COUNT;
     }
 }
