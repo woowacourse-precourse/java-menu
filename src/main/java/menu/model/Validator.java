080cefd8b767ffaa2dbe878ba05860c0b6729c53
2014012606 <2014012606@hanyang.ac.kr>
2022-12-17 15:20:15 +0900

    refactor: 에러 메시지 세분화

--- src/main/java/menu/model/Validator.java
+++ src/main/java/menu/model/Validator.java
@@ -22,15 +22,21 @@ public class Validator {
 
     private static void isValidCoachesSize(List<String> coachesName) {
         int numberOfCoach = coachesName.size();
-        if (numberOfCoach < MIN_COACH_NUMBER || numberOfCoach > MAX_COACH_NUMBER) {
-            throw new IllegalArgumentException(error + "코치는 최소 2명, 최대 5명까지만 함께 식사할 수 있습니다.");
+        if (numberOfCoach < MIN_COACH_NUMBER) {
+            throw new IllegalArgumentException(error + "코치는 최소 2명 이상 입력해야 합니다.");
+        }
+        if (numberOfCoach > MAX_COACH_NUMBER) {
+            throw new IllegalArgumentException(error + "코치는 최대 5명까지 입력할 수 있습니다.");
         }
     }
 
     private static void isValidCoachName(List<String> coachesName) {
         for (String name : coachesName) {
-            if (name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH) {
-                throw new IllegalArgumentException(error + "코치의 이름은 최소 2글자, 최대 4글자 내에서 입력해 주세요.");
+            if (name.length() < MIN_NAME_LENGTH) {
+                throw new IllegalArgumentException(error + "코치의 이름은 최소 2글자 이상 입력야 합니다.");
+            }
+            if (name.length() > MAX_NAME_LENGTH) {
+                throw new IllegalArgumentException(error + "코치의 이름은 최대 4글자까지 입력할 수 있습니다.");
             }
         }
     }
