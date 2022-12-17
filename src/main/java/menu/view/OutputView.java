026607ed91396c43ea80fa3e930f72eb8449d846
은지 <midzy0228@gmail.com>
2022-12-17 15:04:46 +0900

    feat: 예외 상황 메시지 출력 기능 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,6 +1,7 @@
 package menu.view;
 
 public class OutputView {
+    private static final String ERROR = "[ERROR] ";
     private static final String START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
     private static final String RECOMMEND_RESULT = "메뉴 추천 결과입니다.";
     private static final String END_MESSAGE = "추천을 완료했습니다.";
@@ -14,5 +15,9 @@ public class OutputView {
         // ToDo: 추천 결과
         System.out.println();
         System.out.println(END_MESSAGE);
+    }
+
+    public void printExceptionMessage(Exception e) {
+        System.out.println(ERROR + e.getMessage());
     }
 }
