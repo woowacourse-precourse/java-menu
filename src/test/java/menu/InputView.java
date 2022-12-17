753d360492c0aa69742e828d9633845966b0ab0c
kimdahee0815 <diana815@naver.com>
2022-12-17 14:01:10 +0900

    feat(askCoachName) : coachName을 입력받은 뒤에 예외사항을 처리한 후 checkPass 가 true가 되면 while 루프를 나온다

--- src/test/java/menu/InputView.java
+++ src/test/java/menu/InputView.java
@@ -1,24 +1,30 @@
 package menu;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-	ArrayList<String> coachName = new ArrayList<>();
+	ArrayList<String> coachName;
 	public void askCoachName() {
-		boolean check = false;
-		while(!check) {
+		boolean checkPass = false;
+		while(!checkPass) {
 			String coachNames = "";
 			coachNames = Console.readLine();
-			check = errorCatchForCoachName(coachNames);
+			if(checkPass = errorCatchForCoachName(coachNames)) {
+				coachName = new ArrayList<>(List.of(coachNames));
+			}
 		}
 	}
 	
 	public boolean errorCatchForCoachName(String coachNames) {
+		String[] coachStr;
 		try {
 			checkCoachNameFormat(coachNames);
-			checkCoachNameLength(coachNames);
+			coachStr = checkEachCoachNameLength(coachNames);
+			checkCoachNameLength(coachStr);
 		} catch(IllegalArgumentException e) {
 			System.out.println(e.getMessage());
 			return false;
@@ -28,13 +34,28 @@ public class InputView {
 	
 	public boolean checkCoachNameFormat(String coachNames) {
 		String pattern = "^.+,.+$";
-		
+		if(Pattern.matches(pattern, coachNames)) {		
+			return true;
+		}
+		throw new IllegalArgumentException("[ERROaR] 2명 이상 5명 이하의 이름을 ,로 구분지어 입력하세요.");
 	}
 	
-	public boolean checkCoachNameLength(String coachNames) {
-		if(coachNames.length() < 2 || coachNames.length() > 5) {
-			throw new IllegalArgumentException("[ERROR] 이름이 2-4글자가 아닙니다.");
+	public String[] checkEachCoachNameLength(String coachNames) {
+		String[] coachStr = coachNames.split(",");
+		for(String coachName : coachStr) {
+			if(coachName.length() < 2 && coachName.length() > 4) {
+				throw new IllegalArgumentException("[ERROR] 이름이 2-4글자가 아닙니다.");
+			}
+		}
+		return coachStr;
+	}
+	
+	public boolean checkCoachNameLength(String[] coachStr) {
+		if(coachStr.length < 2 || coachStr.length > 5) {
+			throw new IllegalArgumentException("[ERROR] 코치의 수가 2-5명이 아닙니다.");
 		}
 		return true;
 	}
+	
+	
 }
