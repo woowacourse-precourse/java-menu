9268060f5a47aad7d0efe643bcd7eabc5d450db5
kat8813 <ohseo8813@gmail.com>
2022-12-17 13:40:41 +0900

    <feat>(InputView.java): 코치가 싫어하는 메뉴를 입력하는 기능 추가

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -6,6 +6,7 @@ public class Coach {
     private final static int NAME_MIN_LENGTH = 2;
 
     private final String name;
+
     public Coach(String name) {
         validate(name);
         this.name = name;
@@ -15,5 +16,9 @@ public class Coach {
         if (name.length() < NAME_MIN_LENGTH || name.length() > NAME_MAX_LENGTH) {
             throw new IllegalArgumentException("코치의 이름은 최소 2글자, 최대 4글자입니다. 입력값 : " + name);
         }
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -6,8 +6,10 @@ import static java.util.stream.Collectors.toList;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import menu.domain.Coach;
 import menu.domain.Coaches;
+import menu.domain.Menu;
 
 public class InputView {
 
@@ -17,5 +19,13 @@ public class InputView {
         return coachNames.stream()
                 .map(Coach::new)
                 .collect(collectingAndThen(toList(), Coaches::new));
+    }
+
+    public List<Menu> sendHateMenuBySpecificCoach(Coach coach) {
+        System.out.println(coach.getName() + "(이)가 못 먹는 메뉴를 입력해주세요.");
+        List<String> hateMenuNames = Arrays.asList(Console.readLine().split(","));
+        return hateMenuNames.stream()
+                .map(Menu::of)
+                .collect(Collectors.toList());
     }
 }
