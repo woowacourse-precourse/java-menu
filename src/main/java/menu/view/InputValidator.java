7b0bb70c7310f3a041975dd2589855db2e3a29e4
ksh-beginner <ohk9134@naver.com>
2022-12-17 14:00:09 +0900

    feat(InputValidator) : 코치가 2명 미만, 5명 초과인 경우 예외 처리

--- src/main/java/menu/view/InputValidator.java
+++ src/main/java/menu/view/InputValidator.java
@@ -10,10 +10,15 @@ public class InputValidator {
 
     private static final int MIN_COACH_NAME_LENGTH = 2;
     private static final int MAX_COACH_NAME_LENGTH = 4;
+    private static final int MIN_COACH_COUNT = 2;
+    private static final int MAX_COACH_COUNT = 5;
 
     public void validateReadCoachNames(String[] coachNames) {
         if (isWrongCoachNameLength(coachNames)) {
             throw new IllegalArgumentException(ErrorConstants.ERROR_PREFIX + "코치의 이름은 2글자 이상 4글자 이하여야합니다.");
+        }
+        if (isWrongCoachCount(coachNames)) {
+            throw new IllegalArgumentException(ErrorConstants.ERROR_PREFIX + "코치는 최소 2명에서 최대 5명이어야합니다.");
         }
     }
 
@@ -23,5 +28,10 @@ public class InputValidator {
                 .filter(coachName -> coachName < MIN_COACH_NAME_LENGTH || coachName > MAX_COACH_NAME_LENGTH)
                 .count();
         return wrongCoachCount != 0;
+    }
+
+    private boolean isWrongCoachCount(String[] coachNames) {
+        int coachCount = coachNames.length;
+        return coachCount < MIN_COACH_COUNT || coachCount > MAX_COACH_COUNT;
     }
 }
