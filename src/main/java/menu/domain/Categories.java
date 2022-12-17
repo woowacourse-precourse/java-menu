cd5f68914df459cef535795b4695c87195d2f675
dev_kong <ahdjd5@gmail.com>
2022-12-17 15:54:17 +0900

    feat(domain): add Categories class

---
+++ src/main/java/menu/domain/Categories.java
@@ -0,0 +1,28 @@
+package menu.domain;
+
+import menu.utills.constants.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Categories {
+    private final List<Category> categories = new ArrayList<>();
+
+    public void addCategories(Category category) {
+        if(duplicatedDouble(category)) {
+            return;
+        }
+        categories.add(category);
+    }
+    
+    public boolean isFull() {
+        return categories.size() == 5;
+    }
+
+    private boolean duplicatedDouble(Category newCategory) {
+        int count = (int) categories.stream()
+                .filter(category -> category.equals(newCategory))
+                .count();
+        return count >= 2;
+    }
+}
