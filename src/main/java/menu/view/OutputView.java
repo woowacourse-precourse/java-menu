beb6fb322e9ac9764348aec9705ffde15ea505d6
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 16:25:31 +0900

    feat(OutputView): 문구 및 에러메세지 출력 기능 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,5 +1,7 @@
 package menu.view;
 
+import menu.view.constant.Prefix;
+
 public class OutputView {
     private static class InstanceHolder {
         private static final OutputView INSTANCE = new OutputView();
@@ -9,5 +11,13 @@ public class OutputView {
 
     public static OutputView getInstance() {
         return OutputView.InstanceHolder.INSTANCE;
+    }
+
+    public void printPhrase(String phrase) {
+        System.out.println(phrase);
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.println(Prefix.ERROR.getPrefix() + message);
     }
 }
