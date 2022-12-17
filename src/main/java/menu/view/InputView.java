cbae436d428566990a68582d5e0d1c5827690f1c
MoonJeWoong <munjin0201@naver.com>
2022-12-17 16:56:37 +0900

    feat(view): 입력 view 구현
    세부 기능
    - 코치 이름을 입력받는다.
    - 코치별로 못먹는 메뉴를 입력받는다.

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,4 +1,31 @@
 package menu.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+
+    private static final String NAMES_INPUT_DELIMITER = ",";
+    private static final String INPUT_INEDIBLE_MENU_MESSAGE =
+            "\n%s(이)가 못 먹는 메뉴를 입력해 주세요.";
+    private static final String INPUT_COACH_NAMES_MESSAGE = "\n코치의 이름을 입력해 주세요. (, 로 구분)";
+
+    private static final List<String> EMPTY_LIST = List.of("");
+
+    public List<String> readCoachNames() {
+        System.out.println(INPUT_COACH_NAMES_MESSAGE);
+        return Arrays.asList(readLine().split(NAMES_INPUT_DELIMITER));
+    }
+
+    public List<String> readInedibleMenus(String name) {
+        System.out.println(String.format(INPUT_INEDIBLE_MENU_MESSAGE, name));
+        List<String> inputs = Arrays.asList(readLine().split(NAMES_INPUT_DELIMITER));
+        if(inputs.equals(EMPTY_LIST)){
+            return new ArrayList<>();
+        }
+        return inputs;
+    }
 }
