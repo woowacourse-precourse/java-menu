ec7e9f780c12dba8e2195cd715e52c697769bd33
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 17:08:47 +0900

    feat(MainController): 코치 등록 컨트롤러 기능 구현

--- src/main/java/menu/controller/MainController.java
+++ src/main/java/menu/controller/MainController.java
@@ -1,11 +1,16 @@
 package menu.controller;
 
 import menu.domain.Category;
+import menu.domain.Coach;
 import menu.service.CategoryService;
+import menu.service.CoachService;
 import menu.service.RecommendationService;
 import menu.view.InputView;
 import menu.view.OutputView;
+import menu.view.constant.InputPhrase;
 import menu.view.constant.OutputPhrase;
+
+import java.util.List;
 
 public class MainController {
     private static class InstanceHolder {
@@ -16,6 +21,7 @@ public class MainController {
     private OutputView outputView = OutputView.getInstance();
     private CategoryService categoryService = CategoryService.getInstance();
     private RecommendationService recommendationService = RecommendationService.getInstance();
+    private CoachService coachService = CoachService.getInstance();
 
     private MainController(){}
 
@@ -25,5 +31,13 @@ public class MainController {
 
     public void start() {
         outputView.printPhrase(OutputPhrase.START.getPhrase());
+    }
+
+    public void registerCoach() {
+        List<String> coaches = Requester.requestStringListInput(() -> {
+            outputView.printPhrase(InputPhrase.COACH_INPUT.getPhrase());
+            return inputView.readStringList();
+        });
+        coachService.registerCoach(coaches);
     }
 }
