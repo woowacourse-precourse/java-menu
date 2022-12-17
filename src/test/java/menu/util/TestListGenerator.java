2ce677ae12d07aeebc6193b0743b2ac7ddacd45a
JungWoo <rlawjddn103@naver.com>
2022-12-17 17:15:41 +0900

    test(TestListGenerator): 리스트가 들어갈때 앞에서부터 순서대로 하나씩 삭제하여 get(0)시 리스트에 있는 순서대로 값을 받을 수 있게 TestListGenerator 구현

--- src/test/java/menu/util/TestListGenerator.java
+++ src/test/java/menu/util/TestListGenerator.java
@@ -1,11 +1,14 @@
 package menu.util;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TestListGenerator implements ListGenerator{
     @Override
     public List<String> getShuffledList(List<String> list) {
+        List<String> temp = new ArrayList<>(list);
         list.remove(0);
-        return list;
+        return temp;
     }
 }
