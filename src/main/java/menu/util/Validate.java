1c6d2fad001b62b538595af38c23582fb12716f3
thdwoqor <celebarte01@gmail.com>
2022-12-17 15:53:00 +0900

    feat: 못먹는 음식 예외 추가

--- src/main/java/menu/util/Validate.java
+++ src/main/java/menu/util/Validate.java
@@ -18,9 +18,16 @@ public class Validate {
         }
     }
 
+    public static void cantEatLength(List<String> names, int minInclusive, int maxInclusive) {
+        if (names.size() < minInclusive || names.size() > maxInclusive) {
+            throw new IllegalArgumentException(Message.ERROR_DIFFERENT_CANT_EAT_LENGTHS.message);
+        }
+    }
+
     private enum Message {
         ERROR_DIFFERENT_NAME_LENGTHS("[ERROR] 코치의 이름은 최소 2글자, 최대 4글자이다."),
-        ERROR_DIFFERENT_COACH_LENGTHS("[ERROR] 코치는 최소 2명, 최대 5명까지 식사를 함께 한다.");
+        ERROR_DIFFERENT_COACH_LENGTHS("[ERROR] 코치는 최소 2명, 최대 5명까지 식사를 함께 한다."),
+        ERROR_DIFFERENT_CANT_EAT_LENGTHS("[ERROR] 각 코치는 최소 0개, 최대 2개의 못 먹는 메뉴가 있다.");
 
         private final String message;
 
