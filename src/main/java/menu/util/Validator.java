c94e7b6e66bb6511c2a9691d68498ed0875602a7
MINJU <alswn9938@gmail.com>
2022-12-17 15:18:21 +0900

    refactor : 입력된 코치 수 에러 문구 수정

--- src/main/java/menu/util/Validator.java
+++ src/main/java/menu/util/Validator.java
@@ -7,7 +7,7 @@ public class Validator {
     public static void validateCoachNamesSize(List<String> names) {
         if(names.size() < 2 || names.size () > 5){
             throw new IllegalArgumentException(
-                    String.format("코치는 최소 %d명, 최대 %d명까지 식사를 함께할 수 있습니다.",2,5));
+                    String.format("코치는 최소 %d명, 최대 %d명을 입력해야합니다.",2,5));
         }
     }
 
