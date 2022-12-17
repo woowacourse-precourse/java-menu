d39f016528d0d6a4a7145980c54a5cb4d257215f
wnsgml7267 <dlwns7267@naver.com>
2022-12-17 16:21:43 +0900

    feat(controller/MenuController): 카테고리 무작위 생성 기능 구현

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -4,6 +4,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import menu.view.InputView;
 import menu.view.OutputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuController {
     public void run() {
         start();
@@ -16,19 +19,33 @@ public class MenuController {
         if (coach.length < 2 || coach.length > 5){
             throw new IllegalArgumentException("코치 인원을 재설정 해주세요(2~5명 사이)");
             //OutputView.printError("코치 인원을 재설정 해주세요(2~5명 사이)");
+            //start();
         }
-        start();
+        RandomNum();
         System.out.println(coach[0]);
     }
     //카테고리
-//    public void RandomNum(){
-//        int[] randomNum;
-//        while(randomNum.size() < 5) {
-//            int num = Randoms.pickNumberInRange(1, 5); // 1~9까지
-//            if (!randomNum.contains(num)) {
-//                randomNum.add(num);
-//                setNum.add(num);
-//            }
-//        }
-//    }
+    public void RandomNum(){
+        List<Integer> randomNum = new ArrayList<>();
+        for(int i = 0; i < 5; i++) {
+            int num = Randoms.pickNumberInRange(1, 5);
+            randomNum.add(num);
+        }
+        int[] index = new int[6];
+        int max = 0;
+        for (int i = 0; i < 5; i++) {
+            index[randomNum.get(i)]++;
+        }
+        for (int i = 0; i < 5; i++) {
+            if (index[i] > max) {
+                max = index[i];
+            }
+        }
+        if (max >= 3) {
+            RandomNum();
+        }
+        System.out.println(randomNum);
+        System.out.println(max);
+
+    }
 }
