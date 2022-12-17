03bc8b4079f68e2d46a598413f0b300cf77eb0bf
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 13:35:42 +0900

    feat(InputView): 코치 이름 입력 검증 연결 기능 추가
    docs: 코치 이름 입력 검증 연결 완료 체크

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,12 +1,15 @@
 package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import menu.validation.InputValidation;
+
+import java.util.List;
 
 public class InputView {
     private static final String INPUT_COACHES_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
 
-    public String readCoachesName() {
+    public List<String> readCoachesName() {
         System.out.println(INPUT_COACHES_NAME);
-        return Console.readLine();
+        return InputValidation.validateCoachesName(Console.readLine());
     }
 }
