280537f75ddd0855744fd894a65be204125a9c35
java-saeng <gusdn85554@naver.com>
2022-12-17 15:13:24 +0900

    feat : converting 하기 위해 카테고리 번호에 따라 카테고리 맵핑 상수화

---
+++ src/main/java/menu/Constants.java
@@ -0,0 +1,12 @@
+package menu;
+
+import java.util.Map;
+
+public class Constants {
+
+    public static final Map<Integer, String> MENU_NAME = Map.of(1, "일식",
+                                                                2, "한식",
+                                                                3, "중식",
+                                                                4, "아시안",
+                                                                5, "양식");
+}
