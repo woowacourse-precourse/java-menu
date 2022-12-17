489db8033091a6cc7bcdbdcc3ec0c51a5871cb4e
dooboocookie <bkh3030@gmail.com>
2022-12-17 13:43:17 +0900

    feat: 메뉴, 추천메뉴, 못먹는 메뉴 추가하여 코치 생성자 작성

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,4 +1,23 @@
 package menu.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Coach {
+
+    private final String name;
+    private final UnavailableMenu unavailableMenu;
+    private final RecommendMenu recommendMenu;
+
+    private Coach(String name) {
+        this.name = name;
+        this.recommendMenu = new RecommendMenu();
+        this.unavailableMenu = new UnavailableMenu();
+    }
+
+    public static Coach createCoach(String name) {
+        return new Coach(name);
+    }
+
+    
 }
