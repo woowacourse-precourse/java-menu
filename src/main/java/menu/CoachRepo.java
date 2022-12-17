18e5402911e4c042f421e1db26d4e84268d4cd53
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-12-17 13:38:10 +0900

    Feat : 코치 객체들을 담고 있는 Repo 생성

---
+++ src/main/java/menu/CoachRepo.java
@@ -0,0 +1,35 @@
+package menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoachRepo {
+    List<Coach> coachList = new ArrayList<>();
+
+    public void setCoachList(String scanCoachName) {
+        String coachNameList[] = scanCoachName.split(",");
+        for (int i=0; i<coachNameList.length; i++){
+            Coach coach = new Coach(coachNameList[i]);
+            coachList.add(coach);
+        }
+
+    }
+
+    public void setCoachCannotEatMenu(String coachName, String scanCannotEatMenu) {
+        String cannotEatMenuList[] = scanCannotEatMenu.split(",");
+        Coach targetCoach = getTargetCoach(coachName);
+        for (int i=0; i<cannotEatMenuList.length; i++){
+            targetCoach.cannotEatMenuList.add(cannotEatMenuList[i]);
+        }
+
+    }
+
+    private Coach getTargetCoach(String coachName) {
+        for (Coach coach : coachList){
+            if (coach.name == coachName){
+                return coach;
+            }
+        }
+        return new Coach("TEST");
+    }
+}
