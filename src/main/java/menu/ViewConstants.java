e4ef80810d192d210b900c1db050a991c98c9062
kevstevie <jianna442@gmail.com>
2022-12-17 13:23:21 +0900

    feat: 출력 상수 추가

---
+++ src/main/java/menu/ViewConstants.java
@@ -0,0 +1,12 @@
+package menu;
+
+public class ViewConstants {
+
+    private ViewConstants() {}
+
+    public static final String INIT = "점심 메뉴 추천을 시작합니다.";
+    public static final String INPUT_COACH = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    public static final String INPUT_HATE = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
+    public static final String PRINT_RESULT = "메뉴 추천 결과입니다.";
+    public static final String END = "추천을 완료했습니다.";
+}
