bfeac258c9dd9f721fac69ca0b1c12dd4c7d72c7
jaeyeon kim <jakind@naver.com>
2022-12-17 16:37:09 +0900

    feat(MainController): 추천을 실행할 수 있는 메소드 추가

--- src/main/java/menu/MainController.java
+++ src/main/java/menu/MainController.java
@@ -1,4 +1,43 @@
 package menu;
 
+import java.util.function.Supplier;
+
 public class MainController {
+    private final RecommendRepository recommendRepository;
+    private final CoachRepository coachRepository;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public MainController() {
+        this.recommendRepository = new RecommendRepository();
+        this.coachRepository = new CoachRepository();
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void start() {
+        outputView.printStart();
+        outputView.printCoachNameRequest();
+        coachRepository.generateCoaches(repeat(inputView::inputNameOfCoaches));
+
+        for (Coach coach : coachRepository.getCoaches()) {
+            coach.setHateFood(repeat(() -> inputView.inputHateFood(coach)));
+        }
+
+        recommendRepository.recommendStart(coachRepository);
+        outputView.printResult();
+        outputView.printWeek();
+        outputView.printCategory(recommendRepository);
+        outputView.printCoach(coachRepository);
+        outputView.printEnd();
+    }
+
+    private <T> T repeat(Supplier<T> reader) {
+        try {
+            return reader.get();
+        } catch(IllegalArgumentException exception) {
+            outputView.printError(exception);
+            return repeat(reader);
+        }
+    }
 }
