a252cfc78e751e5fe46d86a69c3fe8dee38dd088
CAEL0 <kjongmin26@gmail.com>
2022-12-17 13:53:46 +0900

    feat(MenuConstants): 점심 메뉴 추천 프로그램에서 사용되는 상수 추가

---
+++ src/main/java/menu/constant/MenuConstants.java
@@ -0,0 +1,13 @@
+package menu.constant;
+
+public class MenuConstants {
+    public static final int MINIMUM_NUMBER_OF_COACHES = 2;
+    public static final int MAXIMUM_NUMBER_OF_COACHES = 5;
+
+    public static final int MINIMUM_COACH_NAME_LENGTH = 2;
+    public static final int MAXIMUM_COACH_NAME_LENGTH = 4;
+
+    public static final int MAXIMUM_NUMBER_OF_INEDIBLE_MENU = 2;
+
+    public static final String DELIMITER = ",";
+}
