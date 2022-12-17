2102f5441582991cd4b9bb9fd10b518b8dd4925d
HaODay <ycis5013@naver.com>
2022-12-17 17:35:27 +0900

    feat(ResultMessage): 추천 결과 메시지 관련 열거형 생성

---
+++ src/main/java/menu/util/message/ResultMessage.java
@@ -0,0 +1,9 @@
+package menu.util.message;
+
+public class ResultMessage {
+    public static final String RESULT_TITLE = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]";
+    public static final String RESULT_CATEGORY = "카테고리";
+    public static final String START_SYMBOL = "[ ";
+    public static final String DELIMITER = " | ";
+    public static final String END_SYMBOL = " ]";
+}
