bcb107a5b5c99d3c308ded96f835aa95618f7326
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-12-17 13:59:56 +0900

    feat : 서비스 종료 문구 출력 기능 추가

--- src/main/java/menu/view/Message.java
+++ src/main/java/menu/view/Message.java
@@ -1,9 +1,17 @@
 package menu.view;
 
+import java.util.List;
+
+import menu.model.Coach;
+
 public enum Message {
 	SERVICE_START("점심 메뉴 추천을 시작합니다."),
 	INPUT_COACH_NAMES("코치의 이름을 입력해 주세요. (, 로 구분)"),
-	INPUT_NOT_EAT_FOOD("(이)가 못 먹는 메뉴를 입력해 주세요.");
+	INPUT_NOT_EAT_FOOD("(이)가 못 먹는 메뉴를 입력해 주세요."),
+	PRINT_RECOMMEND_RESULT("메뉴 추천 결과입니다."),
+	PRINT_DAYS("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]"),
+	SERVICE_FINISH("추천을 완료했습니다."),
+	;
 
 	private final String message;
 
@@ -19,4 +27,13 @@ public enum Message {
 		System.out.println(name + INPUT_NOT_EAT_FOOD);
 	}
 
+	public static void printFinalMessage(List<Coach> coachList) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(PRINT_RECOMMEND_RESULT).append('\n');
+		sb.append(PRINT_DAYS).append('\n');
+		for (Coach coach : coachList) {
+			sb.append(coach).append('\n');
+		}
+		sb.append('\n').append(SERVICE_FINISH);
+	}
 }
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,9 +1,18 @@
 package menu.view;
 
+import java.util.List;
+
+import menu.model.Coach;
+
 public class OutputView {
 
 	public void printServiceStart() {
 		Message.printMessage(Message.SERVICE_START);
+	}
+
+	// 메뉴 추천 결과 출력
+	public void recommendMenuPrint(List<Coach> list) {
+		Message.printFinalMessage(list);
 	}
 
 }
