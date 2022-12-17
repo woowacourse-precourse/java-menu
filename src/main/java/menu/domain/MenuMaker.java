886e444999ba6982f455670941d1a2e4f5f5bfd1
alstjr706 <alstjr706@naver.com>
2022-12-17 15:23:18 +0900

    fix : (점심 메뉴 추천) 메뉴 리스트 입력 받는 기능 수정

--- src/main/java/menu/domain/MenuMaker.java
+++ src/main/java/menu/domain/MenuMaker.java
@@ -1,9 +1,18 @@
 package menu.domain;
 
+import menu.view.InputView;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MenuMaker {
+
+    private final InputView inputView;
+
+    public MenuMaker(InputView inputView) {
+        this.inputView = inputView;
+    }
 
     public List<String> nameDivision(String coach) {
         List<String> coaches = new ArrayList<>();
@@ -13,11 +22,15 @@ public class MenuMaker {
         return coaches;
     }
 
-    public List<String> menuDivision(String menu) {
-        List<String> menuContainer = new ArrayList<>();
-        for (String food : menu.split(",")) {
-            menuContainer.add(food);
+    public HashMap<String, String> menuDivision(List<String> coachNames) {
+        HashMap<String, String> coachMenu = new HashMap<>();
+
+        for (String coach : coachNames) {
+            System.out.println(coach + "(이)가 못 먹는 메뉴를 입력해 주세요.");
+            System.out.println();
+            String menu = inputView.menuInput();
+            coachMenu.put(coach, menu);
         }
-        return menuContainer;
+        return coachMenu;
     }
 }
