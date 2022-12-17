4d9d77523dad9303035a6eadba10320e191875f0
nuyh <hjaehyun25@gmail.com>
2022-12-17 15:21:14 +0900

    refactor: 추상 카테고리의 menus 불변 객체화

--- src/main/java/menu/domain/AbstractCategory.java
+++ src/main/java/menu/domain/AbstractCategory.java
@@ -1,10 +1,9 @@
 package menu.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class AbstractCategory {
-    public List<String> menus;
+    public final List<String> menus;
 
     public AbstractCategory(List<String> menus) {
         this.menus = menus;
