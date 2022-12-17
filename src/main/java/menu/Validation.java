e091e32ddab9d1fa93dbd12463a9ec31563a50fb
yejin <kwonyj1022@naver.com>
2022-12-17 14:09:55 +0900

    feat: 입력 구현 및 검증 완료

---
+++ src/main/java/menu/Validation.java
@@ -0,0 +1,19 @@
+package menu;
+
+import java.util.List;
+
+public class Validation {
+    public void validateCoachesName(List<String> coachesName) {
+        for (int i = 0; i < coachesName.size(); i++) {
+            if ((coachesName.size() < 2) || (coachesName.size() > 4)) {
+                throw new IllegalArgumentException("[ERROR] 코치의 이름은 최소 2글자, 최대 4글자입니다.");
+            }
+        }
+    }
+
+    public void validateHateFood(List<String> hateFood) {
+        if (hateFood.size() < 0 || hateFood.size() > 2) {
+            throw new IllegalArgumentException("[ERROR] 못 먹는 메뉴는 최소 0개, 최대 2개입니다.");
+        }
+    }
+}
---
+++ src/main/java/menu/View/InputView.java
@@ -0,0 +1,33 @@
+package menu.View;
+
+import camp.nextstep.edu.missionutils.Console;
+import menu.Validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputView {
+    public List<String> getCoachesName() {
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분");
+        String coachesNameStr = Console.readLine();
+        String[] coachesNameArr = coachesNameStr.split(",");
+        List<String> coachesName = new ArrayList<>();
+        for (int i = 0; i < coachesNameArr.length; i++) {
+            coachesName.add(coachesNameArr[i]);
+        }
+        new Validation().validateCoachesName(coachesName);
+        return coachesName;
+    }
+
+    public List<String> getHateFood(String coachName) {
+        System.out.println(coachName + "(이)가 못 먹는 메뉴를 입력해 주세요.");
+        String hateFoodStr = Console.readLine();
+        String[] hateFoodArr = hateFoodStr.split(",");
+        List<String> hateFood = new ArrayList<>();
+        for (int i = 0; i < hateFoodArr.length; i++) {
+            hateFood.add(hateFoodArr[i]);
+        }
+        new Validation().validateHateFood(hateFood);
+        return hateFood;
+    }
+}
