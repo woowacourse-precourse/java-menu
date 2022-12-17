37e3ace685a133c029df81eb7f53580ff336a774
yenawee <yaena0319@naver.com>
2022-12-17 14:31:25 +0900

    fix : 못먹는 메뉴 인풋 안들어올 시 예외처리

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -6,7 +6,7 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         List<String> coachName = inputView.readCoachName();
-
+        List<Coach> coach = inputView.readDislikeFood(coachName);
 
 
     }
--- src/main/java/menu/InputView.java
+++ src/main/java/menu/InputView.java
@@ -2,6 +2,7 @@ package menu;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,16 +18,22 @@ public class InputView {
         }
     }
 
-    public void readDislikeFood(List<String> coachName){
+    public List<Coach> readDislikeFood(List<String> coachName){
+        List<Coach> coach = new ArrayList<>();
         for (String name : coachName){
             while (true) {
                 System.out.println(name + "(이)가 못 먹는 메뉴를 입력해 주세요.");
                 String input = Console.readLine();
+                if (input.length() == 0){
+                    coach.add(new Coach(name, null));
+                    break ;
+                }
                 if (validate.isValidDislikeFood(input)){
-
+                    coach.add(new Coach(name, Arrays.asList(input.split(","))));
                     break ;
                 }
             }
         }
+        return coach;
     }
 }
