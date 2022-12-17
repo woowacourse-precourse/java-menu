a53e8a9de05c4360f29cd10860cf61517fb081fd
CAEL0 <kjongmin26@gmail.com>
2022-12-17 15:09:34 +0900

    feat(MenuConstants): 카테고리 당 최대 횟수 추가

--- src/main/java/menu/constant/MenuConstants.java
+++ src/main/java/menu/constant/MenuConstants.java
@@ -9,6 +9,8 @@ public class MenuConstants {
 
     public static final int MAXIMUM_NUMBER_OF_INEDIBLE_MENUS = 2;
 
+    public static final int MAXIMUM_NUMBER_OF_CATEGORIES = 2;
+
     public static final int NUMBER_OF_DAYS = 5;
 
     public static final int FIRST_CATEGORY = 1;
