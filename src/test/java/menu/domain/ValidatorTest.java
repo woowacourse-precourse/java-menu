35ad868a04ca899aa69760697fb36e4a34c8d2f9
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 13:45:45 +0900

    test: 코치의 이름 길이를 확인하는 함수 테스트

---
+++ src/test/java/menu/domain/ValidatorTest.java
@@ -0,0 +1,21 @@
+package menu.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ValidatorTest {
+    @Test
+    @DisplayName("코치의 이름의 길이를 확인하는 함수 테스트")
+    void checkCoachesNameTest() {
+        List<String> coaches_1 = List.of("tom","b");
+        assertThatThrownBy(() -> Validator.validateCoachName(coaches_1)).isInstanceOf(IllegalArgumentException.class);
+
+        List<String> coaches_2 = List.of("tom","bellee");
+        assertThatThrownBy(() -> Validator.validateCoachName(coaches_2)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
