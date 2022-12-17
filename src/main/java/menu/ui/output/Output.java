5a0429ec1d6c73cca2debe062fe9b3da3f9ac3ce
KSH <tkdgns543@gmail.com>
2022-12-17 15:51:03 +0900

    feat(ui): 시작 안내문과 마무리 안내문 출력 구현

--- src/main/java/menu/ui/output/Output.java
+++ src/main/java/menu/ui/output/Output.java
@@ -1,4 +1,12 @@
 package menu.ui.output;
 
 public class Output {
+
+    public void notifyInitializing() {
+        System.out.println("점심 메뉴 추천을 시작합니다.");
+    }
+
+    public void notifyEnding() {
+        System.out.println("추천을 완료했습니다.");
+    }
 }
