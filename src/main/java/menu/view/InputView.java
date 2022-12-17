9e3cbf79f230f12fe33012dee803529dc17a6f24
minaver <psmjoshua@gmail.com>
2022-12-17 14:44:47 +0900

    feat(InputView): 추천 시작 메시지 & 코치 이름 입력 기능 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,4 +1,29 @@
 package menu.view;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class InputView {
+
+    public List<String> getCoachName() {
+        System.out.println("점심 메뉴 추천을 시작합니다. \n\n 코치의 이름을 입력해 주세요. (, 로 구분)");
+        String input = Console.readLine();
+        return trimEachInput(Arrays.asList(input.split(",")));
+    }
+
+    
+
+    // 입력값 각각을 trim
+    private List<String> trimEachInput(List<String> inputs) {
+        List<String> returnWords = new ArrayList<>();
+        for(String word : inputs) {
+            returnWords.add(word.trim());
+        }
+        return returnWords;
+    }
+
 }
