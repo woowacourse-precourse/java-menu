4dc9953d56552447ab4bfd9fbc0a264d1bf8f68e
JUN <chltmdwns96@naver.com>
2022-12-17 16:10:57 +0900

    fix : 카테고리 무한 추천 오류 수정

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,5 +1,20 @@
 package menu.domain;
 
+import java.util.Arrays;
+
 public enum Category {
-    KOREAN, JAPANESE, CHINESE, ASIAN, WESTERN
+    JAPANESE(1), KOREAN(2), CHINESE(3), ASIAN(4), WESTERN(5);
+
+    private int id;
+
+    Category(int id) {
+        this.id = id;
+    }
+
+    public static Category findById(int id) {
+        return Arrays.stream(values())
+                .filter(category -> category.id == id)
+                .findAny()
+                .get();
+    }
 }
--- src/main/java/menu/service/CategoryService.java
+++ src/main/java/menu/service/CategoryService.java
@@ -20,38 +20,26 @@ public class CategoryService {
 
     public List<Category> getFiveCategories() {
         List<Category> categories = new ArrayList<>();
-        while(categories.size() != CATEGORY_SIZE) {
-            Category category = pickCategory();
-            System.out.println("category.name() = " + category.name());
+        List<Integer> categoryIds = new ArrayList<>();
 
-            categories.add(category);
+        while (categories.size() != CATEGORY_SIZE) {
+            int categoryId = pickCategory();
+            if (getFrequency(categoryIds, categoryId) != 2) {
+                categoryIds.add(categoryId);
+                categories.add(Category.findById(categoryId));
+            }
         }
-        categories.forEach(category -> System.out.println("category.name() = " + category.name()));
         categoryRepository.addRecommendedCategories(categories);
         return categories;
     }
 
-    private int getFrequency(List<Category> categories, Category category) {
-        List<String> categoryNames = new ArrayList<>();
-        categories.forEach(c -> categoryNames.add(c.name()));
-
-        return Collections.frequency(categoryNames, category.name());
+    private int getFrequency(List<Integer> categoryIds, Integer categoryId) {
+        return Collections.frequency(categoryIds, categoryId);
     }
 
-    private Category pickCategory() {
+    private int pickCategory() {
         int categoryNumber = Randoms.pickNumberInRange(1, CATEGORY_SIZE);
 
-        if (categoryNumber == 1) {
-            return JAPANESE;
-        } else if (categoryNumber == 2) {
-            return KOREAN;
-        } else if (categoryNumber == 3) {
-            return CHINESE;
-        } else if (categoryNumber == 4) {
-            return ASIAN;
-        } else if (categoryNumber == 5) {
-            return WESTERN;
-        }
-        return null;
+        return categoryNumber;
     }
 }
