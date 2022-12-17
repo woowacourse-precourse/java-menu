f3b7cd20fa548ba952412c64d39e4bedf86765bf
yenawee <yaena0319@naver.com>
2022-12-17 13:46:56 +0900

    feat: 코치 객체

---
+++ src/main/java/menu/Coach.java
@@ -0,0 +1,15 @@
+package menu;
+
+import java.util.List;
+
+public class Coach {
+    private String name;
+    private List<String> dislikeFood;
+
+    Coach(String name, List<String> dislikeFood){
+        this.name = name;
+        this.dislikeFood = dislikeFood;
+    }
+
+
+}
