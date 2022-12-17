49de9fd656f2aa07ec7a368945b2e3f58d8f1cd8
java-saeng <gusdn85554@naver.com>
2022-12-17 14:35:46 +0900

    feat : 코치 인원 입력 예외 추가

--- src/main/java/menu/validator/InputValidator.java
+++ src/main/java/menu/validator/InputValidator.java
@@ -8,4 +8,11 @@ public class InputValidator {
             throw new IllegalArgumentException("[ERROR] 코치 이름은 최소 2글자 이상, 4글자 이하 입력해야 합니다.");
         }
     }
+
+    public static void isLimit(final String input) {
+
+        if (input.length() < 2 || input.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 코치는 최소 2명 이상, 5명 이하 입력해야 합니다.");
+        }
+    }
 }
