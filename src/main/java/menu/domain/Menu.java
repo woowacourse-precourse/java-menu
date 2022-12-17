c62fba705986bc532af975aa04de6b4ac01b46f8
CAEL0 <kjongmin26@gmail.com>
2022-12-17 17:09:03 +0900

    refactor(Menu): 검증 부분 추출 및 검증 메서드 대체

--- src/main/java/menu/domain/Menu.java
+++ src/main/java/menu/domain/Menu.java
@@ -1,6 +1,7 @@
 package menu.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import menu.constant.ExceptionConstants;
 import menu.constant.MenuConstants;
 import menu.util.Validator;
 
@@ -15,15 +16,34 @@ public class Menu {
     private final List<String> previousCategories = new ArrayList<>();
 
     public void addCoachNames(String coachNames) {
-        List<String> splittedCoaches = List.of(coachNames.split(MenuConstants.INPUT_DELIMITER));
+        List<String> splittedCoachNames = List.of(coachNames.split(MenuConstants.INPUT_DELIMITER));
+        validateCoachNames(splittedCoachNames);
+        this.coachNames.addAll(splittedCoachNames);
+    }
 
-        Validator.validateNumberOfCoaches(splittedCoaches.size());
-        Validator.validateDuplication(splittedCoaches);
-        splittedCoaches.stream()
+    private void validateCoachNames(List<String> splittedCoachNames) {
+        Validator.validateLowerBound(splittedCoachNames.size(),
+                MenuConstants.MINIMUM_NUMBER_OF_COACHES,
+                ExceptionConstants.NOT_ENOUGH_COACH);
+
+        Validator.validateUpperBound(splittedCoachNames.size(),
+                MenuConstants.MAXIMUM_NUMBER_OF_COACHES,
+                ExceptionConstants.TOO_MANY_COACHES);
+
+        Validator.validateDuplication(splittedCoachNames);
+        splittedCoachNames.stream()
                 .map(String::length)
-                .forEach(Validator::validateCoachNameLength);
+                .forEach(this::validateCoachNameLength);
+    }
 
-        this.coachNames.addAll(splittedCoaches);
+    private void validateCoachNameLength(int coachNameLength) {
+        Validator.validateLowerBound(coachNameLength,
+                MenuConstants.MINIMUM_COACH_NAME_LENGTH,
+                ExceptionConstants.TOO_SHORT_NAME);
+
+        Validator.validateUpperBound(coachNameLength,
+                MenuConstants.MAXIMUM_COACH_NAME_LENGTH,
+                ExceptionConstants.TOO_LONG_NAME);
     }
 
     public void addCoach(String name, String inedibleMenu) {
@@ -31,7 +51,7 @@ public class Menu {
     }
 
     public void recommendForWeek() {
-        for (int i = 0 ; i < MenuConstants.NUMBER_OF_DAYS; i++) {
+        for (int i = 0; i < MenuConstants.NUMBER_OF_DAYS; i++) {
             recommendForDay();
         }
     }
