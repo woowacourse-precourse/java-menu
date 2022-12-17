191b92537fd6519d1b1f9b1e571ab86cd3eca471
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 14:15:32 +0900

    refactor: 변수 이름을 hs -> coachSet 으로 수정

--- src/main/java/menu/domain/MenuRecommand.java
+++ src/main/java/menu/domain/MenuRecommand.java
@@ -7,12 +7,13 @@ import java.util.List;
 import java.util.Map;
 
 public class MenuRecommand {
-    private HashMap<String,List<String>> hs = new HashMap<>();
+    private HashMap<String,List<String>> coachSet = new HashMap<>();
     public MenuRecommand(){}
 
     public void play() {
         List<String> coaches = InputView.inputCoaches();
         for (int i = 0; i < coaches.size(); i++)
-            hs.put(coaches.get(i),InputView.inputInEdibleMenu(coaches.get(i)));
+            coachSet.put(coaches.get(i),InputView.inputInEdibleMenu(coaches.get(i)));
+
     }
 }
