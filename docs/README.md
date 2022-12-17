da89e92c26e80d30283ef9c9858e71b15c324837
donghae <donghae1999@gmail.com>
2022-12-17 13:36:30 +0900

    feat: 코치 이름 입력 개수 검증 추가

--- docs/README.md
+++ docs/README.md
@@ -3,8 +3,8 @@
 ### 기능 요구 사항
 
 - [X] 코치 이름 입력 InputView#coachNameInput
-  - [ ] 최소 2글자, 최대 4글자 InputView#validateName
-  - [ ] 최소 2명 최대 5명
+  - [X] 최소 2글자, 최대 4글자 InputView#validateName
+  - [X] 최소 2명 최대 5명
 - [X] 코치 먹지 못하는 메뉴 입력 InputView#coachCantEatInput
   - [ ] 최소 0개 최대 2개 (,로 구분)
 - [ ] 카테고리 추천
--- src/main/java/menu/InputView.java
+++ src/main/java/menu/InputView.java
@@ -9,6 +9,8 @@ public class InputView {
     public List<String> coachNameInput(){
         List<String> names = new ArrayList<>();
         String[] name = Console.readLine().split(",");
+        if(name.length<2 || name.length>5)
+            throw new IllegalArgumentException("[ERROR] 최소2명 최대 5명 입력");
         for(int i=0; i<name.length;i++){
             if(validateName(name[i])) {
                 names.add(name[i]);
@@ -18,7 +20,7 @@ public class InputView {
     }
     public boolean validateName(String name){
         if(name.length()<2 || name.length()>5){
-            return false;
+            throw new IllegalArgumentException("[ERROR] 최소 2글자, 최대 4글자 입력");
         }
         return true;
     }
