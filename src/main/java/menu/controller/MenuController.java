f36d3b672e2b6ebb515eb1f79690d06da8d7a697
khsrla9806 <kimhunsope@naver.com>
2022-12-17 14:01:32 +0900

    feat(MenuController): 카테고리 랜덤으로 뽑는 기능 구현

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -6,6 +6,8 @@ import menu.view.OutputView;
 import java.util.List;
 import java.util.Map;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class MenuController {
     private List<String> coachName;
     private Map<String, List<String>> coachDislikeFood;
@@ -25,5 +27,11 @@ public class MenuController {
 
     private void setCoachDislikeFood() {
         coachDislikeFood = inputView.readCoachDislikeFood(coachName);
+    }
+
+    public String pickTodayRandomCategory() {
+        List<String> categories = List.of("일식", "한식", "중식", "아시안", "양식");
+        String category = categories.get(Randoms.pickNumberInRange(0, 4));
+        return category;
     }
 }
