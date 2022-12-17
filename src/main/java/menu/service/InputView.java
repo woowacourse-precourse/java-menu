05dafbe66692a3f6fc2b31f78cd3d7491bd2e066
박시균\psk98 <psg980331@naver.com>
2022-12-17 17:01:33 +0900

    Feat : 코치 이름과 코치가 못 먹는 메뉴를 입력받는 기능

---
+++ src/main/java/menu/service/InputView.java
@@ -0,0 +1,14 @@
+package menu.service;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String inputCoachNameWithComma(){
+        return Console.readLine();
+    }
+    public String inputCoachCanNotEatMenu(){
+        return Console.readLine();
+    }
+
+}
