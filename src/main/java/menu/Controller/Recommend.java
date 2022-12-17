b0a6bf6af5486f141255026ff3d5a46ee8526717
Eun-chan Cho <eunchan1001@gmail.com>
2022-12-17 15:28:48 +0900

    feat(Recommend): implement recommend class

---
+++ src/main/java/menu/Controller/Recommend.java
@@ -0,0 +1,56 @@
+package menu.Controller;
+
+import menu.Model.Category;
+import menu.Model.Coachs;
+import menu.Model.Coach;
+
+import java.util.Map;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Recommend {
+    private final int WEEK_NUMBER = 5;
+    private final int START_CATEGORY_RANK = 1;
+    private final int END_CATEGORY_RANK = 5;
+    private Map<Category, Integer> categoryCounts;
+
+    public Recommend() {
+        for (Category cg : Category.values()) {
+            categoryCounts.put(cg, 0);
+        }
+    }
+
+    public void makeRecommendations(Coachs coachs) {
+        for (int i = 0; i < WEEK_NUMBER; i++) {
+            Category cg = pickCategory();
+            chooseMenu(coachs, cg.getMenus());
+        }
+    }
+
+    private void chooseMenu(Coachs coachs, List<String> menus) {
+        for (Coach coach : coachs.getCoachs()) {
+            setMenuUntilValid(coach, menus);
+        }
+    }
+
+    private void setMenuUntilValid(Coach coach, List<String> menus) {
+        while (true) {
+            String menu = Randoms.shuffle(menus).get(0);
+            if (coach.setRecommend(menu))
+                return;
+        }
+    }
+
+    private Category pickCategory() {
+        while (true) {
+            int rank = Randoms.pickNumberInRange(START_CATEGORY_RANK, END_CATEGORY_RANK);
+            Category cg = Category.getCategoryFromNumber(rank);
+            int count = categoryCounts.get(cg);
+            if (!(count > 1)) {
+                categoryCounts.put(cg, count + 1);
+                return cg;
+            }
+        }
+    }
+}
