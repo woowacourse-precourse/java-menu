40cd28ff59794efa69ecc79f89576bca07eb30c6
hong-sile <gurwns9325@naver.com>
2022-12-17 13:46:23 +0900

    feat: 카테고리 필드에 번호를 추가

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -3,23 +3,25 @@ package menu.domain;
 import java.util.Arrays;
 
 public enum Category {
-    KOREA("한식"),
-    CHINESE("중식"),
-    ASIAN("아시안"),
-    JAPANESE("일식"),
-    WEST("양식");
+    JAPANESE("일식", 1),
+    KOREA("한식", 2),
+    CHINESE("중식", 3),
+    ASIAN("아시안", 4),
+    WEST("양식", 5);
 
     private static final String INVALID_CATEGORY_VALUE_MESSAGE = "유효하지 않은 값입니다.";
     private final String name;
+    private final int code;
 
-    Category(String name) {
+    Category(String name, int code) {
         this.name = name;
+        this.code = code;
     }
 
     //TODO: val의 네이밍 꼭 변경하기
-    public Category from(String name) {
+    public Category from(int code) {
         return Arrays.stream(values())
-                .filter(category -> category.name == name)
+                .filter(category -> category.code == code)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(INVALID_CATEGORY_VALUE_MESSAGE));
     }
