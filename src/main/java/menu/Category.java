23c92aad1044cf2cf360f18c464d684d0da199d6
cpot5620 <=cpot5620@naver.com>
2022-12-17 14:45:46 +0900

    refactor(model): modify variables in enum classes

--- src/main/java/menu/Category.java
+++ src/main/java/menu/Category.java
@@ -1,16 +1,25 @@
 package menu;
 
 public enum Category {
-    JAPANESE("일식", "규동,우동,미소시루,스시,가츠동,오니기리,하이라이스,라멘,오코노미야끼"),
-    KOREAN("한식", "김밥,김치찌개,쌈밥,된장찌개,비빔밥,칼국수,불고기,떡볶이,제육볶음"),
-    CHINESE("중식", "깐풍기,볶음면,동파육,짜장면,짬뽕,마파두부,탕수육,토마토 달걀볶음,고추잡채"),
-    ASIAN("아시안", "팟타이,카오 팟,나시고렝,파인애플 볶음밥,쌀국수,똠얌꿍,반미,월남쌈,분짜"),
-    WESTERN("양식", "라자냐,그라탱,뇨끼,끼슈,프렌치토스트,바게트,스파게티,피자,파니니");
+    JAPANESE("일식", Menu.JAPANESE),
+    KOREAN("한식", Menu.KOREAN),
+    CHINESE("중식", Menu.CHINESE),
+    ASIAN("아시안", Menu.ASIAN),
+    WESTERN("양식", Menu.WESTERN);
 
     private final String name;
+    private final Menu menu;
 
-    Category(String name, String menus) {
+    Category(String name, Menu menu) {
         this.name = name;
+        this.menu = menu;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public Menu getMenu(){
+        return menu;
+    }
 }
