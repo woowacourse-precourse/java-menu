75aa5ef9fd68d9307120fc381690f7f13fbd414a
CAEL0 <kjongmin26@gmail.com>
2022-12-17 17:11:09 +0900

    refactor(Validator): 대체된 메서드 삭제

--- src/main/java/menu/util/Validator.java
+++ src/main/java/menu/util/Validator.java
@@ -1,7 +1,6 @@
 package menu.util;
 
 import menu.constant.ExceptionConstants;
-import menu.constant.MenuConstants;
 
 import java.util.List;
 
@@ -16,30 +15,6 @@ public class Validator {
     public static void validateUpperBound(int number, int upperBound, ExceptionConstants exception) {
         if (number > upperBound) {
             throw new IllegalArgumentException(exception.getMessage());
-        }
-    }
-
-    public static void validateNumberOfCoaches(int numberOfCoaches) {
-        if (numberOfCoaches < MenuConstants.MINIMUM_NUMBER_OF_COACHES) {
-            throw new IllegalArgumentException(ExceptionConstants.NOT_ENOUGH_COACH.getMessage());
-        }
-        if (MenuConstants.MAXIMUM_NUMBER_OF_COACHES < numberOfCoaches) {
-            throw new IllegalArgumentException(ExceptionConstants.TOO_MANY_COACHES.getMessage());
-        }
-    }
-
-    public static void validateCoachNameLength(int coachNameLength) {
-        if (coachNameLength < MenuConstants.MINIMUM_COACH_NAME_LENGTH) {
-            throw new IllegalArgumentException(ExceptionConstants.TOO_SHORT_NAME.getMessage());
-        }
-        if (MenuConstants.MAXIMUM_COACH_NAME_LENGTH < coachNameLength) {
-            throw new IllegalArgumentException(ExceptionConstants.TOO_LONG_NAME.getMessage());
-        }
-    }
-
-    public static void validateNumberOfInedibleMenus(int numberOfInedibleMenus) {
-        if (MenuConstants.MAXIMUM_NUMBER_OF_INEDIBLE_MENUS < numberOfInedibleMenus) {
-            throw new IllegalArgumentException(ExceptionConstants.TOO_MANY_MENUS.getMessage());
         }
     }
 
