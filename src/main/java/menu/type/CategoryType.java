029e2a8cec9bb8bb935a1fb9bead08c2b466bbde
iamjooon2 <iamjooon2@gmail.com>
2022-12-17 14:55:12 +0900

    chore: 메뉴 카테고리 enum인 CategoryType 생성 및 적용

---
+++ src/main/java/menu/type/CategoryType.java
@@ -0,0 +1,25 @@
+package menu.type;
+
+public enum CategoryType {
+    JAPAN("일식", 1),
+    KOREA("한식", 2),
+    CHINA("중식", 3),
+    ASIAN("아시안", 4),
+    WESTERN("양식", 5);
+
+    private final String category;
+    private final int number;
+
+    CategoryType(String category, int number) {
+        this.category = category;
+        this.number = number;
+    }
+
+    private String getCategory() {
+        return this.category;
+    }
+
+    private int getNumber() {
+        return this.number;
+    }
+}
