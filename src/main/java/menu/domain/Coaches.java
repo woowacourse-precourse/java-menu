a0809e5547757b22a9a7c4424eeb392f7bc841eb
moonstal1506 <moonstal1506@gmail.com>
2022-12-17 15:29:16 +0900

    feat: 코치는 최소 2명, 최대 5명

---
+++ src/main/java/menu/domain/Coaches.java
@@ -0,0 +1,29 @@
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coaches {
+
+    private final List<Coach> coaches = new ArrayList<>();
+
+    public Coaches(String coaches) {
+        splitNames(coaches);
+        if (!isValid(this.coaches.size())) {
+            throw new IllegalArgumentException("코치는 최소 2명, 최대 5명입니다.");
+        }
+    }
+
+    private boolean isValid(int size) {
+        return 2 <= size && size <= 5;
+    }
+
+    private void splitNames(String coaches) {
+        String[] splitNames = coaches.split(",");
+        for (String splitName : splitNames) {
+            Coach coach = new Coach(splitName);
+            this.coaches.add(coach);
+        }
+    }
+
+}
