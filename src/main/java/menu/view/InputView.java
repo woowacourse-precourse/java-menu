21f245ad28642d63e43b7b8cb3f80fbb259b9bf8
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-12-17 13:54:07 +0900

    feat : 못먹는 음식 입력 기능 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,6 +1,7 @@
 package menu.view;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -22,6 +23,14 @@ public class InputView {
 			coachList.add(new Coach(coaches[i]));
 		}
 		return coachList;
+	}
+
+	// 코치 이름을 받아서 못먹는 메뉴 입력
+	public void readNotEatFood(Coach coach) {
+		Message.printNotEatFoodMessage(coach.getName());
+		String[] notEatFood = Console.readLine().split(",");
+		//TODO : 음식 갯수 예외 검사(0~2개 사이)
+		coach.setNotEatFoodList(new ArrayList<>(Arrays.asList(notEatFood)));
 	}
 
 }
