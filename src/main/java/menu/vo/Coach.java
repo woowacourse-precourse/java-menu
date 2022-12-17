e75327f9e3addc34ca06ed47d31b59f43bc8be3e
minaver <psmjoshua@gmail.com>
2022-12-17 13:23:20 +0900

    feat(Coach): Coach VO 초기화

--- src/main/java/menu/vo/Coach.java
+++ src/main/java/menu/vo/Coach.java
@@ -1,4 +1,13 @@
 package menu.vo;
 
+import java.util.List;
+
 public class Coach {
+    private final String name;
+    private final List<String> menu;
+
+    public Coach(String name, List<String> menu) {
+        this.name = name;
+        this.menu = menu;
+    }
 }
