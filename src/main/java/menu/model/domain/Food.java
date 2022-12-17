d644a375de847e3780c64420dacb3795f8b3c14b
Hayoon <hy97@sju.ac.kr>
2022-12-17 13:39:51 +0900

    feat(Food): 음식종류를 담을 클래스 생성

---
+++ src/main/java/menu/model/domain/Food.java
@@ -0,0 +1,11 @@
+package menu.model.domain;
+
+public class Food {
+    private String name;
+    private Category category;
+
+    public Food(String name, Category category) {
+        this.name = name;
+        this.category = category;
+    }
+}
