2262ec9df8151acf981061cb7d03006a8a5d4840
MinjeongKong <kong1301@ajou.ac.kr>
2022-12-17 16:56:36 +0900

    refactor: 예외처리 수정 및 에러문구 지정

--- src/main/java/menu/utils/MemberValid.java
+++ src/main/java/menu/utils/MemberValid.java
@@ -4,6 +4,7 @@ public class MemberValid {
     public static final int MIN_NAME = 2;
     public static final int MAX_NAME = 4;
     public static final int MIN_COUNT = 2;
+    public static final int MAX_COUNT = 5;
     public static final int MIN_NOTEAT = 0;
     public static final int MAX_NOTEAT = 2;
 
@@ -16,19 +17,19 @@ public class MemberValid {
 
     public static void isValidNotEat(String[] notEats) {
         if (notEats.length < MIN_NOTEAT || notEats.length > MAX_NOTEAT) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("못 먹는 메뉴는 최소 0개, 최대 2개여야 합니다.");
         }
     }
 
     private static void isCorrectLength(String member) {
         if (member.length() < MIN_NAME || member.length() > MAX_NAME) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("코치의 이름은 최소 2글자, 최대 4글자여야 합니다.");
         }
     }
 
     private static void isCorrectCount(String[] members) {
-        if (members.length < MIN_COUNT) {
-            throw new IllegalArgumentException();
+        if (members.length < MIN_COUNT || members.length > MAX_COUNT) {
+            throw new IllegalArgumentException("코치는 최소 2명 이상, 5명이하여야 합니다.");
         }
     }
 }
