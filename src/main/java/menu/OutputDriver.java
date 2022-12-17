5c32eefcafd44cd034f7c926799555812463d2ab
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-12-17 16:18:50 +0900

    Feat : 이름 입력, 못 먹는 메뉴 입력에 대한 에러 메세지 출력 기능 추가

--- src/main/java/menu/OutputDriver.java
+++ src/main/java/menu/OutputDriver.java
@@ -3,7 +3,7 @@ package menu;
 public class OutputDriver {
     public void printStartMessage() {
         System.out.println("점심 메뉴 추천을 시작합니다.\n");
-        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+
     }
 
     public void printScanCannotEatMenuMessage(String name) {
@@ -14,5 +14,37 @@ public class OutputDriver {
         System.out.println("\n메뉴 추천 결과입니다.\n");
         System.out.println(menuListStringReturn);
         System.out.println("\n추천을 완료했습니다.");
+    }
+
+    public void printScanCoachName() {
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+    }
+
+    public void printErrorForScanName() {
+        System.out.println("[ERROR] 이름을 입력하실 때 맨 앞에 , 를 붙이면 안되고, 이름의 길이는 2 - 4 글자, 코치는 2 - 5명 까지만 가능합니다.");
+    }
+
+    public void printErrorForScanMenuNumber() {
+        System.out.println("[ERROR] 못 먹는 메뉴는 0 - 2개 까지만 입력이 가능합니다.");
+    }
+
+    public void printErrorForScanSpliting() {
+        System.out.println("[ERROR] 이름을 입력하실 때 맨 앞에 , 를 붙이시면 안됩니다.");
+    }
+
+    public void printErrorForScanTooManyCoach() {
+        System.out.println("[ERROR] 코치는 최대 5명 이하만 입력헤야 합니다.");
+    }
+
+    public void printErrorForScanTooLessCoach() {
+        System.out.println("[ERROR] 코치는 최소 2명 이상 입력해야 합니다.");System.out.println("[ERROR] 코치는 최소 2명 이상 입력해야 합니다.");
+    }
+
+    public void printErrorForScanMenuSpliting() {
+        System.out.println("[ERROR] 메뉴를 입력하실 때 맨 앞에 , 를 붙이시면 안됩니다.");
+    }
+
+    public void printErrorForScanMenuHasSpace() {
+        System.out.println("[ERROR] 메뉴를 입력하실 때 공백을 넣으시면 안됩니다.");
     }
 }
