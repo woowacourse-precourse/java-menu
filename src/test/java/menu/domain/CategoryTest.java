cd3f7f9782f8fec34caa8f318cebe93ce2ed0b6a
kim0914 <kosa0914@naver.com>
2022-12-17 16:55:27 +0900

    test(Category): 카테고리 추천 횟수에 따른 추천 가능 기능 테스트 추가

--- src/test/java/menu/domain/CategoryTest.java
+++ src/test/java/menu/domain/CategoryTest.java
@@ -1,6 +1,5 @@
 package menu.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -17,8 +16,31 @@ class CategoryTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5})
     void createCategoryIndex(int index) {
+        // given
         List<Category> categories = List.of(Category.JAPAN, Category.KOREA, Category.CHINA, Category.ASIAN, Category.WEST);
 
+        // when, then
         assertThat(Category.getCategory(index)).isEqualTo(categories.get(index - 1));
+    }
+
+    @DisplayName("2번 추천된 카테고리는 추천할 수 없다.")
+    @Test
+    void createOverCategoryCount() {
+        // given
+        Category.KOREA.addCount();
+        Category.KOREA.addCount();
+
+        // when, then
+        assertThat(Category.KOREA.canChoose()).isEqualTo(false);
+    }
+
+    @DisplayName("2번 이하로 추천된 카테고리는 추천할 수 있다.")
+    @Test
+    void createUnderCategoryCount() {
+        // given
+        Category.JAPAN.addCount();
+
+        // when, then
+        assertThat(Category.JAPAN.canChoose()).isEqualTo(true);
     }
 }
