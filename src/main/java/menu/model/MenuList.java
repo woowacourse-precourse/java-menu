00db0e253dbe315c2f321380dc3c5d62d9898cca
SH <gram753@naver.com>
2022-12-17 13:59:27 +0900

    feat: 못 먹는 음식들을 저장하는 객체 구현

---
+++ src/main/java/menu/model/MenuList.java
@@ -0,0 +1,15 @@
+package menu.model;
+
+import java.util.List;
+
+public class MenuList {
+	private List<Menu> menusCantEat;
+
+	public List<Menu> getMenusCantEat() {
+		return menusCantEat;
+	}
+
+	public void setMenusCantEat(List<Menu> menusCantEat) {
+		this.menusCantEat = menusCantEat;
+	}
+}
