febfe495924038e93f3c7c03ce6cfad5a85679c6
jjongwa <troas96@naver.com>
2022-12-17 13:27:34 +0900

    feat: 게임 시작 안내, 코치 이름 입력 기능

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -3,5 +3,8 @@ package menu;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        View view = new View();
+        view.startGame();
+//        System.out.println(view.inputCoachName());
     }
 }
