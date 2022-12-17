c195a2264e45706b4d05c834ccd5c7152c6c6d54
kiarakim <kiarakim413@gmail.com>
2022-12-17 15:33:48 +0900

    feat: 존재 여부 확인 기능 추가

--- src/main/java/menu/CategoryLists.java
+++ src/main/java/menu/CategoryLists.java
@@ -5,14 +5,27 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CategoryLists {
-    List<List<String>> category = Arrays.asList(
+    List<String> categories = Arrays.asList("일식", "한식", "중식", "아시안", "양식");
+    List<List<String>> categoryList = Arrays.asList(
             Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼"),
             Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"),
-            Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토","달걀볶음", "고추잡채"),
+            Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토", "달걀볶음", "고추잡채"),
             Arrays.asList("팟타이", "카오", "팟", "나시고렝", "파인애플", "볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜"),
             Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치", "토스트", "바게트", "스파게티", "피자", "파니니"));
 
     public List<List<String>> getCategory() {
-        return category;
+        return categoryList;
+    }
+
+    public String get(int randNum) {
+        return categories.get(randNum);
+    }
+
+    public boolean isExist(String food) {
+        for (List<String> list : categoryList) {
+            if (list.contains(food))
+                return true;
+        }
+        return false;
     }
 }
