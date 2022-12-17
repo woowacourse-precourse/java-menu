039777861dfdbe70f6dd70c6b13d6ff4d9e47a50
hectick <chyeon1223@cau.ac.kr>
2022-12-17 13:15:41 +0900

    feat: 추천 완료 문구를 출력하는 기능 추가

--- src/test/java/menu/constant/Message.java
+++ src/test/java/menu/constant/Message.java
@@ -2,5 +2,6 @@ package menu.constant;
 
 public class Message {
     public static final String SERVICE_START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    public static final String SERVICE_FINISH_MESSAGE = "추천을 완료했습니다.";
 
 }
--- src/test/java/menu/view/OutputView.java
+++ src/test/java/menu/view/OutputView.java
@@ -7,4 +7,8 @@ public class OutputView {
     public static void printServiceStartMessage(){
         System.out.println(Message.SERVICE_START_MESSAGE);
     }
+
+    public static void printServiceFinishMessage(){
+        System.out.println(Message.SERVICE_FINISH_MESSAGE);
+    }
 }
