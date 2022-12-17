54f375cc8a2a7862199bbc3fbd66eb2045189ab1
hum02 <ehdgma02@gmail.com>
2022-12-17 14:31:26 +0900

    feat: 코치 생성 기능 구현

--- src/main/java/menu/MenuRecommendController.java
+++ src/main/java/menu/MenuRecommendController.java
@@ -1,5 +1,39 @@
 package menu;
 
 
+import menu.domain.Coach;
+import menu.domain.Menu;
+import menu.repository.MenuRepository;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuRecommendController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public MenuRecommendController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void run() {
+        List<Coach> coaches = new ArrayList<>();
+
+        String[] coachNames = inputView.readCoachNames();
+        for (int i = 0; i < coachNames.length; i++) {
+            String[] excludeMenu = inputView.readExcludeMenu(coachNames[i]);
+            List<Menu> excludeMenus = new ArrayList<>();
+
+            for (String menuName: excludeMenu) {
+                excludeMenus.add(MenuRepository.getMenuByName(menuName));
+            }
+            coaches.add(new Coach(coachNames[i], excludeMenus));
+        }
+
+        System.out.println(coaches);
+    }
 }
--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,4 +1,21 @@
 package menu.domain;
 
+import java.util.List;
+
 public class Coach {
+    private final String name;
+    private final List<Menu> excludeMenus;
+
+    public Coach(String name, List<Menu> excludeMenus) {
+        this.name = name;
+        this.excludeMenus = excludeMenus;
+    }
+
+    @Override
+    public String toString() {
+        return "Coach{" +
+                "name='" + name + '\'' +
+                ", excludeMenus=" + excludeMenus +
+                '}';
+    }
 }
