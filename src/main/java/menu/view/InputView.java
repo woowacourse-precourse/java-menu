70e724c23d1edcaafc6cdc03ef861b6778985f79
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2022-12-17 14:10:11 +0900

    feat(menu) : 입력 구현
    - 코치의 이름 입력
    - 코치가 못먹는 음식 입력

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,4 +1,19 @@
 package menu.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
+    public static List<String> readCoachName() {
+        return Arrays.stream(Console.readLine().split(","))
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> readInedibleFoods() {
+        return Arrays.stream(Console.readLine().split(","))
+                .collect(Collectors.toList());
+    }
 }
