337b87f9cb154cf64e9e4b0d848eca6bc7576f0a
anneu <b.imnuj@gmail.com>
2022-12-17 13:56:01 +0900

    fix: 입력 시 Console API를 사용하도록 수정

--- src/main/java/menu/view/Input.java
+++ src/main/java/menu/view/Input.java
@@ -1,15 +1,14 @@
 package menu.view;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
-    private final Scanner scanner = new Scanner(System.in);
 
     public String readCoachName() {
         return read();
     }
 
     public String read() {
-        return scanner.nextLine();
+        return Console.readLine();
     }
 }
