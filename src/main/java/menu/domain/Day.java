24568c3c212f7ae1d88c8991cfbdee53b66b8ccd
EUNJEONGMUN <munej26@gmail.com>
2022-12-17 16:11:55 +0900

    feat: Day enum에 한글요일 멤버 추가

--- src/main/java/menu/domain/Day.java
+++ src/main/java/menu/domain/Day.java
@@ -1,9 +1,19 @@
 package menu.domain;
 
 public enum Day {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY
+    MONDAY("월요일"),
+    TUESDAY("화요일"),
+    WEDNESDAY("수요일"),
+    THURSDAY("목요일"),
+    FRIDAY("금요일");
+
+    private final String day;
+
+    Day(String day) {
+        this.day = day;
+    }
+
+    public String getDay() {
+        return day;
+    }
 }
