03f5b4c4b0cb2c96a00dae5c70f0a802d66b1b3c
shinD <huipulci1@naver.com>
2022-12-17 17:26:40 +0900

    feat: 코치 이름 최대 2글자 수정
    기능 명세 문서 체크에 오류가 있었습니다.
    "못 먹는 음식이 0 ~ 2 개이다" 에 잘못 체크가 되어있었습니다.

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -5,17 +5,31 @@ import menu.domain.enums.Category;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.String.format;
+
 public class Coach {
 
+    private static final int MIN_NAME_SIZE = 2;
+    private static final int MAX_NAME_SIZE = 4;
     private static final int MAX_DUPLICATED_COUNT = 2;
+    private static final String ERROR_NAME_SIZE_FORMAT = "코치의 이름은 최소 %d글자, 최대 %d글자 입니다.";
 
     private final String name;
     private final List<Menu> inedibleMenus;
     private final List<Menu> recommendedMenus = new ArrayList<>();
 
     public Coach(final String name, final List<Menu> inedibleMenus) {
+        validateName(name);
         this.name = name;
         this.inedibleMenus = inedibleMenus;
+    }
+
+    private void validateName(String name) {
+        if (name.length() < MIN_NAME_SIZE || name.length() > MAX_NAME_SIZE) {
+            throw new IllegalArgumentException(
+                    format(ERROR_NAME_SIZE_FORMAT, MIN_NAME_SIZE, MAX_NAME_SIZE)
+            );
+        }
     }
 
     public static Coach ofName(final String name) {
