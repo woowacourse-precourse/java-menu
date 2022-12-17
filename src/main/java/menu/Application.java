b2f0b7bccf03bf44bc5676fa512bbd0f0a3c38f3
WhalesBob <ndw8200@naver.com>
2022-12-17 13:46:28 +0900

    feat : 초기화 이후, 본격적으로 시작하는 기능 추가

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,12 @@
 package menu;
 
 public class Application {
+
+    private static final Initializer initializer = new Initializer();
+    private static final SuggestProgram suggestProgram = new SuggestProgram();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        initializer.initialize();
+        suggestProgram.startProgram();
     }
 }
