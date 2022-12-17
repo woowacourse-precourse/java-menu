4988fa46ed3f0d9bb4bdc460081463dfce59fef0
minaver <psmjoshua@gmail.com>
2022-12-17 13:19:42 +0900

    feat(Week): Week Enum 초기화

--- src/main/java/menu/enums/Week.java
+++ src/main/java/menu/enums/Week.java
@@ -1,4 +1,34 @@
 package menu.enums;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Week {
+    MON("월요일"),
+    TUE("화요일"),
+    WED("수요일"),
+    THU("목요일"),
+    FRI("금요일")
+    ;
+
+    private final String label;
+
+    Week(String label) {
+        this.label = label;
+    }
+
+    public String label() {
+        return label;
+    }
+
+    private static final Map<String, Week> BY_LABEL=
+            Stream.of(values()).collect(Collectors.toMap(Week::label, Function.identity()));
+
+
+    public static Week valueOfLabel(String label) {
+        return BY_LABEL.get(label);
+    }
+
 }
