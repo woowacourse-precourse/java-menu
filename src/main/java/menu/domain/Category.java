8ca138bb77840c81296ad0a4e7f74bd4f2395c7b
woo-chang <clllickme@naver.com>
2022-12-17 14:47:45 +0900

    refactor(Category): 사용되는 값 String -> int 타입 수정

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -4,31 +4,31 @@ import java.util.Arrays;
 
 public enum Category {
 
-    JAPAN("일식", "1"),
-    KOREA("한식", "2"),
-    CHINA("중식", "3"),
-    ASIA("아시안", "4"),
-    AMERICA("양식", "5");
+    JAPAN("일식", 1),
+    KOREA("한식", 2),
+    CHINA("중식", 3),
+    ASIA("아시안", 4),
+    AMERICA("양식", 5);
 
-    Category(String name, String value) {
+    Category(String name, int value) {
         this.name = name;
         this.value = value;
     }
 
     private final String name;
-    private final String value;
+    private final int value;
 
     public static Category from(String name) {
         return Arrays.stream(Category.values())
                 .filter(category -> category.name.equals(name))
                 .findFirst()
-                .orElseThrow(() -> new IllegalStateException("존재하지 않는 카테고리입니다."));
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 카테고리입니다."));
     }
 
-    public static Category of(String value) {
+    public static Category from(int value) {
         return Arrays.stream(Category.values())
-                .filter(category -> category.value.equals(value))
+                .filter(category -> category.value == value)
                 .findFirst()
-                .orElseThrow(() -> new IllegalStateException("존재하지 않는 카테고리입니다."));
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 카테고리입니다."));
     }
 }
