2ba1f6d2d391760767a388d62c333a0b1d512545
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-12-17 17:29:04 +0900

    Fix : 받은 문자열이 비어있으면 빈 리스트로 놔두기

--- src/main/java/menu/CoachRepo.java
+++ src/main/java/menu/CoachRepo.java
@@ -14,11 +14,23 @@ public class CoachRepo {
         }
     }
     public void setCoachCannotEatMenu(String coachName, String scanCannotEatMenu) {
-        String cannotEatMenuList[] = scanCannotEatMenu.split(",");
-        Coach targetCoach = getTargetCoach(coachName);
-        for (int i=0; i<cannotEatMenuList.length; i++){
-            targetCoach.cannotEatMenuList.add(cannotEatMenuList[i]);
+        if(!scanCannotEatMenu.equals("")){
+            String cannotEatMenuList[] = scanCannotEatMenu.split(",");
+            Coach targetCoach = getTargetCoach(coachName);
+            for (int i=0; i<cannotEatMenuList.length; i++){
+                targetCoach.cannotEatMenuList.add(refreshString(cannotEatMenuList[i]));
+            }
         }
+    }
+
+    private String refreshString(String s) {
+        if (s.charAt(0) == ' ') {
+            s=s.substring(1);
+        }
+        if (s.charAt(s.length()-1) == ' '){
+            s=s.substring(0,s.length()-1);
+        }
+        return s;
     }
 
     private Coach getTargetCoach(String coachName) {
