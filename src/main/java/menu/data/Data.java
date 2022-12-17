1c7d741e6b17f069b5fc2c22e49d761c06094f95
JunDonghyuk <ehdgur4814@naver.com>
2022-12-17 14:00:05 +0900

    feat(Menus): 메뉴 외부 데이터 작성

--- src/main/java/menu/data/Data.java
+++
@@ -1,4 +0,0 @@
-package menu.data;
-
-public class Data {
-}
---
+++ src/main/java/menu/data/Menus.java
@@ -0,0 +1,14 @@
+package menu.data;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Menus {
+    List<String> japanMenus = new ArrayList<>(Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼"));
+    List<String> koreaMenus = new ArrayList<>(Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"));
+    List<String> chinaMenus = new ArrayList<>(Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채"));
+    List<String> asiaMenus = new ArrayList<>(Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜"));
+    List<String> westMenus = new ArrayList<>(Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
+
+}
