1eceb8a45c996778bd5d5f736feb39b263f1a95f
Eun-chan Cho <eunchan1001@gmail.com>
2022-12-17 17:08:19 +0900

    fix(InputView): modify prompt message

--- src/main/java/menu/View/InputView.java
+++ src/main/java/menu/View/InputView.java
@@ -10,7 +10,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private final String READ_COACH_NAMES = "코치의 이름을 입력해 주세요. (, 로 구분)";
-    private final String READ_INEDIBLES = "%s가 못 먹는 메뉴를 입력해 주세요.";
+    private final String READ_INEDIBLES = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
 
     private final String INVALID_FOOD = "음식이 메뉴에 없습니다.";
 
