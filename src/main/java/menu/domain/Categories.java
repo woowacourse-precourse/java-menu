b561d69e24ef5f4591ef610db918002b7a488663
sosow0212 <sosow0212@naver.com>
2022-12-17 16:29:25 +0900

    refactor : (Categories) 안쓰는 메서드 제거

--- src/main/java/menu/domain/Categories.java
+++ src/main/java/menu/domain/Categories.java
@@ -29,16 +29,9 @@ public class Categories {
         return false;
     }
 
-    public boolean isDone() {
-        if (categories.size() == 5) {
-            return true;
-        }
-        return false;
-    }
-
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for(String category : categories) {
+        for (String category : categories) {
             sb.append("| " + category + " ");
         }
         return sb.toString();
