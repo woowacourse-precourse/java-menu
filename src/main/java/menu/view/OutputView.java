d5d88d8b728eff7fea1a1c3dcdf5ffbe6d5f5a56
2014012606 <2014012606@hanyang.ac.kr>
2022-12-17 14:32:38 +0900

    feat: 각 코치에게 추천된 메뉴를 출력하는 기능 추가

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -5,6 +5,7 @@ import java.util.List;
 import static menu.view.ViewConstants.*;
 
 public class OutputView {
+    private String barDelimeter = " | ";
     public void showStartRecommender() {
         System.out.println(startLunchRecommenderMessage);
     }
@@ -15,7 +16,12 @@ public class OutputView {
     }
 
     public void showRecommendedCategory(List<String> recommendedCategories) {
-        String categoryMessage = String.join(" | ", recommendedCategories);
+        String categoryMessage = String.join(barDelimeter, recommendedCategories);
         System.out.println("[ 카테고리 | " + categoryMessage + " ]");
+    }
+
+    public void showRecommendedMenu(String coachName, List<String> recommendedMenus) {
+        String menuMessage = String.join(barDelimeter, recommendedMenus);
+        System.out.println("[ " + coachName + barDelimeter + menuMessage + " ]");
     }
 }
