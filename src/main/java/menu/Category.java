538b24b41f9d14e1a29b98d2c765618ea776efa1
youngkyunIm <yee0230@naver.com>
2022-12-17 15:25:00 +0900

    feat(RecommendMeny): 카테고리 5가지 구하기

--- src/main/java/menu/Category.java
+++ src/main/java/menu/Category.java
@@ -1,14 +1,25 @@
 package menu;
 
-public class Category {
+import java.util.Arrays;
 
-    private final String name;
+public enum Category {
 
-    public Category(String name) {
-        this.name = name;
+    일식("1"),
+    한식("2"),
+    중식("3"),
+    아시안("4"),
+    양식("5");
+
+    private final String value;
+
+    Category(String input) {
+        value = input;
     }
 
-    public String getName() {
-        return this.name;
+    public static Category from(String input) {
+        return Arrays.stream(Category.values())
+                .filter(it -> it.value.equals(input))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 입력입니다."));
     }
 }
--- src/main/java/menu/RecommendMenu.java
+++ src/main/java/menu/RecommendMenu.java
@@ -1,5 +1,31 @@
 package menu;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import menu.coach.Coaches;
+
 public class RecommendMenu {
 
+    private final Coaches coaches;
+    private List<String> categoryOrder = new ArrayList<>();
+
+    public RecommendMenu(Coaches coaches) {
+        this.coaches = coaches;
+    }
+
+    public void makeCategoryList() {
+        while (categoryOrder.size() < 5) {
+            categoryOrder.add(validAdd());
+        }
+        System.out.println(categoryOrder);
+    }
+
+    private String validAdd() {
+        String category = Category.from(String.valueOf(Randoms.pickNumberInRange(1, 5))).name();
+        if (categoryOrder.contains(category)) {
+            return validAdd();
+        }
+        return category;
+    }
 }
