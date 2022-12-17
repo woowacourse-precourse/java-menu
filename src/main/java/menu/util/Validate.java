3b55039cfbe50c978bbadd5ad99ceb59e6f54adc
= <=>
2022-12-17 14:14:09 +0900

    feat: 메뉴 입력시 올바른 입력인지 검증하는 예외처리 함수 구현

--- src/main/java/menu/util/Validate.java
+++ src/main/java/menu/util/Validate.java
@@ -1,5 +1,7 @@
 package menu.util;
 
+import menu.service.MenuService;
+
 import java.util.List;
 
 /**
@@ -7,6 +9,7 @@ import java.util.List;
  * @CreateAt : 2022/12/17
  */
 public class Validate {
+    private static final MenuService menuService = new MenuService();
     public static void validateCoachNames(List<String> coachName) {
         if (coachName.size() < 2) {
             throw new IllegalArgumentException(IllegalArgumentExceptionMessage.INPUT_COACH_LENGTH.getMessage());
@@ -14,6 +17,8 @@ public class Validate {
     }
 
     public static void validateHateMenu(List<String> hateMenus) {
-        // TODO: 이후에 메뉴관련 정보를 처리하는 객체와 함수를 만들면, 구현
+        for (String hateMenu : hateMenus) {
+            menuService.findByName(hateMenu);
+        }
     }
 }
