13646c0a0fcc812dd44cfc3ea8a0e3652cc883e1
be-student <presentsong@naver.com>
2022-12-17 15:27:10 +0900

    feat: 숫자로 카테고리를 고를 수 있는 기능을 추가한다

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -3,15 +3,17 @@ package menu.domain;
 import java.util.Arrays;
 
 public enum Category {
-    JAPANESE("일식"),
-    KOREAN("한식"),
-    CHINESE("중식"),
-    ASIAN("아시안"),
-    WESTERN("양식");
+    JAPANESE("일식", 1),
+    KOREAN("한식", 2),
+    CHINESE("중식", 3),
+    ASIAN("아시안", 4),
+    WESTERN("양식", 5);
 
     private final String keyword;
+    private final int key;
 
-    Category(String keyword) {
+    Category(String keyword, int key) {
+        this.key = key;
         this.keyword = keyword;
     }
 
@@ -21,4 +23,10 @@ public enum Category {
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("카테고리가 존재하지 않습니다"));
     }
-}
+
+    public static Category from(int key) {
+        return Arrays.stream(Category.values())
+                .filter(it -> it.key == key)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("카테고리가 존재하지 않습니다"));
+    }
