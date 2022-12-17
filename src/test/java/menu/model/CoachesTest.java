bb1af8327826a901aa8a8eaaff6cc99ac3460e64
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-17 17:44:51 +0900

    refactor(CoachesTest): 사용하지 않는 import 제거

--- src/test/java/menu/model/CoachesTest.java
+++ src/test/java/menu/model/CoachesTest.java
@@ -3,14 +3,11 @@ package menu.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class CoachesTest {
     Coaches coaches = new Coaches();
