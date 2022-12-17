b0adfcec4180422533c1883fb8c5c87d4430685a
Hyeonjae-K <href.hjk@gmail.com>
2022-12-17 13:34:40 +0900

    style: 메서드명 수정

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         Category category = new Category();
 
-        for (String item : category.Choose()) {
+        for (String item : category.choose()) {
             System.out.print(item + " ");
         }
     }
--- src/main/java/menu/Category.java
+++ src/main/java/menu/Category.java
@@ -8,18 +8,18 @@ import java.util.List;
 public class Category {
     final static List<String> categories = Arrays.asList("한식", "일식", "중식", "아시안", "양식");
 
-    public List<String> Choose() {
+    public List<String> choose() {
         List<String> result = new ArrayList<>();
         while (result.size() < 5) {
             String category = categories.get(Randoms.pickNumberInRange(0, 4));
-            if (ContainCount(result, category) < 2) {
+            if (containCount(result, category) < 2) {
                 result.add(category);
             }
         }
         return result;
     }
 
-    private int ContainCount(List<String> list, String category) {
+    private int containCount(List<String> list, String category) {
         int count = 0;
         for (String item : list) {
             if (item.equals(category)) {
