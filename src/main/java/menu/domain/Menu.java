770d9ca988db4efd520bab61088ff873c870ec76
JeonghakLee <wjdgkr0527@naver.com>
2022-12-17 13:19:09 +0900

    feat(Menu): 메뉴 객체 구현

---
+++ src/main/java/menu/domain/Menu.java
@@ -0,0 +1,13 @@
+package menu.domain;
+
+public class Menu {
+
+  private Category category;
+  private String name;
+
+  public Menu(Category category, String name) {
+    this.category = category;
+    this.name = name;
+  }
+
+}
