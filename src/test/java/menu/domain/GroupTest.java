faf12eaf4ac6a9e6d028589e0295b656ae08b1af
woosung1223 <dntjd991223@naver.com>
2022-12-17 16:59:48 +0900

    test: 코치의 수가 범위를 벗어나면 예외를 발생시키는지 테스트

---
+++ src/test/java/menu/domain/GroupTest.java
@@ -0,0 +1,34 @@
+package menu.domain;
+
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class GroupTest {
+    @ParameterizedTest
+    @MethodSource("parameterProvider")
+    void 코치의_수가_범위를_벗어나면_예외를_발생시키는지_테스트(List<Coach> coaches) {
+        assertThatThrownBy(() -> new Group(coaches))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private Stream<Arguments> parameterProvider() {
+        return Stream.of(
+                Arguments.of(
+                        List.of()),
+                Arguments.of(
+                        List.of(new Coach(new CoachName("우성1")))),
+                Arguments.of(
+                        List.of(new Coach(new CoachName("우성1")), new Coach(new CoachName("우성2")),
+                                new Coach(new CoachName("우성3")), new Coach(new CoachName("우성4")),
+                                new Coach(new CoachName("우성5")),  new Coach(new CoachName("우성6"))))
+        );
+    }
+}
