efc773cba1827a879c29cdd531def51573c652ba
minaver <psmjoshua@gmail.com>
2022-12-17 16:37:49 +0900

    refactor(FoodCategory): 카테고리별 메뉴 Enum 화

--- src/main/java/menu/domain/Menu.java
+++ src/main/java/menu/domain/Menu.java
@@ -1,14 +1,13 @@
 package menu.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import menu.dto.CoachMenu;
 import menu.dto.CoachWeekMenu;
 import menu.enums.FoodCategory;
 import menu.vo.Coach;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Menu {
     private final FoodCategory foodCategory;
@@ -30,7 +29,6 @@ public class Menu {
     }
 
     //// private
-
     // 예외 사항을 판단하여 최종 메뉴를 얻는다.
     private String shuffleMenu(Coach coach, List<String> eatMenus){
         String menu;
@@ -64,16 +62,13 @@ public class Menu {
 
     // 카테고리에 따라 foods 초기화
     private void initFoodsByCategory(FoodCategory foodCategory) {
-        if (foodCategory.equals(FoodCategory.KOR)) {
-            foods = Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음");
-        } else if (foodCategory.equals(FoodCategory.JAP)) {
-            foods = Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼");
-        } else if (foodCategory.equals(FoodCategory.CHI)) {
-            foods = Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채");
-        } else if (foodCategory.equals(FoodCategory.ASI)) {
-            foods = Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜");
-        } else if (foodCategory.equals(FoodCategory.WES)) {
-            foods = Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니");
-        }
+        foods = menusToList(foodCategory.menus());
     }
+
+    private List<String> menusToList(String menus) {
+        return Arrays.asList(menus.split(",")).stream()
+                .map(v -> v.trim())
+                .collect(Collectors.toList());
+    }
+
 }
--- src/main/java/menu/enums/FoodCategory.java
+++ src/main/java/menu/enums/FoodCategory.java
@@ -6,19 +6,21 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum FoodCategory {
-    JAP(1,"일식"),
-    KOR(2,"한식"),
-    CHI(3,"중식"),
-    ASI(4,"아시안"),
-    WES(5,"양식")
+    JAP(1,"일식","규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼"),
+    KOR(2,"한식","김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음"),
+    CHI(3,"중식","깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채"),
+    ASI(4,"아시안","팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜"),
+    WES(5,"양식","라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니")
     ;
 
     private final int number;
     private final String label;
+    private final String menus;
 
-    FoodCategory(int number, String label) {
+    FoodCategory(int number, String label, String menus) {
         this.number = number;
         this.label = label;
+        this.menus = menus;
     }
 
     public String label() {
@@ -26,6 +28,9 @@ public enum FoodCategory {
     }
     public int number() {
         return number;
+    }
+    public String menus() {
+        return menus;
     }
 
     private static final Map<String, FoodCategory> BY_LABEL=
@@ -41,4 +46,5 @@ public enum FoodCategory {
     public static FoodCategory valueOfNumber(int number) {
         return BY_NUMBER.get(number);
     }
+
 }
