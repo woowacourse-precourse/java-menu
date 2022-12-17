2e26bf8e2dfe87ebbb9970eacdabd678400e7014
Miseong Kim <miseongk@naver.com>
2022-12-17 17:06:42 +0900

    refactor: 추천 메뉴 외부에서 변경 못하도록 반환

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -5,6 +5,7 @@ import menu.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Coach {
@@ -44,6 +45,6 @@ public class Coach {
     }
 
     public List<String> getRecommendedMenu() {
-        return this.recommendedMenu;
+        return Collections.unmodifiableList(recommendedMenu);
     }
 }
