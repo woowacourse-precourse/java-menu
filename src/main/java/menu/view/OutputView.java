f8f4b7db049c224ef8eb21072a491d5ff63626b6
odo27 <odo27@odo27ui-MacBookAir.local>
2022-12-17 14:51:28 +0900

    feat(OutputView): 서비스 시작 문구 출력 기능 구현

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,11 @@
+package menu.view;
+
+public class OutputView {
+
+    private static final String SERVICE_START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+
+    public void printServiceStartMessage() {
+        System.out.println(SERVICE_START_MESSAGE);
+    }
+
+}
