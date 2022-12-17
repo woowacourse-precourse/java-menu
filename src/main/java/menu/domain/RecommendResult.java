15caa4932a06fa21096c1552f9f9b3ea4d3d7729
Cyma-s <ssol0319@naver.com>
2022-12-17 16:06:17 +0900

    feat(RecommendResult) : 코치의 랜덤 메뉴 생성하는 함수 추가

--- src/main/java/menu/domain/RecommendResult.java
+++ src/main/java/menu/domain/RecommendResult.java
@@ -4,21 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 import menu.CategoryMaker;
 import menu.CategoryRandomNumberGenerator;
+import menu.MenuMaker;
 
 public class RecommendResult {
-    private List<Coach> coaches;
+    private final List<Coach> coaches;
     private List<Category> categories;
-    private CategoryRandomNumberGenerator categoryRandomNumberGenerator;
+    private final CategoryRandomNumberGenerator categoryRandomNumberGenerator;
 
     public RecommendResult(List<Coach> coaches, CategoryRandomNumberGenerator categoryRandomNumberGenerator) {
         this.coaches = coaches;
         this.categories = new ArrayList<>();
         this.categoryRandomNumberGenerator = categoryRandomNumberGenerator;
+        makeRecommendResult();
+    }
+
+    private void makeRecommendResult() {
+        generateRandomCategories();
+        generateRandomMenus();
     }
 
     private void generateRandomCategories() {
         CategoryMaker categoryMaker = new CategoryMaker(categoryRandomNumberGenerator);
         this.categories = categoryMaker.makeCategories();
+    }
+
+    private void generateRandomMenus() {
+        for(Coach coach : coaches) {
+            MenuMaker menuMaker = new MenuMaker(coach, categories);
+            List<Menu> coachMenus = menuMaker.makeRandomMenus();
+            coach.setRecommendMenus(coachMenus);
+        }
     }
 
 }
