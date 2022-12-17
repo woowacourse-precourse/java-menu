067159a01ec11bbd8b936e079abf1ae952a699c0
shinD <huipulci1@naver.com>
2022-12-17 15:39:41 +0900

    feat: Mapper 구현

--- src/main/java/menu/controller/mapper/CoachMapper.java
+++ src/main/java/menu/controller/mapper/CoachMapper.java
@@ -3,10 +3,31 @@ package menu.controller.mapper;
 import menu.domain.Coach;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
 
 public class CoachMapper {
 
-    public static List<Coach> toCoaches(String names) {
+    private static final String COMMA = ",";
+    private static final int MIN_GROUP_SIZE = 2;
+    private static final int MAX_GROUP_SIZE = 5;
 
+    public static List<Coach> toCoaches(String names) {
+        String[] split = names.split(COMMA);
+        validateSize(split);
+        // TODO 이름 글자 수 검증
+        return stream(split)
+                .map(Coach::ofName)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private static void validateSize(String[] split) {
+        if (MIN_GROUP_SIZE > split.length) {
+            throw new IllegalArgumentException("코치는 최소 2명 이상 입력해야 합니다.");
+        }
+        if (MAX_GROUP_SIZE < split.length) {
+            throw new IllegalArgumentException("코치는 최대 5명 까지만 입력해야 합니다.");
+        }
     }
 }
--- src/main/java/menu/controller/mapper/MenuMapper.java
+++ src/main/java/menu/controller/mapper/MenuMapper.java
@@ -4,8 +4,25 @@ import menu.domain.Food;
 
 import java.util.List;
 
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toUnmodifiableList;
+
 public class MenuMapper {
-    public static List<Food> toMenus(String menus) {
-        return null;
+
+    private static final String COMMA = ",";
+    private static final int MAX_INEDIBLE_FOOD_SIZE = 2;
+
+    public static List<Food> toMenus(String input) {
+        String[] menus = input.split(COMMA);
+        validateSize(menus);
+        return stream(menus)
+                .map(Food::ofName)
+                .collect(toUnmodifiableList());
+    }
+
+    private static void validateSize(String[] split) {
+        if (split.length > MAX_INEDIBLE_FOOD_SIZE) {
+            throw new IllegalArgumentException("못 먹는 메뉴는 최대 2개까지만 입력하실 수 있습니다.");
+        }
     }
 }
