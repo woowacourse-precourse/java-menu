83c53e6d3bd805785727b543a531e86027af158d
hojunking96 <junsong96@naver.com>
2022-12-17 16:50:27 +0900

    feats(Categories) : 카테고리의 리스트를 관리하는 객체 구현

---
+++ src/main/java/menu/Categories.java
@@ -0,0 +1,20 @@
+package menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Categories {
+    private final List<Category> categories;
+
+    public Categories() {
+        categories = new ArrayList<>();
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public List<String> getCategoryNames() {
+        return List.of("일식", "한식", "중식", "아시안", "양식");
+    }
+}
