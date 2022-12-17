1ddced0c37864b089da697460b690200377c776e
junheiLee <jun2ee22@naver.com>
2022-12-17 14:34:54 +0900

    fix: 랜덤 카테고리 추천 시, 인덱스 1부터 5사이에 카테고리가 있도록 이넘 변경

--- src/main/java/menu/domain/Food.java
+++ src/main/java/menu/domain/Food.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum Food {
+    CATEGORY("카테고리", List.of("메뉴")),
     JAPANESE("일식", List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
     KOREAN("한식", List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")),
     CHINESE("중식", List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
