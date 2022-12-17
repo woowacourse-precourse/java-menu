ff2562d0f8f7aedb5dd6dda7fe2845895281e4f7
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-17 17:39:49 +0900

    refactor: 불필요한 클래스 삭제

--- src/main/java/menu/domain/menu/repository/CoachRepository.java
+++
@@ -1,8 +0,0 @@
-package menu.domain.menu.repository;
-
-import menu.domain.menu.model.Coach;
-
-public interface CoachRepository {
-
-    Coach save(Coach coach);
-}
--- src/main/java/menu/domain/menu/service/CoachService.java
+++
@@ -1,8 +0,0 @@
-package menu.domain.menu.service;
-
-import menu.domain.menu.model.Coach;
-
-public interface CoachService {
-
-    Coach register(Coach coach);
-}
