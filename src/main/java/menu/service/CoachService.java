f79885f984ee22a6852a31a7780aac07226db459
yubin <dbqls2821@gmail.com>
2022-12-17 13:51:27 +0900

    feat: 코치가 싫어하는 메뉴를 추가하는 메소드

--- src/main/java/menu/service/CoachService.java
+++ src/main/java/menu/service/CoachService.java
@@ -17,4 +17,12 @@ public class CoachService {
     }
   }
 
+  public void insertHateFood(String coachName, String hateFoodNames) {
+    String[] hateFoods = hateFoodNames.split(",");
+    Coach coach = coaches.stream().filter(it -> it.getName().equals(coachName)).findFirst().orElseThrow(IllegalArgumentException::new);
+    for (int i = 0; i < hateFoods.length; i++) {
+      coach.insertHateFood(hateFoods[i]);
+    }
+  }
+
 }
