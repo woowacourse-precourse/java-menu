7891529b0760464c6507bb56c22627959fdd6749
jiwon <ljwon77@daum.net>
2022-12-17 17:24:41 +0900

    feat: 입력 메시지에 대한 개행 추가

--- src/main/java/menu/constant/InputMessage.java
+++ src/main/java/menu/constant/InputMessage.java
@@ -6,8 +6,8 @@ public enum InputMessage implements EnumUtil {
     START_RECOMMEND("점심 메뉴 추천을 시작합니다.\n")
     , GET_COACH_NAME("코치의 이름을 입력해 주세요. (, 로 구분)")
     , GET_NOT_EAT_MENU("\n%s(이)가 못 먹는 메뉴를 입력해 주세요.")
-    , RECOMMEND_RESULT("메뉴 추천 결과입니다.")
-    , STOP_RECOMMEND("추천을 완료했습니다.");
+    , RECOMMEND_RESULT("\n메뉴 추천 결과입니다.")
+    , STOP_RECOMMEND("\n추천을 완료했습니다.");
 
     private final String message;
 
