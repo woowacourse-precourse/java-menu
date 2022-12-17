b0cb1a2d3ef064a9d0b9110dc819a7349f788a4d
2014012606 <2014012606@hanyang.ac.kr>
2022-12-17 14:15:02 +0900

    feat: 코치가 먹지 못하는 음식 목록을 입력받는 기능 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -2,12 +2,22 @@ package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static menu.view.ViewConstants.*;
+
 public class InputView {
     public String getCoachesName() {
-        System.out.println(getCoachesName());
+        System.out.println(getCoachesNameMessage);
         String userInput = Console.readLine();
 
         // 예외 검사
+        return userInput;
+    }
+
+    public String getDislikeMenus(String coachName) {
+        System.out.println(coachName + getDislikeMenusMessage);
+        String userInput = Console.readLine();
+
+        //예외 검사
         return userInput;
     }
 }
