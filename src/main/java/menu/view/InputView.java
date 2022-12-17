e66f341a7bd544125df9a5a6aa9c143a2a11d3c2
EUNJEONGMUN <munej26@gmail.com>
2022-12-17 13:31:14 +0900

    feat: 코치가 못 먹는 음식을 입력받는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -22,4 +22,18 @@ public class InputView {
             throw new IllegalArgumentException("코치는 최소 2명, 최대 5명까지 식사를 함께 할 수 있습니다.");
         }
     }
+
+    public static List<String> readCanNotEatMenu() {
+        String input = Console.readLine();
+        validateCanNotEatMenuFormat(input);
+        return Arrays.stream(input.split(INPUT_DELIMITER))
+                .collect(Collectors.toList());
+    }
+
+    private static void validateCanNotEatMenuFormat(String input) {
+        String[] names = input.split(INPUT_DELIMITER, -1);
+        if (names.length > 2) {
+            throw new IllegalArgumentException("코치는 못 먹는 메뉴는 최소 0개, 최대 2개입니다.");
+        }
+    }
 }
