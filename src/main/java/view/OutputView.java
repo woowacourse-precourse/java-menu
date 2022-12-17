49433e7a9bade466d6e38ed98883dc00cda8c636
dclxxi <dclxxi@kakao.com>
2022-12-17 17:16:59 +0900

    feat: 서비스 종료 문구를 출력하는 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public void printStartService() {
         System.out.println("점심 메뉴 추천을 시작합니다.");
     }
+    
+    public void printEndOfService() {
+        System.out.println("추천을 완료했습니다.");
+    }
 }
