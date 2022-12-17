984038a24a2ed8406fa41b0907ce464bf3553c47
KSH <tkdgns543@gmail.com>
2022-12-17 13:35:53 +0900

    feat(domain): 코치 클래스 생성 및 이름과 못 먹는 메뉴를 저장하는 뼈대 구현

---
+++ src/main/java/menu/domain/Coach.java
@@ -0,0 +1,13 @@
+package menu.domain;
+
+import java.util.List;
+
+public class Coach {
+    private String name;
+    private List<String> bannedMenu;
+
+    Coach(String name, List<String> bannedMenu) {
+        this.name = name;
+        this. bannedMenu = bannedMenu;
+    }
+}
