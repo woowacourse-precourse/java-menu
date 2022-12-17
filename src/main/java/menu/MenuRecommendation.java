980eb040b5908f53e8be6ef36ce1a3c6a6141661
cpot5620 <=cpot5620@naver.com>
2022-12-17 14:06:41 +0900

    feat(model): create method to check validate category

--- src/main/java/menu/MenuRecommendation.java
+++ src/main/java/menu/MenuRecommendation.java
@@ -2,12 +2,34 @@ package menu;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuRecommendation {
-    
-    public Category getRandomCategory() {
-        int number = Randoms.pickNumberInRange(1, 5);
+    private static final int RECOMMEND_SIZE = 5;
+
+    List<Category> randomCategories = new ArrayList<>();
+
+    public List<Category> getRandomCategories() {
         Category[] categories = Category.values();
 
-        return categories[number - 1];
+        while (randomCategories.size() < RECOMMEND_SIZE) {
+            int number = Randoms.pickNumberInRange(1, 5);
+            Category category = categories[number - 1];
+
+            if (isValidCategory(category))
+                randomCategories.add(category);
+        }
+        return randomCategories;
+    }
+
+    private boolean isValidCategory(Category category) {
+        long numberOfCategory = randomCategories.stream()
+                .filter(s -> s.equals(category))
+                .count();
+        if (numberOfCategory > 2)
+            return false;
+
+        return true;
     }
 }
