e1ed0ef5005d4555333951f46fc14e734501e364
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-12-17 16:57:18 +0900

    test(validate): 밥을 함께 먹는 코치의 수가 2~5명인가

--- src/test/java/menu/model/validate/InputVerifierTest.java
+++ src/test/java/menu/model/validate/InputVerifierTest.java
@@ -15,13 +15,25 @@ class InputVerifierTest {
 
     @DisplayName("코치의 이름이 2글자 이상 4글자 이하인가")
     @Test
-    void methodName() {
+    void 코치이름이_2글자_이상_4글자_이하() {
         // given
         String coachName = "토미미미미";
 
         // when, then
         assertThrows(IllegalArgumentException.class, () -> {
             InputVerifier.coachNameIsMin2Max4(coachName);
+        });
+    }
+
+    @DisplayName("밥을 함께 먹는 코치의 수가 2~5명인가")
+    @Test
+    void 같이_밥_먹는_코치_수가_최소2명_최대5명() {
+        // given
+        int coachCount = 6;
+
+        // when, then
+        assertThrows(IllegalArgumentException.class, () -> {
+            InputVerifier.coachesCountMin2Max5(coachCount);
         });
     }
 
