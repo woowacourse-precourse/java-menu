8a06e34e0518f60725fc2d70b4ee36e9df610cfc
ckn1070 <ckn1070@naver.com>
2022-12-17 14:32:07 +0900

    feat(menu): 추천할 카테고리를 정하는 기능 구현

--- src/main/java/menu/Category.java
+++ src/main/java/menu/Category.java
@@ -2,13 +2,33 @@ package menu;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Category {
+
 
     public Category() {
     }
 
-    public String getCategory() {
-        String category = typeToCategory(Randoms.pickNumberInRange(1, 5));
+    public List<String> makeSchedule() {
+        List<Integer> scheduleCount = Arrays.asList(0, 0, 0, 0, 0);
+        List<String> schedule = new ArrayList<>();
+
+        while (schedule.size() < schedule.size()) {
+            int randomNumber = Randoms.pickNumberInRange(1, 5);
+
+            if (scheduleCount.get(randomNumber - 1) < 2) {
+                scheduleCount.set(randomNumber - 1, scheduleCount.get(randomNumber - 1) + 1);
+                schedule.add(getCategory(randomNumber));
+            }
+        }
+        return schedule;
+    }
+
+    public String getCategory(int type) {
+        String category = typeToCategory(type);
         return category;
     }
 
