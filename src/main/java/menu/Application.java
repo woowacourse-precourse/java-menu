fecd5c7a2cb060a6cf43295f0d45e91765acd503
shinD <huipulci1@naver.com>
2022-12-17 15:42:40 +0900

    feat: 기본 애플리케이션 동작 구현

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,26 @@
 package menu;
 
+import menu.common.ConsoleLogger;
+import menu.common.Logger;
+import menu.controller.MenuRecommendController;
+import menu.service.MenuRecommend;
+import menu.step.RecommendStartStep;
+import menu.step.Step;
+import menu.usecase.MenuRecommendUseCase;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        MenuRecommendUseCase menuRecommend = new MenuRecommend();
+        MenuRecommendController controller = new MenuRecommendController(menuRecommend);
+        Logger consoleLogger = new ConsoleLogger();
+        Step step = new RecommendStartStep(controller, consoleLogger);
+
+        run(step);
+    }
+
+    private static void run(Step step) {
+        while (step.executable()) {
+            step = step.execute();
+        }
     }
 }
