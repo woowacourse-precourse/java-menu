73dfc1c7f1ac1d2fbd043eb3bab283ac91e760c9
SH <gram753@naver.com>
2022-12-17 13:58:13 +0900

    feat: 상수 관리 클래스 구현

---
+++ src/main/java/menu/Constant.java
@@ -0,0 +1,8 @@
+package menu;
+
+public class Constant {
+	public static final String START_MSG = "점심 메뉴 추천을 시작합니다.\n";
+	public static final String COACH_INPUT_MSG = "코치의 이름을 입력해 주세요. (, 로 구분)";
+
+	public static final String MENU_INPUT_MSG = "%f(이)가 못 먹는 메뉴를 입력해 주세요.\n";
+}
