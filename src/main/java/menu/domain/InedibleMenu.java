b2c207a40e90e1f8d1e5cd346549b09c9b117f63
CAEL0 <kjongmin26@gmail.com>
2022-12-17 14:16:57 +0900

    feat(InedibleMenu): 못 먹는 메뉴 클래스 생성

---
+++ src/main/java/menu/domain/InedibleMenu.java
@@ -0,0 +1,17 @@
+package menu.domain;
+
+import menu.constant.MenuConstants;
+import menu.util.Validator;
+
+import java.util.List;
+
+public class InedibleMenu {
+    private final List<String> inedibleMenu;
+
+    public InedibleMenu(String inedibleMenu) {
+        List<String> splittedInedibleMenu = List.of(inedibleMenu.split(MenuConstants.DELIMITER));
+        Validator.validateDuplication(splittedInedibleMenu);
+        Validator.validateNumberOfInedibleMenus(splittedInedibleMenu.size());
+        this.inedibleMenu = splittedInedibleMenu;
+    }
+}
