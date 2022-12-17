b9049ac6816f1e6456f63e560c341c776d61d275
2014012606 <2014012606@hanyang.ac.kr>
2022-12-17 14:01:16 +0900

    feat: 코치에게 추천된 메뉴가 중복된 메뉴인지 확인하는 기능 추가

--- src/main/java/menu/model/Coach.java
+++ src/main/java/menu/model/Coach.java
@@ -1,17 +1,26 @@
 package menu.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Coach {
     private String name;
     private List<String> dislikeMenu;
+    private List<String> recommendedMenus = new ArrayList<>();
 
     public Coach(String name, List<String> dislikeMenu) {
         this.name = name;
         this.dislikeMenu = dislikeMenu;
     }
 
-    public boolean canEatMenu (String recommendedMenu) {
+    public boolean isDuplicateMenu(String recommendedMenu) {
+        if (recommendedMenus.contains(recommendedMenu)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean canEatMenu(String recommendedMenu) {
         if (dislikeMenu.contains(recommendedMenu)) {
             return false;
         }
