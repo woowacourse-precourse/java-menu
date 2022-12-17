dd0cdc4145ece75cfbaaba458f6a1d2bc382d458
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 14:29:05 +0900

    feat(FoodCategory): 음식 종류 enum 생성

---
+++ src/main/java/menu/constant/FoodCategory.java
@@ -0,0 +1,35 @@
+package menu.constant;
+
+import java.util.Arrays;
+
+public enum FoodCategory {
+    JAPAN(1, "일식"),
+    KOREA(2, "한식"),
+    CHINA(3, "중국"),
+    ASIA(4, "아시안"),
+    WEST(5, "양식");
+
+    private final int key;
+    private final String foodCategory;
+
+    FoodCategory(int key, String foodCategory) {
+        this.key = key;
+        this.foodCategory = foodCategory;
+    }
+
+    public static String findSthByKey(int key) {
+        return Arrays.stream(FoodCategory.values())
+                .filter(elem -> key == elem.key)
+                .findAny()
+                .orElseThrow()
+                .getFoodCategory();
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public String getFoodCategory() {
+        return foodCategory;
+    }
+}
