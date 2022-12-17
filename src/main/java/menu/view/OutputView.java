0f63dfcae76103a73f5bec72276983d4a0a101ca
This2sho <zx00018@naver.com>
2022-12-17 14:49:51 +0900

    feat(OutputView): 서비스 시작 문구 출력 기능 구현

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,13 @@
+package menu.view;
+
+public class OutputView {
+
+    public void printStart() {
+        System.out.println("점심 메뉴 추천을 시작합니다.");
+        printNewLine();
+    }
+
+    public void printNewLine() {
+        System.out.println();
+    }
+}
