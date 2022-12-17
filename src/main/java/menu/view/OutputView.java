79cbacebdf58f3a7b9027199fff08a8e0373a638
ksh-beginner <ohk9134@naver.com>
2022-12-17 15:06:22 +0900

    feat(OutputView) : 코치별 못 먹는 메뉴 입력 문구 출력 기능 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
 
     private static final String LUNCH_MENU_RECOMMEND_MESSAGE = "점심 메뉴 추천을 시작합니다.";
     private static final String READ_COACH_NAME_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private static final String CANNOT_EAT_FOODS_BY_COACH_MESSAGE = "(이)가 못 먹는 메뉴를 입력해 주세요.";
 
     public void printLunchMenuRecommendMessage() {
         printMessage(LUNCH_MENU_RECOMMEND_MESSAGE);
@@ -12,6 +13,10 @@ public class OutputView {
     public void printReadCoachNameMessage() {
         printBlankLine();
         printMessage(READ_COACH_NAME_MESSAGE);
+    }
+
+    public void printCannotEatFoodsByCoachMessage(String coachName) {
+        printMessage(coachName + CANNOT_EAT_FOODS_BY_COACH_MESSAGE);
     }
 
     private void printBlankLine() {
