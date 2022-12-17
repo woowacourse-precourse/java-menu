3cdd375eca86768ebcee10885ab57073759d5ca8
sosow0212 <sosow0212@naver.com>
2022-12-17 14:23:48 +0900

    feat : (Categories) 요일 별 카테고리 관리를 위한 도메인 생성

---
+++ src/main/java/menu/domain/Categories.java
@@ -0,0 +1,38 @@
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import menu.domain.menu.FoodStyle;
+
+public class Categories {
+    private List<String> categories;
+
+    public Categories() {
+        this.categories = new ArrayList<>();
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void addCategory(String categoryName) {
+        this.categories.add(categoryName);
+    }
+
+    public boolean isFullOfCategory(FoodStyle foodStyle) {
+        long count = categories.stream()
+                .filter(category -> category.equals(foodStyle.getStyle()))
+                .count();
+        if (count >= 2) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isDone() {
+        if (categories.size() == 5) {
+            return true;
+        }
+        return false;
+    }
+}
