21fcc5c15215e71c83ad5ca509c054956064e967
ESeokhwan <huan99@hanmail.net>
2022-12-17 15:30:27 +0900

    feat: N 개의 카테고리를 선택하는 기능 추가

--- src/main/java/menu/model/category/RandomCategoryGenerator.java
+++ src/main/java/menu/model/category/RandomCategoryGenerator.java
@@ -1,12 +1,42 @@
 package menu.model.category;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RandomCategoryGenerator {
-    public List<Category> pickCategories(int num) {
-        // TODO 구현 필요
+    private final int maxCountPerCategory;
 
-        return new ArrayList<>();
+    public RandomCategoryGenerator(int maxCountPerCategory) {
+        this.maxCountPerCategory = maxCountPerCategory;
+    }
+
+    public List<Category> pickCategories(int num) {
+        List<Category> randomCategoryList = new ArrayList<>();
+        Map<Category, Integer> categoryCountMap = makeCategoryCountMap();
+
+        for(int i = 0; i < num; i++) {
+            Category category = Category.get(Randoms.pickNumberInRange(1, 5));
+            if(categoryCountMap.get(category) == maxCountPerCategory) {
+                i--;
+                continue;
+            }
+            randomCategoryList.add(category);
+            categoryCountMap.put(category, (categoryCountMap.get(category) + 1));
+        }
+        return randomCategoryList;
+    }
+
+    private Map<Category, Integer> makeCategoryCountMap() {
+        Map<Category, Integer> categoryCountMap = new HashMap<>();
+        List<Category> allCategories = Category.getValuesWithAscendingOrder();
+
+        for(Category category: allCategories) {
+            categoryCountMap.put(category, 0);
+        }
+        return categoryCountMap;
     }
 }
