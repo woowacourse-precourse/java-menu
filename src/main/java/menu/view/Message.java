d999d971ace27a7ed44ffaa3c7ba7e32c1aa61fc
woo-chang <clllickme@naver.com>
2022-12-17 14:04:05 +0900

    feat(OutputView): 점심 메뉴 추천 시작을 출력하는 기능 구현

--- src/main/java/menu/view/Message.java
+++ src/main/java/menu/view/Message.java
@@ -2,7 +2,8 @@ package menu.view;
 
 public enum Message {
 
-    ERROR("[ERROR] %s");
+    ERROR("[ERROR] %s"),
+    SERVICE_START("점심 메뉴 추천을 시작합니다.");
 
     Message(String message) {
         this.message = message;
--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -2,6 +2,10 @@ package menu.view;
 
 public class OutputView {
 
+    public void printServiceStart() {
+        System.out.println(Message.SERVICE_START.getMessage());
+    }
+
     public static void printErrorMessage(String message) {
         System.out.println(String.format(Message.ERROR.getMessage(), message));
     }
