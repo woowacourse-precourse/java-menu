3bccf86eda90ad9fe9cd0de537d63c6c5c21869a
ckn1070 <ckn1070@naver.com>
2022-12-17 15:27:05 +0900

    feat(menu): 코치별로 메뉴를 추천하는 기능 구현

---
+++ src/main/java/menu/Recommendation.java
@@ -0,0 +1,42 @@
+package menu;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Recommendation {
+    private final Map<String, List<String>> coachInedible;
+    private final Map<String, List<String>> coachMenu = new HashMap<>();
+    private final Category category;
+    private final Menu menu;
+
+    public Recommendation(Map<String, List<String>> coachInedible) {
+        this.coachInedible = coachInedible;
+        this.category = new Category();
+        this.menu = new Menu();
+    }
+
+    public boolean addMenu(String coach, String menu) {
+        if (checkEdible(coach, menu) && checkNotDuplication(coach, menu)) {
+            List<String> currentMenu = coachMenu.get(coach);
+            currentMenu.add(menu);
+            coachMenu.replace(coach, currentMenu);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkNotDuplication(String coach, String menu) {
+        if (coachMenu.get(coach).contains(menu)) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean checkEdible(String coach, String menu) {
+        if (coachInedible.get(coach).contains(menu)) {
+            return false;
+        }
+        return true;
+    }
+}
