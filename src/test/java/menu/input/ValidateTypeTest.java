2d2f787a36fc25b7c22d52cf8124f219f9032c8c
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-12-17 15:52:17 +0900

    test(ValidateType): 메뉴 체크 테스트 생성

--- src/test/java/menu/input/ValidateTypeTest.java
+++ src/test/java/menu/input/ValidateTypeTest.java
@@ -56,4 +56,20 @@ public class ValidateTypeTest {
         boolean canRead = ValidationType.validate(ValidationType.CHECK_MENUS_COUNT, input);
         assertThat(canRead).isTrue();
     }
+
+    @DisplayName("존재하지 않는 메뉴 입력 예외 테스트")
+    @ValueSource(strings = {"비비밥", "복음면", "토마토달걀볶음", "김치", "한식"})
+    @ParameterizedTest
+    void checkMenu_예외(String input) {
+        boolean canRead = ValidationType.validate(ValidationType.CHECK_MENUS, input);
+        assertThat(canRead).isFalse();
+    }
+
+    @DisplayName("메뉴 개수에 따른 정상 테스트")
+    @ValueSource(strings = {"하이라이스", "불고기", "김치찌개", "스시"})
+    @ParameterizedTest
+    void checkMenu_정상(String input) {
+        boolean canRead = ValidationType.validate(ValidationType.CHECK_MENUS, input);
+        assertThat(canRead).isTrue();
+    }
 }
