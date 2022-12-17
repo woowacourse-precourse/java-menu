ec44025dcae479f5d179f9af950d5f58911836db
kimdahee0815 <diana815@naver.com>
2022-12-17 14:57:43 +0900

    feat(pickCategoryForAWeek) : 2개 이상 메뉴가 중복되지 않도록 pickCategoryForAWeek 함수 작성
    checkCategoryDuplicated, categoryCheckMoreThanTwo 함수를 통해서 2개 이상 메뉴가 중복되지 않게 5개까지 입력할 수 있도록 작성

--- src/test/java/menu/RecommendMenu.java
+++ src/test/java/menu/RecommendMenu.java
@@ -8,6 +8,7 @@ public class RecommendMenu {
 	
 	ArrayList<String> coachName;
 	LinkedHashMap<String, ArrayList<String>> notEatingMenuForEachCoach;
+	ArrayList<String> categoryForWeek = new ArrayList<>();
 	
 	enum categories {
 		JAPANESE("일식", 1), KOREAN("한식", 2), CHINESE("중식", 3), ASIAN("아시안", 4), WESTERN("양식", 5);
@@ -36,7 +37,38 @@ public class RecommendMenu {
 	public void printRecommendInstruction() {
 		System.out.println("메뉴 추천 결과입니다.");
 		System.out.println("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]");
-		System.out.println("[ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]");
+	}
+	
+	public void pickCategoryForAWeek() {
+		String category = "";
+		boolean checkPass = false;
+		for(int categoryCount = 0; categoryCount < 5;) {
+			category = pickRandomCategory();
+			if(!(checkPass = checkCategoryDuplicated(category))) {
+				continue;
+			}
+			categoryCount++;
+		}
+	}
+	
+	public boolean checkCategoryDuplicated(String category) {
+		if(categoryForWeek.contains(category)) {
+			if(categoryCheckMoreThanTwo(category) == 2) {
+				return false;
+			}
+		}
+		categoryForWeek.add(category);
+		return true;
+	}
+	
+	public int categoryCheckMoreThanTwo(String category) {
+		int count = 0;
+		for(String menu : categoryForWeek) {
+			if(menu.equals(category)) {
+				count++;
+			}
+		}
+		return count;
 	}
 	
 	public String pickRandomCategory() {
