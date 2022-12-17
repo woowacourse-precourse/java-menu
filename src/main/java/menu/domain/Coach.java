e7fc8217218fc372cc040183024a608828fe19d0
kat8813 <ohseo8813@gmail.com>
2022-12-17 13:54:14 +0900

    <feat>(Coach.java): 코치는 못먹는 메뉴들을 가진다. 최대 2개이다.

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,11 +1,15 @@
 package menu.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Coach {
 
     private final static int NAME_MAX_LENGTH = 4;
     private final static int NAME_MIN_LENGTH = 2;
 
     private final String name;
+    private final List<Menu> hateMenus = new ArrayList<>();
 
     public Coach(String name) {
         validate(name);
@@ -15,6 +19,13 @@ public class Coach {
     private void validate(String name) {
         if (name.length() < NAME_MIN_LENGTH || name.length() > NAME_MAX_LENGTH) {
             throw new IllegalArgumentException("코치의 이름은 최소 2글자, 최대 4글자입니다. 입력값 : " + name);
+        }
+    }
+
+    public void addHateMenus(List<Menu> menus) {
+        hateMenus.addAll(menus);
+        if (hateMenus.size() > 2) {
+            throw new IllegalArgumentException("코치가 못먹는 메뉴는 최대 2개입니다. 현재 못먹는 메뉴 갯수  : " + hateMenus.size());
         }
     }
 
