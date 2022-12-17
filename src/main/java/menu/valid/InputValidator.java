efbb389915aa7c8ab52091ee3ab53b9d55371f10
HaODay <ycis5013@naver.com>
2022-12-17 15:15:27 +0900

    feat(InputValidator): 못 먹는 메뉴 개수 검증 기능 구현

--- src/main/java/menu/valid/InputValidator.java
+++ src/main/java/menu/valid/InputValidator.java
@@ -5,10 +5,14 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static menu.util.NumberProperty.MAX_CANNOT_EAT_MENU_NUMBER;
 import static menu.util.NumberProperty.MAX_COACH_NAME_LENGTH;
 import static menu.util.NumberProperty.MAX_COACH_NUMBER;
 import static menu.util.NumberProperty.MIN_COACH_NAME_LENGTH;
 import static menu.util.NumberProperty.MIN_COACH_NUMBER;
+import static menu.util.message.ErrorMessage.CANNOT_EAT_MENU_OUT_OF_SCOPE;
+import static menu.util.message.ErrorMessage.COACH_NAME_OUT_OF_SCOPE;
+import static menu.util.message.ErrorMessage.COACH_NUMBER_OUT_OF_SCOPE;
 import static menu.util.message.ErrorMessage.DUPLICATED_COACH_NAME;
 
 public class InputValidator {
@@ -24,22 +28,44 @@ public class InputValidator {
     private static void validateDuplicatedCoachName(List<String> names) {
         for (int i = 0; i < names.size(); i++) {
             if (Collections.frequency(names, names.get(i)) > 1) {
-                throw new IllegalArgumentException(String.valueOf(DUPLICATED_COACH_NAME));
+                throw new IllegalArgumentException(DUPLICATED_COACH_NAME.get());
             }
         }
     }
 
     private static void validateNumberOfCoach(List<String> names) {
         if (names.size() < MIN_COACH_NUMBER || names.size() > MAX_COACH_NUMBER) {
-            throw new IllegalArgumentException("코치는 최소 2명, 최대 5명까지 식사를 함께 합니다.");
+            throw new IllegalArgumentException(COACH_NUMBER_OUT_OF_SCOPE.get());
         }
     }
 
     private static void validateCoachNameLength(List<String> names) {
         for (String name : names) {
             if (name.length() < MIN_COACH_NAME_LENGTH || name.length() > MAX_COACH_NAME_LENGTH) {
-                throw new IllegalArgumentException("코치의 이름은 최소 2글자, 최대 4글자입니다.");
+                throw new IllegalArgumentException(COACH_NAME_OUT_OF_SCOPE.get());
             }
         }
+    }
+
+    public static List<String> validateCannotEatMenu(String input){
+        if(isEmpty(input)){
+            return new ArrayList<>();
+        }
+        List<String> cannotEatMenu = new ArrayList<>(Arrays.asList(input.split(",")));
+        validateNumberOfCannotEatMenu(cannotEatMenu);
+        return cannotEatMenu;
+    }
+
+    private static void validateNumberOfCannotEatMenu(List<String> cannotEatMenu) {
+        if (cannotEatMenu.size() > MAX_CANNOT_EAT_MENU_NUMBER) {
+            throw new IllegalArgumentException(CANNOT_EAT_MENU_OUT_OF_SCOPE.get());
+        }
+    }
+
+    private static boolean isEmpty(String input) {
+        if(input.equals("")){
+            return true;
+        }
+        return false;
     }
 }
