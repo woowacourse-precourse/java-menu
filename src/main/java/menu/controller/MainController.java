d13eda984a9c11199bb5c776806526dc73f630e9
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 17:00:10 +0900

    feat(MainController): 시작 기능 구현

--- src/main/java/menu/controller/MainController.java
+++ src/main/java/menu/controller/MainController.java
@@ -1,13 +1,29 @@
 package menu.controller;
 
+import menu.domain.Category;
+import menu.service.CategoryService;
+import menu.service.RecommendationService;
+import menu.view.InputView;
+import menu.view.OutputView;
+import menu.view.constant.OutputPhrase;
+
 public class MainController {
     private static class InstanceHolder {
         private static final MainController INSTANCE = new MainController();
     }
 
+    private InputView inputView = InputView.getInstance();
+    private OutputView outputView = OutputView.getInstance();
+    private CategoryService categoryService = CategoryService.getInstance();
+    private RecommendationService recommendationService = RecommendationService.getInstance();
+
     private MainController(){}
 
     public static MainController getInstance() {
         return MainController.InstanceHolder.INSTANCE;
+    }
+
+    public void start() {
+        outputView.printPhrase(OutputPhrase.START.getPhrase());
     }
 }
