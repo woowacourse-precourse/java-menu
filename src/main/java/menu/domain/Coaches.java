dfba84ec4bf69752e78e0d6f7affcfbc2607f6be
Woojin Lee <dhkep03@gmail.com>
2022-12-17 13:59:26 +0900

    feat(Coaches): 복수의 코치 생성

--- src/main/java/menu/domain/Coaches.java
+++ src/main/java/menu/domain/Coaches.java
@@ -1,4 +1,18 @@
 package menu.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Coaches {
+    private final List<Coach> coaches;
+
+    public Coaches() {
+        this.coaches = new ArrayList<>();
+    }
+
+    public void addCoaches(List<String> coachNames) {
+        coachNames.forEach(name -> {
+            coaches.add(new Coach(name));
+        });
+    }
 }
