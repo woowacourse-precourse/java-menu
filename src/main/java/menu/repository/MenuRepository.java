5d47decdc7f4ad87c20ca0313526af4c156ad59c
JeonghakLee <wjdgkr0527@naver.com>
2022-12-17 13:23:18 +0900

    feat(MenuRepository): 메뉴 저장소 구현

--- src/main/java/menu/repository/MenuRepository.java
+++ src/main/java/menu/repository/MenuRepository.java
@@ -1,2 +1,18 @@
-package menu.repository;public class MenuRepository {
+package menu.repository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import menu.domain.Menu;
+
+public class MenuRepository {
+  private static final List<Menu> menus = new ArrayList<>();
+
+  public static List<Menu> menus() {
+    return Collections.unmodifiableList(menus);
+  }
+
+  public static void addStation(Menu menu) {
+    menus.add(menu);
+  }
 }
