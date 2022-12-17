76e1daf4690e6cfc2996b5ff38e21bfee80b710f
minaver <psmjoshua@gmail.com>
2022-12-17 14:33:17 +0900

    fix(Menu): 카테고리에 따라 객체 안에서 메뉴 추가 방식으로 변경

--- src/main/java/menu/domain/Menu.java
+++ src/main/java/menu/domain/Menu.java
@@ -7,18 +7,19 @@ import menu.enums.FoodCategory;
 import menu.vo.Coach;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Menu {
     private final FoodCategory foodCategory;
-    private final List<String> foods;
+    private List<String> foods;
 
-    public Menu(FoodCategory foodCategory, List<String> foods) {
+    public Menu(FoodCategory foodCategory) {
         this.foodCategory = foodCategory;
-        this.foods = foods;
+        initFoodsByCategory(foodCategory);
     }
 
-    // 카테고리 안에서 메뉴를 선택해준다.
+    // 코치의 정보를 넣으면 카테고리 안에서 메뉴를 선택해준다.
     public List<CoachWeekMenu> chooseShuffleMenu(List<CoachWeekMenu> coaches) {
         String menu;
         for (CoachWeekMenu coachWeekMenu : coaches) {
@@ -59,5 +60,20 @@ public class Menu {
             }
         }
         return true;
+    }
+
+    // 카테고리에 따라 foods 초기화
+    private void initFoodsByCategory(FoodCategory foodCategory) {
+        if (foodCategory.equals(FoodCategory.KOR)) {
+            foods = Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음");
+        } else if (foodCategory.equals(FoodCategory.JAP)) {
+            foods = Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼");
+        } else if (foodCategory.equals(FoodCategory.CHI)) {
+            foods = Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채");
+        } else if (foodCategory.equals(FoodCategory.ASI)) {
+            foods = Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜");
+        } else if (foodCategory.equals(FoodCategory.WES)) {
+            foods = Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니");
+        }
     }
 }
