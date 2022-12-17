5490ec1b27da6bcf709721a19b9e54011f3f5f8b
EUNJEONGMUN <munej26@gmail.com>
2022-12-17 13:40:04 +0900

    feat: 이름과 못먹는 메뉴를 가진 Coach 객체 생성

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,4 +1,17 @@
 package menu.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Coach {
+    private final CoachName name;
+    private final List<String> canNotEatMenus = new ArrayList<>();
+
+    public Coach(CoachName name) {
+        this.name = name;
+    }
+
+    public void addCanNotEatMenus(List<String> menus) {
+        canNotEatMenus.addAll(menus);
+    }
 }
