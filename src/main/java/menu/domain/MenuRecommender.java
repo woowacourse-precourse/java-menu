906e6a9b7271ac9e150d4861ec56c5565d612724
KSH <tkdgns543@gmail.com>
2022-12-17 13:41:16 +0900

    feat(domain): 이전까지 추천 된 카테고리 및 코치들을 저장하는 기능 구현

--- src/main/java/menu/domain/MenuRecommender.java
+++ src/main/java/menu/domain/MenuRecommender.java
@@ -1,4 +1,9 @@
 package menu.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuRecommender {
+    private List<String> categoryHistory = new ArrayList<>();
+    private List<String> coaches = new ArrayList<>();
 }
