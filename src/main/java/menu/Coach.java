7055d2f1e4ebd1b34bf18b70f4e5b19028dfa125
cpot5620 <=cpot5620@naver.com>
2022-12-17 14:46:10 +0900

    feat(model): create class to manage coach's information

--- src/main/java/menu/Coach.java
+++ src/main/java/menu/Coach.java
@@ -1,2 +1,32 @@
-package menu;public class Coach {
+package menu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Coach {
+    private String name;
+    private List<String> unwantedMenus = new ArrayList<>();
+    private List<String> menus = new ArrayList<>();
+
+    public Coach(String name) {
+        this.name = name;
+    }
+
+    public void setUnwantedMenus(String menus) {
+        unwantedMenus = Arrays.asList(menus.split(","));
+    }
+
+    public void setRecommendedMenus(List<String> menus) {
+        this.menus.addAll(menus);
+    }
+
+    public boolean isUnwantedMenu(String menu) {
+        return unwantedMenus.contains(menu);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
 }
