83794608973200f03584dd0fd9f955f904cfdcb4
yubin <dbqls2821@gmail.com>
2022-12-17 16:20:00 +0900

    feat: InputView에서 InputValidation을 사용하여 검증하는 로직 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,16 +1,21 @@
 package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import menu.validation.InputValidation;
 
 public class InputView {
 
+  private InputValidation inputValidation = new InputValidation();
+
   public String readCoachesName() {
     String input = Console.readLine();
+    inputValidation.validateCoachesName(input);
     return input;
   }
 
   public String readFoodCantEat() {
     String input = Console.readLine();
+    inputValidation.validateHateFood(input);
     return input;
   }
 }
