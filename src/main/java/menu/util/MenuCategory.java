0821b4ab72d82f751a90437b9b6e0380fa1dc9d9
HaODay <ycis5013@naver.com>
2022-12-17 17:31:30 +0900

    feat(MenuCategory): 일치하는 카테고리 찾는 기능 구현

--- src/main/java/menu/util/MenuCategory.java
+++ src/main/java/menu/util/MenuCategory.java
@@ -1,19 +1,34 @@
 package menu.util;
 
-public enum MenuCategory {
-    JAPANESE(1),
-    KOREAN(2),
-    CHINESE(3),
-    ASIAN(4),
-    ITALIAN(5);
+import java.util.Arrays;
 
+public enum MenuCategory {
+    JAPANESE("일식", 1),
+    KOREAN("한식", 2),
+    CHINESE("중식", 3),
+    ASIAN("아시안", 4),
+    ITALIAN("양식", 5);
+
+    private final String name;
     private final int randomNumber;
 
-    MenuCategory(int randomNumber) {
+    MenuCategory(String name, int randomNumber) {
+        this.name = name;
         this.randomNumber = randomNumber;
+    }
+
+    public String getName() {
+        return this.name;
     }
 
     public int get() {
         return this.randomNumber;
+    }
+
+    public static MenuCategory findCategory(int randomNumber) {
+        return Arrays.stream(MenuCategory.values())
+                .filter(category -> category.get() == randomNumber)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException());
     }
 }
