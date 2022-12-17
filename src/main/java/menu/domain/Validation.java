bc0fa3065cea6ff06d28b205a4a9d1c50f5c2eee
Miseong Kim <miseongk@naver.com>
2022-12-17 17:03:00 +0900

    refactor: 숫자 상수로 리팩토링

--- src/main/java/menu/domain/Validation.java
+++ src/main/java/menu/domain/Validation.java
@@ -2,26 +2,31 @@ package menu.domain;
 
 public class Validation {
 
+    private static final int MINIMUM_NUMBER = 2;
+    private static final int MAXIMUM_NUMBER = 5;
+    private static final int MINIMUM_SIZE = 2;
+    private static final int MAXIMUM_SIZE = 4;
+    private static final int MAXIMUM_MENU = 2;
     private static final String NUMBER_OF_COACHES_ERROR = "[ERROR] 코치는 최소 2명, 최대 5명을 입력해주세요.";
     private static final String NAME_SIZE_ERROR = "[ERROR] 코치의 이름은 최소 2글자, 최대 4글자로 입력해주세요.";
     private static final String NUMBER_OF_HATE_MENU_ERROR = "[ERROR] 최소 0개, 최대 2개의 못 먹는 메뉴를 입력해주세요.";
 
     public void validateNumberOfCoaches(String[] coaches) {
-        if (coaches.length < 2 || coaches.length > 5) {
+        if (coaches.length < MINIMUM_NUMBER || coaches.length > MAXIMUM_NUMBER) {
             throw new IllegalArgumentException(NUMBER_OF_COACHES_ERROR);
         }
     }
 
     public void validateNameSize(String[] coaches) {
         for (String coach: coaches) {
-            if (coach.length() < 2 || coach.length() > 4) {
+            if (coach.length() < MINIMUM_SIZE || coach.length() > MAXIMUM_SIZE) {
                 throw new IllegalArgumentException(NAME_SIZE_ERROR);
             }
         }
     }
 
     public void validateNumberOfHateMenu(String[] hateMenu) {
-        if (hateMenu.length > 2) {
+        if (hateMenu.length > MAXIMUM_MENU) {
             throw new IllegalArgumentException(NUMBER_OF_HATE_MENU_ERROR);
         }
     }
