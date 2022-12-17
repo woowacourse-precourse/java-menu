e421c4704b1de6b5520abbc6c90e7d805dd33247
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-12-17 17:50:19 +0900

    refactor: 코드 포맷팅

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -3,6 +3,7 @@ package menu;
 import menu.controller.MainController;
 
 public class Application {
+
     public static void main(String[] args) {
         MainController mainController = new MainController();
         mainController.init();
