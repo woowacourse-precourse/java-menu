cf76079b9c15eba4c88d7738961c489fcbb013d6
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2022-12-17 15:36:26 +0900

    feat(MainController): add 랜덤 메뉴 선정 과정

--- src/main/java/menu/controller/MainController.java
+++ src/main/java/menu/controller/MainController.java
@@ -1,5 +1,6 @@
 package menu.controller;
 
+import menu.enums.MenuOption;
 import menu.model.ChoiceMachine;
 import menu.model.Coach;
 import menu.model.CoachRepository;
@@ -10,6 +11,9 @@ import menu.view.OutputView;
 import java.util.List;
 
 public class MainController extends RepeatModule {
+
+    private static final int ZERO = 0;
+    private static final int DAY_COUNT = 5;
 
     private final ChoiceMachine choiceMachine = new ChoiceMachine();
     private final InputView inputView = new InputView();
@@ -32,19 +36,27 @@ public class MainController extends RepeatModule {
             new Coach(coachName, hateFoods);
         }
 
-        //TODO: 삭제해야 함
-        CoachRepository.print();
-
         choiceCategory();
     }
 
     public void choiceCategory() {
-        for (int day = 0; day < 5; day++) {
+        for (int day = ZERO; day < DAY_COUNT; day++) {
             choiceMachine.choiceCategory();
         }
 
         //TODO: 삭제해야 함
         CoachRepository.print();
+        System.out.println();
+
+        choiceMenu();
+    }
+
+    public void choiceMenu() {
+        for (Coach coach : CoachRepository.coaches()) {
+            for (int day = ZERO; day < DAY_COUNT; day++) {
+                choiceMachine.choiceMenu(coach);
+            }
+        }
 
         end();
     }
