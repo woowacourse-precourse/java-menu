0cf9d7ab7053062ca589a5260d216d0a55d82279
JUHA <khabh@naver.com>
2022-12-17 16:04:09 +0900

    refactor(MenuRepository): enum 활용해서 메뉴 저장
    가독성을 고려하여 메뉴의 문자열 리스트를 MENU enum에 저장

--- src/main/java/menu/repository/MenuRepository.java
+++ src/main/java/menu/repository/MenuRepository.java
@@ -1,6 +1,7 @@
 package menu.repository;
 
 import menu.domain.Category;
+import menu.domain.MENU;
 import menu.domain.menucategory.MenuCategory;
 
 import java.util.HashMap;
@@ -12,11 +13,11 @@ public class MenuRepository {
     private final static HashMap<MenuCategory, List<String>> menus = new HashMap<>();
 
     static {
-        menus.put(categories.get(Category.JAPAN.value()), List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼"));
-        menus.put(categories.get(Category.KOREAN.value()), List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"));
-        menus.put(categories.get(Category.CHINA.value()), List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채"));
-        menus.put(categories.get(Category.ASIAN.value()), List.of("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜"));
-        menus.put(categories.get(Category.WESTERN.value()), List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
+        menus.put(categories.get(Category.JAPANESE.order()), MENU.JAPANESE.value());
+        menus.put(categories.get(Category.KOREAN.order()), MENU.KOREAN.value());
+        menus.put(categories.get(Category.CHINESE.order()), MENU.CHINESE.value());
+        menus.put(categories.get(Category.ASIAN.order()), MENU.ASIAN.value());
+        menus.put(categories.get(Category.WESTERN.order()), MENU.WESTERN.value());
     }
 
     private static boolean containsMenu(String menu, MenuCategory menuCategory) {
