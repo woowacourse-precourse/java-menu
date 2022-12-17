d611e7e87fcc14bc4acfbfc31e25ab83aba7d013
khsrla9806 <kimhunsope@naver.com>
2022-12-17 14:11:45 +0900

    feat(Menu): 카테고리와 그에 상응되는 메뉴를 저장하는 도메인 클래스 구현

---
+++ src/main/java/menu/domain/Menu.java
@@ -0,0 +1,20 @@
+package menu.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Menu {
+    private Map<String, List<String>> menu = new HashMap<>() {{
+        put("일식", List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼"));
+        put("한식", List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"));
+        put("중식", List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채"));
+        put("아시안", List.of("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜"));
+        put("양식", List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
+    }};
+
+    public Map<String, List<String>> getMenu() {
+        return menu;
+    }
+}
