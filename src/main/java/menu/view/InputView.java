098aaa05eb63724b168283d57a90534da0fc133c
Guga <103228463+BGuga@users.noreply.github.com>
2022-12-17 15:57:14 +0900

    feat: 코치가 못먹는 음식을 입력받는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -2,7 +2,9 @@ package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import menu.domain.Coach;
+import menu.domain.FoodCategory;
 import menu.domain.FoodPeerCoaches;
+import menu.domain.Menu;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,13 +13,24 @@ import java.util.List;
 public class InputView {
 
     public FoodPeerCoaches readCoachPeers() {
-        try{
+        try {
             System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
             return new FoodPeerCoaches(makeFoodPeerCoachByConsole());
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             OutputView.printErrorMessage(e.getMessage());
             return readCoachPeers();
         }
+    }
+
+    public List<Menu> readHateFoodOf(Coach coach) {
+        try {
+            System.out.println(String.format("%s(이)가 못 먹는 메뉴를 입력해 주세요.", coach.getName()));
+            return readMenuListByConsole();
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            return readHateFoodOf(coach);
+        }
+
     }
 
     private List<Coach> makeFoodPeerCoachByConsole() {
@@ -27,8 +40,21 @@ public class InputView {
 
     private List<Coach> makeStringListToCoach(List<String> coachList) {
         List<Coach> result = new ArrayList<>();
-        for (String coach : coachList){
+        for (String coach : coachList) {
             result.add(new Coach(coach));
+        }
+        return result;
+    }
+
+    private List<Menu> readMenuListByConsole() {
+        List<String> menuData = Arrays.asList(Console.readLine().split(","));
+        return makeStringListToMenu(menuData);
+    }
+
+    private List<Menu> makeStringListToMenu(List<String> menuData) {
+        List<Menu> result = new ArrayList<>();
+        for (String menu : menuData) {
+            result.add(FoodCategory.getMenuOf(menu));
         }
         return result;
     }
