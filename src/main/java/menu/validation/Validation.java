7323d24e6e8f9d12aa05fec8962c6dc4934c46cc
seokhwan-an <asok369258@naver.com>
2022-12-17 13:43:45 +0900

    feat : 입력된 코치의 수가 최소 2명에서 최대 5명인지 검증하는 기능 추가

--- src/main/java/menu/validation/Validation.java
+++ src/main/java/menu/validation/Validation.java
@@ -6,8 +6,16 @@ public class Validation {
 
     private static final int NAME_MIN_LENGTH = 2;
     private static final int NAME_MAX_LENGTH = 4;
+    private static final int COACH_MIN_LENGTH = 2;
+    private static final int COACH_MAX_LENGTH = 5;
     private static final String ERROR_COACH_NAME_LENGTH = "[ERROR] 코치의 이름은 최소2글자 최대 4글자여야 합니다.";
+    private static final String ERROR_COACH_LENGTH = "[ERROR] 코치는 최소 2명 이상 최대 5명까지 입력해야 합니다.";
 
+    public static void validateCoachsLength(List<String> coachName) {
+        if(!(COACH_MIN_LENGTH <= coachName.size() && coachName.size() <= COACH_MAX_LENGTH)) {
+            throw new IllegalArgumentException(ERROR_COACH_LENGTH);
+        }
+    }
 
     public static void validateCoachNameLength(List<String> coachName) {
         boolean checkNameLength = coachName.stream()
