66dda395827fbd821cb6dd30b61c208c43350b74
minaver <psmjoshua@gmail.com>
2022-12-17 14:52:17 +0900

    feat(InputView): 코치 별 못먹는 메뉴 입력 기능 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -2,6 +2,8 @@ package menu.view;
 
 
 import camp.nextstep.edu.missionutils.Console;
+import menu.Constant;
+import menu.vo.Coach;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,11 +13,23 @@ public class InputView {
 
     public List<String> getCoachName() {
         System.out.println("점심 메뉴 추천을 시작합니다. \n\n 코치의 이름을 입력해 주세요. (, 로 구분)");
-        String input = Console.readLine();
-        return trimEachInput(Arrays.asList(input.split(",")));
+        return trimEachInput(splitInput(Console.readLine()));
     }
 
-    
+    public List<Coach> getCoachNotEat(List<String> coachNames) {
+        List<Coach> coaches = new ArrayList<>();
+        for (String name : coachNames) {
+            System.out.printf(Constant.inputNotEatFood,name);
+            List<String> notEatFoods = trimEachInput(splitInput(Console.readLine()));
+            coaches.add(new Coach(name,notEatFoods));
+        }
+        return coaches;
+    }
+
+    // 입력값을 "," 단위로 분해
+    private List<String> splitInput(String input) {
+        return Arrays.asList(input.split(","));
+    }
 
     // 입력값 각각을 trim
     private List<String> trimEachInput(List<String> inputs) {
