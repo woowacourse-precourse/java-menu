e4a3ec6357cf2b8d9eb0ac2b9befe0b62d981faf
jojaeng2 <ds4ouj@naver.com>
2022-12-17 16:54:59 +0900

    feat : 서비스를 호출할 컨트롤러 기능 구현

---
+++ src/main/java/menu/controller/MenuController.java
@@ -0,0 +1,54 @@
+package menu.controller;
+
+import menu.domain.Coach;
+import menu.service.CategoryService;
+import menu.service.CoachService;
+import menu.service.RecommendMenuService;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class MenuController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final CategoryService categoryService;
+    private final CoachService coachService;
+    private final RecommendMenuService recommendMenuService;
+
+    public MenuController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.categoryService = new CategoryService();
+        outputView.printServiceStart();
+        outputView.printRequestCoachName();
+        this.coachService = new CoachService(initCoachList());
+        this.recommendMenuService = new RecommendMenuService();
+    }
+
+    public void run() {
+        List<Coach> coaches = coachService.getCoaches();
+        coaches.forEach(this::initRefuseMenu);
+        List<String> categories = categoryService.getCategories();
+        recommendMenuService.recommend(coaches, categories);
+        outputView.printResult();
+        outputView.printCategoryResult(categories);
+        outputView.printRecommendMenus(coaches);
+        outputView.printServiceExit();
+    }
+
+
+    private void initRefuseMenu(Coach coach) {
+        outputView.printRequestRefuseMenu(coach);
+        List<String> refuseMenu = inputView.readRefuseMenu();
+        coachService.addRefuseMenuToCoach(coach, refuseMenu);
+    }
+
+    private List<Coach> initCoachList() {
+        return inputView.readCoach()
+                .stream().map(Coach::new)
+                .collect(toList());
+    }
+}
