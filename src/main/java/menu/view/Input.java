0ffdd33dbd2aced0e64bdfe813433febf903c01a
solesy <tkddbs9801@gmail.com>
2022-12-17 14:10:35 +0900

    feat(view): 입력 클래스 구현

---
+++ src/main/java/menu/view/Input.java
@@ -0,0 +1,30 @@
+package menu.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import menu.ValidInputChecker;
+
+import java.util.List;
+
+public class Input {
+    public static List<String> readCoachesName() {
+        while(true){
+            try{
+                System.out.println("점심 메뉴 추천을 시작합니다.");
+                return ValidInputChecker.validateCoachesAndToList(Console.readLine());
+            } catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static List<String> readNoFoods(String coachName) {
+        while(true){
+            try{
+                System.out.println(coachName + "(이)가 못 먹는 메뉴를 입력해 주세요.");
+                return ValidInputChecker.validateNoFoodsAndToList(Console.readLine());
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
