e8b7ebab0f8a88f25f755822a4df8aef132ab0cd
CAEL0 <kjongmin26@gmail.com>
2022-12-17 13:27:41 +0900

    feat(OutputView): 프로그램 시작 문구 출력

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,9 @@
+package menu.view;
+
+public class OutputView {
+
+    public void printStartingRecommendation() {
+        System.out.println(ViewConstants.STARTING_RECOMMENDATION);
+        System.out.println();
+    }
+}
