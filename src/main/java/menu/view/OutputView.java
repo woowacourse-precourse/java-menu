8d525bb5ec9f338c5603c4a3983d0b927176eba4
CAEL0 <kjongmin26@gmail.com>
2022-12-17 13:28:35 +0900

    feat(OutputView): 추천 완료 문구 출력

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -6,4 +6,8 @@ public class OutputView {
         System.out.println(ViewConstants.STARTING_RECOMMENDATION);
         System.out.println();
     }
+
+    public void printCompletion() {
+        System.out.println(ViewConstants.COMPLETION);
+    }
 }
