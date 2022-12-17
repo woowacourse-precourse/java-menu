170dd951614b693fd4a10ecc95f00ed8787faaf5
lahezy <jouhi3322@gmail.com>
2022-12-17 14:06:33 +0900

    feat : 싫어하는 음식을 입력받는다

--- src/main/java/controller/MenuRecommendApplication.java
+++ src/main/java/controller/MenuRecommendApplication.java
@@ -1,17 +1,50 @@
 package controller;
 
+import menu.domain.Category;
 import menu.view.InputView;
 import menu.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MenuRecommendApplication {
     private InputView inputView = new InputView();
     private OutputView outputView = new OutputView();
+    private List<String> allMenu = new ArrayList<>();
+
+    public MenuRecommendApplication() {
+        List<List<String>> allMenuPerCategory = Category.getAllMenu();
+        for (List<String> menus : allMenuPerCategory) {
+            allMenu.addAll(menus);
+        }
+        //System.out.println(allMenu);
+    }
 
     public void start() {
         System.out.println("점심 메뉴 추천을 시작합니다.");
         List<String> coachNames = inputView.scanCoachNames();
         //System.out.println(coachNames);
+        List<List<String>> hatesFood = scanHatesFood(coachNames);
+
+//        토미(이)가 못 먹는 메뉴를 입력해 주세요.
+    }
+
+    private List<List<String>> scanHatesFood(List<String> coachNames) {
+        List<List<String>> hatesFood = new ArrayList<>();
+        for (String name : coachNames) {
+            List<String> eachHatesFood = inputView.scanHatesFood(name);
+            //있는 음식인지 확인 해야한다.
+            checkValidFood(eachHatesFood);
+            hatesFood.add(eachHatesFood);
+        }
+        return hatesFood;
+    }
+
+    private void checkValidFood(List<String> hatesFood) {
+        for (String s : hatesFood) {
+            if (!allMenu.contains(s)) {
+                throw new IllegalArgumentException("[ERROR] 없는 메뉴 입니다.");
+            }
+        }
     }
 }
--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class InputView {
     public List<String> scanCoachNames() {
         try {
+            System.out.println();
             System.out.println("코치의 이름을 입력해 주세요(, 로 구분)");
             List<String> names = Arrays.asList(Console.readLine().split(","));
             return validateNames(names);
@@ -22,5 +23,25 @@ public class InputView {
         }
         //TODO : 이름 글자수 확인 해야함
         return names;
+    }
+
+    public List<String> scanHatesFood(String name) {
+        try {
+            System.out.println();
+            System.out.printf("%s(이)가 못 먹는 메뉴를 입력해 주세요.\n",name);
+            List<String> hates = Arrays.asList(Console.readLine().split(","));
+            return validateHatesFood(hates);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR]싫어하는 음식을 잘못 입력 하셨습니다.");
+        }
+
+
+    }
+
+    private List<String> validateHatesFood(List<String> hates) {
+        if (2 < hates.size()) {
+            throw new IllegalArgumentException("[ERROR] 싫어 하는 음식을 너무 많이 입력 하셨습니다.");
+        }
+        return hates;
     }
 }
