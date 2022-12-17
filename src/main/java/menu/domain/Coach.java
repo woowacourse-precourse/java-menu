833a680b694f528fb4b33a2fb0755adbd73676cf
Cyma-s <ssol0319@naver.com>
2022-12-17 13:45:53 +0900

    feat(Coach) : 코치 정보 생성자 생성

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,8 +1,16 @@
 package menu.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Coach {
-    private String coachName;
-    private List<Menu> cannotEatMenus;
+    private final String coachName;
+    private final List<Menu> cannotEatMenus = new ArrayList<>();
+
+    public Coach(String coachName, List<String> cannotEatMenus) {
+        this.coachName = coachName;
+        for(String menu : cannotEatMenus) {
+            this.cannotEatMenus.add(new Menu(menu));
+        }
+    }
 }
