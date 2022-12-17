fbbfdb326f3c9e44a22e2e9580f98bf15697c539
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-12-17 16:37:49 +0900

    feat(OutputView): printCategory 메서드 생성

--- src/main/java/menu/output/OutputView.java
+++ src/main/java/menu/output/OutputView.java
@@ -1,11 +1,14 @@
 package menu.output;
 
+import java.util.List;
+
 public class OutputView {
     private static final String startMessage = "점심 메뉴 추천을 시작합니다.\n";
     private static final String inputCoachMessage = "코치의 이름을 입력해 주세요. (, 로 구분)";
     private static final String inputMenuMessage = "(이)가 못 먹는 메뉴를 입력해 주세요.\n";
     private static final String resultStartMessage = "메뉴 추천 결과입니다.\n";
     private static final String endMessage = "추천을 완료했습니다.";
+    private static final String categoryMessage = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]";
 
     public void printStart() {
         System.out.println(startMessage);
@@ -20,4 +23,15 @@ public class OutputView {
         System.out.println(message);
     }
 
+    public void printResult(List<String> category) {
+        System.out.println(resultStartMessage);
+        System.out.println(categoryMessage);
+        printCategory(category);
+
+    }
+    
+    private void printCategory(List<String> category) {
+        String message = "[ " + String.join(" | ", category) + " ]";
+        System.out.println(message);
+    }
 }
