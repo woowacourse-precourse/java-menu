61e9d144d7bba669b6b915cd37c89ecd61122b34
greeng00se <thegreengoose7@gmail.com>
2022-12-17 15:04:47 +0900

    refactor(InputView): InvalidMenusDto를 받아 코치님들이 못먹는 음식을 설정하도록 수정

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,26 +1,14 @@
 package menu;
 
+import static menu.config.AppConfig.coachService;
+import static menu.config.AppConfig.menuService;
+
 import menu.controller.MenuController;
-import menu.repository.CoachRepository;
-import menu.repository.MenuRepository;
-import menu.service.MenuService;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        MenuController menuController = new MenuController(menuService());
+        MenuController menuController = new MenuController(menuService(), coachService());
         menuController.run();
-    }
-
-    private static MenuService menuService() {
-        return new MenuService(menuRepository(), coachRepository());
-    }
-
-    private static MenuRepository menuRepository() {
-        return new MenuRepository();
-    }
-
-    private static CoachRepository coachRepository() {
-        return new CoachRepository();
     }
 }
--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -6,12 +6,16 @@ import camp.nextstep.edu.missionutils.Console;
 import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.stream.Collectors;
+import menu.domain.Menu;
 import menu.dto.CoachNameDto;
+import menu.dto.InvalidMenusDto;
 
 public class InputView {
 
     private static final String READ_COACH_NAMES_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
     private static final String READ_MENU_NAMES_MESSAGE = "{0}(이)가 못 먹는 메뉴를 입력해 주세요.";
+    private static final String DELIMITER = ",";
+    private static final int SPLIT_INDEX = -1;
 
     public CoachNameDto readCoachNames() {
         System.out.println(READ_COACH_NAMES_MESSAGE);
@@ -20,13 +24,19 @@ public class InputView {
     }
 
     private CoachNameDto toCoachName(String names) {
-        return Arrays.stream(names.split(",", -1))
+        return Arrays.stream(names.split(DELIMITER, SPLIT_INDEX))
                 .collect(Collectors.collectingAndThen(toList(), CoachNameDto::new));
     }
 
-    public String readMenuNames(String name) {
+    public InvalidMenusDto readMenuNames(String name) {
         System.out.println(MessageFormat.format(READ_MENU_NAMES_MESSAGE, name));
-        String menuNames = Console.readLine();
-        return menuNames;
+        String names = Console.readLine();
+        return toMenuNames(names);
+    }
+
+    private InvalidMenusDto toMenuNames(String names) {
+        return Arrays.stream(names.split(DELIMITER, SPLIT_INDEX))
+                .map(Menu::new)
+                .collect(Collectors.collectingAndThen(toList(), InvalidMenusDto::new));
     }
 }
