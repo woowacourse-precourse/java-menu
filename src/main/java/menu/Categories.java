0dc84e9fb02de43bf0dec9afbcfa5cb6ec8dfc22
WhalesBob <ndw8200@naver.com>
2022-12-17 13:36:04 +0900

    feat : 카테고리 기능 초기 추가, 이후 메뉴 업데이트 하는 기능 추가

---
+++ src/main/java/menu/Categories.java
@@ -0,0 +1,26 @@
+package menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum Categories {
+    JPN("일식"),
+    KOR("한식"),
+    CHN("중식"),
+    ASN("아시안"),
+    WES("양식");
+
+    private String name;
+    private List<String> menues;
+
+    Categories(String name) {
+        this.name = name;
+        this.menues = new ArrayList<>();
+    }
+
+    public static void updateMenu(Categories category,List<String> categoryMenu){
+        category.menues.addAll(categoryMenu);
+    }
+
+
+}
