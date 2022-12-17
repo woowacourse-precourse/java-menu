ca863e88e85e6bc42b6b085e3a74c46c91b55324
starwook <star.wookk@gmail.com>
2022-12-17 17:05:22 +0900

    feat: Add validateName method
    이름 길이 검증

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,18 +1,27 @@
 package menu.domain;
 
+import menu.error.ErrorResource;
 import menu.resource.Day;
 
 import java.util.*;
 
 public class Coach {
+    private static int MIN_NAME_LENGTH = 2;
+    private static int MAX_NAME_LENGTH = 4;
 
     private String name;
     private Map<Day,String> foodPerDay = new LinkedHashMap<>();
     private List<String> foodCantEat = new ArrayList<>();
 
     public Coach(String name){
+        validateName(name);
         this.name = name;
         initiateFoodPerDay();
+    }
+    public void validateName(String name){
+        if(name.length()<MAX_NAME_LENGTH || name.length()>MAX_NAME_LENGTH){
+            throw new IllegalArgumentException(ErrorResource.ERROR_START+ErrorResource.WRONG_COACH_NAME_LENGTH);
+        }
     }
 
     public void addCantEatFood(Food food){
