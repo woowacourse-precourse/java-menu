6aff89f75bfbfee3c00c292bd7ad6a88854e809d
CAEL0 <kjongmin26@gmail.com>
2022-12-17 14:29:52 +0900

    feat(Menu): 코치 등록 기능 추가

--- src/main/java/menu/domain/Menu.java
+++ src/main/java/menu/domain/Menu.java
@@ -3,11 +3,12 @@ package menu.domain;
 import menu.constant.MenuConstants;
 import menu.util.Validator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Menu {
     private final List<String> coachNames;
-    private List<Coach> coaches;
+    private final List<Coach> coaches = new ArrayList<>();
 
     public Menu(String coachNames) {
         List<String> splittedCoaches = List.of(coachNames.split(MenuConstants.DELIMITER));
@@ -19,6 +20,10 @@ public class Menu {
                 .forEach(Validator::validateCoachNameLength);
 
         this.coachNames = splittedCoaches;
+    }
+
+    public void addCoach(String name, String inedibleMenu) {
+        coaches.add(new Coach(name, inedibleMenu));
     }
 
     public List<String> getCoachNames() {
