404b5dd54e13b84d27790799e04941efc4e8a9d0
sonsoowon <yuwoo1018@gmail.com>
2022-12-17 16:18:39 +0900

    feat(MenuHistory): 메뉴 추천 기록 관리 클래스

--- src/main/java/menu/domain/MenuHistory.java
+++ src/main/java/menu/domain/MenuHistory.java
@@ -1,4 +1,30 @@
 package menu.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class MenuHistory {
+    private final Map<Day, Menu> weekMenus;
+
+    private MenuHistory(Map<Day, Menu> weekMenus) {
+        this.weekMenus = weekMenus;
+    }
+
+    public void addMenuOfDay(Menu menu, Day day) {
+        if(!hasMenu(menu)) {
+            weekMenus.put(day, menu);
+        }
+    }
+    public boolean hasMenu(Menu menu) {
+        return weekMenus.containsValue(menu);
+    }
+
+    public List<Menu> getMenus() {
+        List<Menu> menus = new ArrayList<>();
+        for (Day day : Day.values()) {
+            menus.add(weekMenus.get(day));
+        }
+        return menus;
+    }
 }
