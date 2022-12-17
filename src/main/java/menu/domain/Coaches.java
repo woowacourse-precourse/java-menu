659334bf852ebcabb37715e3a707d661c1e782f4
anneu <b.imnuj@gmail.com>
2022-12-17 14:19:46 +0900

    feat: 코치가 2명 ~ 5명이 아닐 경우 예외 처리 기능 구현

--- src/main/java/menu/domain/Coaches.java
+++ src/main/java/menu/domain/Coaches.java
@@ -6,6 +6,17 @@ public class Coaches {
     private final List<Coach> coaches;
 
     public Coaches(List<Coach> coaches) {
+        validateSize(coaches);
         this.coaches = coaches;
+    }
+
+    private void validateSize(List<Coach> coaches) {
+        if (!isValidSize(coaches.size())) {
+            throw new IllegalArgumentException("[ERROR] 코치는 2명 ~ 5명까지 가능합니다.");
+        }
+    }
+
+    private static boolean isValidSize(int size) {
+        return 2 <= size && size <= 5;
     }
 }
