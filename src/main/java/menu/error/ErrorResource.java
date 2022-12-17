cc977349189a9556b9f4e381f11cb2cc8f909440
starwook <star.wookk@gmail.com>
2022-12-17 16:52:52 +0900

    feat: Add 에러 메시지 목록

---
+++ src/main/java/menu/error/ErrorResource.java
@@ -0,0 +1,9 @@
+package menu.error;
+
+public class ErrorResource {
+    public static String ERROR_START = "[ERROR] ";
+    public static String COACH_SMALL = "코치 숫자 부족.";
+    public static String COACH_BIG = "코치 숫자 초과.";
+    public static String NO_EXIST_MENU = "없는 메뉴입니다.";
+    public static String TOO_MANY_CANT_EAT ="먹지 못 하는 메뉴가 너무 많습니다.";
+}
