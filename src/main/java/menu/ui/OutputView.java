46b3e53eeafb489d40fc0ba3b3b5060728efe0e8
Woojin Lee <dhkep03@gmail.com>
2022-12-17 13:50:02 +0900

    feat(OutputView): 콘솔에 공백 추가

--- src/main/java/menu/ui/OutputView.java
+++ src/main/java/menu/ui/OutputView.java
@@ -4,5 +4,10 @@ public class OutputView {
 
     public void announceStart() {
         System.out.println("점심 메뉴 추천을 시작합니다.");
+        addEmptyLine();
+    }
+
+    private void addEmptyLine() {
+        System.out.println();
     }
 }
