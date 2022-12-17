b1ac51fb071124efeb477b16db28774631f5a6f2
taeyun-work <wmfajr1357@gmail.com>
2022-12-17 16:58:38 +0900

    refactor: 메소드 명에 통일성을 부여하기 위해 메소드 명 수정
    cantEat -> unEatableMenu로 수정함

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -6,11 +6,12 @@ public class InputView {
 
     private static final String INPUT_COACH_NAMES = "코치의 이름을 입력해 주세요. (, 로 구분)";
     private static final String INPUT_UNEATABLE_MENUS = "(이)가 못 먹는 메뉴를 입력해 주세요.";
+    public static final String INPUT_SEPARATOR = ",";
     public void coachNames() {
         System.out.println(INPUT_COACH_NAMES);
     }
 
-    public void cantEat(Coach coach) {
+    public void unEatableMenu(Coach coach) {
         System.out.println(coach.getName() + INPUT_UNEATABLE_MENUS);
     }
 }
