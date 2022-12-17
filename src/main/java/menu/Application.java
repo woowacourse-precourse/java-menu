273398c1ce709391b7822f21ec7e5376c682050f
starwook <star.wookk@gmail.com>
2022-12-17 16:56:43 +0900

    refactor(Application.java): Refacter main method

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -2,6 +2,8 @@ package menu;
 
 public class Application {
     public static void main(String[] args) {
+        MenuController menuMachine = new MenuController();
+        menuMachine.init();
         // TODO: 프로그램 구현
     }
 }
