0ff4c5192da37d40a8ecfc79e8ae8d3e32da35b1
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 13:36:27 +0900

    feat: 에러 메시지를 반환하는 출력문 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -10,4 +10,8 @@ public class OutputView {
     public static void printInputCoaches() {
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
     }
+
+    public static void printError(String msg) {
+        System.out.println("[ERROR] " + msg);
+    }
 }
