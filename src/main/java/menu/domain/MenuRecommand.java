5703c2c527679b68fb5b7deaf651fe6c4e3a76a7
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 14:13:49 +0900

    feat: 각 코치가 먹지 못하는 메뉴를 저장하는 기능 구현

--- src/main/java/menu/domain/MenuRecommand.java
+++ src/main/java/menu/domain/MenuRecommand.java
@@ -1,2 +1,18 @@
-package menu.domain;public class MenuRecommand {
+package menu.domain;
+
+import menu.view.InputView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MenuRecommand {
+    private HashMap<String,List<String>> hs = new HashMap<>();
+    public MenuRecommand(){}
+
+    public void play() {
+        List<String> coaches = InputView.inputCoaches();
+        for (int i = 0; i < coaches.size(); i++)
+            hs.put(coaches.get(i),InputView.inputInEdibleMenu(coaches.get(i)));
+    }
 }
