10ca8ee6243287354ca4b182b4b9acbdd188f9ac
wnsgml7267 <dlwns7267@naver.com>
2022-12-17 17:38:03 +0900

    refactor(Application) : 오타 수정

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,10 +1,8 @@
 package menu;
 
-import menu.controller.MenuController;
-
 public class Application {
     public static void main(String[] args) {
-        MenuController system = new MenuController();
-        system.run();
+        Controller controller = new Controller();
+        controller.run();
     }
 }
