73137c569b646fe81d9c93ddeafe2013ab9c1d05
YoosunMoon <muzikue@naver.com>
2022-12-17 15:55:36 +0900

    feat: 먹지 못하는 메뉴 입력 받은 후, 유효성 검사

--- src/main/java/menu/InputView.java
+++ src/main/java/menu/InputView.java
@@ -12,6 +12,7 @@ public class InputView {
     private static final String NAME_COUNT_ERROR_MESSAGE = "[ERROR] 코치 이름을 2명 이상 5명 이하로 입력해주세요.";
     private static final String NAME_SIZE_ERROR_MESSAGE = "[ERROR] 코치 이름은 2글자 이상 4글자 이하입니다.";
     private static final String NAME_DUPLICATE_ERROR_MESSAGE = "[ERROR] 코치 이름이 중복되었습니다.";
+    private static final String HATE_MENU_SIZE_ERROR_MESSAGE = "[ERROR] 먹지 못하는 메뉴는 최대 2개까지 입력 가능합니다.";
 
     public static List<String> inputNames() {
         while (true) {
@@ -25,6 +26,21 @@ public class InputView {
                 checkNameSize(coachNames);
                 checkNameDuplicate(coachNames);
                 return coachNames;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static List<String> inputHateFoods() {
+        while (true) {
+            try {
+                String menus = Console.readLine();
+                List<String> hateMenus = Arrays.stream(menus.split(","))
+                        .map(String::trim)
+                        .collect(Collectors.toList());
+                checkHateMenusCount(hateMenus);
+                return hateMenus;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
@@ -51,4 +67,17 @@ public class InputView {
             throw new IllegalArgumentException(NAME_DUPLICATE_ERROR_MESSAGE);
         }
     }
+
+    private static void checkHateMenusCount(List<String> menus) {
+        if (menus.size() > 2) {
+            throw new IllegalArgumentException(HATE_MENU_SIZE_ERROR_MESSAGE);
+        }
+    }
+
+//    private static void checkHateMenusDuplicate(List<String> menus) {
+//        Set<String> notDuplicatedMenus = new HashSet<>(menus);
+//        if (!menus.isEmpty() && menus.size() != notDuplicatedMenus.size()) {
+//            throw new IllegalArgumentException(HATE_MENU_DUPLICATE_ERROR_MESSAGE);
+//        }
+//    }
 }
