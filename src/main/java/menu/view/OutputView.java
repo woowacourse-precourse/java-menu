a05bc559f137e7e4b3b46e1963f8f700e5cb9878
SH <gram753@naver.com>
2022-12-17 14:00:03 +0900

    feat: 출력을 담당하는 기능 구현

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,18 @@
+package menu.view;
+
+import static menu.Constant.*;
+
+public class OutputView {
+
+	public static void printStartMessage() {
+		System.out.println(START_MSG);
+	}
+
+	public static void printCoachNameInputMessage() {
+		System.out.println(COACH_INPUT_MSG);
+	}
+
+	public static void printMenuInputMessage(String name) {
+		System.out.printf(MENU_INPUT_MSG, name);
+	}
+}
