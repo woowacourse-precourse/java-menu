c94a3b5da60ae8fd16350c38cee2b806041b9e02
sangeun-b <baek0007@algonquinlive.com>
2022-12-17 13:23:11 +0900

    feat: 시작 문구 출력

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -7,8 +7,7 @@ import java.util.List;
 
 public class InputView {
 
-    public static final String START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
-    public static final String END_MESSAGE = "메뉴 추천 결과입니다.";
+
     public static final String INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
     public static final String INPUT_FOOD_NAME = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
 
---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,11 @@
+package menu.view;
+
+public class OutputView {
+
+    public static final String START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    public static final String END_MESSAGE = "메뉴 추천 결과입니다.";
+
+    public void printStart(){
+        System.out.println(START_MESSAGE);
+    }
+}
