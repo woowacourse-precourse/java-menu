086ac2e38640d1306d25054ae3698c028e4322f8
Miseong Kim <miseongk@naver.com>
2022-12-17 15:57:14 +0900

    refactor: 카테고리 반환값을 문자열이 아닌 Category로 변경

--- src/main/java/menu/CategoryRandomRecommender.java
+++ src/main/java/menu/CategoryRandomRecommender.java
@@ -11,11 +11,10 @@ public class CategoryRandomRecommender {
     private static final int RANDOM_LOWER_INCLUSIVE = 1;
     private static final int RANDOM_UPPER_INCLUSIVE = 5;
 
-    public String recommend(List<String> categories) {
-        String category = Category.values()[Randoms.pickNumberInRange(RANDOM_LOWER_INCLUSIVE, RANDOM_UPPER_INCLUSIVE) - 1]
-                .getName();
+    public Category recommend(List<Category> categories) {
+        Category category = Category.values()[Randoms.pickNumberInRange(RANDOM_LOWER_INCLUSIVE, RANDOM_UPPER_INCLUSIVE) - 1];
 
-        if (Collections.frequency(categories, category) > 2) {
+        if (Collections.frequency(categories, category) >= 2) {
             return recommend(categories);
         }
 
