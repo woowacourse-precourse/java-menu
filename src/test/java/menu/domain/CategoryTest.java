8a383bc110afda50202f92587e02905a5bea36ea
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 17:17:14 +0900

    test: 카테고리의 무작위 값이 1-5 사이인지 확인하는 테스트

--- src/test/java/menu/domain/CategoryTest.java
+++ src/test/java/menu/domain/CategoryTest.java
@@ -12,10 +12,9 @@ public class CategoryTest {
     @Test
     @DisplayName("카테고리를 추천하는 함수 테스트")
     void checkRecommandMenuCategoryTest() {
-        List<Integer> category = Category.recommandCategory();
-        assertThat(category.size()).isEqualTo(5);
-
-        for (int i = 1; i <=5; i++)
-            assertThat(Collections.frequency(category,i)).isLessThanOrEqualTo(2);
+        Category category = new Category();
+        Integer value = category.recommandDayCategory();
+        assertThat(value).isGreaterThanOrEqualTo(1);
+        assertThat(value).isLessThanOrEqualTo(5);
     }
 }
