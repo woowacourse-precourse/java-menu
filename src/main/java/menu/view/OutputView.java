22250bd6475ed486c13a22b09053ad5c79fbf5b1
pilyang <30036534+pilyang@users.noreply.github.com>
2022-12-17 13:38:29 +0900

    feat: 프로그램 시작 안내 메시지 출력 구현
    - 프로그램 출력을 위한 OutputView 생성
    - 프로그램 시작 안내 미시지 출력 구현

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,10 @@
+package menu.view;
+
+public class OutputView {
+
+    private static final String PROGRAM_START_INFO_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+
+    public void printProgramStartInfo() {
+        System.out.println(PROGRAM_START_INFO_MESSAGE);
+    }
+}
