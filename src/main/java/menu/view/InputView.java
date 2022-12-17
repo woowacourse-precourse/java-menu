1bb97a9b3d212ddbdc870d9a5acc608b8c0cfb37
ksh-beginner <ohk9134@naver.com>
2022-12-17 15:04:01 +0900

    refactor(InputView) : 못 먹는 메뉴 입력 시 빈 값 분기 처리

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -3,6 +3,7 @@ package menu.view;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,6 +12,7 @@ public class InputView {
     private final InputValidator inputValidator;
 
     private static final String SPLIT_DELIMITER = ",";
+    private static final String BLANK = "";
 
     public InputView() {
         this.inputValidator = new InputValidator();
@@ -22,5 +24,17 @@ public class InputView {
                 .collect(Collectors.toList());
         inputValidator.validateReadCoachNames(collectedCoachNames);
         return collectedCoachNames;
+    }
+
+    public List<String> readCannotEatFoods() {
+        String cannotEatFoods = Console.readLine();
+        List<String> cannotEatFoodsToReturn = Collections.emptyList();
+        if (!cannotEatFoods.equals(BLANK)) {
+            List<String> collectedcannotEatFoods = Arrays.stream(cannotEatFoods.split(SPLIT_DELIMITER))
+                    .collect(Collectors.toList());
+            inputValidator.validateReadCannotEatFoods(collectedcannotEatFoods);
+            return collectedcannotEatFoods;
+        }
+        return cannotEatFoodsToReturn;
     }
 }
