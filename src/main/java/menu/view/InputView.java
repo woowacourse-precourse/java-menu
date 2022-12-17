0d574ce25d701dfcff4cf70570aa3cb6286ccf7e
Cyma-s <ssol0319@naver.com>
2022-12-17 17:08:08 +0900

    fix(InputView) : 줄바꿈 삭제

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -10,7 +10,7 @@ import menu.domain.Coach;
 import menu.domain.exception.InputException;
 
 public class InputView {
-    private final static String INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)\n";
+    private final static String INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
     private final static String INPUT_COACH_CAN_NOT_EAT_MENUS = "(이)가 못 먹는 메뉴를 입력해 주세요.";
 
     public List<String> getCoachName() {
