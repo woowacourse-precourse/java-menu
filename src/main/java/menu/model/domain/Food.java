f2356fd04626ae5a0846fd3aedd48caeb81eea88
Hayoon <hy97@sju.ac.kr>
2022-12-17 14:19:54 +0900

    feat(Food): Food 인스턴스 변수 생성

--- src/main/java/menu/model/domain/Food.java
+++ src/main/java/menu/model/domain/Food.java
@@ -8,4 +8,12 @@ public class Food {
         this.name = name;
         this.category = category;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
 }
