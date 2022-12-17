e2d229f43adb38c1b3601dbeef1d74d7172907c3
minaver <psmjoshua@gmail.com>
2022-12-17 13:21:45 +0900

    feat(Category): Category Enum 초기화

--- src/main/java/menu/enums/Category.java
+++ src/main/java/menu/enums/Category.java
@@ -1,4 +1,34 @@
 package menu.enums;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Category {
+    KOR("한식"),
+    JAP("일식"),
+    CHI("중식"),
+    ASI("아시안"),
+    WES("양식")
+    ;
+
+
+    private final String label;
+
+    Category(String label) {
+        this.label = label;
+    }
+
+    public String label() {
+        return label;
+    }
+
+    private static final Map<String, Category> BY_LABEL=
+            Stream.of(values()).collect(Collectors.toMap(Category::label, Function.identity()));
+
+
+    public static Category valueOfLabel(String label) {
+        return BY_LABEL.get(label);
+    }
 }
