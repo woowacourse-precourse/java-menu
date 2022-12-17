0afea8a5e7447a2127271ae1d8569db3e617c333
greeng00se <thegreengoose7@gmail.com>
2022-12-17 14:53:30 +0900

    feat(Coach): 허용된 메뉴와 허용되지 않는 메뉴 필드 추가

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,5 +1,7 @@
 package menu.domain;
 
+import java.util.List;
+
 public class Coach {
 
     private static final int VALID_NAME_LOWER_BOUND = 2;
@@ -7,6 +9,8 @@ public class Coach {
     private static final String INVALID_NAME_MESSAGE = "코치님의 이름은 2 ~ 4글자여야 합니다.";
 
     private final String name;
+    private final Menus invalidMenus = new Menus();
+    private final Menus validMenus = new Menus();
 
     public Coach(String name) {
         validate(name);
@@ -17,6 +21,22 @@ public class Coach {
         if (name.length() < VALID_NAME_LOWER_BOUND || VALID_NAME_UPPER_BOUND < name.length()) {
             throw new IllegalArgumentException(INVALID_NAME_MESSAGE);
         }
+    }
+
+    public void addInvalidMenus(List<Menu> menus) {
+        invalidMenus.addAll(menus);
+    }
+
+    public void addEatMenu(Menu menus) {
+        validMenus.add(menus);
+    }
+
+    public List<String> getEatMenu() {
+        return validMenus.getNames();
+    }
+
+    public boolean isInvalidMenu(Menu menu) {
+        return invalidMenus.isInvalidMenu(menu) || validMenus.isInvalidMenu(menu);
     }
 
     public String getName() {
