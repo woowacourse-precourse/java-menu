5227d6dcbc674dcf3e6098aff38096354e06501b
Cyma-s <ssol0319@naver.com>
2022-12-17 14:40:58 +0900

    feat(InputException) : 지원하지 않는 추천 메뉴가 입력되는 예외 추가

--- src/main/java/menu/domain/InputException.java
+++ src/main/java/menu/domain/InputException.java
@@ -6,7 +6,8 @@ public enum InputException {
     OUT_OF_COACH_NAME_COUNT_MAXIMUM("코치는 최대 5명 이하로 입력해야 합니다."),
     OUT_OF_COACH_NAME_LENGTH_MINIMUM("코치 이름은 최소 2글자 이상 입력해야 합니다."),
     OUT_OF_COACH_NAME_LENGTH_MAXIMUM("코치 이름은 최대 4글자 이하로 입력해야 합니다."),
-    DUPLICATE_COACH_MENUS("중복된 메뉴가 입력되었습니다.");
+    DUPLICATE_COACH_MENUS("중복된 메뉴가 입력되었습니다."),
+    INVALID_MENU("지원하지 않는 메뉴 이름입니다.");
 
 
     private final String exceptionMessage;
