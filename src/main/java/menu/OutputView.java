112424b08ade72090f5cb87bdaa42d9597d84157
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-12-17 17:22:16 +0900

    <refactor>(OutputView.java): formatAsTable() 메서드 리팩토링
     - 내부에서 사용되는 문자열을 상수로 지정해 의미 부여

--- src/main/java/menu/OutputView.java
+++ src/main/java/menu/OutputView.java
@@ -11,12 +11,15 @@ public class OutputView {
     private static final String SERVICE_START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
     private static final String RESULT_INFORM_MESSAGE = "메뉴 추천 결과입니다.";
     private static final String SERVICE_END_MESSAGE = "추천을 완료했습니다.";
-    private static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String ERROR_PREFIX = "[ERROR] ";
     private static final String PICKYEATINGS_INFORM_MESSAGE = "(이)가 못 먹는 메뉴를 입력해 주세요.";
     private static final String COACH_NAME_INFORM_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
     private static final List<String> DAY_OF_THE_WEEKS = Arrays.asList("월요일", "화요일", "수요일", "목요일", "금요일");
     private static final String DIVISION = "구분";
     private static final String CATEGORY = "카테고리";
+    private static final String TABLE_PREFIX = "[ ";
+    private static final String TABLE_SUFFIX = " ]";
+    private static final String SEPARATOR = " | ";
 
     public void printStartMessage() {
         System.out.println(SERVICE_START_MESSAGE);
@@ -58,11 +61,11 @@ public class OutputView {
 
     private String formatAsTable(String title, List<String> contents) {
         StringBuilder form = new StringBuilder();
-        form.append("[ ").append(title);
+        form.append(TABLE_PREFIX).append(title);
         for (int i = 0; i < contents.size(); i++) {
-            form.append(" | ").append(contents.get(i));
+            form.append(SEPARATOR).append(contents.get(i));
         }
-        form.append(" ]");
+        form.append(TABLE_SUFFIX);
 
         return form.toString();
     }
