ede8a44b035d17fd24f032cb2056eee72deee650
solesy <tkddbs9801@gmail.com>
2022-12-17 14:09:58 +0900

    test: ValidateInputChecker 클래스 테스트코드 추가

---
+++ src/main/java/menu/ValidInputChecker.java
@@ -0,0 +1,38 @@
+package menu;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ValidInputChecker {
+    private static String validateCoachName(String name){
+        if(name == null || name.length() < 2 || name.length() > 4)
+            throw new IllegalArgumentException("[ERROR] 코치이름은 2에서 4 사이입니다.");
+        return name;
+    }
+    public static List<String> validateCoachesAndToList(String names){
+        List<String> ret = Arrays.stream(names.split(",", -1))
+                .map(ValidInputChecker::validateCoachName)
+                .collect(Collectors.toList());
+        if(ret.size() < 2 || ret.size() >5)
+            throw new IllegalArgumentException("[ERROR] 코치는 최소 2명 이상 입력해야 합니다.");
+        return ret;
+    }
+
+    private static String validateFoodName(String food){
+        Menu menu = new Menu();
+        if(food == null) return null;
+        if(!menu.isExist(food))
+            throw new IllegalArgumentException("[ERROR] 못 먹는 메뉴가 존재하지 않는 값입니다.");
+        return food;
+    }
+
+    public static List<String> validateNoFoodsAndToList(String foods){
+        List<String> ret = Arrays.stream(foods.split(",", -1))
+                .map(ValidInputChecker::validateFoodName)
+                .collect(Collectors.toList());
+        if(ret.size() > 2)
+            throw new IllegalArgumentException("[ERROR] 못 먹는 메뉴는 최대 2개 입니다.");
+        return ret;
+    }
+}
---
+++ src/test/java/menu/ValidInputCheckerTest.java
@@ -0,0 +1,29 @@
+package menu;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ValidInputCheckerTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @DisplayName("코치 수 초과")
+    @ValueSource(strings = {"토미,제임스,포코,이상윤,상윤이,윤상이", "토미"})
+    @ParameterizedTest
+    void isNotValidCoachesLen(String input){
+        assertThatThrownBy(() -> ValidInputChecker.validateCoachesAndToList(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @DisplayName("못 먹는 음식 에러")
+    @ValueSource(strings = {"동우,시스", "우동,스시,토마토 달걀볶음"})
+    @ParameterizedTest
+    void isNotValidNoFoodsLen(String input){
+        assertThatThrownBy(() -> ValidInputChecker.validateNoFoodsAndToList(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+}
