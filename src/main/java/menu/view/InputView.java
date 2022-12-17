07f89f24f6af89c51af58571c71031c27612f51c
Cyma-s <ssol0319@naver.com>
2022-12-17 13:46:37 +0900

    feat(InputView) : 코치가 못 먹는 음식을 받아오는 함수 추가

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,30 +1,45 @@
 package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import menu.domain.Coach;
 import menu.domain.InputException;
 
 public class InputView {
     private final static String INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)\n";
+    private final static String INPUT_COACH_CAN_NOT_EAT_MENUS = "(이)가 못 먹는 메뉴를 입력해 주세요.";
 
     public List<String> getCoachName() {
         System.out.println(INPUT_COACH_NAME);
-        String userCoachName = Console.readLine();
-        validateCoachNameCount(userCoachName);
-        validateDuplicateCoachName(userCoachName);
-        validateCoachNameLength(userCoachName);
-        List<String> coachNames = convertStringToList(userCoachName);
+        String coachNameInput = Console.readLine();
+        validateCoachNameCount(coachNameInput);
+        validateDuplicateCoachName(coachNameInput);
+        validateCoachNameLength(coachNameInput);
+        List<String> coachNames = convertStringToList(coachNameInput);
         return coachNames;
+    }
+
+    public List<Coach> getCoachCannotEatMenus(List<String> coachNames) {
+        List<Coach> coaches = new ArrayList<>();
+        for (String coachName : coachNames) {
+            System.out.println(coachName + INPUT_COACH_CAN_NOT_EAT_MENUS);
+            String menuNameInput = Console.readLine();
+            List<String> coachCannotEatMenus = convertStringToList(menuNameInput);
+            Coach coach = new Coach(coachName, coachCannotEatMenus);
+            coaches.add(coach);
+        }
+        return coaches;
     }
 
     public void validateCoachNameCount(String uncheckedCoachNames) {
         List<String> coachNames = List.of(uncheckedCoachNames.split(","));
-        if(coachNames.size() < 2) {
+        if (coachNames.size() < 2) {
             throw new IllegalArgumentException(InputException.OUT_OF_COACH_NAME_COUNT_MINIMUM.getExceptionMessage());
         }
-        if(coachNames.size() > 5) {
+        if (coachNames.size() > 5) {
             throw new IllegalArgumentException(InputException.OUT_OF_COACH_NAME_COUNT_MAXIMUM.getExceptionMessage());
         }
     }
@@ -32,19 +47,21 @@ public class InputView {
     private void validateDuplicateCoachName(String uncheckedCoachNames) {
         List<String> coachNames = List.of(uncheckedCoachNames.split(","));
         Set<String> uniqueCoachNames = new HashSet<>(coachNames);
-        if(coachNames.size() != uniqueCoachNames.size()) {
+        if (coachNames.size() != uniqueCoachNames.size()) {
             throw new IllegalArgumentException(InputException.DUPLICATE_COACH_NAME.getExceptionMessage());
         }
     }
 
     private void validateCoachNameLength(String uncheckedCoachNames) {
         List<String> coachNames = List.of(uncheckedCoachNames.split(","));
-        for(String coachName: coachNames) {
-            if(coachName.length() < 2) {
-                throw new IllegalArgumentException(InputException.OUT_OF_COACH_NAME_COUNT_MINIMUM.getExceptionMessage());
+        for (String coachName : coachNames) {
+            if (coachName.length() < 2) {
+                throw new IllegalArgumentException(
+                        InputException.OUT_OF_COACH_NAME_COUNT_MINIMUM.getExceptionMessage());
             }
-            if(coachName.length() > 4) {
-                throw new IllegalArgumentException(InputException.OUT_OF_COACH_NAME_LENGTH_MAXIMUM.getExceptionMessage());
+            if (coachName.length() > 4) {
+                throw new IllegalArgumentException(
+                        InputException.OUT_OF_COACH_NAME_LENGTH_MAXIMUM.getExceptionMessage());
             }
         }
     }
