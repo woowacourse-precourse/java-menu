6ece7ffbcaca9bedfdeec5481836ffdd7a2ae364
saint6839 <saint6839@gmail.com>
2022-12-17 13:26:15 +0900

    feat(domain) : 메뉴에 대한 정보를 담는 Menu 클래스 추가

---
+++ src/main/java/domain/Menu.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Menu {
+    private final String name;
+
+    public Menu(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
