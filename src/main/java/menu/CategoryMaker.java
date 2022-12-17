eb141c9a493c73b0127d815c92de57539bb494cb
Cyma-s <ssol0319@naver.com>
2022-12-17 15:55:25 +0900

    feat(CategoryMaker) : 같은 카테고리를 2개 이상 선택하는 예외 처리

--- src/main/java/menu/CategoryMaker.java
+++ src/main/java/menu/CategoryMaker.java
@@ -3,10 +3,12 @@ package menu;
 import java.util.ArrayList;
 import java.util.List;
 import menu.domain.Category;
+import menu.domain.exception.CategoryException;
 
 public class CategoryMaker {
     private final CategoryRandomNumberGenerator categoryRandomNumberGenerator;
     private final int CATEGORY_COUNT = 5;
+    private final long CATEGORY_DUPLICATE_LIMIT = 2L;
 
     public CategoryMaker(CategoryRandomNumberGenerator categoryRandomNumberGenerator) {
         this.categoryRandomNumberGenerator = categoryRandomNumberGenerator;
@@ -15,10 +17,33 @@ public class CategoryMaker {
     public List<Category> makeCategories() {
         List<Category> categories = new ArrayList<>();
         for(int i = 0; i<CATEGORY_COUNT; i++) {
-            int randomNumber = categoryRandomNumberGenerator.generate();
-            Category category = Category.makeCategory(randomNumber);
+            Category category = makeCategory(categories);
             categories.add(category);
         }
         return categories;
+    }
+
+    public Category makeCategory(List<Category> categories) {
+        try {
+            Category category = generateRandomCategory();
+            validateCategory(category, categories);
+            return category;
+        } catch (IllegalStateException e) {
+            return makeCategory(categories);
+        }
+    }
+
+    private Category generateRandomCategory() {
+        int randomNumber = categoryRandomNumberGenerator.generate();
+        Category category = Category.makeCategory(randomNumber);
+        return category;
+    }
+    public void validateCategory(Category uncheckedCategory, List<Category> categories) {
+        long count = categories.stream()
+                .filter(value -> value.equals(uncheckedCategory))
+                .count();
+        if(count > CATEGORY_DUPLICATE_LIMIT) {
+            throw new IllegalStateException(CategoryException.OUT_OF_DUPLICATE_LIMIT.getExceptionMessage());
+        }
     }
 }
