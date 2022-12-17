35b56387628f929fc0b137ea3bfec37f9d0713c1
kwYoohae <gocksdl7979@gmail.com>
2022-12-17 15:03:15 +0900

    feat: 사용자의 카테고리가 3회 이상 겹친다면 다시뽑는 로직 작성

--- src/main/java/menu/domain/PickMenu.java
+++ src/main/java/menu/domain/PickMenu.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import menu.utils.Category;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PickMenu {
@@ -16,11 +17,30 @@ public class PickMenu {
     }
 
     private List<Category> getRandomCategory() {
+        boolean isDuplicate = true;
         List<Category> makeCategory = new ArrayList<>();
-        for (int i = 0; i < MAX_CATEGORY_NUMBER; i++) {
-            Category category = Category.valueOfPickNumber(Randoms.pickNumberInRange(MIN_CATEGORY_NUMBER,MAX_CATEGORY_NUMBER));
-            makeCategory.add(category);
+        while(isDuplicate) {
+            makeCategory.clear();
+            for (int i = 0; i < MAX_CATEGORY_NUMBER; i++) {
+                Category category = Category.valueOfPickNumber(Randoms.pickNumberInRange(MIN_CATEGORY_NUMBER,MAX_CATEGORY_NUMBER));
+                makeCategory.add(category);
+            }
+            isDuplicate = checkDuplicateFrequency(makeCategory);
         }
         return makeCategory;
+    }
+
+    private boolean checkDuplicateFrequency(List<Category> makeCategory) {
+        if (Collections.frequency(makeCategory, Category.KOREA) > 2)
+            return true;
+        if (Collections.frequency(makeCategory, Category.JAPAN) > 2)
+            return true;
+        if (Collections.frequency(makeCategory, Category.CHINA) > 2)
+            return true;
+        if (Collections.frequency(makeCategory, Category.ASIA) > 2)
+            return true;
+        if (Collections.frequency(makeCategory, Category.WESTERN) > 2)
+            return true;
+        return false;
     }
 }
