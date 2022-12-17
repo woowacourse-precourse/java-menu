6bd9442faf365679e16755809b1c0291a586ee61
jaehee329 <jaehee329@yonsei.ac.kr>
2022-12-17 15:30:40 +0900

    feat(MenuRec.Repo.): 메뉴 추천 결과 저장소 구현

--- src/main/java/menu/repository/MenuRecommendRepository.java
+++ src/main/java/menu/repository/MenuRecommendRepository.java
@@ -42,4 +42,27 @@ public class MenuRecommendRepository {
 		} while (count >= DUP_MAX);
 		categoryByDay.put(day, category);
 	}
+
+	public static void addMenuByDayAndCoach(Day day, Coach coach, Menu menu) throws IllegalArgumentException {
+		initIfNull(day);
+		checkMenuDuplicated(coach, menu);
+		coachMenuByDay.get(day).put(coach, menu);
+	}
+
+	private static void checkMenuDuplicated(Coach coach, Menu menu) {
+		int count = (int)coachMenuByDay.keySet().stream()
+			.map(day1 -> coachMenuByDay.get(day1))
+			.filter(coachMenuMap -> coachMenuMap.keySet().contains(coach))
+			.filter(coachMenuMap -> coachMenuMap.get(coach).equals(menu))
+			.count();
+		if (count >= 1) {
+			throw new IllegalArgumentException("[ERROR] 한 코치에 대한 동일한 메뉴의 입력은 불가합니다.");
+		}
+	}
+
+	private static void initIfNull(Day day) {
+		if (coachMenuByDay.get(day) == null) {
+			coachMenuByDay.put(day, new HashMap<>());
+		}
+	}
 }
--- src/test/java/menu/repository/MenuRecommendRepositoryTest.java
+++ src/test/java/menu/repository/MenuRecommendRepositoryTest.java
@@ -1,17 +1,30 @@
 package menu.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.Map;
-
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import menu.domain.Category;
+import menu.domain.Coach;
 import menu.domain.Day;
+import menu.domain.Menu;
 
 class MenuRecommendRepositoryTest {
 
+	@Test
+	void addCoachDupMenuErrorTest() {
+		Coach coach1 = new Coach("김");
+		Coach coach2 = new Coach("이");
+		Menu menu1 = new Menu("국밥");
+		Menu menu2 = new Menu("깍두기");
+		MenuRecommendRepository.addMenuByDayAndCoach(Day.MONDAY, coach1, menu1);
+		MenuRecommendRepository.addMenuByDayAndCoach(Day.MONDAY, coach2, menu1);
+		MenuRecommendRepository.addMenuByDayAndCoach(Day.TUESDAY, coach1, menu2);
 
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			MenuRecommendRepository.addMenuByDayAndCoach(Day.TUESDAY, coach1, menu2);
+		});
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			MenuRecommendRepository.addMenuByDayAndCoach(Day.FRIDAY, coach1, menu2);
+		});
+	}
 
 }
