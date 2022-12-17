d3389bd724e53493b425f02c3edfd1704e32d17c
wonseok <tkddls23@konkuk.ac.kr>
2022-12-17 15:11:18 +0900

    feat(domain: Menu): 카테고리별 메뉴 받아오기

--- src/main/java/menu/domain/Menu.java
+++ src/main/java/menu/domain/Menu.java
@@ -3,49 +3,78 @@ package menu.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 import menu.validation.CategoryValidation;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import static menu.constant.MenuConstant.*;
 
 public class Menu {
 
-    private Map<Integer, Category> categories;
+    private Map<Integer, Category> categoryNames;
+    private Map<Category,List<String>> categoryValues;
     private List<Category> weekCategories;
 
     public Menu() {
-        categories = new HashMap<>();
+        categoryNames = new HashMap<>();
+        categoryValues = new TreeMap<>();
         weekCategories = new ArrayList<>();
         init();
     }
 
     public void init() {
-        addCategories();
+        addCategoryNames();
+        addCategoryValues();
+    }
+
+    public Map<Category, List<String>> getCategoryValues() {
+        return categoryValues;
     }
 
     public List<Category> getWeekCategories() {
         return weekCategories;
     }
 
-    private void addCategories() {
-        categories.put(1,Category.JAPAN);
-        categories.put(2,Category.KOREA);
-        categories.put(3,Category.CHINA);
-        categories.put(4,Category.ASIA);
-        categories.put(5,Category.WESTERN);
+    private void addCategoryNames() {
+        categoryNames.put(1,Category.JAPAN);
+        categoryNames.put(2,Category.KOREA);
+        categoryNames.put(3,Category.CHINA);
+        categoryNames.put(4,Category.ASIA);
+        categoryNames.put(5,Category.WESTERN);
     }
 
-    public void makeWeekCategories() {
+    private void addCategoryValues() {
+        List<Category> categories = Category.getList();
+        for (Category category : categories) {
+            categoryValues.put(category, generateCategoryMenu(category));
+        }
+    }
+
+    public void makeWeekCategoryNames() {
         while (weekCategories.size() != 5) {
             generateRandomCategory();
         }
     }
 
     private void generateRandomCategory() {
-        Category category = categories.get(Randoms.pickNumberInRange(1, 5));
+        Category category = categoryNames.get(Randoms.pickNumberInRange(1, 5));
         if (CategoryValidation.checkWeekCategoryValid(category, weekCategories)) {
             weekCategories.add(category);
         }
+    }
+
+    private List<String> generateCategoryMenu(Category category) {
+        if (category == Category.JAPAN) {
+            return Arrays.asList(MENU_JAPAN.split(","));
+        }
+        if (category == Category.KOREA) {
+            return Arrays.asList(MENU_KOREA.split(","));
+        }
+        if (category == Category.CHINA) {
+            return Arrays.asList(MENU_CHINA.split(","));
+        }
+        if (category == Category.ASIA) {
+            return Arrays.asList(MENU_ASIA.split(","));
+        }
+        return Arrays.asList(MENU_WESTERN.split(","));
     }
 
 
