b0f07f7ff2b669db7373edfcec1a719ee37c388a
HubCreator <chocomilk8604@gmail.com>
2022-12-17 14:03:08 +0900

    fix(Person) : 입력받은 메뉴들에 대한 쉼표 검증 수정

--- src/main/java/menu/domain/Person.java
+++ src/main/java/menu/domain/Person.java
@@ -33,15 +33,15 @@ public class Person {
     }
 
     private String[] validateUnavailableMenus(String input) {
-        for (int index = 0; index < input.length(); index++) {
-            if (input.charAt(index) != ',' || Character.isAlphabetic(input.charAt(index))) {
+        String[] split = input.split(",");
+        for (String s : split) {
+            if (!s.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*")) {
                 throw new IllegalArgumentException("올바른 입력이 아닙니다. 쉽표로 구분하여 입력해주세요");
             }
         }
-        String[] result = input.split(",");
-        if (result.length > 2) {
+        if (split.length > 2) {
             throw new IllegalArgumentException("최대 2개의 못 먹는 메뉴만 입력할 수 있습니다.");
         }
-        return result;
+        return split;
     }
 }
