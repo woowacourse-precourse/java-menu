3c44c63d3d84bcaa5cb556cba994495a85992013
woo-chang <clllickme@naver.com>
2022-12-17 13:17:13 +0900

    feat(Coach): 코치 이름을 검증하고 생성하는 기능 구현

---
+++ src/main/java/menu/domain/Coach.java
@@ -0,0 +1,36 @@
+package menu.domain;
+
+import java.util.Objects;
+
+public class Coach {
+
+    private final String name;
+
+    public Coach(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (name.length() < 2 || name.length() > 4) {
+            throw new IllegalArgumentException("코치의 이름은 최소 2글자, 최대 4글자이다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Coach coach = (Coach) o;
+        return Objects.equals(name, coach.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
