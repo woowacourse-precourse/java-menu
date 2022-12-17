401ec97d19f61a5d6268d223f9a420d94e87938f
khsrla9806 <kimhunsope@naver.com>
2022-12-17 13:35:24 +0900

    feat(InputView): 각 코치가 먹지 못하는 메뉴를 입력받는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,7 +1,9 @@
 package menu.view;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -19,5 +21,23 @@ public class InputView extends InputException {
             readCoachName();
         }
         return coachName;
+    }
+
+    public Map<String, List<String>> readCoachDislikeFood(List<String> coachName) {
+        Map<String, List<String>> coachDislikeFood = new HashMap<>();
+        for (int i = 0; i < coachName.size(); i++) {
+            System.out.println(coachName.get(i) + "(이)가 못 먹는 메뉴를 입력해 주세요.");
+            String inputData = Console.readLine();
+            List<String> dislikeFood = Arrays.stream(inputData.split(",")).collect(Collectors.toList());
+            try {
+                validateDislikeFoodCount(dislikeFood);
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+                i--;
+                continue;
+            }
+            coachDislikeFood.put(coachName.get(i), dislikeFood);
+        }
+        return coachDislikeFood;
     }
 }
