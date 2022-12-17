1473638167037bd99c63204b948f4c748e55b9f4
kokodak <kokodakadokok@gmail.com>
2022-12-17 14:57:40 +0900

    feat(category): 일주일치 메뉴를 추천하는 메서드 구현

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,5 +1,7 @@
 package menu.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Category {
@@ -14,5 +16,22 @@ public class Category {
 
     public String getName() {
         return name;
+    }
+
+    public List<String> getRecommendMenu(Coach coach) {
+        List<String> recommendedMenu = new ArrayList<>();
+        List<String> cantEatMenu = coach.getCantEatMenu();
+        while (recommendedMenu.size() != 5) {
+            String menu = Randoms.shuffle(this.menu).get(0);
+            if (isExistMenu(recommendedMenu, menu) || isExistMenu(cantEatMenu, menu)) {
+                continue;
+            }
+            recommendedMenu.add(menu);
+        }
+        return recommendedMenu;
+    }
+
+    private boolean isExistMenu(List<String> menuList, String menu) {
+        return menuList.contains(menu);
     }
 }
