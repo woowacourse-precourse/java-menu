186e0bd173b7daad4acea546408e7ebaf26b6cd9
thdwoqor <celebarte01@gmail.com>
2022-12-17 13:38:18 +0900

    feat: 카테고리 생성 기능 추가

---
+++ src/main/java/menu/model/Menu.java
@@ -0,0 +1,47 @@
+package menu.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public enum Menu {
+    JAPAN(1, "일식", Arrays.asList("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼")),
+    KOREA(2, "한식", Arrays.asList("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음")),
+    CHINA(3, "중식", Arrays.asList("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채")),
+    ASIA(4, "아시안", Arrays.asList("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜")),
+    WESTERN(5, "양식", Arrays.asList("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"));
+
+    private final int code;
+    private final String category;
+    private final List<String> namse;
+
+    Menu(int code, String category, List<String> names) {
+        this.code = code;
+        this.category = category;
+        this.namse = names;
+    }
+
+    public static Menu getCodeToMenu(int code) {
+        return Arrays.stream(Menu.values())
+                .filter(e -> e.code == code)
+                .findAny()
+                .orElseGet(() -> getCodeToMenu(Randoms.pickNumberInRange(1, 5)));
+    }
+
+    public static List<String> selectCategory() {
+        List<String> categorys = new ArrayList<>();
+        while (categorys.size() < 5) {
+            String category = getCodeToMenu(Randoms.pickNumberInRange(1, 5)).category;
+            System.out.println(category);
+            int count = Collections.frequency(categorys, category);
+            if (count < 2) {
+                categorys.add(category);
+            }
+        }
+        return categorys;
+    }
+
+}
