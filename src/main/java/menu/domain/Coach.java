2fd798c8fb45b46d5d6b34f3869d7afd034e7408
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-12-17 14:32:28 +0900

    feat(Coach): 코치의 이름과 못먹는 메뉴를 저장하고 추가해주는 클래스 구현

---
+++ src/main/java/menu/domain/Coach.java
@@ -0,0 +1,1 @@
+package menu.domain;import java.util.ArrayList;import java.util.List;public class Coach {    private final String name;    private final List<Menu> unEatableMenu = new ArrayList<>();    public Coach(String name) {        this.name = name;    }    public void addUnEatableMenu(String menu) {        unEatableMenu.add(new Menu(menu));    }}
