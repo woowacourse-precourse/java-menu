bf6fd80e30858427c06d100d8adeb1bf0a75ffa8
saint6839 <saint6839@gmail.com>
2022-12-17 14:17:48 +0900

    feat(domain) : 일치하는 카테고리 찾아오는 메서드 구현

--- src/main/java/domain/Category.java
+++ src/main/java/domain/Category.java
@@ -1,15 +1,25 @@
 package domain;
 
+import java.util.Arrays;
+
 public enum Category {
     JAPANESE("일식"), KOREAN("한식"), CHINESE("중식"), ASIAN("아시안"), WESTERN("양식");
 
-    private final String category;
+    private final String name;
 
     Category(String category) {
-        this.category = category;
+        this.name = category;
     }
 
-    public String getCategory() {
-        return category;
+    public String getName() {
+        return name;
+    }
+
+    public static Category findCategoryByName(String name) {
+        return Arrays.stream(Category.values())
+                     .filter(category -> category.getName()
+                                                 .equals(name))
+                     .findFirst()
+                     .orElseThrow(() -> new IllegalArgumentException("[ERROR] 유효하지 않은 메뉴 입니다."));
     }
 }
