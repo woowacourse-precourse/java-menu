6c3c8b5f71703b9a28adabccda95643ebf72a680
ingpyo <juninpyo72@gmail.com>
2022-12-17 13:57:15 +0900

    refactor:코치 이름 입력 함수이름 수정

--- src/main/java/menu/InputView.java
+++ src/main/java/menu/InputView.java
@@ -6,7 +6,7 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-	public List<String> readinputCoachs() {
+	public List<String> readInputCoachs() {
 		List<String> splitCoachs = new ArrayList<>();
 		String inputCoachs = Console.readLine();
 		splitCoachs.addAll(List.of(inputCoachs.split(",")));
