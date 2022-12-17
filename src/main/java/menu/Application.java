b4f2474cc2f0c277a9837664d44e85c66807a4e2
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-12-17 15:41:22 +0900

    <feat>(Application.java): main 메서드에 프로그램 실행 코드 작성

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,12 @@
 package menu;
 
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            Controller.run();
+        } catch (IllegalArgumentException error) {
+            System.out.println("[ERROR] " + error.getMessage());
+        }
     }
 }
