9964f70c9ba34b147965b64eb665c8174f1a9601
watterricecake <fruturum@nate.com>
2022-12-17 14:30:45 +0900

    feat: view와 컨트롤러를 합친 컨트롤러

---
+++ src/main/java/menu/controller/MainController.java
@@ -0,0 +1,24 @@
+package menu.controller;
+
+import menu.domain.Coach;
+
+import static menu.view.InputView.inputCantView;
+import static menu.view.InputView.inputCoachesView;
+import static menu.view.OutputView.startRecommendView;
+
+public class MainController {
+    CoachController coachController = new CoachController();
+    public void startRecommend(){
+        startRecommendView();
+        coachController.makeCoachList(inputCoachesView());
+        inputCantEat();
+    }
+    private void inputCantEat(){
+        for(String coach:coachController.getCoaches().keySet()){
+            coachController.makeCantEat(coach,inputCantView(coach));
+        }
+    }
+    private void recommendCategory(){
+
+    }
+}
