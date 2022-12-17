8427adda31897d586df5941664d88c24e329a7cd
watterricecake <fruturum@nate.com>
2022-12-17 14:06:57 +0900

    refator: 음식리스트 해쉬로 바꿈

--- src/main/java/menu/domain/Foods.java
+++ src/main/java/menu/domain/Foods.java
@@ -1,17 +1,18 @@
 package menu.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Foods {
-    private List<Food> foodsList;
+    private static HashMap<String,Food> foodsList;
     private final String[] japan = {"규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼"};
     private final String[] korean = {"김밥", "김치찌개", "쌈밥","된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"};
     private final String[] chinese = {"깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채"};
     private final String[] asian = {"팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜"};
     private final String[] western = {"라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"};
     public Foods(){
-        foodsList = new ArrayList<>();
+        foodsList = new HashMap<>();
         makeFood("일식",japan);
         makeFood("한식",korean);
         makeFood("중식",chinese);
@@ -20,8 +21,8 @@ public class Foods {
     }
     private void makeFood(String category,String[] foods){
         for(String food:foods){
-            this.foodsList.add(new Food(category,food));
+            this.foodsList.put(food,new Food(category,food));
         }
     }
-    public List<Food> getFoodsList(){return this.foodsList;}
+    public static HashMap<String, Food> getFoodsList(){return foodsList;}
 }
