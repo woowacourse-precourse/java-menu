ee42e888d82d66393e1a64f4908ff959789b0456
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-12-17 15:08:46 +0900

    Feat : 출력할 결과 문자열 만드는 기능 추가

--- src/main/java/menu/MenuSelectListMaker.java
+++ src/main/java/menu/MenuSelectListMaker.java
@@ -3,33 +3,90 @@ package menu;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class MenuSelectListMaker {
     CoachRepo coachRepo;
-    List<String> categories = List.of("일식", "한식", "중식", "아시안", "양식");
+
     List<String> categoriesForTheWeek;
+    List<String> categories = List.of("일식", "한식", "중식", "아시안", "양식");
     public MenuSelectListMaker(CoachRepo coachRepo){
         this.coachRepo = coachRepo;
     }
-    public void menuListStringReturn() {
-        String returningList = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]\n" + "[ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]";
+    public String menuListStringReturn() {
+        String returningList = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]\n";
         setCoachMenuTable();
+        returningList += getCoachMenuTableString();
+        return returningList;
+    }
+
+    private String getCoachMenuTableString() {
+        String returningString = "";
+        returningString += categoriesString() + "\n";
+        returningString += coachEatingMenuString() ;
+        return returningString;
+    }
+
+    private String coachEatingMenuString() {
+        String returningString="";
+        for (Coach coach : coachRepo.coachList){
+            returningString += "[ " + coach.name;
+            returningString += coachMenuString(coach);
+            returningString += " ]\n";
+        }
+        return returningString;
+    }
+
+    private String coachMenuString(Coach coach) {
+        String returningString ="";
+        for (String menu : coach.eatingMenuList){
+            returningString += " | " + menu;
+        }
+        return returningString;
+    }
+
+    private String categoriesString() {
+        String returningString = "[ 카테고리 ";
+        for (int i=0; i<5; i++){
+            returningString += "| "+ categoriesForTheWeek.get(i)+ " ";
+        }
+        returningString+= "] ";
+        return returningString;
 
     }
 
     private void setCoachMenuTable() {
         categoriesForTheWeek = setCategoriesForTheWeek();
+        for (String category : categoriesForTheWeek){
+            setCoachMenu(category);
+        }
+    }
 
+    private void setCoachMenu(String category) {
+        MenuTable menuTable = new MenuTable();
+        for (Coach coach : coachRepo.coachList){
+            String selectedMenu = Randoms.shuffle(menuTable.menuMap.get(category)).get(0);
+            addAvailableMenu(coach, selectedMenu);
+        }
+    }
+
+    private void addAvailableMenu(Coach coach, String selectedMenu) {
+        while (true){
+            if (coach.cannotEatMenuList.contains(selectedMenu) == false && coach.eatingMenuList.contains(selectedMenu) == false){
+                coach.eatingMenuList.add(selectedMenu);
+                break;
+            }
+        }
     }
 
     private List<String> setCategoriesForTheWeek() {
         List<String> returningList= new ArrayList<>();
         int i=0;
         while(i!=5){
-            String category = categories.get(Randoms.pickNumberInRange(0,4));
+            String category = categories.get(Randoms.pickNumberInRange(1,5)-1);
             if (!returningList.contains(category) || returningList.isEmpty() || (returningList.contains(category) && Collections.frequency(returningList,category) == 1)){
                 returningList.add(category);
                 i++;
