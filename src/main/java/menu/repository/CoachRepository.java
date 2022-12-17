536c4ff22bbbc66bfd21128956d5bcffd1ce6439
skydreamer21 <skydreamer210@gmail.com>
2022-12-17 14:03:59 +0900

    feat(CoachRepository): 필드, 생성자 추가

--- src/main/java/menu/repository/CoachRepository.java
+++ src/main/java/menu/repository/CoachRepository.java
@@ -1,11 +1,20 @@
 package menu.repository;
 
+import menu.domain.Coach;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CoachRepository {
     private static class InstanceHolder {
         private static final CoachRepository INSTANCE = new CoachRepository();
     }
 
-    private CoachRepository(){}
+    private List<Coach> coaches;
+
+    private CoachRepository() {
+        coaches = new ArrayList<>();
+    }
 
     public static CoachRepository getInstance() {
         return CoachRepository.InstanceHolder.INSTANCE;
