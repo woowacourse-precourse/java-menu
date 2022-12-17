bf23ba0d28efb5aa60e4c61a4c5a50a3e246a19d
은지 <midzy0228@gmail.com>
2022-12-17 14:17:49 +0900

    feat: 카테고리 인덱스를 사용하여 추천 기능 구현

--- src/main/java/menu/model/Category.java
+++ src/main/java/menu/model/Category.java
@@ -1,15 +1,29 @@
 package menu.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public enum Category {
-    JAPANESE_FOOD("일식"),
-    KOREAN_FOOD("한식"),
-    CHINESE_FOOD("중식"),
-    ASIAN_FOOD("아시안"),
-    WESTON_FOOD("양식");
+    JAPANESE_FOOD("일식", 1),
+    KOREAN_FOOD("한식", 2),
+    CHINESE_FOOD("중식", 3),
+    ASIAN_FOOD("아시안", 4),
+    WESTON_FOOD("양식", 5);
 
-    private String category;
+    private final String category;
+    private final Integer index;
 
-    Category(String category) {
+    Category(String category, Integer index) {
         this.category = category;
+        this.index = index;
+    }
+
+    public static String recommendCategory() {
+        Integer randomNumber = Randoms.pickNumberInRange(1, 5);
+        for (Category name : Category.values()) {
+            if (name.index.equals(randomNumber)) {
+                return name.category;
+            }
+        }
+        return null;
     }
 }
