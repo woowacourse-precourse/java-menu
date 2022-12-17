0421367365587b7beed5ff5f103585016fb74007
hojunking96 <junsong96@naver.com>
2022-12-17 16:47:28 +0900

    feats(Coaches) : 코치들의 리스트를 관리하는 객체 생성

---
+++ src/main/java/menu/Coaches.java
@@ -0,0 +1,20 @@
+package menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coaches {
+    private final List<Coach> coaches;
+
+    public Coaches() {
+        coaches = new ArrayList<>();
+    }
+
+    public void addCoach(Coach coach) {
+        coaches.add(coach);
+    }
+
+    public List<Coach> getCoaches() {
+        return coaches;
+    }
+}
