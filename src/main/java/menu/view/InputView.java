dd2ef4966b437dcabf361a9e1a73bf03e977064d
sosow0212 <sosow0212@naver.com>
2022-12-17 13:38:22 +0900

    feat : (InputView) 문제에서 요구하는 입력을 위한 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,4 +1,29 @@
 package menu.view;
 
+import static menu.view.OutputView.printError;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public String readCrewsName() {
+        try {
+            String input = Console.readLine();
+            // todo : 예외
+            return input;
+        } catch (IllegalArgumentException exception) {
+            printError(exception.getMessage());
+            return readCrewsName();
+        }
+    }
+
+    public String readBannedFoods() {
+        try {
+            String input = Console.readLine();
+            // todo : 예외
+            return input;
+        } catch (IllegalArgumentException exception) {
+            printError(exception.getMessage());
+            return readBannedFoods();
+        }
+    }
 }
