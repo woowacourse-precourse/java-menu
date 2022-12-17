25b36e4a273c7f960e1f672c15b353b2d498813b
JeonghakLee <wjdgkr0527@naver.com>
2022-12-17 14:33:19 +0900

    feat(InputView): 못먹는 음식 입력 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -6,11 +6,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import menu.domain.Coach;
+import menu.domain.Menu;
 
 public class InputView {
 
   private static final String INPUT_COACHES_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
   private static final String COACHES_SIZE_ERROR = "코치는 2명 이상 5명 이하로 식사를 합니다.";
+  private static final String CANNOT_EAT = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
+  private static final String CANNOT_EAT_SIZE_ERROR = "코치가 못먹는 음식은 2개 이하 입니다.";
 
 
   public List<Coach> readCoaches() {
@@ -29,6 +32,25 @@ public class InputView {
   private void validateCoachNames(List<String> coachNames) {
     if (coachNames.size() < 2 || coachNames.size() > 5) {
       throw new IllegalArgumentException(COACHES_SIZE_ERROR);
+    }
+  }
+
+  public List<String> readCannotEatMenus() {
+    while (true) {
+      try {
+        System.out.println(CANNOT_EAT);
+        List<String> menus = getList(readLine());
+        validateMenus(menus);
+        return menus;
+      } catch (IllegalArgumentException e) {
+        //printErrorMessage(e.getMessage());현
+      }
+    }
+  }
+
+  private void validateMenus(List<String> coachNames) {
+    if (coachNames.size() > 2) {
+      throw new IllegalArgumentException(CANNOT_EAT_SIZE_ERROR);
     }
   }
 
