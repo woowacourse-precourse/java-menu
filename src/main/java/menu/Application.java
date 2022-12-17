2c8ac38f555c07df708ea6626b1b4d18fe43c0d6
MoonJeWoong <munjin0201@naver.com>
2022-12-17 17:02:21 +0900

    feat(application): 메뉴 추천 프로그램 완성

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,20 @@
 package menu;
 
+import menu.controller.Controller;
+import menu.view.InputView;
+import menu.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller(initInputView(),initOutputView());
+        controller.run();
+    }
+
+    private static InputView initInputView() {
+        return new InputView();
+    }
+
+    private static OutputView initOutputView() {
+        return new OutputView();
     }
 }
