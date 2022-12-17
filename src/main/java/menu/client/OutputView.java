166b3dd877238d31872e145fada1c7bd1f252ed1
be-student <presentsong@naver.com>
2022-12-17 15:04:05 +0900

    feat: 결과 출력 기능을 추가한다

--- src/main/java/menu/client/OutputView.java
+++ src/main/java/menu/client/OutputView.java
@@ -1,11 +1,19 @@
 package menu.client;
 
+import java.util.List;
 import menu.di.RecommendResultDto;
 
 public class OutputView {
 
     private static final String ERROR_PREFIX = "[ERROR] ";
     private static final String START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    private static final String START = "[ ";
+    private static final String CATEGORY_START = START + "카테고리 | ";
+    private static final String DELIMITER = " | ";
+    private static final String END = " ]";
+    private static final String DAY_MESSAGE = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]";
+    private static final String FINISH_MESSAGE = "추천을 완료했습니다";
+    private static final String RESULT_START_MESSAGE = "메뉴 추천 결과입니다.";
 
     private OutputView() {
     }
@@ -19,7 +27,28 @@ public class OutputView {
     }
 
     public static void printResult(RecommendResultDto resultDto) {
+        println(RESULT_START_MESSAGE);
+        println(DAY_MESSAGE);
+        printCategory(resultDto.getCategories());
+        resultDto.getCoachMenus().forEach(OutputView::printCoach);
+        println();
+        println(FINISH_MESSAGE);
+    }
 
+    private static void printCategory(List<String> categories) {
+        print(CATEGORY_START);
+        print(String.join(DELIMITER, categories));
+        println(END);
+    }
+
+    private static void printCoach(List<String> coachMenu) {
+        print(START);
+        print(String.join(DELIMITER, coachMenu));
+        println(END);
+    }
+
+    private static void print(String message) {
+        System.out.print(message);
     }
 
     private static void println() {
--- src/main/java/menu/di/RecommendResultDto.java
+++ src/main/java/menu/di/RecommendResultDto.java
@@ -1,5 +1,22 @@
 package menu.di;
 
+import java.util.List;
+
 public class RecommendResultDto {
 
+    private final List<String> categories;
+    private final List<List<String>> coachMenus;
+
+    public RecommendResultDto(List<String> categories, List<List<String>> coachMenus) {
+        this.categories = categories;
+        this.coachMenus = coachMenus;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public List<List<String>> getCoachMenus() {
+        return coachMenus;
+    }
 }
