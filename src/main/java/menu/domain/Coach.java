2b5185c4e9383f37c26e808d9ca28acebca4471f
kim0914 <kosa0914@naver.com>
2022-12-17 15:57:19 +0900

    feat(Coach): 코치 이름 검증 메서드 추가

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -5,14 +5,28 @@ import java.util.List;
 
 public class Coach {
 
+    public static final int NAME_MIN_SIZE = 2;
+    public static final int NAME_MAX_SIZE = 4;
+
     private final String name;
     private final List<Menu> banFoods;
 
     private List<Menu> recommendMenu = new ArrayList<>();
 
     public Coach(String name, List<Menu> banFoods) {
+        validate(name);
         this.name = name;
         this.banFoods = banFoods;
+    }
+
+    public void validate(String name) {
+        if (isWrongNameSize(name)) {
+            throw new IllegalArgumentException("코치 이름은 최소 2자, 최대 4자 입니다.");
+        }
+    }
+
+    private boolean isWrongNameSize(String name) {
+        return name.length() < NAME_MIN_SIZE || name.length() > NAME_MAX_SIZE;
     }
 
     public void addMenu(Menu menu) {
