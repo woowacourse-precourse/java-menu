ceab8c2b6a224c07741fb5e038f9e18cbc7a8145
woo-chang <clllickme@naver.com>
2022-12-17 14:35:33 +0900

    feat(Category): 값으로 카테고리를 찾아서 반환하는 기능 구현

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -4,21 +4,30 @@ import java.util.Arrays;
 
 public enum Category {
 
-    JAPAN("일식"),
-    KOREA("한식"),
-    CHINA("중식"),
-    ASIA("아시안"),
-    AMERICA("양식");
+    JAPAN("일식", "1"),
+    KOREA("한식", "2"),
+    CHINA("중식", "3"),
+    ASIA("아시안", "4"),
+    AMERICA("양식", "5");
 
-    Category(String name) {
+    Category(String name, String value) {
         this.name = name;
+        this.value = value;
     }
 
     private final String name;
+    private final String value;
 
     public static Category from(String name) {
         return Arrays.stream(Category.values())
                 .filter(category -> category.name.equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("존재하지 않는 카테고리입니다."));
+    }
+
+    public static Category of(String value) {
+        return Arrays.stream(Category.values())
+                .filter(category -> category.value.equals(value))
                 .findFirst()
                 .orElseThrow(() -> new IllegalStateException("존재하지 않는 카테고리입니다."));
     }
