16ed06d2bab3e25cf22a5e90aa804f3460687fa4
hodomaroo <enghemdwkd@gmail.com>
2022-12-17 13:38:07 +0900

    (feat/Couch) 이미 추천받은 메뉴와 코치 이름을 담는 Couch 도메인 정의

---
+++ src/main/java/menu/domain/Couch.java
@@ -0,0 +1,35 @@
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Couch {
+
+    private final String name;
+    private final Set<Menu> alreadyRecommended = new HashSet<>();
+
+    public Couch(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isAlreadyRecommended(Menu menu) {
+        return alreadyRecommended.contains(menu);
+    }
+
+    public void recommendMenu(Menu menu){
+        alreadyRecommended.add(menu);
+    }
+
+    public List<Menu> getAbleMenus(List<Menu> menus) {
+        List<Menu> copied = new ArrayList<>(menus);
+
+        copied.removeAll(alreadyRecommended);
+        return copied;
+    }
+}
