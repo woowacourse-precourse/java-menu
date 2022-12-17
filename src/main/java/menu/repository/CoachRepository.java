80a4130c5451990a82dc1c3aa22818583623f24a
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-12-17 15:17:44 +0900

    Feat(CoachRepository): 변경 CoachRepository
    데이터 저장 및 탐색 기능 추가

--- src/main/java/menu/repository/CoachRepository.java
+++ src/main/java/menu/repository/CoachRepository.java
@@ -3,7 +3,11 @@ package menu.repository;
 import menu.Coach;
 import menu.service.SuggestMenuService;
 
+import java.util.*;
+
 public class CoachRepository {
+
+    private final Map<String, Coach> coachDB = new HashMap<>();
 
     private static final CoachRepository instance = new CoachRepository();
 
@@ -16,6 +20,13 @@ public class CoachRepository {
     }
 
     public void save(Coach coach) {
+        if (coachDB.containsKey(coach.getName()))
+            throw new IllegalStateException("이미 존재하는 코치");
 
+        coachDB.put(coach.getName(), coach);
+    }
+
+    public List<Coach> findAll() {
+        return new ArrayList<>(coachDB.values());
     }
 }
