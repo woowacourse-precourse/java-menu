fa3c57f03e17dfb2038356de3f093995e8d273f8
This2sho <zx00018@naver.com>
2022-12-17 17:18:02 +0900

    test(RecommendService): 한 주에 중복된 음식을 추천하는 기능 테스트 추가

--- src/main/java/menu/utils/MenuInitiator.java
+++ src/main/java/menu/utils/MenuInitiator.java
@@ -9,7 +9,7 @@ import menu.repository.MenuRepository;
 
 public class MenuInitiator {
 
-    private static final String FOOD_DELIMITER = ",";
+    private static final String FOOD_DELIMITER = ", ";
     private static final String japaneseFoods = "규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼";
     private static final String koreanFoods = "김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음";
     private static final String chineseFoods = "깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채";
--- src/test/java/menu/service/RecommendServiceTest.java
+++ src/test/java/menu/service/RecommendServiceTest.java
@@ -1,7 +1,10 @@
 package menu.service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import menu.domain.Coach;
 import menu.domain.FoodCategory;
 import menu.domain.Menu;
@@ -55,5 +58,25 @@ class RecommendServiceTest {
         Menu recommendMenu = recommendService.recommendMenu(FoodCategory.KOREAN, coach);
         //then
         Assertions.assertThat(recommendMenu).isNotEqualTo(dislikeMenu);
+    }
+
+    @Test
+    @DisplayName("한주에 중복된 음식은 추천하지 않는다.")
+    void recommendDuplicateTest() {
+        //given
+        Coach coach = new Coach("철수", List.of());
+        MenuInitiator.init();
+        RecommendService recommendService = new RecommendService();
+        Set<String> koreanFoods = Set.of("김치찌개", "김밥", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음");
+
+        //when
+        Set<String> recommendMenus = new HashSet<>();
+        for (int i = 0; i < 5; i++) {
+            Menu recommendMenu = recommendService.recommendMenu(FoodCategory.KOREAN, coach);
+            recommendMenus.add(recommendMenu.getName());
+        }
+        //then
+        Assertions.assertThat(recommendMenus)
+            .isSubsetOf(koreanFoods);
     }
 }
