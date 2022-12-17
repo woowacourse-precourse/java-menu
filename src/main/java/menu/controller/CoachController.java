202538048272d81a931124c8655634bc895a06c3
watterricecake <fruturum@nate.com>
2022-12-17 14:48:36 +0900

    feat: 랜덤으로 카테고리 추천 2이상일시 다시 추천

--- src/main/java/menu/controller/CoachController.java
+++ src/main/java/menu/controller/CoachController.java
@@ -1,5 +1,6 @@
 package menu.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import menu.domain.Coach;
 import menu.domain.Food;
 import menu.domain.Foods;
@@ -7,6 +8,8 @@ import menu.domain.Foods;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import static menu.option.Option.CATEGORIES;
 
 public class CoachController {
     Coach coach;
@@ -24,6 +27,21 @@ public class CoachController {
     }
     public void makeCantEat(String coach,String cantEat){
         this.coaches.get(coach).makeCantEatList(cantEat);
+    }
+    private void recommendFood(String coach){
+
+    }
+
+    private String recommendCategory(String coach){
+        String randomCategory = makeRandomCategory();
+        if(coaches.get(coach).getCategoryCnt().get(randomCategory)<2){
+            coaches.get(coach).addCategoryCnt(randomCategory);
+            return randomCategory;
+        }
+        return recommendCategory(coach);
+    }
+    private String makeRandomCategory(){
+        return CATEGORIES[Randoms.pickNumberInRange(0,4)];
     }
     public HashMap<String, Coach> getCoaches(){return this.coaches;}
 }
--- src/main/java/menu/controller/MainController.java
+++ src/main/java/menu/controller/MainController.java
@@ -2,6 +2,7 @@ package menu.controller;
 
 import menu.domain.Coach;
 
+import static menu.option.Option.CATEGORIES;
 import static menu.view.InputView.inputCantView;
 import static menu.view.InputView.inputCoachesView;
 import static menu.view.OutputView.startRecommendView;
