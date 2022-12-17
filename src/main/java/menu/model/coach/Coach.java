ca31a5c3e2033bbdcd28207876a31250c7127caa
ESeokhwan <huan99@hanmail.net>
2022-12-17 14:46:01 +0900

    feat: 코치의 이름을 입력받아 유효한 이름인지 확인 후 코치를 생성하는 기능 추가

--- src/main/java/menu/model/coach/Coach.java
+++ src/main/java/menu/model/coach/Coach.java
@@ -6,8 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Coach {
-    public Coach() {
-        // TODO 구현 필요
+    private static final int MAX_NAME_SIZE = 4;
+    private static final int MIN_NAME_SIZE = 2;
+
+    private final String name;
+    private final List<Menu> hatingMenuList;
+
+    public Coach(String name) {
+        validate(name);
+
+        this.name = name;
+        this.hatingMenuList = new ArrayList<>();
     }
 
     public String getName() {
@@ -24,5 +33,29 @@ public class Coach {
 
     public void addHatingMenu(Menu hatingMenu) {
 
+    }
+
+    private void validate(String str) {
+        if(hasComma(str)) {
+            throw new IllegalArgumentException("이름에 ,가 들어갈 수 없습니다.");
+        }
+        if (doesExceedMaxLimitSize(str)) {
+            throw new IllegalArgumentException("이름은 최대 4글자입니다.");
+        }
+        if (doesNotExceedMinLimitSize(str)) {
+            throw new IllegalArgumentException("이름은 최소 2글자입니다.");
+        }
+    }
+
+    private boolean hasComma(String str) {
+        return str.contains(",");
+    }
+
+    private boolean doesExceedMaxLimitSize(String str) {
+        return str.length() > MAX_NAME_SIZE;
+    }
+
+    private boolean doesNotExceedMinLimitSize(String str) {
+        return str.length() < MIN_NAME_SIZE;
     }
 }
